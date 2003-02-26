b4a1df5faac7b46caeabe3a71e2d4e090046769f
edwards <edwards>
2003-02-26 03:19:09 +0000

    Added formfac_w.h

--- lib/meas/hadron/hadron.h
+++ lib/meas/hadron/hadron.h
@@ -1,12 +1,12 @@
 // -*- C++ -*-
-// $Id: hadron.h,v 1.1 2003-02-15 05:54:26 edwards Exp $
+// $Id: hadron.h,v 1.2 2003-02-26 03:19:09 edwards Exp $
 
 #ifndef HADRON_INCLUDE
 #define HADRON_INCLUDE
 
-#include "srcfil.h"
-
 #include "baryon_w.h"
+#include "formfac_w.h"
 #include "mesons_w.h"
+#include "srcfil.h"
 
 #endif
