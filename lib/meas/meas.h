e24f75b2a945a5683f76b0c6099c78d53baa22bc
flemingg <flemingg>
2003-03-03 16:04:51 +0000

    Added #include "hadron/hadron.h"

--- lib/meas/meas.h
+++ lib/meas/meas.h
@@ -1,11 +1,12 @@
 // -*- C++ -*-
-// $Id: meas.h,v 1.2 2003-02-16 18:39:36 edwards Exp $
+// $Id: meas.h,v 1.3 2003-03-03 16:04:51 flemingg Exp $
 
 #ifndef MEAS_INCLUDE
 #define MEAS_INCLUDE
 
 #include "smear/smear.h"
 #include "glue/glue.h"
+#include "hadron/hadron.h"
 
 #endif
 
