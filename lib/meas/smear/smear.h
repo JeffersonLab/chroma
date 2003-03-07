e5542551925a8f01e02b40980ce13d04925185fb
edwards <edwards>
2003-03-07 05:42:52 +0000

    Changed sink_smear name.

--- lib/meas/smear/smear.h
+++ lib/meas/smear/smear.h
@@ -1,11 +1,11 @@
 // -*- C++ -*-
-// $Id: smear.h,v 1.2 2003-03-07 05:42:01 edwards Exp $
+// $Id: smear.h,v 1.3 2003-03-07 05:42:52 edwards Exp $
 
 #ifndef SMEAR_INCLUDE
 #define SMEAR_INCLUDE
 
 #include "gaus_smear.h"
-#include "sink_smear2.h"
+#include "sink_smear2_w.h"
 
 #endif
 
