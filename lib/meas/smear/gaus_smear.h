ea763326828636ff5003c7ae2aac0360c20cc205
edwards <edwards>
2003-03-07 05:22:01 +0000

    Added a

--- lib/meas/smear/gaus_smear.h
+++ lib/meas/smear/gaus_smear.h
@@ -1,11 +1,15 @@
 // -*- C++ -*-
-// $Id: gaus_smear.h,v 1.1 2003-02-15 05:54:26 edwards Exp $
+// $Id: gaus_smear.h,v 1.2 2003-03-07 05:22:01 edwards Exp $
 
 #ifndef GAUS_SMEAR_INCLUDE
 #define GAUS_SMEAR_INCLUDE
 
 void gausSmear(const multi1d<LatticeColorMatrix>& u, 
 	       LatticeColorVector& chi, 
+	       const Real& width, int ItrGaus, int j_decay);
+
+void gausSmear(const multi1d<LatticeColorMatrix>& u, 
+	       LatticePropagator& chi, 
 	       const Real& width, int ItrGaus, int j_decay);
 
 #endif
