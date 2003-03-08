eaf384cba218c041084b311811cb8a7284838cd3
edwards <edwards>
2003-03-08 04:42:15 +0000

    Added barcomp

--- lib/meas/hadron/hadron.h
+++ lib/meas/hadron/hadron.h
@@ -1,10 +1,11 @@
 // -*- C++ -*-
-// $Id: hadron.h,v 1.2 2003-02-26 03:19:09 edwards Exp $
+// $Id: hadron.h,v 1.3 2003-03-08 04:42:15 edwards Exp $
 
 #ifndef HADRON_INCLUDE
 #define HADRON_INCLUDE
 
 #include "baryon_w.h"
+#include "barcomp_w.h"
 #include "formfac_w.h"
 #include "mesons_w.h"
 #include "srcfil.h"
