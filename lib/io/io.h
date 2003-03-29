25e674955430a62b2032e0cbe0035611b785f466
edwards <edwards>
2003-03-29 05:25:44 +0000

    Removed ioqprop . Added readszin, readszinqprop_w.h.

--- lib/io/io.h
+++ lib/io/io.h
@@ -1,8 +1,9 @@
-// $Id: io.h,v 1.1 2003-03-26 22:09:59 dgr Exp $
+// $Id: io.h,v 1.2 2003-03-29 05:25:44 edwards Exp $
 
 #ifndef IO_INCLUDE
 #define IO_INCLUDE
 
-#include "ioqprop/ioqprop.h"
+#include "readszin.h"
+#include "readszinqprop_w.h"
 
 #endif
