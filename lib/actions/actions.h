6eba93e8a5c28d5f6a2550ec5f1d55d0c4d0e2a1
edwards <edwards>
2003-03-30 18:33:25 +0000

    Added construct.h

--- lib/actions/actions.h
+++ lib/actions/actions.h
@@ -1,9 +1,10 @@
 // -*- C++ -*-
-// $Id: actions.h,v 1.2 2003-03-26 22:05:27 dgr Exp $
+// $Id: actions.h,v 1.3 2003-03-30 18:33:25 edwards Exp $
 
 #ifndef ACTIONS_INCLUDE
 #define ACTIONS_INCLUDE
 
+#include "construct/construct.h"
 #include "dirac/dirac.h"
 #include "dslash/dslash.h"
 #include "invert/invert.h"
