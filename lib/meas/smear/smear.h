0a887053fe82a42e7a754bded2b7b3a8e160abab
heller <heller>
2003-03-30 22:08:43 +0000

    Added hyp_smear.h

--- lib/meas/smear/smear.h
+++ lib/meas/smear/smear.h
@@ -1,11 +1,12 @@
 // -*- C++ -*-
-// $Id: smear.h,v 1.3 2003-03-07 05:42:52 edwards Exp $
+// $Id: smear.h,v 1.4 2003-03-30 22:08:43 heller Exp $
 
 #ifndef SMEAR_INCLUDE
 #define SMEAR_INCLUDE
 
 #include "gaus_smear.h"
 #include "sink_smear2_w.h"
+#include "hyp_smear.h"
 
 #endif
 
