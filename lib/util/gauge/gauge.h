12c6bd73e605e6cadc577bb6efca45d7641f5138
edwards <edwards>
2003-03-31 19:38:38 +0000

    Added doxygen comments.

--- lib/util/gauge/gauge.h
+++ lib/util/gauge/gauge.h
@@ -1,8 +1,21 @@
 // -*- C++ -*-
-// $Id: gauge.h,v 1.2 2003-03-30 21:42:20 heller Exp $
+// $Id: gauge.h,v 1.3 2003-03-31 19:38:38 edwards Exp $
 
-#ifndef GAUGE_INCLUDE
-#define GAUGE_INCLUDE
+/*! \file
+ * \brief Include all utility routines
+ *
+ * Central include file for all utilities
+ */
+
+/*! \defgroup util Utility routines
+ * \ingroup lib
+ *
+ * Utility routines for simple manipulation of gauge fields and fermions.
+ * Support for projection onto momentum states (fourier transforms).
+ */
+
+#ifndef __gauge_h__
+#define __gauge_h__
 
 #include "expm12.h"
 #include "hotst.h"
--- lib/util/util.h
+++ lib/util/util.h
@@ -1,8 +1,21 @@
 // -*- C++ -*-
-// $Id: util.h,v 1.2 2003-03-14 05:06:06 flemingg Exp $
+// $Id: util.h,v 1.3 2003-03-31 19:38:38 edwards Exp $
 
-#ifndef UTIL_INCLUDE
-#define UTIL_INCLUDE
+/*! \file
+ * \brief Include all utility routines
+ *
+ * Central include file for all utilities
+ */
+
+/*! \defgroup util Utility routines
+ * \ingroup lib
+ *
+ * Utility routines for simple manipulation of gauge fields and fermions.
+ * Support for projection onto momentum states (fourier transforms).
+ */
+
+#ifndef __util_h__
+#define __util_h__
 
 #include "ferm/ferm.h"
 #include "ft/ft.h"
