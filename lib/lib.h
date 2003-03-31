ab1fdf9aff65b08432ec4b547ce205a096ca8d98
edwards <edwards>
2003-03-31 19:38:06 +0000

    Added a central include file.

---
+++ lib/lib.h
@@ -0,0 +1,27 @@
+// -*- C++ -*-
+// $Id: lib.h,v 1.1 2003-03-31 19:38:06 edwards Exp $
+
+/*! \file
+ * \brief Chroma Lattice Field Theory library
+ *
+ * Central include file for all pieces of the Chroma lattice field theory library
+ */
+
+/*! \defgroup lib Chroma Lattice Field Theory library
+ *
+ * Central include file for all pieces of the Chroma lattice field theory library
+ */
+
+
+#ifndef __lib_h__
+#define __lib_h__
+
+#include "actions/actions.h"
+#include "util/util.h"
+#include "meas/meas.h"
+#include "update/update.h"
+#include "io/io.h"
+
+#endif
+
+
