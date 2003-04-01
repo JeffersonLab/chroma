eaaee287a9ffaa8c2afddd28b0681721f0c15b53
edwards <edwards>
2003-04-01 01:40:13 +0000

    Added doxygen comments.

--- lib/meas/meas.h
+++ lib/meas/meas.h
@@ -1,12 +1,31 @@
 // -*- C++ -*-
-// $Id: meas.h,v 1.3 2003-03-03 16:04:51 flemingg Exp $
+// $Id: meas.h,v 1.4 2003-04-01 01:40:13 edwards Exp $
 
-#ifndef MEAS_INCLUDE
-#define MEAS_INCLUDE
+/*! \file
+ * \brief Include all measurement routines
+ *
+ * Central include file for all measurements
+ */
 
-#include "smear/smear.h"
+/*! \defgroup meas Measurement routines
+ * \ingroup lib
+ *
+ * Support for measurements including eigenvalues, gluonic
+ * observables, hadron spectroscopy, formfactors, and structure
+ * functions, psibar-psi, schroedinger functional. Smearing
+ * is supported.
+ */
+
+#ifndef __meas_h__
+#define __meas_h__
+
+#include "eig/eig.h"
+#include "gfix/gfix.h"
 #include "glue/glue.h"
 #include "hadron/hadron.h"
+#include "pbp/pbp.h"
+#include "schrfun/schrfun.h"
+#include "smear/smear.h"
 
 #endif
 
