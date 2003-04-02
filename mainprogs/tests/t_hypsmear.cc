2312f036acab64f95792e8b6027c94956d7a8df8
heller <heller>
2003-04-02 19:30:15 +0000

    Added t_hypsmear: test hyp-smearing routine.

---
+++ mainprogs/tests/t_hypsmear.cc
@@ -0,0 +1,79 @@
+// $Id: t_hypsmear.cc,v 1.1 2003-04-02 19:30:15 heller Exp $
+
+#include <iostream>
+#include <cstdio>
+
+#include "chroma.h"
+
+using namespace QDP;
+
+int main(int argc, char *argv[])
+{
+  // Put the machine into a known state
+  QDP_initialize(&argc, &argv);
+
+  // Setup the layout
+  const int foo[] = {4,4,4,8};
+  multi1d<int> nrow(Nd);
+  nrow = foo;  // Use only Nd elements
+  Layout::setLattSize(nrow);
+  Layout::create();
+
+  NmlWriter nml("t_hypsmear.nml");
+
+  push(nml,"lattis");
+  Write(nml,Nd);
+  Write(nml,Nc);
+  Write(nml,nrow);
+  pop(nml);
+
+  //! Example of calling a plaquette routine
+  /*! NOTE: the STL is *not* used to hold gauge fields */
+  multi1d<LatticeColorMatrix> u(Nd);
+  Double w_plaq, s_plaq, t_plaq, link;
+
+//  cerr << "Reading test_purgaug.cfg1\n";
+//  Seed seed_old;
+//  readSzin(u, 0, "../test_purgaug.cfg1", seed_old);
+  cerr << "Start gaussian\n";
+  for(int m=0; m < u.size(); ++m)
+    gaussian(u[m]);
+
+  // Reunitarize the gauge field
+  for(int m=0; m < u.size(); ++m)
+    reunit(u[m]);
+
+  // Try out the plaquette routine
+  MesPlq(u, w_plaq, s_plaq, t_plaq, link);
+  cerr << "w_plaq = " << w_plaq << endl;
+  cerr << "link = " << link << endl;
+
+  // Write out the results
+  push(nml,"observables");
+  Write(nml,w_plaq);
+  Write(nml,link);
+  pop(nml);
+
+  // Now hyp smear
+  multi1d<LatticeColorMatrix> u_hyp(Nd);
+  Real BlkAccu = 1.0e-5;
+  int BlkMax = 100;
+  Real alpha1 = 0.7;
+  Real alpha2 = 0.6;
+  Real alpha3 = 0.3;
+  Hyp_Smear(u, u_hyp, alpha1, alpha2, alpha3, BlkAccu, BlkMax);
+
+  // Measure again
+  MesPlq(u_hyp, w_plaq, s_plaq, t_plaq, link);
+  cerr << "w_plaq = " << w_plaq << endl;
+  cerr << "link = " << link << endl;
+
+  // Write out the results
+  push(nml,"HYP_observables");
+  Write(nml,w_plaq);
+  Write(nml,link);
+  pop(nml);
+
+  // Time to bolt
+  QDP_finalize();
+}
