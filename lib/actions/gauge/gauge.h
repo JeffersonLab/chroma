f7ea905ca21970cfd14f21e9abb784779c068ea3
edwards <edwards>
2003-04-08 21:37:05 +0000

    New.

---
+++ lib/actions/gauge/gauge.h
@@ -0,0 +1,22 @@
+// -*- C++ -*-
+// $Id: gauge.h,v 1.1 2003-04-08 21:37:05 edwards Exp $
+
+/*! \file
+ * \brief Gauge action support
+ *
+ * Central include file for all gauge action support
+ */
+
+/*! \defgroup gaugeact Gauge action support
+ * \ingroup actions
+ *
+ * Support for construction of gauge actions, including 
+ * improved gauge actions
+ */
+
+#ifndef __actgaugee_h__
+#define __actgaugee_h__
+
+#endif
+
+
