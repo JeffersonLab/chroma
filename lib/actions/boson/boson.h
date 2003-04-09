f88366bea02ba0d9b9e920c95db91e82cc17d414
edwards <edwards>
2003-04-09 17:27:56 +0000

    Central include file.

---
+++ lib/actions/boson/boson.h
@@ -0,0 +1,23 @@
+// -*- C++ -*-
+// $Id: boson.h,v 1.1 2003-04-09 17:27:56 edwards Exp $
+
+/*! \file
+ * \brief Boson action support
+ *
+ * Central include file for all boson action support
+ */
+
+/*! \defgroup boson Boson action support
+ * \ingroup actions
+ *
+ * Currently a very limited set operations by bosons
+ */
+
+#ifndef __actbosons_h__
+#define __actbosons_h__
+
+#include "operator/klein_gord.h"
+
+#endif
+
+
