be36613d40afc836602485125c0f9f3a780e4924
edwards <edwards>
2003-04-09 17:27:24 +0000

    Control file.

---
+++ lib/actions/ferm/fermacts/fermacts.h
@@ -0,0 +1,24 @@
+// -*- C++ -*-
+// $Id: fermacts.h,v 1.1 2003-04-09 17:27:24 edwards Exp $
+
+/*! \file
+ * \brief Fermion actions
+ *
+ * Various fermion actions
+ */
+
+/*! \defgroup fermact Fermion actions
+ * \ingroup actions
+ *
+ * Various fermion actions
+ */
+
+#ifndef __fermactss_h__
+#define __fermactss_h__
+
+#include "unprec_wilson_fermact_w.h"
+// #include "zolotarev4d_fermact_w.h"
+
+#endif
+
+
