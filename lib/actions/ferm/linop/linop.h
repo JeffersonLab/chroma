6617b913949d4df471a436920bb5f4dd6fa90d2e
edwards <edwards>
2003-04-24 18:48:25 +0000

    Added overlapbu_linop_w.

--- lib/actions/ferm/linop/linop.h
+++ lib/actions/ferm/linop/linop.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: linop.h,v 1.2 2003-04-09 17:16:25 edwards Exp $
+// $Id: linop.h,v 1.3 2003-04-24 18:48:25 edwards Exp $
 
 /*! \file
  * \brief Linear operators
@@ -19,6 +19,8 @@
 // #include "lmpsim_w.h"
 #include "lwldslash_w.h"
 #include "unprec_wilson_linop_w.h"
+#include "overlapbu_linop_w.h"
+
 #endif
 
 
