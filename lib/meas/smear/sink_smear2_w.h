0aebd796b5405c53ea7dd5a5da7e31a348ed91d5
flemingg <flemingg>
2003-04-25 20:46:31 +0000

    prototype was incorrect. fixed.

--- lib/meas/smear/sink_smear2_w.h
+++ lib/meas/smear/sink_smear2_w.h
@@ -1,11 +1,12 @@
 // -*- C++ -*-
-// $Id: sink_smear2_w.h,v 1.2 2003-04-01 01:56:19 edwards Exp $
+// $Id: sink_smear2_w.h,v 1.3 2003-04-25 20:46:31 flemingg Exp $
 
 #ifndef __sink_smear2_h__
 #define __sink_smear2_h__
 
-void sink_smear2(const multi1d<LatticeColorMatrix>& u, 
-		 LatticePropagator& quark_propagator, 
-		 const Real& wvf_type, int wvf_param, int WvfIntPar, int j_decay);
+void
+sink_smear2(const multi1d<LatticeColorMatrix>& u,
+            LatticePropagator& quark_propagator, int wvf_type,
+            const Real& wvf_param, int WvfIntPar, int j_decay) ;
 
 #endif
