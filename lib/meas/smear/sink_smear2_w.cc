88bc8af354e3220af9634f6ae89ca61a7952993f
flemingg <flemingg>
2003-05-08 22:55:49 +0000

    Incorporated 'enum WvfKind' into sink_smear2().

--- lib/meas/smear/sink_smear2_w.cc
+++ lib/meas/smear/sink_smear2_w.cc
@@ -1,54 +1,59 @@
-// $Id: sink_smear2_w.cc,v 1.2 2003-04-01 02:38:26 edwards Exp $
+// $Id: sink_smear2_w.cc,v 1.3 2003-05-08 22:55:49 flemingg Exp $
 /*! \file
  *  \brief Control routine for types of propagator smearing
  */
 
 #include "chromabase.h"
 #include "primitives.h"
-#include "meas/smear/gaus_smear.h"
+#include "meas/smear/sink_smear2_w.h"
 
 using namespace QDP;
 
 //! "Smear" the quark propagator at the sink by a covariant Gaussian
-/*!
- * \ingroup smear
- *
- * This routine is specific to Wilson fermions!
- *
- * Arguments:
- *
- *  \param u                   gauge field ( Read )
- *  \param quark_propagator    quark propagator ( Modify )
- *  \param wvf_type            wave function type: Gaussian or exponential ( Read )
- *  \param wvf_param           wvf_param of "shell" wave function ( Read )
- *  \param WvfIntPar           number of iterations to approximate Gaussian
- *                             or terminate CG inversion for Wuppertal smearing ( Read )
- *  \param j_decay             direction of decay ( Read ) 
- */
+//!
+// \ingroup smear
+//
+// This routine is specific to Wilson fermions!
+//
+// Arguments:
+//
+//  \param u                   gauge field ( Read )
+//  \param quark_propagator    quark propagator ( Modify )
+//  \param Wvf_kind            wave function kind: Gaussian or exponential
+//                             ( Read )
+//  \param wvf_param           wvf_param of "shell" wave function ( Read )
+//  \param WvfIntPar           number of iterations to approximate Gaussian
+//                             or terminate CG inversion for Wuppertal smearing
+//                             ( Read )
+//  \param j_decay             direction of decay ( Read ) 
+//
 
-void sink_smear2(const multi1d<LatticeColorMatrix>& u, 
-		 LatticePropagator& quark_propagator, 
-		 int wvf_type, const Real& wvf_param, int WvfIntPar, int j_decay)
+void sink_smear2(const multi1d<LatticeColorMatrix>& u,
+                 LatticePropagator& quark_propagator, WvfKind Wvf_kind,
+                 const Real& wvf_param, int WvfIntPar, int j_decay)
 {
   Real RsdCG;
 
   START_CODE("sink_smear2");
 
-  switch (wvf_type)
-  {
-  case OPTION_GAUGE_INV_GAUSSIAN_WVF:
-    gausSmear(u, quark_propagator, wvf_param, WvfIntPar, j_decay);
-    break;
+  switch (Wvf_kind) {
+  case WVF_KIND_GAUGE_INV_GAUSSIAN :
+    gausSmear(u, quark_propagator, wvf_param, WvfIntPar, j_decay) ;
+    break ;
 
 #if 0
-  case OPTION_WUPPERTAL_WVF:
-    RsdCG = fuzz;
-    wupp_smear(u, quark_propagator, wvf_param, WvfIntPar, j_decay, RsdCG);
-    break;
+  case WVF_KIND_WUPPERTAL :
+    RsdCG = fuzz ;
+    wupp_smear(u, quark_propagator, wvf_param, WvfIntPar, j_decay, RsdCG) ;
+    break ;
 #endif
 
-  default:
-    QDP_error_exit("Unknown gauge invariant wave function", wvf_type);
+  default :
+    cerr << "Unknown gauge invariant wave function" << endl ;
+    if (Wvf_kind == WVF_KIND_UNKNOWN) {
+      cerr << "  Wvf_kind = WVF_KIND_UNKNOWN" << endl ;
+    }
+    QDP_abort(1) ;
   }
     
   END_CODE("sink_smear2");
--- lib/meas/smear/sink_smear2_w.h
+++ lib/meas/smear/sink_smear2_w.h
@@ -1,12 +1,20 @@
 // -*- C++ -*-
-// $Id: sink_smear2_w.h,v 1.3 2003-04-25 20:46:31 flemingg Exp $
+// $Id: sink_smear2_w.h,v 1.4 2003-05-08 22:55:49 flemingg Exp $
 
 #ifndef __sink_smear2_h__
 #define __sink_smear2_h__
 
+#include "meas/smear/gaus_smear.h"
+
+enum WvfKind {
+  WVF_KIND_GAUGE_INV_GAUSSIAN,
+  WVF_KIND_WUPPERTAL,
+  WVF_KIND_UNKNOWN
+} ;
+
 void
 sink_smear2(const multi1d<LatticeColorMatrix>& u,
-            LatticePropagator& quark_propagator, int wvf_type,
+            LatticePropagator& quark_propagator, WvfKind Wvf_kind,
             const Real& wvf_param, int WvfIntPar, int j_decay) ;
 
 #endif
