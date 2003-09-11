668b839edc21992df19389b2228d57a5305d2175
bjoo <bjoo>
2003-09-11 12:31:20 +0000

    Reads prop and computes goldstone in staggered

--- mainprogs/tests/t_follana_io_s.cc
+++ mainprogs/tests/t_follana_io_s.cc
@@ -1,4 +1,4 @@
-// $Id: t_follana_io_s.cc,v 1.1 2003-09-11 11:12:21 bjoo Exp $
+// $Id: t_follana_io_s.cc,v 1.2 2003-09-11 12:31:20 bjoo Exp $
 
 #include <iostream>
 #include <cstdio>
@@ -7,6 +7,25 @@
 #include "follana_io.h"
 
 using namespace QDP;
+
+
+
+//! Function object used for constructing the time-slice set
+class TimeSliceFunc : public SetFunc
+{
+public:
+  TimeSliceFunc(int dir): dir_decay(dir) {}
+                                                                                
+  int operator() (const multi1d<int>& coordinate) const {return coordinate[dir_decay];}
+  int numSubsets() const {return Layout::lattSize()[dir_decay];}
+                                                                                
+  int dir_decay;
+                                                                                
+private:
+  TimeSliceFunc() {}  // hide default constructor
+};
+
+
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +36,8 @@ int main(int argc, char *argv[])
   const int foo[] = {16,16,16,32};
   multi1d<int> nrow(Nd);
   nrow = foo;  // Use only Nd elements
+
+
   Layout::setLattSize(nrow);
   Layout::create();
 
@@ -25,10 +46,25 @@ int main(int argc, char *argv[])
   LatticePropagator qprop;
   readQpropFollana("./prop.0", qprop);
 
+  LatticeComplex corr_fn = trace(adj(qprop)*qprop);
+
+  UnorderedSet timeslice;
+  timeslice.make(TimeSliceFunc(3));
+
+  /* Project on zero momentum: Do a slice-wise sum. */
+  multi1d<DComplex> hsum(nrow[3]);
+  
+  
+  hsum = sumMulti(corr_fn, timeslice);
+
+
   NmlWriter nml("prop.nml");
 
-  push(nml, "propagator");
-  Write(nml, qprop);
+  // push(nml, "propagator");
+  //Write(nml, qprop);
+  // pop(nml);
+  push(nml, "goldstone");
+  Write(nml,hsum);
   pop(nml);
 
   // Time to bolt
