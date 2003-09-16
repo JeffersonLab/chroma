f608b174afc4204a3e97f9faf1bde3a3e1d7ab76
bjoo <bjoo>
2003-09-16 13:47:01 +0000

    Added dslash_w.h file, to switch in the right WilsonDslash class

---
+++ lib/actions/ferm/linop/dslash_w.h
@@ -0,0 +1,27 @@
+#ifndef DSLASH_W_H
+#define DSLASH_W_H
+
+
+// The QDP naive dslash class: QDPWilsonDslash
+#include "lwldslash_w.h"
+
+// The following is an ifdef lis that switches in optimised
+// Dslash-es. Currently only optimised dslash is the SSE One;
+
+#ifdef BUILD_SSE_WILSON_DSLASH
+// The lwldslash_w_sse.h defines the SSE Dslash class
+// The following typedef switches it in.
+# include "lwldslash_w_sse.h"
+typedef SSEWilsonDslash WilsonDslash;
+
+// Many #elif clauses could come in here for other opotimised Dslash-es
+
+#else
+
+// Bottom line, if no optimised Dslash-s exist then the naive QDP Dslash
+// becomes the WilsonDslash
+typedef QDPWilsonDslash WilsonDslash;
+#endif
+
+
+#endif
