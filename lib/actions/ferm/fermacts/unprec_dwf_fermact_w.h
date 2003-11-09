db6f329fdc5147286b364c7e223ca7aa3fa3c62c
edwards <edwards>
2003-11-09 22:33:23 +0000

    Added a Pauli-Villars linop routine.

--- lib/actions/ferm/fermacts/unprec_dwf_fermact_w.h
+++ lib/actions/ferm/fermacts/unprec_dwf_fermact_w.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: unprec_dwf_fermact_w.h,v 1.2 2003-11-08 04:21:47 edwards Exp $
+// $Id: unprec_dwf_fermact_w.h,v 1.3 2003-11-09 22:33:23 edwards Exp $
 /*! \file
  *  \brief Unpreconditioned domain-wall fermion action
  */
@@ -36,6 +36,9 @@ public:
 
   //! Produce a linear operator M^dag.M for this action
   const LinearOperator<LatticeDWFermion>* lMdagM(const multi1d<LatticeColorMatrix>& u) const;
+
+  //! Produce a linear operator for this action but with quark mass 1
+  const LinearOperator<LatticeDWFermion>* linOpPV(const multi1d<LatticeColorMatrix>& u) const;
 
   //! A new quark propagator routine for 4D fermions
   void qprop(LatticeFermion& psi, 
