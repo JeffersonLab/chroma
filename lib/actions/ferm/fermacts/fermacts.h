0a10fc0e34c15ce17bed41f6602df7797f04035e
edwards <edwards>
2003-11-15 03:53:00 +0000

    Added DWF base class and ovdwf fermacts.

--- lib/actions/ferm/fermacts/fermacts.h
+++ lib/actions/ferm/fermacts/fermacts.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: fermacts.h,v 1.4 2003-11-13 18:17:37 edwards Exp $
+// $Id: fermacts.h,v 1.5 2003-11-15 03:53:00 edwards Exp $
 
 /*! \file
  * \brief Fermion actions
@@ -19,6 +19,7 @@
 #include "unprec_wilson_fermact_w.h"
 #include "unprec_dwf_fermact_w.h"
 #include "unprec_dwf_fermact_array_w.h"
+#include "unprec_ovdwf_fermact_array_w.h"
 
 // #include "zolotarev4d_fermact_w.h"
 
