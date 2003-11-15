f7d0c2ffd467fcabe7899f75bfabbfb1ff94ef64
edwards <edwards>
2003-11-15 04:25:02 +0000

    Base class versions of dwf-like unprec. actions.

---
+++ lib/actions/ferm/fermacts/unprec_dwf_fermact_base_w.cc
@@ -0,0 +1,143 @@
+// $Id: unprec_dwf_fermact_base_w.cc,v 1.1 2003-11-15 04:25:02 edwards Exp $
+/*! \file
+ *  \brief Base class for unpreconditioned domain-wall-like fermion actions
+ */
+
+#include "chromabase.h"
+#include "actions/ferm/fermacts/unprec_dwf_fermact_base_w.h"
+#include "actions/ferm/invert/invcg2.h"
+#include "actions/ferm/linop/dwffld_w.h"
+
+using namespace QDP;
+
+//! Propagator of an un-preconditioned DWF linear operator
+/*!
+ *
+ * \param psi      quark propagator ( Modify )
+ * \param u        gauge field ( Read )
+ * \param chi      source ( Read )
+ * \param invType  inverter type ( Read (
+ * \param RsdCG    CG (or MR) residual used here ( Read )
+ * \param MaxCG    maximum number of CG iterations ( Read )
+ * \param ncg_had  number of CG iterations ( Write )
+ */
+
+void 
+UnprecDWFermActBase::qprop(LatticeFermion& psi, 
+			   const multi1d<LatticeColorMatrix>& u, 
+			   const LatticeFermion& chi, 
+			   enum InvType invType,
+			   const Real& RsdCG, 
+			   int MaxCG, int& ncg_had) const
+{
+  START_CODE("UnprecDWFermActBase::qprop");
+
+  const Real m_q = quark_mass();
+
+  int n_count;
+  
+  // Initialize the 5D fields
+  //  tmp5 = (chi,0,0,0,..,0)^T
+  LatticeDWFermion chi5, tmp5 = zero;
+  pokeDW(tmp5, chi, 0);
+
+  // chi5 = P . tmp5
+  DwfFld(chi5, tmp5, PLUS);
+
+  // tmp5 = D5(1) . chi5 =  D5(1) . P . (chi,0,0,..,0)^T 
+  {
+    // Create a Pauli-Villars linop and use it for just this part
+    const LinearOperator<LatticeDWFermion>* B = linOpPV(u);
+
+    tmp5 = (*B)(chi5, PLUS);
+
+    delete B;
+  }
+
+  //  psi5 = (psi,0,0,0,...,0)^T
+  LatticeDWFermion psi5 = zero;
+  pokeDW(psi5, psi, 0);
+
+  QDPIO::cout << "|psi5|^2 = " << norm2(psi5) << endl;
+  QDPIO::cout << "|chi5|^2 = " << norm2(chi5) << endl;
+
+  // Construct the linear operator
+  const LinearOperator<LatticeDWFermion>* A = linOp(u);
+
+  switch(invType)
+  {
+  case CG_INVERTER: 
+    // chi5 = D5^\dagger(m) . tmp5 =  D5^dagger(m) . D5(1) . P . (chi,0,0,..,0)^T
+    chi5 = (*A)(tmp5, MINUS);
+    
+    // psi5 = (D^dag * D)^(-1) chi5
+    InvCG2 (*A, chi5, psi5, RsdCG, MaxCG, n_count);
+    break;
+  
+#if 0
+  case MR_INVERTER:
+    // psi5 = D^(-1) * tmp5
+    InvMR (*A, tmp5, psi5, MRover, RsdCG, MaxCG, n_count);
+    break;
+
+  case BICG_INVERTER:
+    // psi5 = D^(-1) tmp5
+    InvBiCG (*A, tmp5, psi5, RsdCG, MaxCG, n_count);
+    break;
+#endif
+  
+  default:
+    QDP_error_exit("Unknown inverter type", invType);
+  }
+  
+  if ( n_count == MaxCG )
+    QDP_error_exit("no convergence in the inverter", n_count);
+  
+  ncg_had = n_count;
+  
+  // Overall normalization
+  Real ftmp1 = Real(1) / Real(1 - m_q);
+
+  // Project out first slice after  tmp5 <- P^(-1) . psi5
+  DwfFld(tmp5, psi5, MINUS);
+
+  // Normalize and remove contact term
+  psi = ftmp1*(peekDW(tmp5, 0) - chi);
+
+  // Call the virtual destructor of A
+  delete A;
+
+  END_CODE("UnprecDWFermActBase::qprop");
+}
+
+
+//! Computes the derivative of the fermionic action respect to the link field
+/*!
+ *         |  dS      dS_f
+ * ds_u -- | ----   + -----   ( Write )
+ *         |  dU       dU
+ *
+ * psi -- [1./(M_dag*M)]*chi_  ( read ) 
+ *
+ * \param u        gauge field ( Read )
+ * \param psi      solution to linear system ( Read )
+ */
+
+multi1d<LatticeColorMatrix> 
+UnprecDWFermActBase::dsdu(const multi1d<LatticeColorMatrix>& u, 
+			  const LatticeDWFermion& psi) const
+{
+  multi1d<LatticeColorMatrix> ds_u(Nd);
+
+  START_CODE("UnprecDWFermActBase::dsdu");
+
+  // hack
+  ds_u = 0;
+
+  QDPIO::cerr << "UnprecDWFermActBase::dsdu not implemented" << endl;
+  QDP_abort(1);
+
+  END_CODE("UnprecDWFermActBase::dsdu");
+
+  return ds_u;
+}
---
+++ lib/actions/ferm/fermacts/unprec_dwf_fermact_base_w.h
@@ -0,0 +1,43 @@
+// -*- C++ -*-
+// $Id: unprec_dwf_fermact_base_w.h,v 1.1 2003-11-15 04:25:02 edwards Exp $
+/*! \file
+ *  \brief Base class for unpreconditioned domain-wall fermion action
+ */
+
+#ifndef __unprec_dwf_fermact_base_w_h__
+#define __unprec_dwf_fermact_base_w_h__
+
+#include "fermact.h"
+
+using namespace QDP;
+
+//! Base class for unpreconditioned domain-wall fermion action
+/*! \ingroup fermact
+ *
+ * Unprecondition domain-wall fermion action. The conventions used here
+ * are specified in Phys.Rev.D63:094505,2001 (hep-lat/0005002).
+ */
+
+class UnprecDWFermActBase : public UnprecWilsonTypeFermAct<LatticeDWFermion>
+{
+public:
+  //! Return the quark mass
+  virtual Real quark_mass() const = 0;
+
+  //! Produce a linear operator for this action but with quark mass 1
+  virtual const LinearOperator<LatticeDWFermion>* linOpPV(const multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Redefine quark propagator routine for 4D fermions
+  void qprop(LatticeFermion& psi, 
+	     const multi1d<LatticeColorMatrix>& u, 
+	     const LatticeFermion& chi, 
+	     enum InvType invType,
+	     const Real& RsdCG, 
+	     int MaxCG, int& ncg_had) const;
+
+  //! Compute dS_f/dU
+  multi1d<LatticeColorMatrix> dsdu(const multi1d<LatticeColorMatrix>& u,
+				   const LatticeDWFermion& psi) const;
+};
+
+#endif
