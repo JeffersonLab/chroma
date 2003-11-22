397e219d2590887165d5da90225e918d74e54f03
edwards <edwards>
2003-11-22 21:32:10 +0000

    Removed proto-dsdu code and moved it to precondition version - that's what this stuff is.

--- lib/actions/ferm/fermacts/unprec_wilson_fermact_w.cc
+++ lib/actions/ferm/fermacts/unprec_wilson_fermact_w.cc
@@ -1,4 +1,4 @@
-// $Id: unprec_wilson_fermact_w.cc,v 1.8 2003-11-20 05:43:41 edwards Exp $
+// $Id: unprec_wilson_fermact_w.cc,v 1.9 2003-11-22 21:32:10 edwards Exp $
 /*! \file
  *  \brief Unpreconditioned Wilson fermion action
  */
@@ -69,76 +69,8 @@ UnprecWilsonFermAct::dsdu(multi1d<LatticeColorMatrix> & ds_u,
 {
   START_CODE("UnprecWilsonFermAct::dsdu");
   
-//  multi1d<LatticeColorMatrix> ds_u(Nd);
-
-  // hack
-  ds_u = 0;
-
-#if 0
-  LatticeColorMatrix utmp_1;
-  LatticeFermion phi;
-  LatticeFermion rho;
-  LatticeFermion sigma;
-  LatticeFermion ftmp_1;
-  LatticeFermion ftmp_2;
-  Double ddummy;
-  Real dummy;
-  int cb;
-
-  // Do the usual Wilson fermion dS_f/dU
-  const LinearOperator* A = linOp(u);
-
-  WilsonDslash  D(u);
-
-  //  phi = M(u)*psi
-  LatticeFermion phi = A(psi, PLUS);
-    
-    /* rho = Dslash(0<-1) * psi */
-  rho = D.apply(psi, PLUS, 1);
-    
-  /* phi = (KappaMD^2)*phi = -(KappaMD^2)*M*psi */
-  dummy = -(KappaMD*KappaMD);
-  phi *= dummy;
-    
-    /* sigma = Dslash_dag(0 <- 1) * phi */
-  sigma = D.apply(phi, MINUS, 1);
-    
-  for(int mu = 0; mu < Nd; ++mu)
-  {
-    cb = 0;
-
-    /* ftmp_2 = (gamma(mu))*psi */
-    ftmp_2 = Gamma(1<<mu) * psi;
-    /* ftmp_2(x) = -(psi(x) - ftmp_2(x)) = -(1 - gamma(mu))*psi( x )  */
-    ftmp_2 -= psi;
-    utmp_1 = -(shift(ftmp_2, cb, FORWARD, mu) * adj(sigma));
-
-    /* ftmp_2 = (gamma(mu))*phi */
-    SPIN_PRODUCT(phi,(INTEGER_LSHIFT_FUNCTION(1,mu)),ftmp_2);
-    /* ftmp_2 = phi + ftmp_2 = (1 + gamma(mu))*phi( x)  */
-    ftmp_2 += phi;
-    utmp_1 += shift(ftmp_2, cb, FORWARD, mu) * adj(rho);
-    ds_u[mu][cb] += u[mu][cb] * utmp_1;
-      
-    cb = 1;
-
-    /* ftmp_2 = (gamma(mu))*ftmp_1 */
-    SPIN_PRODUCT(rho,(INTEGER_LSHIFT_FUNCTION(1,mu)),ftmp_2);
-    /* ftmp_2 = -(rho - ftmp_2) = -(1 - gamma(mu))*rho( x )  */
-    ftmp_2 -= rho;
-    utmp_1 = -(shift(ftmp_2, cb, FORWARD, mu) * adj(phi));
-      
-    /* ftmp_2 = (gamma(mu))*sigma */
-    SPIN_PRODUCT(sigma,(INTEGER_LSHIFT_FUNCTION(1,mu)),ftmp_2);
-    /* ftmp_1 = ftmp_1 + ftmp_2 = (1 + gamma(mu))*sigma( x + mu)  */
-    ftmp_2 += sigma;
-    utmp_1 += shift(ftmp_2, cb, FORWARD, mu) * adj(psi);
-    ds_u[mu][cb] += u[mu][cb] * utmp_1;
-      
-  }
-
-  delete A;
-#endif
+  QDPIO::cerr << "UnprecWilsonFermAct::dsdu not implemented" << endl;
+  QDP_abort(1);
 
   END_CODE("UnprecWilsonFermAct::dsdu");
 }
