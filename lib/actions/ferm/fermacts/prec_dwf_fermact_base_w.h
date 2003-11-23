c7b7857c54ed5e92c45df19ef0bb9bbe296d99f2
edwards <edwards>
2003-11-23 05:52:20 +0000

    Base class for even-odd prec DWF-like actions.

---
+++ lib/actions/ferm/fermacts/prec_dwf_fermact_base_w.h
@@ -0,0 +1,39 @@
+// -*- C++ -*-
+// $Id: prec_dwf_fermact_base_w.h,v 1.1 2003-11-23 05:52:20 edwards Exp $
+/*! \file
+ *  \brief Base class for even-odd preconditioned domain-wall-like fermion actions
+ */
+
+#ifndef __prec_dwf_fermact_base_w_h__
+#define __prec_dwf_fermact_base_w_h__
+
+#include "fermact.h"
+
+using namespace QDP;
+
+//! Base class for unpreconditioned domain-wall-like fermion actions
+/*! \ingroup fermact
+ *
+ * Unprecondition domain-wall fermion action. The conventions used here
+ * are specified in Phys.Rev.D63:094505,2001 (hep-lat/0005002).
+ */
+
+class EvenOddPrecDWFermActBase : public EvenOddPrecWilsonTypeFermAct<LatticeDWFermion>
+{
+public:
+  //! Return the quark mass
+  virtual Real quark_mass() const = 0;
+
+  //! Produce a linear operator for this action but with quark mass 1
+  virtual const EvenOddPrecLinearOperator<LatticeDWFermion>* linOpPV(const multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Define quark propagator routine for 4D fermions
+  void qprop(LatticeFermion& psi, 
+	     const multi1d<LatticeColorMatrix>& u, 
+	     const LatticeFermion& chi, 
+	     enum InvType invType,
+	     const Real& RsdCG, 
+	     int MaxCG, int& ncg_had) const;
+};
+
+#endif
