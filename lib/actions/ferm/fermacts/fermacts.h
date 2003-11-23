3f1b0b6f57122dc5dfb2c71c9f67d945eed1165b
edwards <edwards>
2003-11-23 05:52:47 +0000

    Added prec. DWF support.

--- lib/actions/ferm/fermacts/fermacts.h
+++ lib/actions/ferm/fermacts/fermacts.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: fermacts.h,v 1.6 2003-11-16 06:19:56 edwards Exp $
+// $Id: fermacts.h,v 1.7 2003-11-23 05:52:47 edwards Exp $
 
 /*! \file
  * \brief Fermion actions
@@ -17,10 +17,18 @@
 #define __fermactss_h__
 
 #include "unprec_wilson_fermact_w.h"
+#include "prec_wilson_fermact_w.h"
+
+#include "unprec_dwf_fermact_base_w.h"
 #include "unprec_dwf_fermact_w.h"
+#include "unprec_dwf_fermact_base_array_w.h"
 #include "unprec_dwf_fermact_array_w.h"
 #include "unprec_ovdwf_fermact_array_w.h"
 #include "unprec_ovext_fermact_array_w.h"
+
+#include "prec_dwf_fermact_base_w.h"
+#include "prec_dwf_fermact_base_array_w.h"
+#include "prec_dwf_fermact_array_w.h"
 
 // #include "zolotarev4d_fermact_w.h"
 
