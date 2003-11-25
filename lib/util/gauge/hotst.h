cb1490bb8d45cbc1212f75623e637cd63737cbcc
edwards <edwards>
2003-11-25 04:18:56 +0000

    Added doxygen comments.

--- lib/util/gauge/hotst.h
+++ lib/util/gauge/hotst.h
@@ -1,9 +1,29 @@
 // -*- C++ -*-
-// $Id: hotst.h,v 1.1 2003-02-15 05:54:26 edwards Exp $
+// $Id: hotst.h,v 1.2 2003-11-25 04:18:56 edwards Exp $
+/*! \file
+ *  \brief Set a gauge field from a sample of (almost) Haar measure
+ */
 
 #ifndef HOTST_INCLUDE
 #define HOTST_INCLUDE
 
+//! Set a gauge field from a sample of (almost) Haar measure
+/*!
+ * \ingroup gauge
+ *
+ * u = exp(A), where A = random traceless antihermitian matrix.
+ *
+ * Arguments:
+ *
+ *  \param u          Gauge field               (Modify)
+ *
+ *
+ * NOTE!!!!: we are using a hack. The twelth_order exponentiation followed
+ *   by a reunitarization should be replace by an exact exponentiation.
+ *   So, the gauge field distriubution is NOT from Haar measure, but
+ *   if someone can think of a really good reason why it should be Haar
+ *   measure, then something can be done about it. 
+ */
 void HotSt(multi1d<LatticeColorMatrix>& u);
 
 #endif
