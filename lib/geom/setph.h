2e88c541bd4bffbf049e4df9ed48d422727533d4
edwards <edwards>
2003-12-16 21:54:38 +0000

    Simple-minded version of BC phase-factor constructor.

---
+++ lib/geom/setph.h
@@ -0,0 +1,36 @@
+// -*- C++ -*-
+// $Id: setph.h,v 1.1 2003-12-16 21:54:38 edwards Exp $
+/*! \file
+ *  \brief  This routine is specific to Wilson fermions!
+ */
+
+#ifndef __setph_h__
+#define __setph_h__
+
+//!  Initialize phase factors
+/*!
+ * \ingroup gauge
+ *
+ * Setup the "phases" used by routine PHFCTR that multiplies the gauge
+ * fields so as to handle boundary conditions.
+ *
+ * Arguments:
+ *
+ *  \param boundary       boundary conditions of the lattice       (Read)
+ */
+
+void setph(const multi1d<int>& boundary);
+
+
+//! Multiply the gauge fields by the phase factors (-1)^X
+/*!
+ * \ingroup gauge
+ *
+ * Arguments:
+ *
+ *  \param u          Gauge field               (Modify)
+ */
+void phfctr(multi1d<LatticeColorMatrix>& u);
+
+
+#endif
