5735de6e4eebf29ae73e2a228aea51e92cc2853a
edwards <edwards>
2003-12-26 20:00:38 +0000

    More changes.

--- mainprogs/main/seqprop.cc
+++ mainprogs/main/seqprop.cc
@@ -1,4 +1,4 @@
-// $Id: seqprop.cc,v 1.2 2003-12-17 05:04:28 edwards Exp $
+// $Id: seqprop.cc,v 1.3 2003-12-26 20:00:38 edwards Exp $
 /*! \file
  *  \brief Main code for sequential propagator generation
  */
@@ -12,6 +12,249 @@
 #define MAIN
 
 #include "chroma.h"
+
+/*
+ *  Here we have various temporary definitions
+ */
+enum CfgType {
+  CFG_TYPE_MILC = 0,
+  CFG_TYPE_NERSC,
+  CFG_TYPE_SCIDAC,
+  CFG_TYPE_SZIN,
+  CFG_TYPE_UNKNOWN
+} ;
+
+enum PropType {
+  PROP_TYPE_SCIDAC = 2,
+  PROP_TYPE_SZIN,
+  PROP_TYPE_UNKNOWN
+} ;
+
+enum FermType {
+  FERM_TYPE_WILSON,
+  FERM_TYPE_UNKNOWN
+};
+
+
+
+using namespace QDP;
+
+
+/*
+ * Input 
+ */
+struct IO_version_t
+{
+  int version;
+};
+
+// Parameters which must be determined from the XML input
+// and written to the XML output
+struct Param_t
+{
+  FermType     FermTypeP;
+  Real         Kappa;      // Wilson mass
+ 
+  CfgType  cfg_type;       // storage order for stored gauge configuration
+  PropType prop_type;      // storage order for stored propagator
+
+  enum InvType  invType;   // Inverter type
+  Real RsdCG;
+  int MaxCG;		   // Iteration parameters
+
+  multi1d<int> nrow;
+  multi1d<int> boundary;
+  multi1d<int> t_srce;
+};
+
+struct Cfg_t
+{
+  string       cfg_file;
+};
+
+struct Prop_t
+{
+  string       source_file;
+  string       prop_file;
+};
+
+struct Propagator_input_t
+{
+  IO_version_t     io_version;
+  Param_t          param;
+  Cfg_t            cfg;
+  Prop_t           prop;
+};
+
+
+//
+void read(XMLReader& xml, const string& path, Cfg_t& input)
+{
+  XMLReader inputtop(xml, path);
+
+  read(inputtop, "cfg_file", input.cfg_file);
+}
+
+
+//
+void read(XMLReader& xml, const string& path, Prop_t& input)
+{
+  XMLReader inputtop(xml, path);
+
+  read(inputtop, "source_file", input.source_file);
+  read(inputtop, "prop_file", input.prop_file);
+}
+
+
+
+// Reader for input parameters
+void read(XMLReader& xml, const string& path, Propagator_input_t& input)
+{
+  XMLReader inputtop(xml, path);
+
+
+  // First, read the input parameter version.  Then, if this version
+  // includes 'Nc' and 'Nd', verify they agree with values compiled
+  // into QDP++
+
+  // Read in the IO_version
+  try
+  {
+    read(inputtop, "IO_version/version", input.io_version.version);
+  }
+  catch (const string& e) 
+  {
+    QDPIO::cerr << "Error reading data: " << e << endl;
+    throw;
+  }
+
+
+  // Currently, in the supported IO versions, there is only a small difference
+  // in the inputs. So, to make code simpler, extract the common bits 
+
+  // Read the uncommon bits first
+  try
+  {
+    XMLReader paramtop(inputtop, "param"); // push into 'param' group
+
+    switch (input.io_version.version) 
+    {
+      /**************************************************************************/
+    case 1 :
+      /**************************************************************************/
+      break;
+
+    default :
+      /**************************************************************************/
+
+      QDPIO::cerr << "Input parameter version " << input.io_version.version << " unsupported." << endl;
+      QDP_abort(1);
+    }
+  }
+  catch (const string& e) 
+  {
+    QDPIO::cerr << "Error reading data: " << e << endl;
+    throw;
+  }
+
+
+  // Read the common bits
+  try 
+  {
+    XMLReader paramtop(inputtop, "param"); // push into 'param' group
+
+    {
+      string ferm_type_str;
+      read(paramtop, "FermTypeP", ferm_type_str);
+      if (ferm_type_str == "WILSON") {
+	input.param.FermTypeP = FERM_TYPE_WILSON;
+      } else {
+	input.param.FermTypeP = FERM_TYPE_UNKNOWN;
+      }
+    }
+
+    // GTF NOTE: I'm going to switch on FermTypeP here because I want
+    // to leave open the option of treating masses differently.
+    switch (input.param.FermTypeP) {
+    case FERM_TYPE_WILSON :
+
+      QDPIO::cout << " PROPAGATOR: Propagator for Wilson fermions" << endl;
+
+//      read(paramtop, "numKappa", input.param.numKappa);
+      read(paramtop, "Kappa", input.param.Kappa);
+
+#if 0
+      for (int i=0; i < input.param.numKappa; ++i) {
+	if (toBool(input.param.Kappa[i] < 0.0)) {
+	  QDPIO::cerr << "Unreasonable value for Kappa." << endl;
+	  QDPIO::cerr << "  Kappa[" << i << "] = " << input.param.Kappa[i] << endl;
+	  QDP_abort(1);
+	} else {
+	  QDPIO::cout << " Spectroscopy Kappa: " << input.param.Kappa[i] << endl;
+	}
+      }
+#endif
+
+      break;
+
+    default :
+      QDPIO::cerr << "Fermion type not supported." << endl;
+      if (input.param.FermTypeP == FERM_TYPE_UNKNOWN) {
+	QDPIO::cerr << "  FermTypeP = UNKNOWN" << endl;
+      }
+      QDP_abort(1);
+    }
+
+    {
+      string cfg_type_str;
+      read(paramtop, "cfg_type", cfg_type_str);
+      if (cfg_type_str == "SZIN") {
+	input.param.cfg_type = CFG_TYPE_SZIN;
+      } else {
+	input.param.cfg_type = CFG_TYPE_UNKNOWN;
+      }
+    }
+
+    {
+      string prop_type_str;
+      read(paramtop, "prop_type", prop_type_str);
+      if (prop_type_str == "SZIN") {
+	input.param.prop_type = PROP_TYPE_SZIN;
+      } else {
+	input.param.prop_type = PROP_TYPE_UNKNOWN;
+      }
+    }
+
+//    read(paramtop, "invType", input.param.invType);
+//    input.param.invType = CG_INVERTER;   //need to fix this
+    read(paramtop, "RsdCG", input.param.RsdCG);
+    read(paramtop, "MaxCG", input.param.MaxCG);
+
+    read(paramtop, "nrow", input.param.nrow);
+    read(paramtop, "boundary", input.param.boundary);
+    read(paramtop, "t_srce", input.param.t_srce);
+  }
+  catch (const string& e) 
+  {
+    QDPIO::cerr << "Error reading data: " << e << endl;
+    throw;
+  }
+
+
+  // Read in the gauge configuration file name
+  try
+  {
+    read(inputtop, "Cfg", input.cfg);
+    read(inputtop, "Prop", input.prop);
+  }
+  catch (const string& e) 
+  {
+    QDPIO::cerr << "Error reading data: " << e << endl;
+    throw;
+  }
+}
+
+
 
 //! Sequential propagator generation
 /*
