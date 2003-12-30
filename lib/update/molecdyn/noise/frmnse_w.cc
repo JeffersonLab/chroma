20f4fe126bed7161937ac8cb740126fd131e617a
edwards <edwards>
2003-12-30 20:07:56 +0000

    Rough versions of noise terms.

---
+++ lib/update/molecdyn/noise/frmnse_w.cc
@@ -0,0 +1,64 @@
+// $Id: frmnse_w.cc,v 1.1 2003-12-30 20:07:56 edwards Exp $
+
+#error "NOT FULLY CONVERTED - NEED TO FIT TO USE VIRTUAL FUNCS"
+
+#include "chromabase.h"
+
+using namespace QDP;
+
+/* This routine is specific to Wilson fermions! */
+
+//! Calculates the Pseudofermion fields and the pseudofermionic action
+/*!
+ * \ingroup molecdyn
+ *
+ */
+/* u           -- gauge field ( Read ) */
+/* chi         -- pseudofermion field. ( Write ) */
+/* psi         -- pseudofermion field. ( Write ) */
+/* chi_norm    -- | chi | ( Write ) */
+/* n_zero      -- Number and type of zero modes (for Overlap) (Read) */
+/* Ncb         -- Number of checkerboards (Read) */
+/* Npf         -- Number of pseudofermion fields (Read) */
+/* n_congrd    -- Number of CG iteration ( Write ) */
+
+/* eta = Gaussian random noise */
+/* chi = M_dag * eta */
+/* psi = (M_dag*M)^(-1) * chi */
+
+void FrmNse(multi1d<LatticeColorMatrix>& u,
+	    multi1d<LatticeFermion>& chi,
+	    multi1d<LatticeFermion>& psi,
+	    int Npf,
+	    int& n_congrd;
+	    int n_zero)
+{
+  START_CODE("FrmNse");
+  
+  int n_count;
+  n_congrd = 0;
+
+  for(int i = 0; i < Npf; ++i)
+  {
+    switch (FermAct)
+    {
+    case WILSON:
+    case PARITY_BREAKING_WILSON:
+    case CLOVER:
+      /* The noise is generated the same for these fermions */
+      WlFrmNse(u, chi[i][0], psi[i][0], n_count);
+      break;
+    
+    case OVERLAP_POLE:
+      OvFrmNse(u, chi[i][0], psi[i][0], n_zero, n_count);
+      break;
+
+    default:
+      QDP_error_exit("Unsupported fermion action", FermAct);
+    }
+
+    n_congrd += n_count;
+  }
+
+  END_CODE("FrmNse");
+}
---
+++ lib/update/molecdyn/noise/wlfrmnse_w.cc
@@ -0,0 +1,68 @@
+// $Id: wlfrmnse_w.cc,v 1.1 2003-12-30 20:07:56 edwards Exp $
+
+#error "NOT FULLY CONVERTED"
+
+#include "chromabase.h"
+
+using namespace QDP;
+
+/* This routine is specific to Wilson fermions! */
+
+//! Calculates the Pseudofermion fields and the pseudofermionic action
+/*!
+ * \ingroup molecdyn
+ *
+ */
+/* u           -- gauge field ( Read ) */
+/* chi         -- pseudofermion field. ( Write ) */
+/* psi         -- pseudofermion field. ( Write ) */
+/* n_congrd    -- Number of CG iteration ( Write ) */
+
+/* eta = Gaussian random noise */
+/* chi = M_dag * eta */
+/* psi = (M_dag*M)^(-1) * chi */
+
+void WlFrmNse(const multi1d<LatticeColorMatrix>& u,
+	      LatticeFermion& chi,
+	      LatticeFermion& psi,
+	      int& n_congrd,
+{
+  START_CODE("WlFrmNse");
+  
+  LINEAR_OPERATOR(A);
+
+  phfctr(u, FORWARD);              /* ON */
+
+  /* Construct the linear operator */
+  /* This allocates field for the appropriate action */
+  ConsLinOp(A, u, KappaMC, FermAct);
+
+  /* Initialize psi */
+  psi = 0;
+  
+  /* Fill eta with random gaussian noise: < eta_dagger * eta > = 1 */
+  LatticeFermion eta;
+  gaussian(eta);
+  
+#if 0
+  /* If using Schroedinger functional, zero out the boundaries */
+  if ( SchrFun > 0 )
+  {
+    FILLMASK(eta, lSFmaskF(1), ZERO);
+  }
+#endif
+
+  /* chi = M_dag*eta */
+  A(A, eta, chi, 1, MINUS);
+
+  /* psi = (1/(M_dag*M))*chi */
+  InvCG2(A, chi, psi, RsdCGMC, 1, n_congrd);
+
+  
+  /* Clean up from the operator */
+  DestLinOp (A, FermAct);
+    
+  phfctr (u, BACKWARD);              /* OFF */
+
+  END_CODE("WlFrmNse");
+}
