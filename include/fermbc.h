b74f9ca7f78204a37a10b5e59e1d7ee26cfddc28
edwards <edwards>
2004-01-02 03:05:47 +0000

    Boundary condition support.

---
+++ include/fermbc.h
@@ -0,0 +1,283 @@
+// -*- C++ -*-
+// $Id: fermbc.h,v 1.1 2004-01-02 03:05:47 edwards Exp $
+/*! @file
+ * @brief Fermion action boundary conditions
+ */
+
+#ifndef __fermbc_h__
+#define __fermbc_h__
+
+using namespace QDP;
+
+#include "gaugebc.h"
+#include "handle.h"
+
+//! Base class for all gauge action boundary conditions
+/*! @ingroup actions
+ *
+ */
+template<class T>
+class FermBC
+{
+public:
+  //! Virtual destructor to help with cleanup;
+  virtual ~FermBC() {}
+
+  //! Modify U fields according to the fermion BC in place
+  virtual void modifyU(multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Modify fermion fields in place
+  virtual void modifyF(T& psi) const = 0;
+ 
+  //! Says if there are fermion non-trivial 
+  virtual bool nontrivialP() const = 0;
+};
+
+
+//! Abstract class for all gauge action boundary conditions with Schroedinger BC
+/*! @ingroup actions
+ *
+ *  Schroedinger BC implies periodic in dirs orthog to decay dir, and some
+ *  kind of fixed BC in the decay dir.
+ */
+template<class T>
+class SchrFermBC : public FermBC<T>
+{
+public:
+  //! Virtual destructor to help with cleanup;
+  virtual ~SchrFermBC() {}
+};
+
+
+//! Concrete class for 1-link gauge action boundary conditions with Schroedinger BC
+/*! @ingroup actions
+ *
+ *  Schroedinger BC for gauge actions that have only 1 link for padding
+ *  in the decay direction - e.g., Wilson-like gauge action
+ */
+template<class T>
+class Schr1LinkFermBC : public SchrFermBC<T>
+{
+public:
+  //! Schroedinger BC
+  /*! 
+   * \param gbc_    a 1-link Schroedinger GAUGE BC
+   * \param theta_  twist angle for BC
+   */
+  Schr1LinkFermBC(const Schr1LinkGaugeBC& gbc_, const Real& theta_) : gbc(gbc_) 
+    {init(theta);}
+
+  //! Copy constructor
+  Schr1LinkFermBC(const Schr1LinkFermBC& a) : gbc(a.gbc), mask(a.mask), fld(a.fld) {}
+
+  //! Destructor is automatic
+  ~Schr1LinkFermBC() {}
+
+  //! Assignment
+  Schr1LinkFermBC& operator=(const Schr1LinkFermBC& a)
+    {gbc = a.gbc; mask = a.mask; fld = a.fld; return *this;}
+
+  //! Modify U fields in place
+  virtual void modifyU(multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Modify fermion fields in place
+  virtual void modifyF(T& psi) const = 0;
+ 
+  //! Says if there are fixed links within the lattice
+  bool nontrivialP() const {return true;}
+
+protected:
+  void init(const Real& theta);
+    
+private:
+  // Hide default constuctor and operator=
+  Schr1LinkFermBC() {}
+
+private:
+  Schr1LinkGaugeBC  gbc;
+  multi1d<LatticeBoolean> mask;
+  multi1d<LatticeColorMatrix> fld;
+};
+
+
+//! Concrete class for 2-link gauge action boundary conditions with Schroedinger BC
+/*! @ingroup actions
+ *
+ *  Schroedinger BC for gauge actions that have only 2 links for padding
+ *  in the decay direction - e.g., Symanzik-like gauge action
+ */
+template<class T>
+class Schr2LinkFermBC : public SchrFermBC<T>
+{
+public:
+  //! Schroedinger BC
+  /*! 
+   * \param gbc_    a 2-link Schroedinger GAUGE BC
+   * \param theta_  twist angle for BC
+   */
+  Schr2LinkFermBC(const Schr2LinkGaugeBC& gbc_, const Real& theta_) : gbc(gbc_) 
+    {init(theta);}
+
+  //! Copy constructor
+  Schr2LinkFermBC(const Schr2LinkFermBC& a) : gbc(a.gbc), mask(a.mask), fld(a.fld) {}
+
+  //! Destructor is automatic
+  ~Schr2LinkFermBC() {}
+
+  //! Assignment
+  Schr2LinkFermBC& operator=(const Schr2LinkFermBC& a)
+    {gbc = a.gbc; mask = a.mask; fld = a.fld; return *this;}
+
+  //! Type of Schroedinger BC
+  SchrFunType_t getSFermBC() const {return SchrFun;}
+
+  //! Modify U fields in place
+  virtual void modifyU(multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Modify fermion fields in place
+  virtual void modifyF(T& psi) const = 0;
+ 
+  //! Says if there are fixed links within the lattice
+  bool nontrivialP() const {return true;}
+
+protected:
+  void init(const Real& theta);
+    
+private:
+  // Hide default constuctor and operator=
+  Schr2LinkFermBC() {}
+
+private:
+  Schr2LinkGaugeBC  gbc;
+  multi1d<LatticeBoolean> mask;
+  multi1d<LatticeColorMatrix> fld;
+};
+
+
+//! Concrete class for all gauge actions with trivial boundary conditions
+/*! @ingroup actions
+ *
+ *  No BC
+ */
+template<class T>
+class PeriodicFermBC : public FermBC<T>
+{
+public:
+  //! Only full constructor
+  PeriodicFermBC() {}
+
+  //! Copy constructor
+  PeriodicFermBC(const PeriodicFermBC& a) {}
+
+  //! Destructor is automatic
+  ~PeriodicFermBC() {}
+
+  //! Assignment
+  PeriodicFermBC& operator=(const PeriodicFermBC&) {return *this;}
+
+  //! Modify U fields in place
+  /*! NOP */
+  void modifyU(multi1d<LatticeColorMatrix>& u) const {}
+
+  //! Modify fermion fields in place
+  /*! NOP */
+  void modifyF(T& psi) const {}
+ 
+  //! Says if there are non-trivial BC links
+  bool nontrivialP() const {return false;}
+};
+
+
+//! Concrete class for all gauge actions with simple boundary conditions
+/*! @ingroup actions
+ *
+ *  Simple BC, where boundary array is multiplied on the links on the
+ *  edge of the lattice
+ */
+template<class T>
+class SimpleFermBC : public FermBC<T>
+{
+public:
+  //! Only full constructor
+  /*!
+   * \param boundary  multiply links on edge of lattice by boundary
+   *
+   * NOTE: there is no real reason this is of type int, could be more general
+   *       like Complex
+   */
+  SimpleFermBC(const multi1d<int>& boundary_) : 
+    gbc(new PeriodicGaugeBC()), boundary(boundary_)
+    {init();}
+  
+  //! Use both a Gauge BC and a boundary field
+  /*!
+   * \param gbc       A simple Gauge BC
+   * \param boundary  multiply links on edge of lattice by boundary
+   *
+   * The type of gbc is restricted here mainly for paranoia.
+   * The type could be loosened (in fact to an abstract type), but it makes
+   * no sense to apply these type FermBC to a Schr1LinkGaugeBC, for example.
+   *
+   * Currently, no code uses this capability...
+   */
+  SimpleFermBC(const SimpleGaugeBC& gbc_, const multi1d<int>& boundary_) : 
+    gbc(new SimpleGaugeBC(gbc_)), boundary(boundary_)
+    {init();}
+  
+  //! Copy constructor
+  SimpleFermBC(const SimpleFermBC& a) : 
+    gbc(a.gbc), boundary(a.boundary), nontriv(a.nontriv) {}
+
+  //! Destructor is automatic
+  ~SimpleFermBC() {}
+
+  //! Assignment
+  SimpleFermBC& operator=(const SimpleFermBC& a)
+    {gbc = a.gbc; boundary = a.boundary; nontriv = a.nontriv; return *this;}
+
+  //! Modify U fields in place
+  void modifyU(multi1d<LatticeColorMatrix>& u) const
+    {
+      gbc->modify(u);   // first modify the U fields
+
+      if (nontrivialP())
+      {
+	// phases for all the directions
+	for(int m = 0; m < Nd; ++m)
+	{
+	  /* u[m] *=  (coord(m) == nrow(m)-1 ) ? boundary[m] : 1 */
+	  u[m] *= where(Layout::latticeCoordinate(m) == Layout::lattSize()[m]-1,
+			Integer(boundary[m]), Integer(1));
+	}
+      }
+    }
+
+  //! Modify fermion fields in place
+  /*! NOP */
+  void modifyF(T& psi) const {}
+ 
+  //! Says if there are non-trivial BC links
+  bool nontrivialP() const {return nontriv;}
+
+protected:
+  void init()
+    {
+      // Check triviality of boundary
+      nontriv = false;
+      for(int m = 0; m < Nd; ++m)
+	if (boundary[m] != 1)
+	  nontriv |= true;
+    }
+
+private:
+  // No empty constructor
+  SimpleFermBC() {}
+
+private:
+  Handle<GaugeBC> gbc;
+  multi1d<int> boundary;
+  bool nontriv;
+};
+
+
+#endif
---
+++ include/gaugebc.h
@@ -0,0 +1,320 @@
+// -*- C++ -*-
+// $Id: gaugebc.h,v 1.1 2004-01-02 03:05:47 edwards Exp $
+/*! @file
+ * @brief Gauge boundary conditions
+ */
+
+#ifndef __gaugebc_h__
+#define __gaugebc_h__
+
+using namespace QDP;
+
+
+//! Base class for all gauge action boundary conditions
+/*! @ingroup actions
+ *
+ */
+class GaugeBC
+{
+public:
+  //! Virtual destructor to help with cleanup;
+  virtual ~GaugeBC() {}
+
+  //! Apply the BC onto the U fields in place
+  virtual void modify(multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Zero the U fields in place on the masked links
+  virtual void zero(multi1d<LatticeColorMatrix>& u) const = 0;
+
+#if defined(EXPOSE_THIS_STUFF)
+  // NOT SURE THIS STUFF IS ABSOLUTELY REQUIRED - TRY TO AVOID EXPOSING THIS
+  //! Mask which lattice sites have fixed gauge links
+  virtual const multi1d<LatticeBoolean>& lbmaskU() const = 0;
+
+  //! Fixed gauge links on only the lbmaskU() sites
+  virtual const multi1d<LatticeColorMatrix>& lFldU() const = 0;
+#endif
+
+  //! Says if there are fixed links within the lattice
+  virtual bool nontrivialP() const = 0;
+};
+
+
+//! Schroedinger Functional type Boundary Conditions
+enum SchrFunType_t {
+  SF_NONE = 0, 
+  SF_TRIVIAL = 1,
+  SF_NONPERT = 2,
+  SF_COUPLING = 3,
+  SF_CHROMOMAG = 4,
+  SF_DIRICHLET = 10,
+};
+
+
+
+//! Abstract class for all gauge action boundary conditions with Schroedinger BC
+/*! @ingroup actions
+ *
+ *  Schroedinger BC implies periodic in dirs orthog to decay dir, and some
+ *  kind of fixed BC in the decay dir.
+ */
+class SchrGaugeBC : public GaugeBC
+{
+public:
+  //! Virtual destructor to help with cleanup;
+  virtual ~SchrGaugeBC() {}
+
+#if defined(EXPOSE_THIS_STUFF)
+  //! Type of Schroedinger BC
+  virtual SchrFunType_t getSFBC() const = 0;
+#endif
+
+  //! Decay direction
+  virtual int getDir() const = 0;
+};
+
+
+//! Concrete class for 1-link gauge action boundary conditions with Schroedinger BC
+/*! @ingroup actions
+ *
+ *  Schroedinger BC for gauge actions that have only 1 link for padding
+ *  in the decay direction - e.g., Wilson-like gauge action
+ */
+class Schr1LinkGaugeBC : public SchrGaugeBC
+{
+public:
+  //! Schroedinger BC
+  /*! 
+   * \param SchrFun_      type of Schroedinger BC
+   * \param SchrPhiMult_  factor to rescale fixed field
+   */
+  Schr1LinkGaugeBC(SchrFunType_t SchrFun_, const Real& SchrPhiMult_);
+
+  //! Copy constructor
+  Schr1LinkGaugeBC(const Schr1LinkGaugeBC& a) : SchrFun(a.SchrFun), 
+    decay_dir(a.decay_dir), mask(a.mask), fld(a.fld) {}
+
+  //! Destructor is automatic
+  ~Schr1LinkGaugeBC() {}
+
+  //! Assignment
+  Schr1LinkGaugeBC& operator=(const Schr1LinkGaugeBC& a)
+    {
+      SchrFun = a.SchrFun; decay_dir = a.decay_dir; 
+      mask = a.mask; fld = a.fld;
+      return *this;
+    }
+
+#if defined(EXPOSE_THIS_STUFF)
+  //! Type of Schroedinger BC
+  SchrFunType_t getSFBC() const {return SchrFun;}
+#endif
+
+  //! Modify U fields in place
+  virtual void modify(multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Zero the U fields in place on the masked links
+  virtual void zero(multi1d<LatticeColorMatrix>& u) const = 0;
+
+#if defined(EXPOSE_THIS_STUFF)
+  // NOT SURE THIS STUFF IS ABSOLUTELY REQUIRED - TRY TO AVOID EXPOSING THIS
+  //! Mask which lattice sites have fixed gauge links
+  const multi1d<LatticeBoolean>& lbmaskU() const {return mask;}
+
+  //! Fixed gauge links on only the lbmaskU() sites
+  const multi1d<LatticeColorMatrix>& lFldU() const {return fld;}
+#endif
+
+  //! Says if there are fixed links within the lattice
+  bool nontrivialP() const {return true;}
+
+  //! Decay direction
+  int getDir() const {return decay_dir;}
+
+private:
+  // Hide default constuctor
+  Schr1LinkGaugeBC() {}
+
+private:
+  SchrFunType_t SchrFun;
+  int decay_dir;
+  multi1d<LatticeBoolean> mask;
+  multi1d<LatticeColorMatrix> fld;
+};
+
+
+//! Concrete class for 2-link gauge action boundary conditions with Schroedinger BC
+/*! @ingroup actions
+ *
+ *  Schroedinger BC for gauge actions that have only 2 links for padding
+ *  in the decay direction - e.g., Symanzik-like gauge action
+ */
+class Schr2LinkGaugeBC : public SchrGaugeBC
+{
+public:
+  //! Schroedinger BC
+  /*! 
+   * \param SchrFun_      type of Schroedinger BC
+   * \param SchrPhiMult_  factor to rescale fixed field
+   */
+  Schr2LinkGaugeBC(SchrFunType_t SchrFun_, const Real& SchrPhiMult_);
+
+  //! Copy constructor
+  Schr2LinkGaugeBC(const Schr2LinkGaugeBC& a) : SchrFun(a.SchrFun), 
+    decay_dir(a.decay_dir), mask(a.mask), fld(a.fld) {}
+
+  //! Destructor is automatic
+  ~Schr2LinkGaugeBC() {}
+
+  //! Assignment
+  Schr2LinkGaugeBC& operator=(const Schr2LinkGaugeBC& a)
+    {
+      SchrFun = a.SchrFun; decay_dir = a.decay_dir; 
+      mask = a.mask; fld = a.fld;
+      return *this;
+    }
+
+#if defined(EXPOSE_THIS_STUFF)
+  //! Type of Schroedinger BC
+  SchrFunType_t getSFBC() const {return SchrFun;}
+#endif
+
+  //! Modify U fields in place
+  virtual void modify(multi1d<LatticeColorMatrix>& u) const = 0;
+
+  //! Zero the U fields in place on the masked links
+  virtual void zero(multi1d<LatticeColorMatrix>& u) const = 0;
+
+#if defined(EXPOSE_THIS_STUFF)
+  // NOT SURE THIS STUFF IS ABSOLUTELY REQUIRED - TRY TO AVOID EXPOSING THIS
+  //! Mask which lattice sites have fixed gauge links
+  const multi1d<LatticeBoolean>& lbmaskU() const {return mask;}
+
+  //! Fixed gauge links on only the lbmaskU() sites
+  const multi1d<LatticeColorMatrix>& lFldU() const {return fld;}
+#endif
+
+  //! Says if there are fixed links within the lattice
+  bool nontrivialP() const {return true;}
+
+  //! Decay direction
+  int getDir() const {return decay_dir;}
+
+private:
+  // Hide default constuctor
+  Schr2LinkGaugeBC() {}
+
+private:
+  SchrFunType_t SchrFun;
+  int decay_dir;
+  multi1d<LatticeBoolean> mask;
+  multi1d<LatticeColorMatrix> fld;
+};
+
+
+//! Concrete class for gauge actions with periodic boundary conditions
+/*! @ingroup actions
+ *
+ *  No gauge BC - e.g., only periodic
+ */
+class PeriodicGaugeBC : public GaugeBC
+{
+public:
+  //! Only empty constructor
+  PeriodicGaugeBC() {}
+
+  //! Copy constructor
+  PeriodicGaugeBC(const PeriodicGaugeBC& a) {}
+
+  //! Destructor is automatic
+  ~PeriodicGaugeBC() {}
+
+  //! Assignment
+  PeriodicGaugeBC& operator=(const PeriodicGaugeBC&) {return *this;}
+
+  //! Modify U fields in place
+  /*! NOP */
+  void modify(multi1d<LatticeColorMatrix>& u) const {}
+
+  //! Zero the U fields in place on the masked links
+  void zero(multi1d<LatticeColorMatrix>& u) const {}
+
+#if defined(EXPOSE_THIS_STUFF)
+  // NOT SURE THIS STUFF IS ABSOLUTELY REQUIRED - TRY TO AVOID EXPOSING THIS
+  //! Mask which lattice sites have fixed gauge links
+  const multi1d<LatticeBoolean>& lbmaskU() const {return mask;}
+
+  //! Fixed gauge links on only the lbmaskU() sites
+  const multi1d<LatticeColorMatrix>& lFldU() const {return fld;}
+#endif
+
+  //! Says if there are non-trivial BC links
+  bool nontrivialP() const {return false;}
+
+private:
+#if defined(EXPOSE_THIS_STUFF)
+  multi1d<LatticeBoolean> mask;     // is empty
+  multi1d<LatticeColorMatrix> fld;  // is empty
+#endif
+};
+
+
+//! Concrete class for gauge actions with simple boundary conditions
+/*! @ingroup actions
+ *
+ *  Simple BC, where boundary array is multiplied on the links on the
+ *  edge of the lattice
+ */
+class SimpleGaugeBC : public GaugeBC
+{
+public:
+  //! Only full constructor
+  /*!
+   * \param boundary  multiply links on edge of lattice by boundary
+   *
+   * NOTE: the type of boundary can be generalized
+   */
+  SimpleGaugeBC(const multi1d<Complex>& boundary_) : boundary(boundary_) {}
+
+  //! Copy constructor
+  SimpleGaugeBC(const SimpleGaugeBC& a) : boundary(a.boundary) {}
+
+  //! Destructor is automatic
+  ~SimpleGaugeBC() {}
+
+  //! Assignment
+  SimpleGaugeBC& operator=(const SimpleGaugeBC& a)
+    {boundary = a.boundary; return *this;}
+
+  //! Modify U fields in place
+  void modify(multi1d<LatticeColorMatrix>& u) const
+    {
+      // phases for all the directions
+      for(int m = 0; m < Nd; ++m)
+      {
+	/* u[m] *=  (coord(m) == nrow(m)-1 ) ? boundary[m] : 1 */
+	u[m] *= where(Layout::latticeCoordinate(m) == Layout::lattSize()[m]-1,
+		      boundary[m], Complex(1));
+      }
+    }
+
+  //! Zero the U fields in place on the masked links
+  void zero(multi1d<LatticeColorMatrix>& u) const {}
+
+  //! Says if there are non-trivial BC links
+  /*! 
+   * This is a simple implementation - always do the work. 
+   * Could be improved by checking boundary
+   */
+  bool nontrivialP() const {return true;}
+
+private:
+  // Hide empty constructor
+  SimpleGaugeBC() {}
+
+private:
+  multi1d<Complex> boundary;
+};
+
+
+#endif
