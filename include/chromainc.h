1274486a8337e48c04fa091cf2ecb30ed7c57aa9
edwards <edwards>
2004-01-02 01:42:59 +0000

    Removed some unneeded includes. Removed Exp. enum. (moved to expmat).

--- include/chromainc.h
+++ include/chromainc.h
@@ -1,4 +1,4 @@
-// $Id: chromainc.h,v 1.11 2003-10-15 22:03:45 edwards Exp $
+// $Id: chromainc.h,v 1.12 2004-01-02 01:42:59 edwards Exp $
 //
 // Include file that includes all the include files.
 
@@ -9,14 +9,13 @@
 #include "qdp_iogauge.h"
 
 #include "linearop.h"
-#include "primitives.h"
-#include "common_declarations.h"
+//#include "primitives.h"
+//#include "common_declarations.h"
 
 #include "lib.h"
 
 using namespace QDP;
 
-enum Exponentiate {TWELWTH_ORDER, EXACT};
 enum Sources {POINT_SOURCE, WALL_SOURCE, SHELL_SOURCE, BNDST_SOURCE, POINT_AND_BNDST_SOURCE, 
 	      SHELL_AND_BNDST_SOURCE, POINT_AND_SHELL_AND_BNDST_SOURCE};
 enum Sinks {POINT_SINK, WALL_SINK, POINT_AND_WALL_SINK, SHELL_SINK, POINT_AND_SHELL_SINK, 
