f2940673af60922ac8d8bb5f93d8d79b9dc0185d
edwards <edwards>
2004-01-13 03:56:53 +0000

    Moved out Sources and Sinks enums to meas/source/srcsnktype.h

--- lib/chromainc.h
+++ lib/chromainc.h
@@ -1,4 +1,4 @@
-// $Id: chromainc.h,v 1.1 2004-01-08 03:11:47 edwards Exp $
+// $Id: chromainc.h,v 1.2 2004-01-13 03:56:53 edwards Exp $
 //
 // Include file that includes all the include files.
 
@@ -8,17 +8,8 @@
 #include "qdp.h"
 #include "qdp_iogauge.h"
 
-#include "linearop.h"
-//#include "primitives.h"
-//#include "common_declarations.h"
-
 #include "lib.h"
 
 using namespace QDP;
-
-enum Sources {POINT_SOURCE, WALL_SOURCE, SHELL_SOURCE, BNDST_SOURCE, POINT_AND_BNDST_SOURCE, 
-	      SHELL_AND_BNDST_SOURCE, POINT_AND_SHELL_AND_BNDST_SOURCE};
-enum Sinks {POINT_SINK, WALL_SINK, POINT_AND_WALL_SINK, SHELL_SINK, POINT_AND_SHELL_SINK, 
-	    BNDST_SINK, POINT_AND_BNDST_SINK, SHELL_AND_BNDST_SINK, POINT_AND_SHELL_AND_BNDST_SINK};
 
 #endif
