a11ffc237e0d4545a8d9dbca76193d2fd7fde8a3
edwards <edwards>
2004-01-27 22:28:36 +0000

    Added xml output.

--- mainprogs/tests/t_lwldslash_sse.cc
+++ mainprogs/tests/t_lwldslash_sse.cc
@@ -1,4 +1,4 @@
-// $Id: t_lwldslash_sse.cc,v 1.16 2003-11-20 05:43:41 edwards Exp $
+// $Id: t_lwldslash_sse.cc,v 1.17 2004-01-27 22:28:36 edwards Exp $
 
 #include <iostream>
 #include <cstdio>
@@ -27,11 +27,23 @@ int main(int argc, char **argv)
   Layout::create();
 
   XMLFileWriter xml("t_lwldslash.xml");
+  push(xml,"t_lwldslash");
+
+  push(xml,"lattis");
+  write(xml,"latt_size",nrow);
+  write(xml,"logical_size",Layout::logicalSize());
+  write(xml,"subgrid_size",Layout::subgridLattSize());
+  pop(xml);
 
   // Make up a random gauge field.
   multi1d<LatticeColorMatrix> u(Nd);
+#if 1
   for(int m=0; m < u.size(); ++m)
     gaussian(u[m]);
+#else
+  u = 1;
+#endif
+
 
   // Make up a gaussian source and a zero result vector
   LatticeFermion psi, chi, chi2;
@@ -46,6 +58,8 @@ int main(int argc, char **argv)
 
   QDPIO::cout << "Done" << endl;
 
+#define DEBUG
+
   int isign, cb, loop;
   bool first = true;
   for(isign = 1; isign >= -1; isign -= 2) {
@@ -54,8 +68,9 @@ int main(int argc, char **argv)
       clock_t myt1;
       clock_t myt2;
       double mydt;
-      int iter; 
+      int iter=1; 
      
+#ifndef DEBUG
       if (first) 
       {
 	for(iter=1; ; iter <<= 1)
@@ -75,6 +90,7 @@ int main(int argc, char **argv)
 	  }
 	}
       }
+#endif
 	
       QDPIO::cout << "Applying D for timings" << endl;
       
@@ -107,8 +123,9 @@ int main(int argc, char **argv)
       clock_t myt1;
       clock_t myt2;
       double mydt;
-      int iter; 
+      int iter=1; 
       
+#ifndef DEBUG
       if (first) 
       {
 	for(iter=1; ; iter <<= 1)
@@ -128,6 +145,7 @@ int main(int argc, char **argv)
 	  }
 	}
       }
+#endif
 
       QDPIO::cout << "Applying D for timings" << endl;
       
@@ -177,20 +195,33 @@ int main(int argc, char **argv)
 
       chi = chi3;
       chi2 = chi3;
-      D_opt.apply(chi, psi, (isign > 0 ? PLUS : MINUS), cb);
-      D.apply(chi2, psi, (isign > 0 ? PLUS : MINUS), cb);
+      D.apply(chi, psi, (isign > 0 ? PLUS : MINUS), cb);
+      D_opt.apply(chi2, psi, (isign > 0 ? PLUS : MINUS), cb);
       
       n2 = norm2( chi2 - chi );
+
+#if 0
+      QDPIO::cout << "|D(psi, "
+		  << (isign > 0 ? "+, " : "-, ") <<  cb << ") = " << norm2(chi) << endl
+		  << "D_opt(psi, " 
+		  << (isign > 0 ? "+, " : "-, ") <<  cb << ") = " << norm2(chi2) << endl; 
+#endif
 
       QDPIO::cout << "OPT test: || D(psi, "
 		  << (isign > 0 ? "+, " : "-, ") <<  cb 
 		  << ") - D_opt(psi, " 
 		  << (isign > 0 ? "+, " : "-, ") <<  cb << " ) ||  = " << n2 
 		  << endl;
+
+      push(xml,"OPT test");
+      Write(xml,isign);
+      Write(xml,cb);
+      write(xml,"norm2_diff",n2);
+      pop(xml);
     }
   }
 	  
-      
+  pop(xml);
   
   // Time to bolt
   QDP_finalize();
