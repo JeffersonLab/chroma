fc7f6b9b84ea217111fc8e53be1ba158b78926f0
kostas <kostas>
2004-02-06 16:47:41 +0000

    Ward identity check works. Fixed the local current sign

--- lib/actions/ferm/qprop/dwf_quarkprop4_w.cc
+++ lib/actions/ferm/qprop/dwf_quarkprop4_w.cc
@@ -1,6 +1,9 @@
-// $Id: dwf_quarkprop4_w.cc,v 1.13 2004-02-05 21:15:05 kostas Exp $
+// $Id: dwf_quarkprop4_w.cc,v 1.14 2004-02-06 16:47:41 kostas Exp $
 // $Log: dwf_quarkprop4_w.cc,v $
-// Revision 1.13  2004-02-05 21:15:05  kostas
+// Revision 1.14  2004-02-06 16:47:41  kostas
+// Ward identity check works. Fixed the local current sign
+//
+// Revision 1.13  2004/02/05 21:15:05  kostas
 // fixed all bugs. Needs some clean up still
 //
 // Revision 1.12  2004/02/05 20:10:38  kostas
@@ -54,6 +57,7 @@ void dwf_conserved_axial_ps_corr(LatticeComplex& corr,
 
 void check_dwf_ward_identity(const multi1d<LatticeColorMatrix>& u,
 			     const multi1d<LatticePropagator>& p5d,
+			     const LatticePropagator& src,
 			     const LatticePropagator& q_q,
 			     const LatticePropagator& q_mp_q,
 			     const Real& m_q)
@@ -69,32 +73,36 @@ void check_dwf_ward_identity(const multi1d<LatticeColorMatrix>& u,
     divA += tt - shift(tt,BACKWARD,mu) ; 
   }
 
-  LatticeComplex mpps_ps, ps_ps, contact ;
+  LatticeComplex mpps_ps, ps_ps, q_bar_q ;
   mpps_ps = localNorm2(q_mp_q) ;
   ps_ps = localNorm2(q_q) ;
 
-  //contact = 
+  q_bar_q = trace(adj(src)*q_q) ;
 
   LatticeComplex diff ;
 
-  diff = divA - 2.0 * m_q * ps_ps - 2.0*mpps_ps ;
+  diff = divA - 2.0 * m_q * ps_ps - 2.0*mpps_ps + 2.0*q_bar_q;
 
   multi1d<Double> corr ;     
   SftMom trick(0,false,3) ;
   corr = sumMulti(localNorm2(diff), trick.getSet());
-  QDPIO::cout<<"check_dwf_ward_identity: Ward Identity violation per timeslice: "<<endl;
+  QDPIO::cout<<"check_dwf_ward_identity: ";
+  QDPIO::cout<<"Ward Identity violation per timeslice: "<<endl;
   for(int t(0);t<corr.size(); t++){
-    QDPIO::cout<<"        "<<t<<"                "<< corr[t]<<endl ;
+    QDPIO::cout<<"        "<<t<<"                "<< sqrt(corr[t])<<endl ;
   }
-  
-  
+
   QDPIO::cout<<"check_dwf_ward_identity: Ward Identity violation: ";
-  QDPIO::cout<<norm2(diff)<<endl ;
+  QDPIO::cout<<sqrt(norm2(diff))<<endl ;
 
-  QDPIO::cout<<"check_dwf_ward_identity: |divA|^2    : "<< norm2(divA)<<endl;
-  QDPIO::cout<<"check_dwf_ward_identity: |ps_ps|^2   : "<< norm2(ps_ps)<<endl;
-  QDPIO::cout<<"check_dwf_ward_identity: |mpps_ps|^2 : "<< norm2(mpps_ps)<<endl;
-
+  QDPIO::cout<<"check_dwf_ward_identity: |divA|^2    : "<<norm2(divA)<<endl;
+  QDPIO::cout<<"check_dwf_ward_identity: |ps_ps|^2   : "<<norm2(ps_ps)<<endl;
+  QDPIO::cout<<"check_dwf_ward_identity: |mpps_ps|^2 : "<<norm2(mpps_ps)<<endl;
+  QDPIO::cout<<"check_dwf_ward_identity: |q_bar_q|^2 : "<<norm2(q_bar_q)<<endl;
+  diff = m_q*ps_ps + mpps_ps - q_bar_q ;
+  Double gmor( sqrt(norm2(sum(diff))) ) ;
+  QDPIO::cout<<"check_dwf_ward_identity: GMOR        : "<<gmor<<endl;
+  
 }
 
 
@@ -125,6 +133,8 @@ void dwf_quarkProp4_a(LatticePropagator& q_sol,
   START_CODE("dwf_quarkProp4");
 
   push(xml_out, "DWF_QuarkProp4");
+
+  int t_dir = Nd-1 ; // Hard code Nd-1 to be the time direction 
 
   ncg_had = 0;
 
@@ -210,36 +220,35 @@ void dwf_quarkProp4_a(LatticePropagator& q_sol,
 
   // constuct the conserved axial current correlator
   LatticeComplex cfield ;
-  dwf_conserved_axial_ps_corr(cfield,state->getLinks(),prop5d,3);
+  dwf_conserved_axial_ps_corr(cfield,state->getLinks(),prop5d,t_dir);
 			       
   multi1d<DComplex> corr ;  
    
-  SftMom trick(0,false,3) ;
+  SftMom trick(0,false,t_dir) ;
    
   corr = sumMulti(cfield, trick.getSet());
-  // Length of lattice in decay direction
+  // Length of lattice in time direction
   int length = trick.numSubsets();
   multi1d<Real> mesprop(length);
-  //multi1d<Complex> axps(length);
   for(int t(0);t<length; t++){
     int t_eff( (t - t_src + length) % length ) ;
     mesprop[t_eff] = real(corr[t]) ; 
   }
 
-  //push(xml_out, "t_dir");
-  //Write(xml_out, 3);
-  //pop(xml_out);
+  push(xml_out, "time_direction");
+  write(xml_out, "t_dir",t_dir);
+  pop(xml_out);
   push(xml_out, "DWF_ConservedAxial");
-  //write(xml_out, "corr", corr);
   Write(xml_out, mesprop); 
   pop(xml_out);
 
   // The local axial corruent pseudoscalar correlator
-  cfield = trace( adj(q_sol)*Gamma(8)*q_sol ) ;
+  int d(1<<t_dir);
+  cfield = trace( adj(q_sol)*Gamma(d)*q_sol ) ;
   corr = sumMulti(cfield, trick.getSet()) ;
   for(int t(0);t<length; t++){
     int t_eff( (t - t_src + length) % length ) ;
-    mesprop[t_eff] = real(corr[t]) ; 
+    mesprop[t_eff] = -real(corr[t]) ; // sign fix
   }
   push(xml_out, "DWF_LocalAxial");
   Write(xml_out, mesprop); 
@@ -269,7 +278,8 @@ void dwf_quarkProp4_a(LatticePropagator& q_sol,
 
   pop(xml_out);   // DWF_QuarkProp4
 
-  check_dwf_ward_identity(state->getLinks(),prop5d,q_sol,q_mp,S_f.quark_mass());
+  check_dwf_ward_identity(state->getLinks(),prop5d,q_src,
+			  q_sol,q_mp,S_f.quark_mass());
 
 
   END_CODE("dwf_quarkProp4");
@@ -295,13 +305,11 @@ void dwf_conserved_axial_ps_corr(LatticeComplex& corr,
   //               G_1 --> Gamma(2)
   //               G_2 --> Gamma(4)
   //               G_3 --> Gamma(8)
-  int d(1);
-  for(int c(0);c<mu;c++)
-    d *= 2 ;
+  int d(1<<mu);
 
   int g5(Ns*Ns - 1) ;
 
-  LatticeComplex  C(0.0) ;
+  LatticeComplex  C ;
   corr = 0.0 ;
 
   int N5(p5d.size());
