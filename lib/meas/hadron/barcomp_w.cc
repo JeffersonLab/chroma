ea0753f351e41bde614feae68a362a0e5abc6729
edwards <edwards>
2004-02-06 04:22:28 +0000

    Overhauled interface. Now return barprop. Let driver code write data how it chooses.

--- lib/meas/hadron/barcomp_w.cc
+++ lib/meas/hadron/barcomp_w.cc
@@ -1,28 +1,12 @@
-//  $Id: barcomp_w.cc,v 1.9 2004-01-05 01:00:33 edwards Exp $
+//  $Id: barcomp_w.cc,v 1.10 2004-02-06 04:22:28 edwards Exp $
 /*! \file
  *  \brief Construct all components of a baryon propagator
  */
 
 #include "chromabase.h"
-#include "io/qprop_io.h"
 #include "meas/hadron/barcomp_w.h"
 
 using namespace QDP;
-
-//! Function object used for constructing the time-slice set
-class TimeSliceFunc : public SetFunc
-{
-public:
-  TimeSliceFunc(int dir): dir_decay(dir) {}
-
-  int operator() (const multi1d<int>& coordinate) const {return coordinate[dir_decay];}
-  int numSubsets() const {return Layout::lattSize()[dir_decay];}
-
-  int dir_decay;
-
-private:
-  TimeSliceFunc() {}  // hide default constructor
-};
 
  
 //! Construct all components of a baryon propagator
@@ -34,46 +18,34 @@ private:
  * In all baryons the colour components are contracted with the totally
  * antisymmetric 'tensor' eps(a,b,c) = antisym_tensor(a,b,c).
  *
- * \param quark_propagator_{1,2,3} quark propagators (read)
- * \param header_{1,2,3}           second quark propagator ( Read )
- * \param quark_propagator_3       third quark propagator  ( Read )
  * \param barprop                  baryon correlation function (in real space) ( Write )
- * \param num_mom                  number of non-zero momenta ( Read )
- * \param t0       cartesian coordinates of the source in the j_decay direction ( Read )
- * \param j_decay                  direction of the exponential decay ( Read )
+ * \param quark_propagator_1       quark propagator ( Read )
+ * \param quark_propagator_2       quark propagator ( Read )
+ * \param quark_propagator_3       quark propagator ( Read )
+ * \param phases                   object holds list of momenta ( Read )
+ * \param t0                       coordinates of source in decay direction ( Read )
  * \param bc_spec                  boundary condition for spectroscopy ( Read )
- * \param file                     the filename of the binary output file
  */
 
-void barcomp(const LatticePropagator& quark_propagator_1, 
-	     const PropHead& head_1,
+void barcomp(multiNd<Complex>& barprop,
+	     const LatticePropagator& quark_propagator_1, 
 	     const LatticePropagator& quark_propagator_2,
-	     const PropHead& head_2,
 	     const LatticePropagator& quark_propagator_3,
-	     const PropHead& head_3,
-	     int t0, int j_decay, int bc_spec,
-	     const char file[],
-	     NmlWriter& nml)
+	     const SftMom& phases,
+	     int t0, int bc_spec)
 {
-  // Create the time-slice set
-  UnorderedSet timeslice;
-  timeslice.make(TimeSliceFunc(j_decay));
+  START_CODE("barcomp");
 
-  // Length of lattice in j_decay direction
-  int length = timeslice.numSubsets();
+  // Length of lattice in decay direction
+  int length  = phases.numSubsets();
+  int j_decay = phases.getDir();
 
-  multi1d<DComplex> hsum(length);
+  multi1d<DComplex> hsum;
   
-  push(nml, "head_1"); writePropHeadinNml(head_1, nml); pop(nml);
-  push(nml, "head_2"); writePropHeadinNml(head_2, nml); pop(nml);
-  push(nml, "head_3"); writePropHeadinNml(head_3, nml);
-
   multi1d<int> ranks(7);
   ranks = Ns;
   ranks[6] = length;
-  multiNd<Complex> barprop(ranks);
-
-  /*  START_CODE("barcomp");*/
+  barprop.resize(ranks);
 
   /* Initialize to zero */
   LatticeComplex b_prop;
@@ -95,7 +67,7 @@ void barcomp(const LatticePropagator& quark_propagator_1,
 				       ranks[3],ranks[0])); // (si_3,sf_3)
 
 	      /* Project on zero momentum: Do a slice-wise sum. */
-	      hsum = sumMulti(b_prop, timeslice);
+	      hsum = sumMulti(b_prop, phases.getSet());
       
 	      for(ranks[6] = 0; ranks[6] < length; ++ranks[6])   // t
 	      {
@@ -107,11 +79,5 @@ void barcomp(const LatticePropagator& quark_propagator_1,
 	      }
 	    }
 
-  /*
-   *  Now write out the file
-   */
-
-  writeBarcomp(file, barprop, head_1, head_2, head_3,j_decay);
-
-  /*  END_CODE("barcomp");*/
+  END_CODE("barcomp");
 }
--- lib/meas/hadron/barcomp_w.h
+++ lib/meas/hadron/barcomp_w.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: barcomp_w.h,v 1.6 2004-01-05 01:00:33 edwards Exp $
+// $Id: barcomp_w.h,v 1.7 2004-02-06 04:22:28 edwards Exp $
 /*! \file
  *  \brief Construct all components of a baryon propagator
  */
@@ -8,6 +8,7 @@
 #define __barcomp_h__
 
 #include "io/qprop_io.h"
+#include "util/ft/sftmom.h"
 
 //! Construct all components of a baryon propagator
 /*!
@@ -18,24 +19,20 @@
  * In all baryons the colour components are contracted with the totally
  * antisymmetric 'tensor' eps(a,b,c) = antisym_tensor(a,b,c).
  *
- * \param quark_propagator_{1,2,3} quark propagators (read)
- * \param header_{1,2,3}           second quark propagator ( Read )
- * \param quark_propagator_3       third quark propagator  ( Read )
  * \param barprop                  baryon correlation function (in real space) ( Write )
- * \param num_mom                  number of non-zero momenta ( Read )
- * \param t0       cartesian coordinates of the source in the j_decay direction ( Read )
- * \param j_decay                  direction of the exponential decay ( Read )
+ * \param quark_propagator_1       quark propagator ( Read )
+ * \param quark_propagator_2       quark propagator ( Read )
+ * \param quark_propagator_3       quark propagator ( Read )
+ * \param phases                   object holds list of momenta ( Read )
+ * \param t0                       coordinates of source in decay direction ( Read )
  * \param bc_spec                  boundary condition for spectroscopy ( Read )
- * \param file                     the filename of the binary output file
  */
-void barcomp(const LatticePropagator& quark_propagator_1, 
-	     const PropHead& header_1,
+
+void barcomp(multiNd<Complex>& barprop,
+	     const LatticePropagator& quark_propagator_1, 
 	     const LatticePropagator& quark_propagator_2,
-	     const PropHead& header_2,
 	     const LatticePropagator& quark_propagator_3,
-	     const PropHead& header_3,
-	     int t0, int j_decay, int bc_spec,
-	     const char file[],
-	     NmlWriter& nml);
+	     const SftMom& phases,
+	     int t0, int bc_spec);
 
 #endif
