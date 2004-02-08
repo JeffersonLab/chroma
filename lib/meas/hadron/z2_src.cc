50b9cbc36403798256b668d14b41a49799f64dd0
mcneile <mcneile>
2004-02-08 11:31:22 +0000

    These routines compute a z2 complex volume source. These sources can be used to compute disconnected diagrams. I have written first attempt at a time slice z2 noise source.

---
+++ lib/meas/hadron/z2_src.cc
@@ -0,0 +1,84 @@
+// $Id: z2_src.cc,v 1.1 2004-02-08 11:31:22 mcneile Exp $
+/*! \file
+ *  \brief Variety of Z2 noise sources
+ */
+
+#include "chromabase.h"
+//#include "meas/hadron/z2_src.h"
+
+using namespace QDP;
+
+//! Volume source of complex Z2 noise
+/*!
+ * \ingroup hadron
+ *
+ * This routine is specific to Wilson fermions! 
+ *
+ * \param a      Source fermion
+ *
+ *
+ *  This type of source is required to compute disconnected
+ *  diagrams. The source is complex Z2 noise, hence there
+ *  is an additional normalization factor of 1/sqrt(2).
+ *
+ *
+ */
+
+void z2_src(LatticeFermion& a)
+{
+
+  a = zero ; 
+  LatticeReal rnd ;
+  LatticeReal ar ,ai ;
+  LatticeColorVector colorvec = zero;
+
+  for(int spin_index= 0 ; spin_index < Ns ; ++spin_index)
+  for(int color_index= 0 ; color_index < Nc ; ++color_index)
+    {
+      random(rnd) ; 
+      ar = where( rnd > 0.5 , LatticeReal(1) , LatticeReal(-1) );
+      random(rnd) ; 
+      ai = where( rnd > 0.5 , LatticeReal(1) , LatticeReal(-1) );
+      LatticeComplex c = cmplx(ar,ai) ;
+
+      colorvec = peekSpin(a,spin_index);
+
+      pokeSpin(a,pokeColor(colorvec,c,color_index),spin_index);
+
+    }
+
+}
+
+
+
+//! Timeslice source of complex Z2 noise
+/*!
+ * \ingroup hadron
+ *
+ * This routine is specific to Wilson fermions! 
+ *
+ * \param a      Source fermion
+ * \param slice        time slice
+ * \param mu           direction of slice
+ *
+ *
+ *  This type of source is useful for computing hadronic
+ *  decay like diagrams. 
+ *
+ *  QUARK MASS DEPENDENCE OF HADRON MASSES FROM LATTICE QCD.
+ * By UKQCD Collaboration (M. Foster et al.)
+ * Published in Phys.Rev.D59:074503,1999
+ * e-Print Archive: hep-lat/9810021 
+ *
+ */
+
+
+void z2_src(LatticeFermion& a, int slice, int mu)
+{
+
+  // compute a volume source of z2 noise
+  LatticeFermion tmp; 
+  z2_src(tmp) ;
+
+  a = where(Layout::latticeCoordinate(mu) == slice, tmp, LatticeFermion(zero));
+}
---
+++ lib/meas/hadron/z2_src.h
@@ -0,0 +1,20 @@
+// -*- C++ -*-
+// $Id: z2_src.h,v 1.1 2004-02-08 11:31:22 mcneile Exp $
+/*! \file
+ *  \brief Volume source of Z2 noise
+ */
+
+
+//
+/ 
+//
+//
+
+#ifndef  Z2_SRC_INC
+#define  Z2_SRC_INC 
+
+void z2_src(LatticeFermion& a) ;
+
+void z2_src(LatticeFermion& a, int slice, int mu) ;
+
+#endif
