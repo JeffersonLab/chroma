2791de44673b18acc9c5e2ed927dc3132e8ad5de
edwards <edwards>
2004-02-13 20:57:11 +0000

    Initial (not-complete) versions of preconditioned Overlap-DWF (Borici).

---
+++ lib/actions/ferm/linop/prec_ovdwf_linop_array_w.cc
@@ -0,0 +1,252 @@
+// $Id: prec_ovdwf_linop_array_w.cc,v 1.1 2004-02-13 20:57:11 edwards Exp $
+/*! \file
+ *  \brief 4D Even Odd preconditioned Overlap-DWF (Borici) linear operator
+ */
+
+#include "chromabase.h"
+#include "actions/ferm/linop/prec_ovdwf_linop_array_w.h"
+
+
+//! Creation routine
+/*! \ingroup fermact
+ *
+ * \param u_            gauge field   (Read)
+ * \param WilsonMass_   DWF height    (Read)
+ * \param m_q_          quark mass    (Read)
+ * \param N5_           extent of 5D  (Read)
+ */
+void 
+EvenOddPrecOvDWLinOpArray::create(const multi1d<LatticeColorMatrix>& u_, 
+				  const Real& WilsonMass_, const Real& m_q_, int N5_)
+{
+  WilsonMass = WilsonMass_;
+  m_q = m_q_;
+  a5  = 1.0;
+  N5  = N5_;
+
+  D.create(u_);
+
+  InvTwoKappa = 5.0 - WilsonMass ; 
+  //InvTwoKappa =  WilsonMass - 5.0;
+  TwoKappa = 1.0 / InvTwoKappa ;
+  Kappa = TwoKappa/2.0 ;
+  
+  invDfactor =1.0/(1.0  + m_q/pow(InvTwoKappa,N5)) ;
+  
+}
+
+
+//! Apply the even-even (odd-odd) coupling piece of the Borici operator
+/*!
+ * \ingroup linop
+ *
+ * The operator acts on the entire lattice
+ *
+ * \param psi 	  Pseudofermion field     	       (Read)
+ * \param isign   Flag ( PLUS | MINUS )   	       (Read)
+ * \param cb      checkerboard ( 0 | 1 )               (Read)
+ */
+void 
+EvenOddPrecOvDWLinOpArray::applyDiag(multi1d<LatticeFermion>& chi, 
+				     const multi1d<LatticeFermion>& psi, 
+				     enum PlusMinus isign,
+				     const int cb) const
+{
+  switch ( isign ) {
+    
+  case PLUS:
+  {
+    for(int s(1);s<N5-1;s++) // 1/2k psi[s] + P_- * psi[s+1] + P_+ * psi[s-1]
+      chi[s][rb[cb]] = InvTwoKappa*psi[s] - 
+	0.5*( psi[s+1] + psi[s-1] + GammaConst<Ns,Ns*Ns-1>()*(psi[s-1] - psi[s+1]) ) ;
+      
+    int N5m1(N5-1) ;
+    //s=0 -- 1/2k psi[0] - P_- * psi[1] + mf* P_+ * psi[N5-1]
+    chi[0][rb[cb]] = InvTwoKappa*psi[0] - 
+      0.5*( psi[1]   - m_q*psi[N5m1] - GammaConst<Ns,Ns*Ns-1>()*(m_q*psi[N5m1] + psi[1]) ) ;
+      
+    int N5m2(N5-2);
+    //s=N5-1 -- 1/2k psi[N5-1] +mf* P_- * psi[0]  -  P_+ * psi[N5-2]
+    chi[N5m1][rb[cb]] = InvTwoKappa*psi[N5m1] - 
+      0.5*( psi[N5m2] - m_q *psi[0] + GammaConst<Ns,Ns*Ns-1>()*(psi[N5m2] + m_q * psi[0]) );
+  }
+  break ;
+
+  case MINUS:
+  {    
+    for(int s(1);s<N5-1;s++) // 1/2k psi[s] - P_+ * psi[s+1] - P_- * psi[s-1]
+      chi[s][rb[cb]] = InvTwoKappa*psi[s] - 
+	0.5*( psi[s+1] + psi[s-1] + GammaConst<Ns,Ns*Ns-1>()*(psi[s+1] - psi[s-1]) ) ;
+      
+    int N5m1(N5-1) ;
+    //s=0 -- 1/2k psi[0] - P_+ * psi[1] + mf* P_- * psi[N5-1]
+    chi[0][rb[cb]] = InvTwoKappa*psi[0] - 
+      0.5*( psi[1]   - m_q*psi[N5m1] + GammaConst<Ns,Ns*Ns-1>()*( psi[1]+m_q*psi[N5m1]) ) ;
+      
+    int N5m2(N5-2);
+    //s=N5-1 -- 1/2k psi[N5-1] + mf* P_+ * psi[0]  -  P_- * psi[N5-2]
+    chi[N5m1][rb[cb]] = InvTwoKappa*psi[N5m1] - 
+      0.5*( psi[N5m2] - m_q *psi[0] - GammaConst<Ns,Ns*Ns-1>()*(psi[N5m2] + m_q * psi[0]) );
+  }
+  break ;
+  }
+}
+
+
+//! Apply the inverse even-even (odd-odd) coupling piece of the Borici fermion operator
+/*!
+ * \ingroup linop
+ *
+ * The operator acts on the entire lattice
+ *
+ * \param psi 	  Pseudofermion field     	       (Read)
+ * \param isign   Flag ( PLUS | MINUS )   	       (Read)
+ * \param cb      checkerboard ( 0 | 1 )               (Read)
+ */
+void 
+EvenOddPrecOvDWLinOpArray::applyDiagInv(multi1d<LatticeFermion>& chi, 
+					const multi1d<LatticeFermion>& psi, 
+					enum PlusMinus isign,
+					const int cb) const
+{
+  switch ( isign ) {
+
+  case PLUS:
+  {
+    // Copy and scale by TwoKappa (1/M0)
+    for(int s(0);s<N5;s++)
+      chi[s][rb[cb]] = TwoKappa * psi[s] ;
+      
+    // First apply the inverse of Lm 
+    Real fact(0.5*m_q*TwoKappa) ;
+    for(int s(0);s<N5-1;s++){
+      chi[N5-1][rb[cb]] -= fact * (chi[s] - GammaConst<Ns,Ns*Ns-1>()*chi[s])  ;
+      fact *= TwoKappa ;
+    }
+      
+    //Now apply the inverse of L. Forward elimination 
+    for(int s(1);s<N5;s++)
+      chi[s][rb[cb]] += Kappa*(chi[s-1] + GammaConst<Ns,Ns*Ns-1>()*chi[s-1]) ;
+      
+    //The inverse of D  now
+    chi[N5-1][rb[cb]] *= invDfactor ;
+    // That was easy....
+      
+    //The inverse of R. Back substitution...... Getting there! 
+    for(int s(N5-2);s>-1;s--)
+      chi[s][rb[cb]] += Kappa*(chi[s+1] - GammaConst<Ns,Ns*Ns-1>()*chi[s+1]) ;
+      
+    //Finally the inverse of Rm 
+    LatticeFermion tt;
+    fact = 0.5*m_q*TwoKappa;
+    tt[rb[cb]] = fact*(chi[N5-1] + GammaConst<Ns,Ns*Ns-1>()*chi[N5-1]);
+    for(int s(0);s<N5-1;s++){
+      chi[s][rb[cb]] -= tt  ;
+      tt[rb[cb]] *= TwoKappa ;
+    }
+  }
+  break ;
+    
+  case MINUS:
+  {
+    // Copy and scale by TwoKappa (1/M0)
+    for(int s(0);s<N5;s++)
+      chi[s][rb[cb]] = TwoKappa * psi[s] ;
+      
+    // First apply the inverse of Lm 
+    Real fact(0.5*m_q*TwoKappa) ;
+    for(int s(0);s<N5-1;s++){
+      chi[N5-1][rb[cb]] -= fact * (chi[s] + GammaConst<Ns,Ns*Ns-1>()*chi[s])  ;
+      fact *= TwoKappa ;
+    }
+      
+    //Now apply the inverse of L. Forward elimination 
+    for(int s(1);s<N5;s++)
+      chi[s][rb[cb]] += Kappa*(chi[s-1] - GammaConst<Ns,Ns*Ns-1>()*chi[s-1]) ;
+      
+    //The inverse of D  now
+    chi[N5-1][rb[cb]] *= invDfactor ;
+    // That was easy....
+      
+    //The inverse of R. Back substitution...... Getting there! 
+    for(int s(N5-2);s>-1;s--)
+      chi[s][rb[cb]] += Kappa*(chi[s+1] + GammaConst<Ns,Ns*Ns-1>()*chi[s+1]) ;
+      
+    //Finally the inverse of Rm 
+    LatticeFermion tt;
+    tt[rb[cb]] = (0.5*m_q*TwoKappa)*(chi[N5-1] - GammaConst<Ns,Ns*Ns-1>()*chi[N5-1]);
+    for(int s(0);s<N5-1;s++){
+      chi[s][rb[cb]] -= tt  ;
+      tt[rb[cb]] *= TwoKappa ;
+    }
+  }
+  break ;
+  }
+}
+
+
+//! Apply the even-odd (odd-even) coupling piece of the Borici operator
+/*!
+ * \ingroup linop
+ *
+ * The operator acts on the entire lattice
+ *
+ * \param psi 	  Pseudofermion field     	       (Read)
+ * \param isign   Flag ( PLUS | MINUS )   	       (Read)
+ * \param cb      checkerboard ( 0 | 1 )               (Read)
+ */
+void 
+EvenOddPrecOvDWLinOpArray::applyOddDiag(multi1d<LatticeFermion>& chi, 
+					const multi1d<LatticeFermion>& psi, 
+					enum PlusMinus isign,
+					const int cb) const
+{
+  switch ( isign ) 
+  {
+  case PLUS:
+  {
+    LatticeFermion tmp;
+
+    for(int s(1);s<N5-1;s++)
+    {
+      tmp = psi[s] + psi[s+1] + psi[s-1] + GammaConst<Ns,Ns*Ns-1>()*(psi[s-1] - psi[s+1]);
+      D.apply(chi[s],tmp,isign,cb);
+      chi[s][rb[cb]] *= (-0.5);
+    }
+      
+    int N5m1(N5-1);
+    chi[0][rb[cb]] = psi[0]  + psi[1] - m_q*psi[N5m1] 
+      - GammaConst<Ns,Ns*Ns-1>()*(m_q*psi[N5m1] + psi[1]);
+    D.apply(chi[0],tmp,isign,cb);
+    chi[0][rb[cb]] *= (-0.5);
+      
+    int N5m2(N5-2);
+    chi[N5m1][rb[cb]] = psi[N5m1] + psi[N5m2] - m_q *psi[0] 
+      + GammaConst<Ns,Ns*Ns-1>()*(psi[N5m2] + m_q * psi[0]);
+  }
+  break ;
+
+  case MINUS:
+  {    
+    for(int s(1);s<N5-1;s++) // 1/2k psi[s] - P_+ * psi[s+1] - P_- * psi[s-1]
+      chi[s][rb[cb]] = InvTwoKappa*psi[s] - 
+	0.5*( psi[s+1] + psi[s-1] + GammaConst<Ns,Ns*Ns-1>()*(psi[s+1] - psi[s-1]) ) ;
+      
+    int N5m1(N5-1) ;
+    //s=0 -- 1/2k psi[0] - P_+ * psi[1] + mf* P_- * psi[N5-1]
+    chi[0][rb[cb]] = InvTwoKappa*psi[0] - 
+      0.5*( psi[1]   - m_q*psi[N5m1] + GammaConst<Ns,Ns*Ns-1>()*( psi[1]+m_q*psi[N5m1]) ) ;
+      
+    int N5m2(N5-2);
+    //s=N5-1 -- 1/2k psi[N5-1] + mf* P_+ * psi[0]  -  P_- * psi[N5-2]
+    chi[N5m1][rb[cb]] = InvTwoKappa*psi[N5m1] - 
+      0.5*( psi[N5m2] - m_q *psi[0] - GammaConst<Ns,Ns*Ns-1>()*(psi[N5m2] + m_q * psi[0]) );
+  }
+  break ;
+  }
+
+  //Done! That was not that bad after all....
+  //See, I told you so...
+
+
+}
---
+++ lib/actions/ferm/linop/prec_ovdwf_linop_array_w.h
@@ -0,0 +1,148 @@
+// -*- C++ -*-
+// $Id: prec_ovdwf_linop_array_w.h,v 1.1 2004-02-13 20:57:11 edwards Exp $
+/*! \file
+ *  \brief 4D Even Odd preconditioned Overlap-DWF (Borici) linear operator
+ */
+
+#ifndef __prec_ovdwf_linop_array_w_h__
+#define __prec_ovdwf_linop_array_w_h__
+
+#include "linearop.h"
+#include "actions/ferm/linop/dslash_w.h"
+
+using namespace QDP;
+
+//! 4D Even Odd preconditioned Overlap-DWF (Borici) linear operator
+/*!
+ * \ingroup linop
+ *
+ * This routine is specific to Wilson fermions!
+ */
+
+class EvenOddPrecOvDWLinOpArray : public EvenOddPrecLinearOperator< multi1d<LatticeFermion> >
+{
+public:
+  //! Partial constructor
+  EvenOddPrecOvDWLinOpArray() {}
+
+  //! Full constructor
+  EvenOddPrecOvDWLinOpArray(const multi1d<LatticeColorMatrix>& u_, 
+			    const Real& WilsonMass_, const Real& m_q, int N5_)
+    {create(u_,WilsonMass_,m_q,N5_);}
+
+  //! Creation routine
+  void create(const multi1d<LatticeColorMatrix>& u_, 
+	      const Real& WilsonMass_, const Real& m_q_, int N5_);
+
+  //! Destructor is automatic
+  ~EvenOddPrecOvDWLinOpArray() {}
+
+  //! Length of DW flavor index/space
+  int size() const {return N5;}
+
+  //! Apply the even-even block onto a source vector
+  inline
+  void evenEvenLinOp(multi1d<LatticeFermion>& chi, 
+		     const multi1d<LatticeFermion>& psi, 
+		     enum PlusMinus isign) const
+    {
+      applyDiag(chi, psi, isign, 0);
+    }
+  
+  //! Apply the inverse of the even-even block onto a source vector
+  inline
+  void evenEvenInvLinOp(multi1d<LatticeFermion>& chi, 
+			const multi1d<LatticeFermion>& psi, 
+			enum PlusMinus isign) const
+    {
+      applyDiagInv(chi, psi, isign, 0);
+    }
+  
+  //! Apply the the even-odd block onto a source vector
+  void evenOddLinOp(multi1d<LatticeFermion>& chi, 
+		    const multi1d<LatticeFermion>& psi, 
+		    enum PlusMinus isign) const
+    {
+      applyOffDiag(chi, psi, isign, 0);
+    }
+
+  //! Apply the the odd-even block onto a source vector
+  void oddEvenLinOp(multi1d<LatticeFermion>& chi, 
+		    const multi1d<LatticeFermion>& psi, 
+		    enum PlusMinus isign) const
+    {
+      applyOffDiag(chi, psi, isign, 1);
+    }
+
+  //! Apply the the odd-odd block onto a source vector
+  inline
+  void oddOddLinOp(multi1d<LatticeFermion>& chi, 
+		   const multi1d<LatticeFermion>& psi, 
+		   enum PlusMinus isign) const
+    {
+      applyDiag(chi, psi, isign, 1);
+    }
+
+  //! Apply the inverse of the odd-odd block onto a source vector
+  inline
+  void oddOddInvLinOp(multi1d<LatticeFermion>& chi, 
+			const multi1d<LatticeFermion>& psi, 
+			enum PlusMinus isign) const
+    {
+      applyDiagInv(chi, psi, isign, 1);
+    }
+
+protected:
+
+  //! Apply the even-even (odd-odd) coupling piece of the Borici fermion operator
+  /*!
+   * \param chi     result     	                   (Modify)
+   * \param psi     source     	                   (Read)
+   * \param isign   Flag ( PLUS | MINUS )          (Read)
+   * \param cb      checkerboard ( 0 | 1 )         (Read)
+   */
+  void applyDiag(multi1d<LatticeFermion>& chi, 
+	     const multi1d<LatticeFermion>& psi, 
+	     enum PlusMinus isign,
+	     const int cb) const;
+
+  //! Apply the inverse even-even (odd-odd) coupling piece of the Borici fermion operator
+  /*!
+   * \param chi     result     	                   (Modify)
+   * \param psi     source     	                   (Read)
+   * \param isign   Flag ( PLUS | MINUS )   	   (Read)
+   * \param cb      checkerboard ( 0 | 1 )         (Read)
+   */
+  void applyDiagInv(multi1d<LatticeFermion>& chi, 
+		    const multi1d<LatticeFermion>& psi, 
+		    enum PlusMinus isign,
+		    const int cb) const;
+    
+
+  //! Apply the even-odd (odd-even) coupling piece of the Borici operator
+  /*!
+   * \param chi     result     	                   (Modify)
+   * \param psi     source     	                   (Read)
+   * \param isign   Flag ( PLUS | MINUS )          (Read)
+   * \param cb      checkerboard ( 0 | 1 )         (Read)
+   */
+  void applyDiag(multi1d<LatticeFermion>& chi, 
+		 const multi1d<LatticeFermion>& psi, 
+		 enum PlusMinus isign,
+		 const int cb) const;
+
+private:
+  Real WilsonMass;
+  Real m_q;
+  Real a5;
+  int  N5;
+
+  Real InvTwoKappa ;
+  Real TwoKappa ;
+  Real Kappa;
+  Real invDfactor ;
+
+  WilsonDslash  D;
+};
+
+#endif
