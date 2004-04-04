ae66778a8444ca7e2d477b73aeabab254c46bd71
edwards <edwards>
2004-04-04 03:54:40 +0000

    Updated to new chroma prop model.

--- mainprogs/main/wallformfac.cc
+++ mainprogs/main/wallformfac.cc
@@ -1,4 +1,4 @@
-// $Id: wallformfac.cc,v 1.7 2004-02-11 12:51:35 bjoo Exp $
+// $Id: wallformfac.cc,v 1.8 2004-04-04 03:54:40 edwards Exp $
 /*! \file
  * \brief Main program for computing 3pt functions with a wall sink
  *
@@ -24,34 +24,56 @@ struct Prop_t
 // and written to the XML output
 struct Param_t
 {
-  FermType FermTypeP;
-
-//  multi1d<Real> Mass;      // array of mass values
-
-  CfgType cfg_type;        // storage order for stored gauge configuration
-  int j_decay;             // direction to measure propagation
-
-  SourceType source_type;  // source type (POINT_SOURCE, SHELL_SOURCE)
-
-  int t_sink;
-
   int mom2_max;            // (mom)^2 <= mom2_max. mom2_max=7 in szin.
 
-  SmearingParam_t  smearParam;
-
   multi1d<int> formfac_type;
-
   multi1d<int> nrow;
-  multi1d<int> t_srce;
 };
 
 struct WallFormFac_input_t
 {
-  IO_version_t     io_version;
   Param_t          param;
   Cfg_t            cfg;
   Prop_t           prop;
 };
+
+
+//! Propagator filenames
+void read(XMLReader& xml, const string& path, Prop_t& input)
+{
+  XMLReader inputtop(xml, path);
+
+  read(inputtop, "forwprop_file",input.forwprop_file);
+  read(inputtop, "backprop_file",input.backprop_file);
+}
+
+
+//! Parameter input
+void read(XMLReader& xml, const string& path, Param_t& input)
+{
+  XMLReader paramtop(xml, path);
+
+  int version;
+  read(paramtop, "version", version);
+
+  switch (version) 
+  {
+    /**************************************************************************/
+  case 1:
+    break;
+
+  default:
+    /**************************************************************************/
+    QDPIO::cerr << "Input parameter version " << version 
+		<< " unsupported." << endl;
+    QDP_abort(1);
+  }
+
+  read(paramtop, "mom2_max", param.mom2_max);
+  read(paramtop, "formfac_type", param.formfac_type);
+  read(paramtop, "nrow", param.nrow);
+}
+
 
 
 // Reader for input parameters
@@ -59,123 +81,24 @@ void read(XMLReader& xml, const string& path, WallFormFac_input_t& input)
 {
   XMLReader inputtop(xml, path);
 
-
-  // First, read the input parameter version.  Then, if this version
-  // includes 'Nc' and 'Nd', verify they agree with values compiled
-  // into QDP++
-
-  // Read in the IO_version
+  // Read the input
   try
   {
-    read(inputtop, "IO_version/version", input.io_version.version);
+    // Parameters for source construction
+    read(inputtop, "Param", input.param);
+
+    // Read in the gauge configuration info
+    read(inputtop, "Cfg", input.cfg);
+
+    // Read in the output propagator/source configuration info
+    read(inputtop, "Prop", input.prop);
   }
-  catch (const string& e) 
+  catch(const string& e)
   {
-    QDPIO::cerr << "Error reading data: " << e << endl;
-    throw;
-  }
-
-
-  // Currently, in the supported IO versions, there is only a small difference
-  // in the inputs. So, to make code simpler, extract the common bits 
-
-  // Read the uncommon bits first
-  try
-  {
-    XMLReader paramtop(inputtop, "param"); // push into 'param' group
-
-    switch (input.io_version.version) 
-    {
-      /**************************************************************************/
-    case 1:
-      /**************************************************************************/
-      break;
-
-    default:
-      /**************************************************************************/
-      QDPIO::cerr << "Input parameter version " << input.io_version.version 
-		  << " unsupported." << endl;
-      QDP_abort(1);
-    }
-  }
-  catch (const string& e) 
-  {
-    QDPIO::cerr << "Error reading data: " << e << endl;
-    throw;
-  }
-
-  QDPIO::cout << " FORMFAC: Baryon form factors for Wilson fermions" << endl;
-
-  // Read the common bits
-  try 
-  {
-    XMLReader paramtop(inputtop, "param"); // push into 'param' group
-
-    read(paramtop, "FermTypeP", input.param.FermTypeP);
-//    read(paramtop, "Mass", input.param.Mass);
-
-#if 0
-    for (int i=0; i < input.param.Mass.size(); ++i) {
-      if (toBool(input.param.Mass[i] < 0.0)) {
-	QDPIO::cerr << "Unreasonable value for Mass." << endl;
-	QDPIO::cerr << "  Mass[" << i << "] = " << input.param.Mass[i] << endl;
-	QDP_abort(1);
-      } else {
-	QDPIO::cout << " Spectroscopy Mass: " << input.param.Mass[i] << endl;
-      }
-    }
-#endif
-
-    read(paramtop, "cfg_type", input.param.cfg_type);
-    read(paramtop, "j_decay", input.param.j_decay);
-    if (input.param.j_decay < 0 || input.param.j_decay >= Nd) {
-      QDPIO::cerr << "Bad value: j_decay = " << input.param.j_decay << endl;
-      QDP_abort(1);
-    }
-
-    read(paramtop, "source_type", input.param.source_type);
-
-    read(paramtop, "mom2_max", input.param.mom2_max);
-
-    if (input.param.source_type == SRC_TYPE_SHELL_SOURCE)
-      read(paramtop, "SmearingParam", input.param.smearParam);
-
-    read(paramtop, "nrow", input.param.nrow);
-    read(paramtop, "t_srce", input.param.t_srce);
-
-    // Now we read in the information associated with the sequential sources
-    read(paramtop, "t_sink", input.param.t_sink);
-    read(paramtop, "formfac_type", input.param.formfac_type);
-
-#if 1
-    for (int seq_src_ctr=0; seq_src_ctr<input.param.formfac_type.size(); ++seq_src_ctr) 
-    {
-      QDPIO::cout << "Computing formfactors for "
-		  << input.param.formfac_type[seq_src_ctr] << endl;
-    }
-#endif
-  }
-  catch (const string& e) 
-  {
-    QDPIO::cerr << "Error reading data: " << e << endl;
-    throw;
-  }
-
-
-  // Read in the gauge configuration file name
-  // Also read in props
-  try
-  {
-    read(inputtop, "Cfg/cfg_file",input.cfg.cfg_file);
-    read(inputtop, "Prop/forwprop_file",input.prop.forwprop_file);
-    read(inputtop, "Prop/backprop_file",input.prop.backprop_file);
-  }
-  catch (const string& e) 
-  {
-    QDPIO::cerr << "Error reading data: " << e << endl;
-    throw;
+    QDP_error_exit("Error reading in wallformfac: %s", e.c_str());
   }
 }
+
 
 
 
@@ -200,21 +123,8 @@ main(int argc, char *argv[])
   Layout::setLattSize(input.param.nrow);
   Layout::create();
 
-  // Sanity checks
-  for (int i=0; i<Nd; ++i) {
-    if (input.param.t_srce[i] < 0 || input.param.t_srce[i] >= input.param.nrow[i]) {
-      QDPIO::cerr << "Quark propagator source coordinate incorrect." << endl;
-      QDPIO::cerr << "t_srce[" << i << "] = " << input.param.t_srce[i] << endl;
-      QDP_abort(1);
-    }
-  }
-
-  if (input.param.t_sink < 0 || input.param.t_sink >= input.param.nrow[input.param.j_decay]) {
-    QDPIO::cerr << "Sink time coordinate incorrect." << endl;
-    QDPIO::cerr << "t_sink = " << input.param.t_sink << endl;
-    QDP_abort(1);
-  }
-
+  QDPIO::cout << " WALLFORMFAC: Form factors for Wilson-like fermions" << endl;
+  QDPIO::cout << endl << "     Gauge group: SU(" << Nc << ")" << endl;
   QDPIO::cout << "     volume: " << input.param.nrow[0];
   for (int i=1; i<Nd; ++i) {
     QDPIO::cout << " x " << input.param.nrow[i];
@@ -255,10 +165,6 @@ main(int argc, char *argv[])
   // Write out the config info
   write(xml_out, "Config_info", gauge_xml);
 
-  push(xml_out, "IO_version");
-  write(xml_out, "version", input.io_version.version);
-  pop(xml_out);
-
   push(xml_out, "Output_version");
   write(xml_out, "out_version", 1);
   pop(xml_out);
@@ -278,39 +184,97 @@ main(int argc, char *argv[])
   xml_out.flush();
 
 
-  // Read the quark propagator
-  QDPIO::cout << "Attempt to read forward propagator" << endl;
-  
+  //
+  // Read the quark propagator and extract headers
+  //
+  XMLReader forwprop_file_xml, forwprop_record_xml;
   LatticePropagator forward_quark_prop;
+  ChromaProp_t forward_prop_header;
+  PropSource_t forward_source_header;
   {
-    XMLReader forwprop_xml;
-//    stringstream prop_file;
-//    prop_file << "propagator_0";
-//    readSzinQprop(forwprop_xml, forward_quark_prop, prop_file.str());
-    readSzinQprop(forwprop_xml, forward_quark_prop, input.prop.forwprop_file);
-    
-    write(xml_out, "Forward_prop_info", forwprop_xml);
+    QDPIO::cout << "Attempt to read forward propagator" << endl;
+    readQprop(forwprop_file_xml, 
+	      forwprop_record_xml, forward_quark_prop,
+	      input.prop.forwprop_file, QDPIO_SERIAL);
+   
+    // Try to invert this record XML into a ChromaProp struct
+    // Also pull out the id of this source
+    try
+    {
+      read(forwprop_record_xml, "/Propagator/ForwardProp", forward_prop_header);
+      read(forwprop_record_xml, "/Propagator/PropSource", forward_source_header);
+    }
+    catch (const string& e) 
+    {
+      QDPIO::cerr << "Error extracting forward_prop header: " << e << endl;
+      throw;
+    }
   }
   QDPIO::cout << "Forward propagator successfully read" << endl;
-   
+
+  // Derived from input prop
+  int  j_decay = forward_source_header.j_decay;
+  multi1d<int> t_source = forward_source_header.t_source;
+
+  // Sanity check - write out the norm2 of the forward prop in the j_decay direction
+  // Use this for any possible verification
+  {
+    // Initialize the slow Fourier transform phases
+    SftMom phases(0, true, j_decay);
+
+    multi1d<Double> forward_prop_corr = sumMulti(localNorm2(forward_quark_prop), 
+						 phases.getSet());
+
+    push(xml_out, "Forward_prop_correlator");
+    write(xml_out, "forward_prop_corr", forward_prop_corr);
+    pop(xml_out);
+  }
+
 
   // Read the backward propagator
-  QDPIO::cout << "Attempt to read backward propagator" << endl;
-
+  XMLReader backprop_file_xml, backprop_record_xml;
   LatticePropagator backward_quark_prop;
+  ChromaProp_t backward_prop_header;
+  PropSource_t backward_source_header;
   {
-    XMLReader backprop_xml;
-//    stringstream prop_file;
-//    prop_file << "backprop_0";
-//    readSzinQprop(backprop_xml, backward_quark_prop, prop_file.str());
-    readSzinQprop(backprop_xml, backward_quark_prop, input.prop.backprop_file);
-    
-    write(xml_out, "Backward_prop_info", backprop_xml);
+    QDPIO::cout << "Attempt to read backward propagator" << endl;
+    readQprop(backprop_file_xml, 
+	      backprop_record_xml, backward_quark_prop,
+	      input.prop.backprop_file, QDPIO_SERIAL);
+   
+    // Try to invert this record XML into a ChromaProp struct
+    // Also pull out the id of this source
+    try
+    {
+      read(backprop_record_xml, "/Propagator/ForwardProp", backward_prop_header);
+      read(backprop_record_xml, "/Propagator/PropSource", backward_source_header);
+    }
+    catch (const string& e) 
+    {
+      QDPIO::cerr << "Error extracting backward_prop header: " << e << endl;
+      throw;
+    }
   }
   QDPIO::cout << "Backward propagator successfully read" << endl;
    
+  // Sanity check - write out the norm2 of the backward prop in the j_decay direction
+  // Use this for any possible verification
+  {
+    // Initialize the slow Fourier transform phases
+    SftMom phases(0, true, j_decay);
+
+    multi1d<Double> backward_prop_corr = sumMulti(localNorm2(backward_quark_prop), 
+						 phases.getSet());
+
+    push(xml_out, "Backward_prop_correlator");
+    write(xml_out, "backward_prop_corr", backward_prop_corr);
+    pop(xml_out);
+  }
+
   
+  //
   // Now the 3pt contractions
+  //
   SftMom phases(input.param.mom2_max, false, input.param.j_decay);
 
 #if 1
