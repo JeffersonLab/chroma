3bdbc5862ccb4ebdaf29040f1902e58aff715436
bjoo <bjoo>
2004-04-07 11:06:17 +0000

    Now works in single and double, and tested on a large IBM machien

--- lib/io/readszinferm_w.cc
+++ lib/io/readszinferm_w.cc
@@ -1,4 +1,4 @@
-// $Id: readszinferm_w.cc,v 1.5 2003-12-30 17:27:15 bjoo Exp $: readszinqprop_w.cc,v 1.6 2003/04/30 21:19:33 edwards Exp $
+// $Id: readszinferm_w.cc,v 1.6 2004-04-07 11:06:17 bjoo Exp $: readszinqprop_w.cc,v 1.6 2003/04/30 21:19:33 edwards Exp $
 /*!
  * @file
  * @brief  Read an old SZIN-style (checkerboarded) lattice Dirac fermion
@@ -23,15 +23,46 @@ void readSzinFerm(LatticeFermion& q, const string& file)
 {
   BinaryReader cfg_in(file);
 
-  multi1d<Real64>  buffer(Ns*Nc*2);  // Buffer for a fermion
+#if 1
+  multi1d<DFermion> sitebuf_prec(Layout::vol());  // Buffer for a fermion
+  multi1d<Fermion>  sitebuf(Layout::vol());
 
-  //
-  // Read propagator field
-  //
+  read(cfg_in, sitebuf_prec, Layout::vol());
+
+  /* Downcast here unfortunately Fermion s(DFermion x) doesn't work
+     only DFermion s(Fermion x) */
+  /* However, since I am not doing any site peeking, perhaps this will
+     be OK */
+
+  for(int site=0; site < Layout::vol(); site++) { 
+	DFermion site_tmp_d = sitebuf_prec[site];
+	Fermion site_tmp_r;
+
+	for(int spin=0; spin < Ns; spin++) { 
+	  DColorVector d_col_vec;
+	  ColorVector col_vec;
+	  
+	  d_col_vec = peekSpin(site_tmp_d, spin);
+          for(int color=0; color < Nc; color++) {
+	     DComplex elem = peekColor(d_col_vec, color); 
+	     Double elem_re = real(elem);
+	     Double elem_im = imag(elem);
+
+	     Complex elem_r = cmplx(Real(elem_re), Real(elem_im));
+	     pokeColor(col_vec, elem_r, color);
+          }
+          pokeSpin(site_tmp_r, col_vec, spin);
+        }	  
+        sitebuf[site] = site_tmp_r;
+  }
+  // sitebuf_prec no longer needed. Free up memory
+  sitebuf_prec.resize(0); 
+
+  /* Now get out of lexicographic order */   
   multi1d<int> lattsize_cb = Layout::lattSize();
   lattsize_cb[0] /= 2;  // checkerboard in the x-direction in szin
 
-  // Read prop
+  int index = 0; 
   for(int cb=0; cb < 2; ++cb)
   {
     for(int sitecb=0; sitecb < Layout::vol()/2; ++sitecb)
@@ -40,46 +71,18 @@ void readSzinFerm(LatticeFermion& q, const string& file)
 
       // construct the checkerboard offset
       int sum = 0;
-      for(int m=1; m<Nd; m++)
+      for(int m=1; m<Nd; m++) {
 	sum += coord[m];
-
+      }
       // The true lattice x-coord
       coord[0] = 2*coord[0] + ((sum + cb) & 1);
 
+      pokeSite(q, sitebuf[index++], coord);
 
-      read(cfg_in, buffer, Nc*Ns*2);
-      Fermion q_tmp;
-      int bufindex = 0;
-      
-
-      for(int spin=0; spin < Ns; ++spin) {
-	ColorVector tmp_vec;
-	
-	for(int col = 0; col < Nc; ++col) {
-	  Complex tmp_cmpx;
-	  Real64 re, im;
-
-	  re = buffer[bufindex];
-	  bufindex++;
-	  im = buffer[bufindex];
-	  bufindex++;
-	  tmp_cmpx = cmplx((Real)re,(Real)im);
-
-	  pokeColor(tmp_vec, 
-		    tmp_cmpx,
-		    col);
-	}
-	pokeSpin(q_tmp, 
-		 tmp_vec, 
-		 spin);
-      }
-
-      pokeSite(q, q_tmp, coord);
-
-      //cout << q_tmp << endl;
-      //read(cfg_in, q, coord); 	// Read in a site propagator
     }
   }
+
+#endif
 
   cfg_in.close();
 }
