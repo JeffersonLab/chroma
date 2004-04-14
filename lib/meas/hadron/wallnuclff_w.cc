8dd67a3195240cc25383cd5527d5dbdbe576f5e0
edwards <edwards>
2004-04-14 20:59:55 +0000

    Some more mods.

--- lib/meas/hadron/wallnuclff_w.cc
+++ lib/meas/hadron/wallnuclff_w.cc
@@ -1,4 +1,4 @@
-// $Id: wallnuclff_w.cc,v 1.5 2004-04-14 04:42:06 edwards Exp $
+// $Id: wallnuclff_w.cc,v 1.6 2004-04-14 20:59:55 edwards Exp $
 /*! \file
  *  \brief Wall-sink nucleon form-factors 
  *
@@ -10,17 +10,6 @@
 #include "meas/hadron/wallnuclff_w.h"
 
 using namespace QDP;
-
-/*
- * Structures for hadron parts
- *
- * \ingroup hadron
- *
- * @{
- */
-
-
-/*! @} */  // end of group hadron
 
 
 //! Compute contractions for current insertion 3-point functions.
@@ -53,10 +42,13 @@ void wallNuclFormFac(XMLWriter& xml,
 
   // Start new array group
   XMLArrayWriter xml_array(xml, Nd);
-  push(xml_array, "WallNuclFormFac");
+  push(xml_array, "FormFac");
 
   // Length of lattice in j_decay direction and 3pt correlations fcns
   int length = phases.numSubsets();
+
+  multi1d<Complex> local_cur3ptfn(length);
+  multi1d<Complex> nonlocal_cur3ptfn(length);
 
   int G5 = Ns*Ns-1;
   
@@ -72,46 +64,12 @@ void wallNuclFormFac(XMLWriter& xml,
 //  form.formFac.resize(Nd*Nd);
 
   // Loop over gamma matrices of the insertion current of insertion current
-  for(int gamma_value = 0; gamma_value < Nd*Nd; ++gamma_value)
+  for(int mu = 0; mu < Nd; ++mu)
   {
     push(xml_array);
-    write(xml_array, "gamma_value", gamma_value);
+    write(xml_array, "mu", mu);
 
-    //  For the case where the gamma value indicates we are evaluating either
-    //  the vector or axial vector currents, we will also evaluate
-    //  the non-local currents.  The non-local vector current is the conserved
-    //  current.  The non-local axial vector current would be partially
-    //  conserved but for the Wilson term.  In these cases we will set
-    //  mu = corresponding direction.  In all other cases, we will set mu = -1.
-
-    bool compute_nonlocal;
-    int mu;
-
-    switch(gamma_value){
-    case  1:
-    case 14:
-      mu = 0;
-      compute_nonlocal = true;
-      break;
-    case  2:
-    case 13:
-      mu = 1;
-      compute_nonlocal = true;
-      break;
-    case  4:
-    case 11:
-      mu = 2;
-      compute_nonlocal = true;
-      break;
-    case  8:
-    case  7:
-      mu = 3;
-      compute_nonlocal = true;
-      break;
-    default:
-      mu = -1;
-      compute_nonlocal = false;
-    }
+    int gamma_value = 1 << mu;
 
     /* "\bar u O u" insertion in proton, ie. "(u C gamma_5 d) u" */
     /* T = (1 + gamma_4) / 2 = (1 + Gamma(8)) / 2 */
@@ -119,10 +77,12 @@ void wallNuclFormFac(XMLWriter& xml,
 
     q1_tmp = 1;
 
-    // The local non-conserved vector-current matrix element 
+    /*
+     * The local non-conserved vector-current matrix element 
+     */
     LatticeComplex corr_local_fn;
-    LatticePropagator anti_u_prop = Gamma(15)*back_u_prop*Gamma(15);
-    LatticePropagator anti_d_prop = Gamma(15)*back_d_prop*Gamma(15);
+    LatticePropagator anti_u_prop = Gamma(G5)*back_u_prop*Gamma(G5);
+    LatticePropagator anti_d_prop = Gamma(G5)*back_d_prop*Gamma(G5);
 
     // Term 1
     corr_local_fn = -e_u*trace(anti_u_prop*Gamma(gamma_value)*forw_u_prop*Gamma(5)*
@@ -151,29 +111,30 @@ void wallNuclFormFac(XMLWriter& xml,
 					       Gamma(5)*anti_d_prop*Gamma(gamma_value)*forw_d_prop));
     corr_local_fn *= 0.5;
 
+    multi2d<DComplex> hsum_local = phases.sft(corr_local_fn);
 
-    multi2d<DComplex> hsum, hsum_nonlocal;
-    hsum = phases.sft(corr_local_fn);
 
-#if 0
-    // Construct the non-local current matrix element 
-    //
-    // The form of J_mu = (1/2)*[psibar(x+mu)*U^dag_mu*(1+gamma_mu)*psi(x) -
-    //                           psibar(x)*U_mu*(1-gamma_mu)*psi(x+mu)]
-    // NOTE: the 1/2  is included down below in the sumMulti stuff
+    /*
+     * Construct the non-local current matrix element 
+     *
+     * The form of J_mu = (1/2)*[psibar(x+mu)*U^dag_mu*(1+gamma_mu)*psi(x) -
+     *                           psibar(x)*U_mu*(1-gamma_mu)*psi(x+mu)]
+     * NOTE: the 1/2  is included down below in the sumMulti stuff
+     */
     LatticeComplex corr_nonlocal_fn;
-    if(compute_nonlocal){
-      corr_nonlocal_fn =
-        trace(adj(u[mu] * shift(anti_quark_prop, FORWARD, mu)) *
-          (quark_propagator + Gamma(gamma_value) * quark_propagator));
-      LatticePropagator tmp_prop1 = u[mu] *
-        shift(quark_propagator, FORWARD, mu);
-      corr_nonlocal_fn -= trace(adj(anti_quark_prop) *
-                            (tmp_prop1 - Gamma(gamma_value) * tmp_prop1));
-
-      hsum_nonlocal = phases.sft(corr_nonlocal_fn);
-    }
+#if 0
+    corr_nonlocal_fn =
+      trace(adj(u[mu] * shift(anti_quark_prop, FORWARD, mu)) *
+	    (quark_propagator + Gamma(gamma_value) * quark_propagator));
+    LatticePropagator tmp_prop1 = u[mu] *
+      shift(quark_propagator, FORWARD, mu);
+    corr_nonlocal_fn -= trace(adj(anti_quark_prop) *
+			      (tmp_prop1 - Gamma(gamma_value) * tmp_prop1));
+#else
+    corr_nonlocal_fn = zero;
 #endif
+    
+    multi2d<DComplex> hsum_nonlocal = phases.sft(corr_nonlocal_fn);
   
 //    form.formFac[gamma_value].gamma_value = gamma_value;
 //    form.formFac[gamma_value].momenta.resize(phases.numMom());  // hold momenta output
@@ -190,24 +151,17 @@ void wallNuclFormFac(XMLWriter& xml,
 
 //      form.formFac[gamma_value].momenta[inser_mom_num].inser_mom = phases.numToMom(inser_mom_num);
 
-      multi1d<Complex> local_cur3ptfn(length); // always compute
-//      multi1d<Complex> nonlocal_cur3ptfn;
-//      if (compute_nonlocal)
-//	nonlocal_cur3ptfn.resize(length);      // possibly compute
-
       for (int t=0; t < length; ++t) 
       {
         int t_eff = (t - t0 + length) % length;
 
-        local_cur3ptfn[t_eff] = Complex(hsum[inser_mom_num][t]);
-//        if (compute_nonlocal)
-//          nonlocal_cur3ptfn[t_eff] = 0.5 * Complex(hsum_nonlocal[inser_mom_num][t]);
-
+        local_cur3ptfn[t_eff] = Complex(hsum_local[inser_mom_num][t]);
+        nonlocal_cur3ptfn[t_eff] = 0.5 * Complex(hsum_nonlocal[inser_mom_num][t]);
       } // end for(t)
 
       // Print out the results
       write(xml_inser_mom, "local_cur3ptfn", local_cur3ptfn);
-//o      write(xml_inser_mom, "nonlocal_cur3ptfn", nonlocal_cur3ptfn);
+      write(xml_inser_mom, "nonlocal_cur3ptfn", nonlocal_cur3ptfn);
 
 //      form.formFac[gamma_value].momenta[inser_mom_num].local_current    = local_cur3ptfn;
 //      form.formFac[gamma_value].momenta[inser_mom_num].nonlocal_current = nonlocal_cur3ptfn;
