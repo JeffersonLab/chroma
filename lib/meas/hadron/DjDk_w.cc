cfc70f470a166e610dadecb195653c73e241f7c1
ikuro <ikuro>
2004-04-20 05:04:40 +0000

    *** empty log message ***

--- lib/meas/hadron/DjDk_w.cc
+++ lib/meas/hadron/DjDk_w.cc
@@ -1,4 +1,4 @@
-// $Id: DjDk_w.cc,v 1.2 2003-08-20 11:33:52 bjoo Exp $
+// $Id: DjDk_w.cc,v 1.3 2004-04-20 05:04:40 ikuro Exp $
 /*
  *
  */
@@ -23,6 +23,11 @@ void DjDk(const multi1d<LatticeColorMatrix>& u,
     prop_jk = 0.5 * (u[1]*shift(p_tmp, FORWARD, 1) - shift(adj(u[1])*p_tmp, BACKWARD, 1));
   }
 
+  else if (direction == 00) {   // dxdx
+    prop_jk = u[0]*shift(prop, FORWARD, 0) + shift(adj(u[0])*prop, BACKWARD, 0)
+             - 2.0 * prop;
+  }
+
   else if (direction == 11) {   // dydy
     prop_jk = u[1]*shift(prop, FORWARD, 1) + shift(adj(u[1])*prop, BACKWARD, 1)
              - 2.0 * prop;
@@ -32,4 +37,11 @@ void DjDk(const multi1d<LatticeColorMatrix>& u,
     prop_jk = u[2]*shift(prop, FORWARD, 2) + shift(adj(u[2])*prop, BACKWARD, 2)
              - 2.0 * prop;
   }
+
+  else {
+    cout << "Invalid direction\n"
+	 << "Exit here\n";
+
+    exit(1);
+      }
 }
