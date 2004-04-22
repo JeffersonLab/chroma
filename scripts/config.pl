315ccc1525a553276a6b5775eeb48a9cb02b62c3
edwards <edwards>
2004-04-22 18:54:56 +0000

    Sample config.pl script.

---
+++ scripts/config.pl
@@ -0,0 +1,23 @@
+# $Id: config.pl,v 1.1 2004-04-22 18:54:56 edwards Exp $
+#
+# This is a sample config.pl to use in the various formfac
+# analysis scripts.
+#
+$mom2_max = 3;
+@sink_mom = (0,0,0);
+$a = 0.1;
+$L_s = 4;
+
+$nam = 'nonlocal';
+$cur = 'n';
+
+$t_src = 1;
+$t_snk = 6;
+$spext = 'D-7104.DG3_1.P_1.SP';
+$ssext = 'D-7104.DG3_1.DG3_1.SS';
+$swext = 'D-7104.DG3_1.W_1.SW';
+$wpext = 'D-7104.W_1.P_1.WP';
+$wsext = 'D-7104.W_1.DG3_1.WS';
+$wwext = 'D-7104.W_1.W_1.WW';
+$norm = '1';
+
