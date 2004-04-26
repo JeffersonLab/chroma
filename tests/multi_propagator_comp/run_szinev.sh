183a84729cf506f7f127f3da4e69b5a99d851ec8
bjoo <bjoo>
2004-04-26 11:22:55 +0000

    Added SZIN ev results for multi_prop_comp

---
+++ tests/multi_propagator_comp/run_szinev.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+cp  multi_propagator_comp_zolo4_szinev_c0.xml.ini DATA
+./multi_propagator_comp
+cp XMLDAT XMLDAT_c0
+
+cp  multi_propagator_comp_zolo4_szinev_c1.xml.ini DATA
+./multi_propagator_comp
+cp XMLDAT XMLDAT_c1
+
+cp multi_propagator_comp_zolo4_szinev_c2.xml.ini DATA
+./multi_propagator_comp
+cp XMLDAT XMLDAT_c2
+
+# Re use same data file
+./collect_multi_propcomp
