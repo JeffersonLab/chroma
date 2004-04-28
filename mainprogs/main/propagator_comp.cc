b321e07bb414ea5ff77b55ca822cbdc5bf8fcf4d
bjoo <bjoo>
2004-04-28 14:04:17 +0000

    Fixed propagator_comp for new propagator structure. Propagator stuff appears to work. Yet to check seqsource

--- mainprogs/main/propagator_comp.cc
+++ mainprogs/main/propagator_comp.cc
@@ -1,6 +1,9 @@
-// $Id: propagator_comp.cc,v 1.2 2004-04-26 11:19:13 bjoo Exp $
+// $Id: propagator_comp.cc,v 1.3 2004-04-28 14:04:17 bjoo Exp $
 // $Log: propagator_comp.cc,v $
-// Revision 1.2  2004-04-26 11:19:13  bjoo
+// Revision 1.3  2004-04-28 14:04:17  bjoo
+// Fixed propagator_comp for new propagator structure. Propagator stuff appears to work. Yet to check seqsource
+//
+// Revision 1.2  2004/04/26 11:19:13  bjoo
 // Added support for Reading EV-s in SZIN format. Must provide e-values in XML tho.
 //
 // Revision 1.1  2004/04/23 11:23:38  bjoo
@@ -177,11 +180,13 @@ void read(XMLReader& xml, const string& path, PropagatorComponent_input_t& input
 // Forward declaration
 void saveComponent(const ChromaProp_t& param, 
 		   const Prop_t& prop,
-		   const PropSource_t& source_header,
+		   XMLReader& source_record_xml,
 		   const Component_t& component,
-		    XMLReader& gauge_xml,
-		    XMLWriter& xml_out,
-		    const LatticeFermion& psi);
+		   XMLReader& gauge_xml,
+		   XMLWriter& xml_out,
+		   const LatticeFermion& psi,
+		   bool make_sourceP,
+		   bool seqsourceP);
 //! Propagator generation
 /*! \defgroup propagator Propagator generation
  *  \ingroup main
@@ -201,7 +206,13 @@ int main(int argc, char **argv)
   XMLReader xml_in("DATA");
 
   // Read data
-  read(xml_in, "/propagatorComp", input);
+  try {
+    read(xml_in, "/propagatorComp", input);
+  }
+  catch( const string& e) { 
+    QDPIO::cerr << "Caught Exception : " << e << endl;
+    QDP_abort(1);
+  }
 
   // Specify lattice size, shape, etc.
   Layout::setLattSize(input.param.nrow);
@@ -226,16 +237,69 @@ int main(int argc, char **argv)
 
   // Try to invert this record XML into a source struct
   // Also pull out the id of this source
-  PropSource_t source_header;
+  int t0;
+  int j_decay;
+  multi1d<int> boundary;
+  bool make_sourceP = false;;
+  bool seqsourceP = false;
+  {
+    // ONLY SciDAC mode is supported for propagators!!
+    QDPIO::cout << "Attempt to read source" << endl;
+    readQprop(source_file_xml, 
+	      source_record_xml, quark_prop_source,
+	      input.prop.source_file, QDPIO_SERIAL);
+    QDPIO::cout << "Forward propagator successfully read" << endl;
 
-  try
+    // Try to invert this record XML into a source struct
+    try
+    {
+      // First identify what kind of source might be here
+      if (source_record_xml.count("/MakeSource") != 0)
+      {
+	PropSource_t source_header;
+
+	read(source_record_xml, "/MakeSource/PropSource", source_header);
+	j_decay = source_header.j_decay;
+	t0 = source_header.t_source[j_decay];
+	boundary = input.param.boundary;
+	make_sourceP = true;
+      }
+      else if (source_record_xml.count("/SequentialSource") != 0)
+      {
+	ChromaProp_t prop_header;
+	PropSource_t source_header;
+	SeqSource_t seqsource_header;
+
+	read(source_record_xml, "/SequentialSource/SeqSource", seqsource_header);
+	// Any source header will do for j_decay
+	read(source_record_xml, "/SequentialSource/ForwardProps/elem[1]/ForwardProp", 
+	     prop_header);
+	read(source_record_xml, "/SequentialSource/ForwardProps/elem[1]/PropSource", 
+	     source_header);
+	j_decay = source_header.j_decay;
+	t0 = seqsource_header.t_sink;
+	boundary = prop_header.boundary;
+	seqsourceP = true;
+      }
+      else
+	throw "No appropriate header found";
+    }
+    catch (const string& e) 
+    {
+      QDPIO::cerr << "Error extracting source_header: " << e << endl;
+      QDP_abort(1);
+    }
+  }    
+
+  // Sanity check
+  if (seqsourceP)
   {
-    read(source_record_xml, "/MakeSource/PropSource", source_header);
-  }
-  catch (const string& e) 
-  {
-    QDPIO::cerr << "Error extracting source_header: " << e << endl;
-    throw;
+    for(int i=0; i < boundary.size(); ++i)
+      if (boundary[i] != input.param.boundary[i])
+      {
+	QDPIO::cerr << "Incompatible boundary between input and seqsource" << endl;
+	QDP_abort(1);
+      }
   }
 
 
@@ -298,6 +362,134 @@ int main(int argc, char **argv)
    */
   Handle< FermBC<LatticeFermion> >  fbc(new SimpleFermBC<LatticeFermion>(input.param.boundary));
   Handle< FermBC<multi1d<LatticeFermion> > >  fbc_a(new SimpleFermBC<multi1d<LatticeFermion> >(input.param.boundary));
+  //
+  // Initialize fermion action
+  //
+  FermionAction<LatticeFermion>* S_f_ptr = 0;
+  FermionAction< multi1d<LatticeFermion> >* S_f_a_ptr = 0;
+
+  switch (input.param.FermActHandle->getFermActType() ) {
+  case FERM_ACT_WILSON:
+    {
+      const WilsonFermActParams& wils = dynamic_cast<const WilsonFermActParams&>(*(input.param.FermActHandle));
+      
+      QDPIO::cout << "FERM_ACT_WILSON" << endl;
+      S_f_ptr = new EvenOddPrecWilsonFermAct(fbc, wils.Mass,
+					     wils.anisoParam);
+    }
+    break;
+    
+  case FERM_ACT_UNPRECONDITIONED_WILSON:
+    {
+      const WilsonFermActParams& wils = dynamic_cast<const WilsonFermActParams&>(*(input.param.FermActHandle));
+      
+      QDPIO::cout << "FERM_ACT_UNPRECONDITIONED_WILSON" << endl;
+      S_f_ptr = new UnprecWilsonFermAct(fbc, wils.Mass);
+    }
+    break;
+    
+  case FERM_ACT_ZOLOTAREV_4D:
+    {
+      QDPIO::cout << "FERM_ACT_ZOLOTAREV_4D" << endl;
+      const Zolotarev4DFermActParams& zolo4d = dynamic_cast<const Zolotarev4DFermActParams& > (*(input.param.FermActHandle));
+      
+      // Construct Fermact -- now uses constructor from the zolo4d params
+      // struct
+      S_f_ptr = new Zolotarev4DFermAct(fbc, zolo4d, xml_out);
+    }
+    break;
+  
+  case FERM_ACT_ZOLOTAREV_5D:
+    {
+      QDPIO::cout << "FERM_ACT_ZOLOTAREV_5D" << endl;
+      const Zolotarev5DFermActParams& zolo5d = dynamic_cast<const Zolotarev5DFermActParams& > (*(input.param.FermActHandle));
+    
+      // Construct Fermact -- now uses constructor from the zolo4d params
+      // struct
+      S_f_a_ptr = new Zolotarev5DFermActArray(fbc_a, fbc, zolo5d, xml_out);
+    }
+    break;
+
+  case FERM_ACT_DWF:
+    {
+      const DWFFermActParams& dwf = dynamic_cast<const DWFFermActParams&>(*(input.param.FermActHandle));
+      
+      QDPIO::cout << "FERM_ACT_DWF" << endl;
+      S_f_a_ptr = new EvenOddPrecDWFermActArray(fbc_a,
+						dwf.chiralParam.OverMass, 
+						dwf.Mass, 
+						dwf.chiralParam.N5);
+    }
+  break;
+
+  case FERM_ACT_UNPRECONDITIONED_DWF:
+    {
+      const DWFFermActParams& dwf = dynamic_cast<const DWFFermActParams&>(*(input.param.FermActHandle));
+      
+      QDPIO::cout << "FERM_ACT_UNPRECONDITONED_DWF" << endl;
+      S_f_a_ptr = new UnprecDWFermActArray(fbc_a,
+					   dwf.chiralParam.OverMass, 
+					   dwf.Mass, 
+					   dwf.chiralParam.N5);
+    }
+    break;
+  default:
+    QDPIO::cerr << "Unsupported fermion action" << endl;
+    QDP_abort(1);
+  }
+
+  // Create a useable handle on the action
+  // The handle now owns the pointer
+  Handle< FermionAction<LatticeFermion> > S_f(S_f_ptr);
+  Handle< FermionAction< multi1d<LatticeFermion> > > S_f_a(S_f_a_ptr);
+  
+  // FIrst we have to set up the state -- this is fermact dependent
+  const ConnectState *state_ptr;
+
+
+  switch(input.param.FermActHandle->getFermActType()) {
+  case FERM_ACT_WILSON:
+    state_ptr = S_f->createState(u);
+    break;
+  case FERM_ACT_UNPRECONDITIONED_WILSON:
+    state_ptr = S_f->createState(u);
+    break;
+  case FERM_ACT_DWF:
+    state_ptr = S_f->createState(u);
+    break;
+  case FERM_ACT_UNPRECONDITIONED_DWF:
+    state_ptr = S_f->createState(u);
+    break;
+    
+  case FERM_ACT_ZOLOTAREV_4D:
+    {    
+      const Zolotarev4DFermActParams& zolo4d = dynamic_cast<const Zolotarev4DFermActParams& > (*(input.param.FermActHandle));
+      const Zolotarev4DFermAct& S_zolo4 = dynamic_cast<const Zolotarev4DFermAct&>(*S_f);
+      
+      state_ptr = S_zolo4.createState(u, zolo4d.StateInfo, xml_out,zolo4d.AuxFermActHandle->getMass());
+      
+    }
+    break;
+  case FERM_ACT_ZOLOTAREV_5D:
+    {
+      const Zolotarev5DFermActParams& zolo5d = dynamic_cast<const Zolotarev5DFermActParams& > (*(input.param.FermActHandle));
+      const Zolotarev5DFermActArray& S_zolo5 = dynamic_cast<const Zolotarev5DFermActArray&>(*S_f_a);
+      
+      
+      state_ptr = S_zolo5.createState(u, zolo5d.StateInfo, xml_out, zolo5d.AuxFermActHandle->getMass());
+    }
+    break;
+    
+  default:
+    QDPIO::cerr << "Unsupported fermion action (state creation)" << endl;
+    QDP_abort(1);
+  }
+  
+  // Now do the quarkprop here again depending on which of the
+  // action pointers is not null
+  
+  Handle<const ConnectState> state(state_ptr);  // inserts any BC
+
 
   //
   // Loop over the source color and spin, creating the source
@@ -309,143 +501,71 @@ int main(int argc, char **argv)
   
   LatticeFermion psi;
   int ncg_had = 0;
+  for(int comp = 0; comp < input.components.size(); comp++) { 
+    LatticeFermion chi;
 
-  //
-  // Initialize fermion action
-  //
-  switch (input.param.FermActHandle->getFermActType()) {
+    PropToFerm(quark_prop_source, chi, input.components[comp].color,
+	       input.components[comp].spin);
+    
 
-
-    /***********************************************************************/
-    /* 4D ZOlotarev                                                        */
-    /***********************************************************************/
-
-  case FERM_ACT_ZOLOTAREV_4D:
-    {
-      QDPIO::cout << "FERM_ACT_ZOLOTAREV_4D" << endl;
-      const Zolotarev4DFermActParams& zolo4d = dynamic_cast<const Zolotarev4DFermActParams& > (*(input.param.FermActHandle));
-      
-      // Construct Fermact -- now uses constructor from the zolo4d params
-      // struct
-      Zolotarev4DFermAct S(fbc, zolo4d, xml_out);
-
-      
-      // Make a state. Now calls create state with the state info 
-      // params struct. Loads Evalues etc if needed, will in the 
-      // future recompute them as needed
-      Handle<const ConnectState> state(S.createState(u, zolo4d.StateInfo, xml_out, zolo4d.AuxFermActHandle->getMass() ));
-      
-
-      for(int comp = 0; comp < input.components.size(); comp++) { 
-	
-	LatticeFermion chi;
-	PropToFerm(quark_prop_source, chi, input.components[comp].color,
-		   input.components[comp].spin);
-
-
-	// Zero out initial guess
-	psi = zero;
-
-	// Normalise source
-	Real fact = Real(1) / sqrt(norm2(chi));
-	chi *= fact;
-
-	int n_count = 0;
-	S.qprop(psi, 
-		state, 
-		chi, 
-		input.param.invParam.invType, 
-		input.param.invParam.RsdCG, 
-		input.param.invParam.MaxCG, 
-		n_count);
+    // Zero out initial guess
+    psi = zero;
 	
 
-	// Remove source normalisation
-	fact = Real(1) / fact;
-	psi *= fact;
-	  
-	push(xml_out,"Relaxation_Iterations");
-	write(xml_out, "ncg_had", n_count);
-	pop(xml_out);
+    // Normalise source
+    Real fact = Real(1) / sqrt(norm2(chi));
+    chi *= fact;
 
-	saveComponent( input.param,
-			input.prop,
-			source_header,
-			input.components[comp],
-			gauge_xml,
-			xml_out,
-			psi );
-
-      }
+    int n_count = 0;
+      
+    if( S_f_ptr != 0x0 ) { 
+      
+      S_f->qprop(psi,
+		 state,
+		 chi,
+		 input.param.invParam.invType, 
+		 input.param.invParam.RsdCG, 
+		 input.param.invParam.MaxCG,
+		 n_count);
+	
     }
-    break;
-
-  case FERM_ACT_ZOLOTAREV_5D:
-    {
-      QDPIO::cout << "FERM_ACT_ZOLOTAREV_5D" << endl;
-      const Zolotarev5DFermActParams& zolo5d = dynamic_cast<const Zolotarev5DFermActParams& > (*(input.param.FermActHandle));
-      
-      // Construct Fermact -- now uses constructor from the zolo4d params
-      // struct
-      Zolotarev5DFermActArray S(fbc_a, fbc, zolo5d, xml_out);
-
-      
-      // Make a state. Now calls create state with the state info 
-      // params struct. Loads Evalues etc if needed, will in the 
-      // future recompute them as needed
-      Handle<const ConnectState> state(S.createState(u, zolo5d.StateInfo, xml_out, zolo5d.AuxFermActHandle->getMass()));
-      
-
-      for(int comp = 0; comp < input.components.size(); comp++) { 
+    else if ( S_f_a_ptr != 0x0 ) { 
 	
-	LatticeFermion chi;
-	PropToFerm(quark_prop_source, chi, input.components[comp].color,
-		   input.components[comp].spin);
-
-
-	// Zero out initial guess
-	psi = zero;
-	
-
-	// Normalise source
-	Real fact = Real(1) / sqrt(norm2(chi));
-	chi *= fact;
-
-	int n_count = 0;
-	S.qprop(psi, 
-		state, 
-		chi, 
-		input.param.invParam.invType, 
-		input.param.invParam.RsdCG, 
-		input.param.invParam.MaxCG, 
-		n_count);
-	
-
-	// Remove source normalisation
-	fact = Real(1) / fact;
-	psi *= fact;
-	  
-	push(xml_out,"Relaxation_Iterations");
-	write(xml_out, "ncg_had", n_count);
-	pop(xml_out);
-
-	saveComponent( input.param,
-			input.prop,
-		       source_header,
-			input.components[comp],
-			gauge_xml,
-			xml_out,
-			psi );
-
-      }
+      S_f_a->qprop(psi,
+		   state,
+		   chi,
+		   input.param.invParam.invType, 
+		   input.param.invParam.RsdCG, 
+		   input.param.invParam.MaxCG,
+		   n_count);
     }
-    break;
-  default:
-    QDPIO::cerr << "Unsupported fermion action" << endl;
-    QDP_abort(1);
+    else {
+      QDPIO::cerr << "Both S_f_ptr and S_f_a_ptr == 0 " << endl;
+      QDP_abort(1);
+    }
+      
+    ncg_had += n_count;
+      
+    fact = Real(1) / fact;
+    psi *= fact;
+      
+    push(xml_out,"Relaxation_Iterations");
+    write(xml_out, "ncg_had", n_count);
+    pop(xml_out);
+    
+      
+    saveComponent( input.param,
+		   input.prop,
+		   source_record_xml,
+		   input.components[comp],
+		   gauge_xml,
+		   xml_out,
+		   psi,
+		   make_sourceP,
+		   seqsourceP);
+
   }
-
-
+  
   pop(xml_out);  // propagator
   
   xml_out.close();
@@ -459,11 +579,13 @@ int main(int argc, char **argv)
 
 void saveComponent(const ChromaProp_t& param, 
 		   const Prop_t& prop,
-		   const PropSource_t& source_header,
+		   XMLReader& source_record_xml,
 		   const Component_t& component,
 		   XMLReader& gauge_xml,
 		   XMLWriter& xml_out,
-		   const LatticeFermion& psi)
+		   const LatticeFermion& psi,
+		   bool make_sourceP,
+		   bool seqsourceP)
 {
 
   SftMom phases(0, true, Nd-1);
@@ -475,24 +597,37 @@ void saveComponent(const ChromaProp_t& param,
   write(xml_out, "prop_corr", prop_corr);
   pop(xml_out);
 
+  
+    
   XMLBufferWriter file_xml;
+  int id = 0;
   push(file_xml, "propagatorComponent");
-  int id = 0;    // NEED TO FIX THIS - SOMETHING NON-TRIVIAL NEEDED
+  // NEED TO FIX THIS - SOMETHING NON-TRIVIAL NEEDED
   write(file_xml, "id", id);
   pop(file_xml);
   
+  XMLBufferWriter record_xml;
+
+  if( make_sourceP) {
     
 
-  XMLBufferWriter record_xml;
-  push(record_xml, "PropagatorComponent");
-    
-  write(record_xml, "ForwardProp", param);
-  write(record_xml, "PropSource", source_header);
-  write(record_xml, "Config_info", gauge_xml);
-  write(record_xml, "Component", component);
-    
-  pop(record_xml);
-    
+    XMLReader xml_tmp(source_record_xml, "/MakeSource");
+
+    push(record_xml, "Propagator");    
+    write(record_xml, "ForwardProp", param);
+    record_xml << xml_tmp;
+    write(record_xml, "Component", component);
+    pop(record_xml);
+  }
+  else {
+    XMLReader xml_tmp(source_record_xml, "/SequentialSource");
+
+    push(record_xml, "SeqProp");    
+    write(record_xml, "ForwardProp", param);
+    record_xml << xml_tmp;
+    write(record_xml, "Component", component);
+    pop(record_xml);
+  }
   ostringstream outfile;
 
   // Zero suffix for compatibility with multi mass version
