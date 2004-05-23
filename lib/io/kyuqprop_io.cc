f9e057d100369d1fb36969ea94aa3efe61ee5d92
edwards <edwards>
2004-05-23 21:44:40 +0000

    Changed all reading and ops to explicitly double precision.

--- lib/io/kyuqprop_io.cc
+++ lib/io/kyuqprop_io.cc
@@ -1,4 +1,4 @@
-// $Id: kyuqprop_io.cc,v 1.5 2004-05-19 03:31:23 edwards Exp $
+// $Id: kyuqprop_io.cc,v 1.6 2004-05-23 21:44:40 edwards Exp $
 /*!
  * @file
  * @brief  Read/write a Kentucky quark propagator
@@ -41,15 +41,13 @@ void readKYUQprop(LatticePropagator& q, const string& file)
      x,y,z,t,snk_col,snk_spin,ri,src_col,src_spin 
      x is fastest (Fortran Order)
   */
-  LatticePropagator q_old;
-  multi2d<LatticeFermion> source(Nc,Ns);
-  multi2d<LatticeReal> re(Nc,Ns);
-  multi2d<LatticeReal> im(Nc,Ns);
-  LatticeFermion f;
-  LatticeColorVector  cv;
-
-//  LatticeReal64  tmp;  // KYU always uses 64 bits
-  LatticeDouble  tmp;  // KYU always uses 64 bits
+  // KYU always uses 64 bits
+  LatticePropagatorD q_old;
+  multi2d<LatticeFermionD> source(Nc,Ns);
+  multi2d<LatticeRealD> re(Nc,Ns);
+  multi2d<LatticeRealD> im(Nc,Ns);
+  LatticeFermionD f;
+  LatticeColorVectorD  cv;
 
   for(int src_spin=0; src_spin < Ns; ++src_spin)
     for(int src_color=0; src_color < Nc; ++src_color)
@@ -57,15 +55,13 @@ void readKYUQprop(LatticePropagator& q, const string& file)
       for(int snk_spin=0; snk_spin < Ns; ++snk_spin)
 	for(int snk_color=0; snk_color < Nc; ++snk_color)
 	{
-	  read(bin, tmp);
-	  re(snk_color,snk_spin) = tmp;
+	  read(bin, re(snk_color,snk_spin));
 	}
 
       for(int snk_spin=0; snk_spin < Ns; ++snk_spin)
 	for(int snk_color=0; snk_color < Nc; ++snk_color)
 	{
-	  read(bin, tmp);
-	  im(snk_color,snk_spin) = tmp;
+	  read(bin, im(snk_color,snk_spin));
 	}
 
       // Stuff into a fermion
@@ -93,21 +89,21 @@ void readKYUQprop(LatticePropagator& q, const string& file)
   // Finally, stuff the rotated result into the propagator
   for(int src_color=0; src_color < Nc; ++src_color)
   {
-    FermToProp(LatticeFermion(source(src_color,0)+source(src_color,2)),
+    FermToProp(LatticeFermionD(source(src_color,0)+source(src_color,2)),
 	       q_old, src_color, 0);
-    FermToProp(LatticeFermion(source(src_color,1)+source(src_color,3)),
+    FermToProp(LatticeFermionD(source(src_color,1)+source(src_color,3)),
 	       q_old, src_color, 1);
-    FermToProp(LatticeFermion(source(src_color,2)-source(src_color,0)),
+    FermToProp(LatticeFermionD(source(src_color,2)-source(src_color,0)),
 	       q_old, src_color, 2);
-    FermToProp(LatticeFermion(source(src_color,3)-source(src_color,1)),
+    FermToProp(LatticeFermionD(source(src_color,3)-source(src_color,1)),
 	       q_old, src_color, 3);
   }
 
   // Rescale
-  q_old *= Real(1.0 / sqrt(2.0));
+  q_old *= RealD(1.0) / sqrt(RealD(2));
 
   // Now that we have read the prop, need to change the spin basis
-  SpinMatrix U = DiracToDRMat();
+  SpinMatrixD U = DiracToDRMat();
 
   // And finally...
   q = U * q_old * adj(U);   // note, adj(U) = -U
