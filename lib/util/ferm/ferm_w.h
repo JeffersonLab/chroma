2fd32bccb1b3c7a6bdc51cc6bdaf5ecd43e3a140
edwards <edwards>
2004-05-25 16:16:39 +0000

    Added paulitodr.h

--- lib/util/ferm/ferm_w.h
+++ lib/util/ferm/ferm_w.h
@@ -1,9 +1,10 @@
 // -*- C++ -*-
-// $Id: ferm_w.h,v 1.1 2004-05-14 00:22:24 edwards Exp $
+// $Id: ferm_w.h,v 1.2 2004-05-25 16:16:39 edwards Exp $
 
 #ifndef __ferm_w_h__
 #define __ferm_w_h__
 
 #include "diractodr.h"
+#include "paulitodr.h"
 
 #endif
