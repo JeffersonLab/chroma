6745a129afcd6ad40e3b7793074eeafacd577376
edwards <edwards>
2004-08-01 20:23:10 +0000

    Added SSE_DWF_CG support.

---
+++ lib/actions/ferm/fermacts/prec_dwf_fermact_array_sse_w.cc
@@ -0,0 +1,250 @@
+// $Id: prec_dwf_fermact_array_sse_w.cc,v 1.1 2004-08-01 20:23:10 edwards Exp $
+/*! \file
+ *  \brief SSE 4D style even-odd preconditioned domain-wall fermion action
+ */
+
+#include "chromabase.h"
+#include "actions/ferm/fermacts/prec_dwf_fermact_array_sse_w.h"
+#include "actions/ferm/linop/unprec_dwf_linop_array_w.h"
+#include "actions/ferm/linop/prec_dwf_linop_array_w.h"
+#include "actions/ferm/linop/lmdagm.h"
+
+#include <sse_dwf_cg.h>
+
+//! Private internal initializer
+void 
+SSEEvenOddPrecDWFermActArray::init()
+{
+  QDPIO::cout << "entering SSEEvenOddPrecDWFermActArray::init" << endl;
+
+  if (Nd != 4 || Nc != 3)
+  {
+    QDPIO::cerr << "SSEEvenOddPrecDWFermActArray: only supports Nd=4 and Nc=3" << endl;
+    QDP_abort(1);
+  }
+
+  multi1d<int> lattice_size(Nd+1);
+  lattice_size[Nd] = size();
+  for(int i=0; i < Nd; ++i)
+    lattice_size[i] = Layout::lattSize()[i];
+
+  int err = SSE_DWF_init(lattice_size.slice(), SSE_DWF_FLOAT, NULL, NULL);
+  if (err != 0)
+  {
+    QDPIO::cerr << "__func__ : error in SSE_DWF_init" << endl;
+    QDP_abort(1);
+  }
+
+  QDPIO::cout << "exiting SSEEvenOddPrecDWFermActArray::init" << endl;
+}
+
+
+//! Private internal destructor
+void 
+SSEEvenOddPrecDWFermActArray::fini()
+{
+  SSE_DWF_fini();
+}
+
+
+
+//! Produce a linear operator for this action
+/*!
+ * \ingroup fermact
+ *
+ * The operator acts on the odd sublattice
+ *
+ * \param state 	    gauge field     	       (Read)
+ */
+const EvenOddPrecLinearOperator<multi1d<LatticeFermion> >*
+SSEEvenOddPrecDWFermActArray::linOp(Handle<const ConnectState> state) const
+{
+  return new EvenOddPrecDWLinOpArray(state->getLinks(),WilsonMass,m_q,N5);
+}
+
+//! Produce a M^dag.M linear operator for this action
+/*!
+ * The operator acts on the odd sublattice
+ *
+ * \param state 	    gauge field     	       (Read)
+ */
+const LinearOperator<multi1d<LatticeFermion> >*
+SSEEvenOddPrecDWFermActArray::lMdagM(Handle<const ConnectState> state) const
+{
+  return new lmdagm<multi1d<LatticeFermion> >(linOp(state));
+}
+
+//! Produce a linear operator for this action but with quark mass 1
+/*!
+ * The operator acts on the entire lattice
+ *
+ * \param state	    gauge field     	       (Read)
+ */
+const LinearOperator<multi1d<LatticeFermion> >*
+SSEEvenOddPrecDWFermActArray::linOpPV(Handle<const ConnectState> state) const
+{
+  // For the PV operator, use the **unpreconditioned** one
+  // fixed to quark mass 1
+  return new UnprecDWLinOpArray(state->getLinks(),WilsonMass,1.0,N5);
+}
+
+
+//! Gauge field reader
+double
+DWF_gauge_reader(const void *ptr, void *env, 
+		 const int latt_coord[4], int mu, int row, int col, int reim)
+{
+  /* Translate arg */
+  multi1d<LatticeColorMatrix>& u = *(multi1d<LatticeColorMatrix>*)ptr;
+
+  // Get node and index
+  multi1d<int> coord(4);
+  coord = latt_coord;
+  int node = Layout::nodeNumber(coord);
+  int linear = Layout::linearSiteIndex(coord);
+
+  if (node != Layout::nodeNumber())
+  {
+    QDPIO::cerr << "__func__ : wrong coordinates for this node" << endl;
+    QDP_abort(1);
+  }
+ 
+  // Get the value
+  // NOTE: it would be nice to use the "peek" functions, but they will
+  // broadcast to all nodes the value since they are platform independent.
+  // We don't want that, so we poke into the on-node data
+  double val = (reim == 0) ? 
+    toDouble(u[mu].elem(linear).elem().elem(row,col).real()) : 
+    toDouble(u[mu].elem(linear).elem().elem(row,col).imag());
+
+  return val;
+}
+
+
+//! Fermion field reader
+double
+DWF_fermion_reader(const void *ptr, void *env, 
+		   const int latt_coord[5], int color, int spin, int reim)
+{
+  /* Translate arg */
+  multi1d<LatticeFermion>& psi = *(multi1d<LatticeFermion>*)ptr;
+
+  // Get node and index
+  int s = latt_coord[4];
+  multi1d<int> coord(4);
+  coord = latt_coord;
+  int node = Layout::nodeNumber(coord);
+  int linear = Layout::linearSiteIndex(coord);
+
+  if (node != Layout::nodeNumber())
+  {
+    QDPIO::cerr << "__func__ : wrong coordinates for this node" << endl;
+    QDP_abort(1);
+  }
+ 
+  // Get the value
+  // NOTE: it would be nice to use the "peek" functions, but they will
+  // broadcast to all nodes the value since they are platform independent.
+  // We don't want that, so we poke into the on-node data
+  double val = (reim == 0) ? 
+    double(psi[s].elem(linear).elem(spin).elem(color).real()) : 
+    double(psi[s].elem(linear).elem(spin).elem(color).imag());
+
+  return val;
+}
+
+
+//! Fermion field reader
+void
+DWF_fermion_writer(void *ptr, void *env, 
+		   const int latt_coord[5], int color, int spin, int reim,
+		   double val)
+{
+  /* Translate arg */
+  multi1d<LatticeFermion>& psi = *(multi1d<LatticeFermion>*)ptr;
+
+  // Get node and index
+  int s = latt_coord[4];
+  multi1d<int> coord(4);
+  coord = latt_coord;
+  int node = Layout::nodeNumber(coord);
+  int linear = Layout::linearSiteIndex(coord);
+
+  if (node != Layout::nodeNumber())
+  {
+    QDPIO::cerr << "__func__ : wrong coordinates for this node" << endl;
+    QDP_abort(1);
+  }
+ 
+  // Get the value
+  // NOTE: it would be nice to use the "peek" functions, but they will
+  // broadcast to all nodes the value since they are platform independent.
+  // We don't want that, so we poke into the on-node data
+  if (reim == 0)
+    psi[s].elem(linear).elem(spin).elem(color).real() = val;
+  else
+    psi[s].elem(linear).elem(spin).elem(color).imag() = val;
+
+}
+
+
+
+//! Optimized inverter - this is temporary
+void 
+SSEEvenOddPrecDWFermActArray::opt_qpropT(multi1d<LatticeFermion>& psi, 
+					 Handle<const ConnectState> state, 
+					 const multi1d<LatticeFermion>& chi, 
+					 enum InvType invType,
+					 const Real& RsdCG, 
+					 int MaxCG, int& ncg_had) const
+{
+  QDPIO::cout << "entering SSEEvenOddPrecDWFermActArray::opt_qpropT" << endl;
+
+  START_CODE();
+
+  const multi1d<LatticeColorMatrix>& u = state->getLinks();
+
+  // Get a shift copy of the gauge fields
+  multi1d<LatticeColorMatrix> v(u.size());
+  for(int mu=0; mu < Nd; ++mu)
+    v[mu] = shift(u[mu], FORWARD, mu);
+
+  // Initialize the SSE specific fields
+  SSE_DWF_Gauge*   g   = SSE_DWF_load_gauge(&u, &v, NULL, DWF_gauge_reader);
+  SSE_DWF_Fermion* rhs = SSE_DWF_load_fermion(&chi, NULL, DWF_fermion_reader);
+  SSE_DWF_Fermion* x0  = SSE_DWF_load_fermion(&psi, NULL, DWF_fermion_reader);
+  SSE_DWF_Fermion* x   = SSE_DWF_allocate_fermion();
+
+  // Call the solver
+  double out_epsilon = 0.0;
+  double M_0 = toDouble(WilsonMass);    // WARNING: check sign convention!
+  double mq  = toDouble(m_q);
+  double rsd = toDouble(RsdCG);
+  int err;
+
+  cerr << "M_0=" << M_0 << " mq=" << mq << " rsd=" << rsd
+       << "out_epsilon=" << out_epsilon << endl;
+
+  err = SSE_DWF_cg_solver(x, &out_epsilon, &ncg_had,
+			  g, M_0, mq,
+			  x0, rhs, 
+			  rsd, MaxCG);
+  if (err != 0)
+  {
+    QDPIO::cerr << "__func__ : convergence not found" << endl;
+    QDP_abort(1);
+  }
+
+  // Save the result
+  SSE_DWF_save_fermion(&psi, NULL, DWF_fermion_writer, x);
+
+  // Cleanup
+  SSE_DWF_delete_fermion(x);
+  SSE_DWF_delete_fermion(x0);
+  SSE_DWF_delete_fermion(rhs);
+  SSE_DWF_delete_gauge(g);
+
+  END_CODE();
+
+  QDPIO::cout << "exiting SSEEvenOddPrecDWFermActArray::opt_qpropT" << endl;
+}
---
+++ lib/actions/ferm/fermacts/prec_dwf_fermact_array_sse_w.h
@@ -0,0 +1,108 @@
+// -*- C++ -*-
+// $Id: prec_dwf_fermact_array_sse_w.h,v 1.1 2004-08-01 20:23:10 edwards Exp $
+/*! \file
+ *  \brief 4D style even-odd preconditioned domain-wall fermion action
+ */
+
+#ifndef __prec_dwf_fermact_array_sse_w_h__
+#define __prec_dwf_fermact_array_sse_w_h__
+
+#include "fermact.h"
+#include "actions/ferm/fermacts/prec_dwf_fermact_base_array_w.h"
+
+using namespace QDP;
+
+//! 4D style even-odd preconditioned domain-wall fermion action
+/*! \ingroup fermact
+ *
+ * 4D style even-odd preconditioned domain-wall fermion action. 
+ * Follows notes of Orginos (10/2003)
+ *
+ * Hopefully, the conventions used here
+ * are specified in Phys.Rev.D63:094505,2001 (hep-lat/0005002).
+ */
+
+class SSEEvenOddPrecDWFermActArray : public EvenOddPrecDWFermActBaseArray<LatticeFermion>
+{
+public:
+  //! General FermBC
+  SSEEvenOddPrecDWFermActArray(Handle< FermBC< multi1d<LatticeFermion> > > fbc_, 
+			    const Real& WilsonMass_, const Real& m_q_, int N5_) : 
+    fbc(fbc_), WilsonMass(WilsonMass_), m_q(m_q_), N5(N5_) 
+    {
+      a5=1;
+      QDPIO::cout << "Construct SSEEvenOddPrecDWFermActArray: WilsonMass = " << WilsonMass 
+		  << "  m_q = " << m_q 
+		  << "  N5 = " << N5 
+		  << "  a5 = " << a5 
+		  << endl;
+
+      init();
+    }
+
+  //! Copy constructor
+  SSEEvenOddPrecDWFermActArray(const SSEEvenOddPrecDWFermActArray& a) : 
+    fbc(a.fbc), WilsonMass(a.WilsonMass), m_q(a.m_q), a5(a.a5), N5(a.N5) {}
+
+  //! Assignment
+  SSEEvenOddPrecDWFermActArray& operator=(const SSEEvenOddPrecDWFermActArray& a)
+    {fbc=a.fbc; WilsonMass=a.WilsonMass; m_q=a.m_q; a5=a.a5; N5=a.N5; return *this;}
+
+  //! Return the fermion BC object for this action
+  const FermBC< multi1d<LatticeFermion> >& getFermBC() const {return *fbc;}
+
+  //! Length of DW flavor index/space
+  int size() const {return N5;}
+
+  //! Return the quark mass
+  Real quark_mass() const {return m_q;}
+
+  //! Produce a linear operator for this action
+  const EvenOddPrecLinearOperator< multi1d<LatticeFermion> >* linOp(Handle<const ConnectState> state) const;
+
+  //! Produce a linear operator M^dag.M for this action
+  const LinearOperator< multi1d<LatticeFermion> >* lMdagM(Handle<const ConnectState> state) const;
+
+  //! Produce a linear operator for this action but with quark mass 1
+  const LinearOperator< multi1d<LatticeFermion> >* linOpPV(Handle<const ConnectState> state) const;
+
+  //! Optimized inverter - this is temporary
+  /*! 
+   * Solves  M.psi = chi
+   *
+   * Optimized inverter - will rename to qpropT
+   *
+   * \param psi      quark propagator ( Modify )
+   * \param state    gauge connection state ( Read )
+   * \param chi      source ( Modify )
+   * \param invType  inverter type ( Read (
+   * \param RsdCG    CG (or MR) residual used here ( Read )
+   * \param MaxCG    maximum number of CG iterations ( Read )
+   * \param ncg_had  number of CG iterations ( Write )
+   */
+  void opt_qpropT(multi1d<LatticeFermion>& psi, 
+		  Handle<const ConnectState> state, 
+		  const multi1d<LatticeFermion>& chi, 
+		  enum InvType invType,
+		  const Real& RsdCG, 
+		  int MaxCG, int& ncg_had) const;
+  
+  //! Destructor is automatic
+  ~SSEEvenOddPrecDWFermActArray() {fini();}
+
+protected:
+  //! Private internal initializer
+  void init();
+
+  //! Private internal destructor
+  void fini();
+
+private:
+  Handle< FermBC< multi1d<LatticeFermion> > >  fbc;
+  Real WilsonMass;
+  Real m_q;
+  Real a5;
+  int  N5;
+};
+
+#endif
