85063ffd7325add2dceca62393ad48328c66a511
bjoo <bjoo>
2004-08-05 15:56:25 +0000

    Added Force terms

--- lib/actions/ferm/fermacts/unprec_wilson_fermact_w.cc
+++ lib/actions/ferm/fermacts/unprec_wilson_fermact_w.cc
@@ -1,4 +1,4 @@
-// $Id: unprec_wilson_fermact_w.cc,v 1.17 2004-07-28 02:38:01 edwards Exp $
+// $Id: unprec_wilson_fermact_w.cc,v 1.18 2004-08-05 15:56:25 bjoo Exp $
 /*! \file
  *  \brief Unpreconditioned Wilson fermion action
  */
@@ -53,9 +53,48 @@ UnprecWilsonFermAct::dsdu(multi1d<LatticeColorMatrix> & ds_u,
 			  const LatticeFermion& psi) const
 {
   START_CODE();
-  
-  QDPIO::cerr << "UnprecWilsonFermAct::dsdu not implemented" << endl;
-  QDP_abort(1);
 
+  // The phi <=> X so I define the Y field as MX 
+  
+  // Get at the U matrices
+  const multi1d<LatticeColorMatrix>& u = state->getLinks();
+  
+  // Get a linear operator
+  Handle<const LinearOperator<LatticeFermion> > M(linOp(state));
+
+  // Compute MY
+  LatticeFermion Y;
+  (*M)(Y, psi, PLUS);
+
+  // Usually this is Kappa. In our normalisation it is 0.5
+  Real prefactor=Real(0.5);
+
+  // Two temporaries
+  LatticeFermion f_tmp;
+  LatticeColorMatrix u_tmp;
+  for(int mu = 0; mu < Nd; mu++) { 
+
+    // f_tmp = (1 + gamma_mu) Y 
+    f_tmp = Gamma(1<<mu)*Y;
+    f_tmp += Y;
+
+    //   trace_spin ( X^{dag}_x ( 1 + gamma_mu ) Y_x+mu )
+    // = trace_spin(  Y_x+mu X^dag_x (1 + gamma_mu )  )
+    u_tmp = trace(adj(psi)*shift(f_tmp, FORWARD, mu));
+
+    // f_tmp = -(1 -gamma_mu) X
+    f_tmp = Gamma(1<<mu)*psi;
+    f_tmp -= psi;
+
+    //  -trace_spin( -Y^{dag}_x( 1 - gamma_mu) X_x+mu )
+    //= +trace_spin( X_{x+mu} Y^{dag}_x ( 1 - gamma_mu ) 
+    u_tmp -= trace(adj(Y)*shift(f_tmp, FORWARD, mu));
+    
+    // accumulate with prefactor
+    ds_u[mu] += prefactor*u[mu]*u_tmp;
+  }
+
+     
+    
   END_CODE();
 }
