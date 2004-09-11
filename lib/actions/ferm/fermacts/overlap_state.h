931d512c439dc25fac56d1b1d489a05beb944356
edwards <edwards>
2004-09-11 16:35:06 +0000

    Added Chroma namespace.

--- lib/actions/ferm/fermacts/overlap_state.h
+++ lib/actions/ferm/fermacts/overlap_state.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: overlap_state.h,v 1.2 2004-01-08 11:53:08 bjoo Exp $
+// $Id: overlap_state.h,v 1.3 2004-09-11 16:35:55 edwards Exp $
 /*! @file
  * @brief Connection state holding eigenvectors
  *
@@ -13,94 +13,98 @@
 
 using namespace QDP;
 
-template<typename T>
-class OverlapConnectStateBase : public ConnectState 
+namespace Chroma
 {
-public: 
-  //! Return the eigenvalues
-  virtual const multi1d<Real>& getEigVal() const = 0;
+  template<typename T>
+  class OverlapConnectStateBase : public ConnectState 
+  {
+  public: 
+    //! Return the eigenvalues
+    virtual const multi1d<Real>& getEigVal() const = 0;
 
-  //! Return the eigenvectors
-  virtual const multi1d<T>& getEigVec() const = 0;
+    //! Return the eigenvectors
+    virtual const multi1d<T>& getEigVec() const = 0;
 
-  //! Return the eigenvectors
-  virtual const Real& getEigValMax() const = 0;
+    //! Return the eigenvectors
+    virtual const Real& getEigValMax() const = 0;
 
-  //! Return approximation lower bound
-  virtual const Real& getApproxMin() const = 0;
-  virtual const Real& getApproxMax() const = 0;
+    //! Return approximation lower bound
+    virtual const Real& getApproxMin() const = 0;
+    virtual const Real& getApproxMax() const = 0;
 
-};
+  };
 
 
-template<typename T>
-class OverlapConnectState : public OverlapConnectStateBase<T>
-{
-public:
+  template<typename T>
+  class OverlapConnectState : public OverlapConnectStateBase<T>
+  {
+  public:
 
-  typedef Real WordBase_t;
+    typedef Real WordBase_t;
   
-  //! Constructor with no eigenvalues
-  OverlapConnectState(const multi1d<LatticeColorMatrix>& u_,  // gauge field
+    //! Constructor with no eigenvalues
+    OverlapConnectState(const multi1d<LatticeColorMatrix>& u_,  // gauge field
 			const WordBase_t& approxMin_,          // epsilon
 			const WordBase_t& approxMax_           // approx max
-    )  {
-    u = u_;
-    eigValMax = 0;
-    approxMin = approxMin_ ;
-    approxMax = approxMax_ ;
-  }
+      )  {
+      u = u_;
+      eigValMax = 0;
+      approxMin = approxMin_ ;
+      approxMax = approxMax_ ;
+    }
 
-  //! Constructor with e-values and e-vectors
-  OverlapConnectState(const multi1d<LatticeColorMatrix>& u_,
+    //! Constructor with e-values and e-vectors
+    OverlapConnectState(const multi1d<LatticeColorMatrix>& u_,
 			const multi1d<WordBase_t>& val_, 
 			const multi1d<T>& vec_,
 			const WordBase_t& val_max_,
 			const WordBase_t& approxMin_,
 			const WordBase_t& approxMax_)  {
 
-    // hhmm, for now make a copy
-    u = u_;
-    eigVal = val_;
-    eigVec = vec_;
-    eigValMax = val_max_;
-    approxMin = approxMin_;
-    approxMax = approxMax_;
-  }
+      // hhmm, for now make a copy
+      u = u_;
+      eigVal = val_;
+      eigVec = vec_;
+      eigValMax = val_max_;
+      approxMin = approxMin_;
+      approxMax = approxMax_;
+    }
 
-  OverlapConnectState(const OverlapConnectState& a) : u(a.u), eigVal(a.eigVal), eigVec(a.eigVec), eigValMax(a.eigValMax), approxMin(a.approxMin), approxMax(a.approxMax) {}
+    OverlapConnectState(const OverlapConnectState& a) : u(a.u), eigVal(a.eigVal), eigVec(a.eigVec), eigValMax(a.eigValMax), approxMin(a.approxMin), approxMax(a.approxMax) {}
 
-  ~OverlapConnectState() {};
+    ~OverlapConnectState() {};
 
-  //! Return the link fields needed in constructing linear operators
-  const multi1d<LatticeColorMatrix>& getLinks() const {return u;}
+    //! Return the link fields needed in constructing linear operators
+    const multi1d<LatticeColorMatrix>& getLinks() const {return u;}
   
 
-  //! Return the eigenvalues
-  const multi1d<WordBase_t>& getEigVal() const {return eigVal;}
+    //! Return the eigenvalues
+    const multi1d<WordBase_t>& getEigVal() const {return eigVal;}
 
-  //! Return the eigenvectors
-  const multi1d<T>& getEigVec() const {return eigVec;}
+    //! Return the eigenvectors
+    const multi1d<T>& getEigVec() const {return eigVec;}
   
-  //! Return the max eigenvalues
-  const WordBase_t& getEigValMax() const {return eigValMax;}
+    //! Return the max eigenvalues
+    const WordBase_t& getEigValMax() const {return eigValMax;}
 
-  const WordBase_t& getApproxMin() const { return approxMin; }
-  const WordBase_t& getApproxMax() const { return approxMax; }
+    const WordBase_t& getApproxMin() const { return approxMin; }
+    const WordBase_t& getApproxMax() const { return approxMax; }
 
-private:
-  OverlapConnectState() {}  // hide default constructur
-  void operator=(const OverlapConnectState&) {} // hide =
+  private:
+    OverlapConnectState() {}  // hide default constructur
+    void operator=(const OverlapConnectState&) {} // hide =
 
-private:
-  multi1d<LatticeColorMatrix> u;
-  multi1d<WordBase_t>  eigVal;
-  multi1d<T>           eigVec;
-  WordBase_t           eigValMax;
-  WordBase_t           approxMin;
-  WordBase_t           approxMax;
+  private:
+    multi1d<LatticeColorMatrix> u;
+    multi1d<WordBase_t>  eigVal;
+    multi1d<T>           eigVec;
+    WordBase_t           eigValMax;
+    WordBase_t           approxMin;
+    WordBase_t           approxMax;
 
-};
+  };
 	
+}
+
 
 #endif
--- lib/io/overlap_state_info.h
+++ lib/io/overlap_state_info.h
@@ -6,128 +6,132 @@
 #include "io/eigen_io.h"
 
 #include <string>
-using namespace std;
 using namespace QDP;
 
-class OverlapStateInfo {
- public:
+namespace Chroma
+{
 
-  virtual const Real& getApproxMin(void) const = 0;
-  virtual const Real& getApproxMax(void) const = 0;
+  class OverlapStateInfo {
+  public:
 
-  virtual int   getNWilsVec(void) const = 0;
-  virtual bool  loadEigVec(void) const = 0;
-  virtual bool  computeEigVec(void) const = 0;
+    virtual const Real& getApproxMin(void) const = 0;
+    virtual const Real& getApproxMax(void) const = 0;
 
-  virtual const EigenIO_t& getEigenIO(void) const = 0;
-  virtual const RitzParams_t& getRitzParams(void) const = 0;
+    virtual int   getNWilsVec(void) const = 0;
+    virtual bool  loadEigVec(void) const = 0;
+    virtual bool  computeEigVec(void) const = 0;
 
-  // Virtual Destructor
-  virtual ~OverlapStateInfo(void) {}
-};
+    virtual const EigenIO_t& getEigenIO(void) const = 0;
+    virtual const RitzParams_t& getRitzParams(void) const = 0;
 
-class ZolotarevStateInfo : public OverlapStateInfo { 
- private:
-  bool initedP;
-  Real ApproxMin;
-  Real ApproxMax;
-  int  NWilsVec;
-  bool load_eigenP;
-  EigenIO_t eigen_io;
-  RitzParams_t ritzery;
+    // Virtual Destructor
+    virtual ~OverlapStateInfo(void) {}
+  };
 
-  void notInited(void) const { 
-    QDPIO::cerr << "ZolotarevStateInfo not inited" << endl;
-    QDP_abort(1);
-  }
+  class ZolotarevStateInfo : public OverlapStateInfo { 
+  private:
+    bool initedP;
+    Real ApproxMin;
+    Real ApproxMax;
+    int  NWilsVec;
+    bool load_eigenP;
+    EigenIO_t eigen_io;
+    RitzParams_t ritzery;
 
-  void notLoadEig(void) const { 
-    QDPIO::cerr << "No loadable Eigenvalues. No eigen_io member " << endl;
-    QDP_abort(1);
-  }
+    void notInited(void) const { 
+      QDPIO::cerr << "ZolotarevStateInfo not inited" << endl;
+      QDP_abort(1);
+    }
 
-  void notComputeEig(void) const {
-    QDPIO::cerr << "Eigenvalues should not be computed. No Ritz Member" << endl;
-    QDP_abort(1);
-  }
+    void notLoadEig(void) const { 
+      QDPIO::cerr << "No loadable Eigenvalues. No eigen_io member " << endl;
+      QDP_abort(1);
+    }
 
- public:
+    void notComputeEig(void) const {
+      QDPIO::cerr << "Eigenvalues should not be computed. No Ritz Member" << endl;
+      QDP_abort(1);
+    }
+
+  public:
 
 
-  ZolotarevStateInfo(void);   
+    ZolotarevStateInfo(void);   
   
-  void init(const Real& _ApproxMin,
-	    const Real& _ApproxMax,
-	    const int&  _NWilsVec,
-	    const bool& _load_eigenP,
-	    const EigenIO_t& _eigen_io,
-	    const RitzParams_t& _ritzery) {
-    ApproxMin = _ApproxMin;
-    ApproxMax =_ApproxMax;
-    NWilsVec= _NWilsVec;
-    load_eigenP = _load_eigenP;
-    eigen_io = _eigen_io;
-    ritzery = _ritzery;
-    initedP = true; 
-  }
+    void init(const Real& _ApproxMin,
+	      const Real& _ApproxMax,
+	      const int&  _NWilsVec,
+	      const bool& _load_eigenP,
+	      const EigenIO_t& _eigen_io,
+	      const RitzParams_t& _ritzery) {
+      ApproxMin = _ApproxMin;
+      ApproxMax =_ApproxMax;
+      NWilsVec= _NWilsVec;
+      load_eigenP = _load_eigenP;
+      eigen_io = _eigen_io;
+      ritzery = _ritzery;
+      initedP = true; 
+    }
 
 
-  const Real& getApproxMin(void) const { 
-    if( ! initedP )
-      notInited();
-    return ApproxMin;
-  } 
+    const Real& getApproxMin(void) const { 
+      if( ! initedP )
+	notInited();
+      return ApproxMin;
+    } 
 
-  const Real& getApproxMax(void) const { 
-    if( ! initedP )
-      notInited();
-    return ApproxMax; 
-  }
+    const Real& getApproxMax(void) const { 
+      if( ! initedP )
+	notInited();
+      return ApproxMax; 
+    }
 
-  int   getNWilsVec(void) const { 
-    if( ! initedP )
-      notInited();
-    return NWilsVec; 
-  }
+    int   getNWilsVec(void) const { 
+      if( ! initedP )
+	notInited();
+      return NWilsVec; 
+    }
 
-  bool  loadEigVec(void) const {
-    if( ! initedP )
-      notInited();
-    return ( load_eigenP && (NWilsVec > 0) );
-  }
+    bool  loadEigVec(void) const {
+      if( ! initedP )
+	notInited();
+      return ( load_eigenP && (NWilsVec > 0) );
+    }
 
-  bool  computeEigVec(void) const { 
-    if( ! initedP )
-      notInited();
-    return ( (!load_eigenP) && (NWilsVec > 0));
-  }
+    bool  computeEigVec(void) const { 
+      if( ! initedP )
+	notInited();
+      return ( (!load_eigenP) && (NWilsVec > 0));
+    }
 
-  const EigenIO_t& getEigenIO(void) const {
-    if( ! initedP)
-      notInited();
+    const EigenIO_t& getEigenIO(void) const {
+      if( ! initedP)
+	notInited();
 
-    if( ! loadEigVec() )
-      notLoadEig();
+      if( ! loadEigVec() )
+	notLoadEig();
 
-    return eigen_io; 
-  }
+      return eigen_io; 
+    }
 
-  const RitzParams_t& getRitzParams(void) const {
-    if (! initedP)
-      notInited();
+    const RitzParams_t& getRitzParams(void) const {
+      if (! initedP)
+	notInited();
 
-    if( computeEigVec() )
-      notComputeEig();
+      if( computeEigVec() )
+	notComputeEig();
 
-    return ritzery; 
-  }
+      return ritzery; 
+    }
 
-  // Virtual Destructor
-  ~ZolotarevStateInfo(void) {}
-};
+    // Virtual Destructor
+    ~ZolotarevStateInfo(void) {}
+  };
 
-void read(XMLReader& xml_in, const string& path, ZolotarevStateInfo& info);
-void write(XMLWriter& xml_out, const string& path, const ZolotarevStateInfo& info);
+  void read(XMLReader& xml_in, const std::string& path, ZolotarevStateInfo& info);
+  void write(XMLWriter& xml_out, const std::string& path, const ZolotarevStateInfo& info);
+
+}
 
 
 #endif
