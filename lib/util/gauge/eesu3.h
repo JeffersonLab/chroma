e1a8cd646556dcc3b9893909342d2b7b78a65951
edwards <edwards>
2004-10-12 15:28:43 +0000

    Exact exponentiate a su3 lie algebra element.

---
+++ lib/util/gauge/eesu3.h
@@ -0,0 +1,18 @@
+// -*- C++ -*-
+// $Id: eesu3.h,v 1.1 2004-10-12 15:28:43 edwards Exp $
+/*! \file
+ *  \brief Exactly exponentiate a SU(3) lie algebra element
+ */
+
+#ifndef __eesu3_h__
+#define __eesu3_h__
+
+//! Exactly exponentiate a SU(3) lie algebra element
+/*!
+ * \ingroup gauge
+ *
+ *  \param m        LatticeColorMatrix          (Modify)
+ */
+void eesu3(LatticeColorMatrix& m);
+
+#endif
