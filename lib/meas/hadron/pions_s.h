e8da012e374d1d737a5e8fd89536aa0fdc806b30
mcneile <mcneile>
2004-10-30 10:47:05 +0000

    The XML tags are now initialized in this class.

--- lib/meas/hadron/pions_s.h
+++ lib/meas/hadron/pions_s.h
@@ -14,8 +14,9 @@
 
 */
 
+class staggered_hadron_corr ; 
 
-class staggered_pions
+class staggered_pions  : public staggered_hadron_corr
 {
 
 
@@ -23,58 +24,31 @@ class staggered_pions
     void compute(multi1d<LatticeStaggeredPropagator>& quark_props,
 	  int j_decay) ;
 
-  /*
-    Write the correlators out
-  */
-  void dump(int t_source, XMLFileWriter &xml_out)
-    {
-      multi2d<Real> pi_corr(no_channel, t_length);
-      multi2d<Real> re_corr_fn(no_channel, t_length);
-      pi_corr = zero;
 
-      // Take the real part of the correlator and average over the sources
-      for(int i=0; i < no_channel ; i++){
-	for(int t=0; t < t_length; t++){
-	  int t_eff = (t - t_source + t_length)% t_length;
-	  re_corr_fn[i][t_eff] = real(corr_fn[i][t]);
+  staggered_pions(int t_len)  : staggered_hadron_corr(t_len,no_pions)
+    {
+      outer_tag = "Here_are_all_16_pions"  ; 
+      inner_tag = "Pi" ; 
+
+      tag_names.resize(no_pions) ; 
+      for(int i = 0 ; i <  no_pions ; ++i ) 
+	{
+	  ostringstream tag;
+	  tag << "re_pion" << i;
+	  tag_names[i] = tag.str() ; 
 	}
-      }
-                  
-      multi1d<Real> Pi(t_length) ; 
-      push(xml_out, "Here_are_all_16_pions");
-      for(int i=0; i < no_channel; i++) {
-	Pi = re_corr_fn[i];
-	ostringstream tag;
-	tag << "re_pion" << i;
-	push(xml_out, tag.str());
-	write(xml_out, "Pi", Pi);
-	pop(xml_out);
-      }
-      pop(xml_out);
 
-
-
-    }
-
-  staggered_pions(int t_len) 
-    {
-      t_length = t_len ; 
-      corr_fn.resize(no_channel, t_length);
     }
 
   ~staggered_pions()
     {
-      corr_fn.resize(1, 1);
     }
 
 
  protected:
 
-  multi2d<DComplex> corr_fn ; 
-
   private :
-    static const int no_channel = 16 ; 
-  int t_length ; 
+    static const int no_pions = 16 ; 
 
 } ;
 
