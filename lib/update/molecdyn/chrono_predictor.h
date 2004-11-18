5f4fcf23cde881fe257c2275765b7064877d4214
bjoo <bjoo>
2004-11-18 22:14:55 +0000

    Oops

--- lib/update/molecdyn/chrono_predictor.h
+++ lib/update/molecdyn/chrono_predictor.h
@@ -1,21 +1,26 @@
 #ifndef chrono_predictor_h
 #define chrono_predictor_h
 
-namespace Chroma {
+#include "chromabase.h"
 
+using namespace QDP;
+using namespace std;
+
+namespace Chroma {
+  
   // Abstract interface for a Chronological Solution predictor
   template<typename T>
   class AbsChronologicalPredictor {
   public:
-
+    
     // Set psi to be the next initial guess
     virtual void operator()(T& psi) const = 0;
-
+    
     // Present new vector for use in future chronological
     // Predictors
     virtual void newVector(const T& psi) = 0;
   };
-
+  
 
   // A Special Chronological inverter that is not chronological
   // at all but always provides a zero guess
@@ -24,12 +29,12 @@ namespace Chroma {
   public:
     // Set T to the next initial guess
     void operator()(T& psi) const {
-      // This works for multi1d-s too right ?
       psi = zero;
-    };
+    }
 
     // This is just for form... Doesnt do anything
     void newVector(const T& psi) {}
-  }
+  };
+
 }; // End namespace
 #endif
