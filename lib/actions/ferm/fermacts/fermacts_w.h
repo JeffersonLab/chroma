5bfac7f262677b0192ae6b92a3d9a692c219dc38
edwards <edwards>
2004-11-20 21:17:23 +0000

    Moved around prec zolo.

--- lib/actions/ferm/fermacts/fermacts_w.h
+++ lib/actions/ferm/fermacts/fermacts_w.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: fermacts_w.h,v 1.16 2004-11-02 10:33:49 bjoo Exp $
+// $Id: fermacts_w.h,v 1.17 2004-11-20 21:17:23 edwards Exp $
 
 /*! \file
  * \brief Fermion actions
@@ -28,13 +28,13 @@
 #include "unprec_dwf_fermact_array_w.h"
 #include "unprec_nef_fermact_array_w.h"
 #include "unprec_zolo_nef_fermact_array_w.h"
-#include "prec_zolo_nef_fermact_array_w.h"
 #include "unprec_ovdwf_fermact_array_w.h"
 #include "unprec_ovext_fermact_array_w.h"
 
 #include "prec_dwf_fermact_base_array_w.h"
 #include "prec_dwf_fermact_array_w.h"
 #include "prec_nef_fermact_array_w.h"
+#include "prec_zolo_nef_fermact_array_w.h"
 #include "prec_ovdwf_fermact_array_w.h"
 
 #if defined(BUILD_SSE_DWF_CG)
