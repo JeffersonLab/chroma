30093ceb6911735f52e5fb969cd267841fa6eee9
edwards <edwards>
2004-12-07 20:21:03 +0000

    Supports linear combinations of an operator.

---
+++ lib/actions/ferm/linop/llincomb.h
@@ -0,0 +1,58 @@
+// -*- C++ -*-
+// $Id: llincomb.h,v 1.1 2004-12-07 20:21:03 edwards Exp $
+
+#ifndef __llincomb_h__
+#define __llincomb_h__
+
+#include "handle.h"
+#include "linearop.h"
+
+using namespace QDP;
+
+namespace Chroma
+{
+  //! Linear combination of a Linear Operator
+  /*!
+   * \ingroup linop
+   *
+   * This operator does the linear combination  (z1 + z2*B)*psi
+   */
+  template<typename T, class C>
+  class llincomb : public LinearOperator<T>
+  {
+  public:
+    //! Initialize pointer with existing pointer
+    /*! Requires that the pointer p is a return value of new */
+    llincomb(const LinearOperator<T>* p, const C& add_const_, const C& scale_fact_) : 
+      A(p), add_const(add_const_), scale_fact(scale_fact_)  {}
+
+    //! Copy pointer (one more owner)
+    llincomb(Handle<const LinearOperator<T> > p, const C& add_const_, const C& scale_fact_) : 
+      A(p), add_const(add_const_), scale_fact(scale_fact_) {}
+
+    //! Destructor
+    ~llincomb() {}
+
+    //! Subset comes from underlying operator
+    inline const OrderedSubset& subset() const {return A->subset();}
+
+    //! Apply the operator onto a source vector
+    /*! For this operator, the sign is ignored */
+    inline void operator() (T& chi, const T& psi, enum PlusMinus isign) const
+      {
+	const OrderedSubset& sub = A->subset();
+	(*A)(chi, psi, isign);
+	chi[sub] *= scale_fact;
+	chi[sub] += add_const*psi;
+      }
+
+  private:
+    const Handle< const LinearOperator<T> > A;
+    const C add_const;
+    const C scale_fact;
+  };
+}
+
+using namespace Chroma;
+
+#endif
