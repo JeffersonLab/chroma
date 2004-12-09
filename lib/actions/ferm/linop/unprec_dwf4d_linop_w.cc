c330d9e7dde662a4cc6a339efd115d1c8a67b759
edwards <edwards>
2004-12-09 03:55:30 +0000

    Added MINUS version as well.

--- lib/actions/ferm/linop/unprec_dwf4d_linop_w.cc
+++ lib/actions/ferm/linop/unprec_dwf4d_linop_w.cc
@@ -1,4 +1,4 @@
-// $Id: unprec_dwf4d_linop_w.cc,v 1.4 2004-11-17 16:53:23 edwards Exp $
+// $Id: unprec_dwf4d_linop_w.cc,v 1.5 2004-12-09 03:55:30 edwards Exp $
 /*! \file
  *  \brief Unpreconditioned projected DWF operator to 4D
  */
@@ -39,39 +39,63 @@ namespace Chroma
     multi1d<LatticeFermion> chi5(N5);
 
     switch(isign)
+    {
+    case PLUS:
+    {
+      // chi5 = D5(m_q) . tmp5 =  D5(m_q) . P . (chi,0,0,..,0)^T 
+      (*D)(chi5, tmp5, PLUS);
+
+      // Solve  D5(1) . psi5 = chi5
+      psi5 = chi5;
+  
+      switch(invParam.invType)
       {
-      case PLUS:
-	{
-	  // chi5 = D5(m_q) . tmp5 =  D5(m_q) . P . (chi,0,0,..,0)^T 
-	  (*D)(chi5, tmp5, PLUS);
-
-	  // Solve  D5(1) . psi5 = chi5
-	  psi5 = chi5;
-  
-	  switch(invParam.invType)
-	    {
-	    case CG_INVERTER: 
-	      {
-		/* tmp5 = M_dag(u) * chi5 */
-		(*PV)(tmp5, chi5, MINUS);
+      case CG_INVERTER: 
+      {
+	/* tmp5 = M_dag(u) * chi5 */
+	(*PV)(tmp5, chi5, MINUS);
 	  
-		/* psi5 = (M^dag * M)^(-1) chi5 */
-		InvCG2(*PV, tmp5, psi5, invParam.RsdCG, invParam.MaxCG, n_count);
-	      }
-	      break;
-  
-	    default:
-	      QDP_error_exit("Unknown inverter type", invParam.invType);
-	    }
-	}
-	break;
-
-      case MINUS:
-	{
-	  QDP_error_exit("MINUS not supported at the moment");
-	}
-	break;
+	/* psi5 = (M^dag * M)^(-1) chi5 */
+	InvCG2(*PV, tmp5, psi5, invParam.RsdCG, invParam.MaxCG, n_count);
       }
+      break;
+  
+      default:
+	QDP_error_exit("Unknown inverter type", invParam.invType);
+      }
+    }
+    break;
+
+    case MINUS:
+    {
+      // Solve  D5(1) . psi5 = tmp5
+      psi5 = tmp5;
+  
+      switch(invParam.invType)
+      {
+      case CG_INVERTER: 
+      {
+	/* psi5 = (M^dag * M)^(-1) tmp5 */
+	InvCG2(*PV, tmp5, psi5, invParam.RsdCG, invParam.MaxCG, n_count);
+
+	/* tmp5 = M_dag(u) * psi5 */
+	(*PV)(tmp5, psi5, PLUS);
+      }
+      break;
+  
+      default:
+	QDP_error_exit("Unknown inverter type", invParam.invType);
+      }
+
+      // psi5 = D5(m_q)^dag . tmp5 
+      (*D)(psi5, tmp5, MINUS);
+
+    }
+    break;
+
+    default:
+      QDP_error_exit("dwf4d: unknown isign");
+    }
     
     if ( n_count == invParam.MaxCG )
       QDP_error_exit("no convergence in the inverter", n_count);
--- lib/actions/ferm/linop/unprec_pdwf4d_linop_w.cc
+++ lib/actions/ferm/linop/unprec_pdwf4d_linop_w.cc
@@ -1,4 +1,4 @@
-// $Id: unprec_pdwf4d_linop_w.cc,v 1.1 2004-11-17 16:53:23 edwards Exp $
+// $Id: unprec_pdwf4d_linop_w.cc,v 1.2 2004-12-09 03:55:30 edwards Exp $
 /*! \file
  *  \brief Unpreconditioned projected DWF operator to 4D using prec 5D bits
  */
@@ -39,71 +39,91 @@ namespace Chroma
     multi1d<LatticeFermion> chi5(N5);
 
     switch(isign)
+    {
+    case PLUS:
+    {
+      // chi5 = D5(m_q) . tmp5 =  D5(m_q) . P . (chi,0,0,..,0)^T 
+      D->unprecLinOp(chi5, tmp5, PLUS);
+
+      // Solve  D5(1) . psi5 = chi5
+      psi5 = chi5;
+  
+#if defined(HACK_USE_PREC)
+      /* Step (i) */
+      /* chi_tmp_o =  chi_o - D_oe * A_ee^-1 * chi_e */
+      multi1d<LatticeFermion> chi_tmp(N5);
       {
-      case PLUS:
-	{
-	  // chi5 = D5(m_q) . tmp5 =  D5(m_q) . P . (chi,0,0,..,0)^T 
-	  D->unprecLinOp(chi5, tmp5, PLUS);
+	multi1d<LatticeFermion> tmp1(N5);
+	multi1d<LatticeFermion> tmp2(N5);
 
-	  // Solve  D5(1) . psi5 = chi5
-	  psi5 = chi5;
-  
-#if defined(HACK_USE_PREC)
-	  /* Step (i) */
-	  /* chi_tmp_o =  chi_o - D_oe * A_ee^-1 * chi_e */
-	  multi1d<LatticeFermion> chi_tmp(N5);
-	  {
-	    multi1d<LatticeFermion> tmp1(N5);
-	    multi1d<LatticeFermion> tmp2(N5);
-
-	    PV->evenEvenInvLinOp(tmp1, chi5, PLUS);
-	    PV->oddEvenLinOp(tmp2, tmp1, PLUS);
-	    for(int n=0; n < N5; ++n)
-	      chi_tmp[n][rb[1]] = chi5[n] - tmp2[n];
-	  }
-#else
-	  multi1d<LatticeFermion> chi_tmp(N5);
-	  chi_tmp = chi5;
-#endif
-
-	  switch(invParam.invType)
-	    {
-	    case CG_INVERTER: 
-	      {
-		/* tmp5 = M_dag(u) * chi_tmp */
-		(*PV)(tmp5, chi_tmp, MINUS);
-
-		/* psi5 = (M^dag * M)^(-1) chi_tmp */
-		InvCG2 (*PV, tmp5, psi5, invParam.RsdCG, invParam.MaxCG, n_count);
-	      }
-	      break;
-  
-	    default:
-	      QDP_error_exit("Unknown inverter type", invParam.invType);
-	    }
-
-#if defined(HACK_USE_PREC)
-	  /* Step (ii) */
-	  /* psi_e = A_ee^-1 * [chi_e  -  D_eo * psi_o] */
-	  {
-	    multi1d<LatticeFermion> tmp1(N5);
-	    multi1d<LatticeFermion> tmp2(N5);
-
-	    PV->evenOddLinOp(tmp1, psi5, PLUS);
-	    for(int n=0; n < N5; ++n)
-	      tmp2[n][rb[0]] = chi5[n] - tmp1[n];
-	    PV->evenEvenInvLinOp(psi5, tmp2, PLUS);
-	  }
-#endif
-  	}
-	break;
-
-      case MINUS:
-	{
-	  QDP_error_exit("MINUS not supported at the moment");
-	}
-	break;
+	PV->evenEvenInvLinOp(tmp1, chi5, PLUS);
+	PV->oddEvenLinOp(tmp2, tmp1, PLUS);
+	for(int n=0; n < N5; ++n)
+	  chi_tmp[n][rb[1]] = chi5[n] - tmp2[n];
       }
+#else
+      multi1d<LatticeFermion> chi_tmp(N5);
+      chi_tmp = chi5;
+#endif
+
+      switch(invParam.invType)
+      {
+      case CG_INVERTER: 
+      {
+	/* tmp5 = M_dag(u) * chi_tmp */
+	(*PV)(tmp5, chi_tmp, MINUS);
+
+	/* psi5 = (M^dag * M)^(-1) chi_tmp */
+	InvCG2 (*PV, tmp5, psi5, invParam.RsdCG, invParam.MaxCG, n_count);
+      }
+      break;
+  
+      default:
+	QDP_error_exit("Unknown inverter type", invParam.invType);
+      }
+
+#if defined(HACK_USE_PREC)
+      /* Step (ii) */
+      /* psi_e = A_ee^-1 * [chi_e  -  D_eo * psi_o] */
+      {
+	multi1d<LatticeFermion> tmp1(N5);
+	multi1d<LatticeFermion> tmp2(N5);
+
+	PV->evenOddLinOp(tmp1, psi5, PLUS);
+	for(int n=0; n < N5; ++n)
+	  tmp2[n][rb[0]] = chi5[n] - tmp1[n];
+	PV->evenEvenInvLinOp(psi5, tmp2, PLUS);
+      }
+#endif
+    }
+    break;
+
+    case MINUS:
+    {
+      // Solve  D5(1) . psi5 = tmp5
+      psi5 = tmp5;
+  
+      switch(invParam.invType)
+      {
+      case CG_INVERTER: 
+      {
+	/* psi5 = (M^dag * M)^(-1) tmp5 */
+	InvCG2(*PV, tmp5, psi5, invParam.RsdCG, invParam.MaxCG, n_count);
+
+	/* tmp5 = M_dag(u) * psi5 */
+	(*PV)(tmp5, psi5, PLUS);
+      }
+      break;
+  
+      default:
+	QDP_error_exit("Unknown inverter type", invParam.invType);
+      }
+
+      // psi5 = D5(m_q)^dag . tmp5 
+      D->unprecLinOp(psi5, tmp5, MINUS);
+    }
+    break;
+    }
     
     if ( n_count == invParam.MaxCG )
       QDP_error_exit("no convergence in the inverter", n_count);
