42e2ba17172bc6db02889e9ef70245eb5d5c0a98
edwards <edwards>
2004-12-14 17:01:15 +0000

    Commented out dwf install

--- install_scripts/jlab/config_chroma_parscalar-gm.sh
+++ install_scripts/jlab/config_chroma_parscalar-gm.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-../configure --prefix=/usr/local/chroma/parscalar-gm --with-qdp=/usr/local/qdp++/parscalar-gm CXXFLAGS="-static" --enable-sse-wilson-dslash --enable-sse-dwf-cg
+../configure --prefix=/usr/local/chroma/parscalar-gm --with-qdp=/usr/local/qdp++/parscalar-gm CXXFLAGS="-static" --enable-sse-wilson-dslash 
+
+# --enable-sse-dwf-cg
 
