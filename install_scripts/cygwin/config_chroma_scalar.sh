c0a9aad1ce387033b4ca7f595d7c2e57453f6611
edwards <edwards>
2004-12-26 22:53:52 +0000

    Removed enable-dwf-cg flag

--- install_scripts/cygwin/config_chroma_scalar.sh
+++ install_scripts/cygwin/config_chroma_scalar.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-../configure --prefix=/usr/local/share/chroma/scalar --with-qdp=/usr/local/share/qdp++/scalar CXXFLAGS="" --enable-sse-wilson-dslash --enable-sse-dwf-cg
+../configure --prefix=/usr/local/share/chroma/scalar --with-qdp=/usr/local/share/qdp++/scalar CXXFLAGS="" --enable-sse-wilson-dslash 
+
+# --enable-sse-dwf-cg
