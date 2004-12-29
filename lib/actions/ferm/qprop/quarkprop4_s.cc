ab2a36b045ff85a25c4fa4f1f8a834892e72f5d2
edwards <edwards>
2004-12-29 22:12:05 +0000

    Staggered quarkprop support.

---
+++ lib/actions/ferm/qprop/quarkprop4_s.cc
@@ -0,0 +1,142 @@
+// $Id: quarkprop4_s.cc,v 1.1 2004-12-29 22:12:05 edwards Exp $
+/*! \file
+ *  \brief Full quark propagator solver
+ *
+ *  Given a complete propagator as a source, this does all the inversions needed
+ */
+
+#include "chromabase.h"
+#include "fermact.h"
+#include "util/ferm/transf.h"
+#include "actions/ferm/qprop/quarkprop4_s.h"
+
+using namespace QDP;
+
+namespace Chroma 
+{
+  //! Given a complete propagator as a source, this does all the inversions needed
+  /*! \ingroup qprop
+   *
+   * This routine is actually generic to all Wilson-like fermions
+   *
+   * \param q_sol    quark propagator ( Write )
+   * \param q_src    source ( Read )
+   * \param invParam inverter parameters ( Read )
+   * \param RsdCG    CG (or MR) residual used here ( Read )
+   * \param MaxCG    maximum number of CG iterations ( Read )
+   * \param ncg_had  number of CG iterations ( Write )
+   */
+
+  template<typename T, template<class> class C>
+  void quarkProp_a(LatticeStaggeredPropagator& q_sol, 
+		   XMLWriter& xml_out,
+		   const LatticeStaggeredPropagator& q_src,
+		   const C<T>& S_f,
+		   Handle<const ConnectState> state,
+		   const InvertParam_t& invParam,
+		   bool nonRelProp,
+		   int& ncg_had)
+  {
+    START_CODE();
+
+    push(xml_out, "QuarkProp");
+
+    ncg_had = 0;
+
+//  LatticeStaggeredFermion psi = zero;  // note this is ``zero'' and not 0
+
+    // This version loops over all color and spin indices
+    for(int color_source = 0; color_source < Nc; ++color_source)
+    {
+      LatticeStaggeredFermion psi = zero;  // note this is ``zero'' and not 0
+      LatticeStaggeredFermion chi;
+
+      // Extract a fermion source
+      PropToFerm(q_src, chi, color_source);
+
+      // Use the last initial guess as the current initial guess
+
+      /* 
+       * Normalize the source in case it is really huge or small - 
+       * a trick to avoid overflows or underflows
+       */
+      Real fact = Real(1) / sqrt(norm2(chi));
+      chi *= fact;
+
+      // Compute the propagator for given source color/spin.
+      int n_count;
+
+      S_f.qprop(psi, state, chi, invParam, n_count);
+      ncg_had += n_count;
+
+      push(xml_out,"Qprop");
+      write(xml_out, "RsdCG", invParam.RsdCG);
+      write(xml_out, "n_count", n_count);
+      pop(xml_out);
+
+      // Unnormalize the source following the inverse of the normalization above
+      fact = Real(1) / fact;
+      psi *= fact;
+
+      /*
+       * Move the solution to the appropriate components
+       * of quark propagator.
+       */
+      FermToProp(psi, q_sol, color_source);
+    } /* end loop over color_source */
+
+    pop(xml_out);
+
+    END_CODE();
+  }
+
+
+  //! Given a complete propagator as a source, this does all the inversions needed
+  /*! \ingroup qprop
+   *
+   * This routine is actually generic to all Wilson-like fermions
+   *
+   * \param q_sol    quark propagator ( Write )
+   * \param q_src    source ( Read )
+   * \param invParam inverter parameters ( Read )
+   * \param ncg_had  number of CG iterations ( Write )
+   */
+  void quarkProp4(LatticeStaggeredPropagator& q_sol, 
+		  XMLWriter& xml_out,
+		  const LatticeStaggeredPropagator& q_src,	
+		  const StaggeredTypeFermAct<LatticeStaggeredFermion>& S_f,
+		  Handle<const ConnectState> state,
+		  const InvertParam_t& invParam,
+		  bool nonRelProp,
+		  int& ncg_had)
+  {
+    quarkProp_a<LatticeStaggeredFermion,StaggeredTypeFermAct>(q_sol, xml_out, q_src, S_f, state, invParam, nonRelProp, ncg_had);
+  }
+
+
+
+  //! Given a complete propagator as a source, this does all the inversions needed
+  /*! \ingroup qprop
+   *
+   * This routine is actually generic to all Staggered-like fermions
+   *
+   * \param q_sol    quark propagator ( Write )
+   * \param q_src    source ( Read )
+   * \param invParam inverter parameters ( Read )
+   * \param ncg_had  number of CG iterations ( Write )
+   */
+  template<>
+  void 
+  StaggeredTypeFermAct<LatticeStaggeredFermion>::quarkProp(LatticeStaggeredPropagator& q_sol, 
+							   XMLWriter& xml_out,
+							   const LatticeStaggeredPropagator& q_src,
+							   Handle<const ConnectState> state,
+							   const InvertParam_t& invParam,
+							   bool nonRelProp,
+							   int& ncg_had)
+  {
+    quarkProp4(q_sol, xml_out, q_src, *this, state, invParam, nonRelProp, ncg_had);
+  }
+
+
+}; // namespace Chroma
---
+++ lib/actions/ferm/qprop/quarkprop4_s.h
@@ -0,0 +1,36 @@
+// $Id: quarkprop4_s.h,v 1.1 2004-12-29 22:12:05 edwards Exp $
+/*! \file
+ *  \brief Full quark propagator solver
+ *
+ *  Given a complete propagator as a source, this does all the inversions needed
+ */
+
+#ifndef __quarkprop4_s_h__
+#define __quarkprop4_s_h__
+
+#include "fermact.h"
+
+namespace Chroma
+{
+  //! Given a complete propagator as a source, this does all the inversions needed
+  /*! \ingroup qprop
+   *
+   * This routine is actually generic to all Wilson-like fermions
+   *
+   * \param q_sol    quark propagator ( Write )
+   * \param q_src    source ( Read )
+   * \param invParam inverter parameters ( Read )
+   * \param ncg_had  number of CG iterations ( Write )
+   */
+
+  void quarkProp4(LatticeStaggeredPropagator& q_sol, 
+		  XMLWriter& xml_out,
+		  const LatticeStaggeredPropagator& q_src,
+		  const StaggeredTypeFermAct<LatticeStaggeredFermion>& S_f,
+		  Handle<const ConnectState> state,
+		  const InvertParam_t& invParam,
+		  bool nonRelProp,
+		  int& ncg_had);
+
+};
+#endif
