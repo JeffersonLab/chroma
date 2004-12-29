756f882a6b964441db173b5994a87934fc5e976e
edwards <edwards>
2004-12-29 22:08:48 +0000

    Added Staggered support.

--- lib/util/ferm/transf.cc
+++ lib/util/ferm/transf.cc
@@ -1,4 +1,4 @@
-// $Id: transf.cc,v 1.3 2004-11-12 10:50:51 mcneile Exp $
+// $Id: transf.cc,v 1.4 2004-12-29 22:08:48 edwards Exp $
 /*! \file
  *  \brief Insertion/Extraction utilities for vectors/fermions/propagators
  */
@@ -8,157 +8,242 @@
 
 using namespace QDP;
 
-//! Convert (insert) a LatticeColorVector into a LatticeFermion
-/*!
- * \ingroup ferm
- *
- * \param a      Source color vector
- * \param b      Destination fermion
- * \param spin_index   Spin index
- */
-void CvToFerm(const LatticeColorVectorF& a, LatticeFermionF& b, 
-	      int spin_index)
+namespace Chroma
 {
-  pokeSpin(b, a, spin_index);
-}
-
-//! Convert (insert) a LatticeColorVector into a LatticeFermion
-/*!
- * \ingroup ferm
- *
- * \param a      Source color vector
- * \param b      Destination fermion
- * \param spin_index   Spin index
- */
-void CvToFerm(const LatticeColorVectorD& a, LatticeFermionD& b, 
-	      int spin_index)
-{
-  pokeSpin(b, a, spin_index);
-}
-
-
-//! Insert a LatticeFermion into a LatticeFermion
-/*!
- * \ingroup ferm
- *
- * \param a      Source fermion
- * \param b      Destination propagator
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void FermToProp(const LatticeFermionF& a, LatticePropagatorF& b, 
-		int color_index, int spin_index)
-{
-  for(int j = 0; j < Ns; ++j)
+  //! Convert (insert) a LatticeColorVector into a LatticeFermion
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source color vector
+   * \param b      Destination fermion
+   * \param spin_index   Spin index
+   */
+  void CvToFerm(const LatticeColorVectorF& a, LatticeFermionF& b, 
+		int spin_index)
   {
-    LatticeColorMatrixF bb = peekSpin(b, j, spin_index);
-    LatticeColorVectorF aa = peekSpin(a, j);
+    pokeSpin(b, a, spin_index);
+  }
+
+  //! Convert (insert) a LatticeColorVector into a LatticeFermion
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source color vector
+   * \param b      Destination fermion
+   * \param spin_index   Spin index
+   */
+  void CvToFerm(const LatticeColorVectorD& a, LatticeFermionD& b, 
+		int spin_index)
+  {
+    pokeSpin(b, a, spin_index);
+  }
+
+
+  //! Convert (insert) a LatticeColorVector into a LatticeStaggeredFermion
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source color vector
+   * \param b      Destination fermion
+   */
+  void CvToFerm(const LatticeColorVectorF& a, LatticeStaggeredFermionF& b)
+  {
+    pokeSpin(b, a, 0);
+  }
+
+  //! Convert (insert) a LatticeColorVector into a LatticeStaggeredFermion
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source color vector
+   * \param b      Destination fermion
+   */
+  void CvToFerm(const LatticeColorVectorD& a, LatticeFermionD& b)
+  {
+    pokeSpin(b, a, 0);
+  }
+
+
+  //! Insert a LatticeFermion into a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void FermToProp(const LatticeFermionF& a, LatticePropagatorF& b, 
+		  int color_index, int spin_index)
+  {
+    for(int j = 0; j < Ns; ++j)
+    {
+      LatticeColorMatrixF bb = peekSpin(b, j, spin_index);
+      LatticeColorVectorF aa = peekSpin(a, j);
+
+      for(int i = 0; i < Nc; ++i)
+	pokeColor(bb, peekColor(aa, i), i, color_index);
+
+      pokeSpin(b, bb, j, spin_index);
+    }
+  }
+
+  //! Insert a LatticeFermion into a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void FermToProp(const LatticeFermionD& a, LatticePropagatorD& b, 
+		  int color_index, int spin_index)
+  {
+    for(int j = 0; j < Ns; ++j)
+    {
+      LatticeColorMatrixD bb = peekSpin(b, j, spin_index);
+      LatticeColorVectorD aa = peekSpin(a, j);
+
+      for(int i = 0; i < Nc; ++i)
+	pokeColor(bb, peekColor(aa, i), i, color_index);
+
+      pokeSpin(b, bb, j, spin_index);
+    }
+  }
+
+
+  //! Insert a LatticeStaggeredFermion into a LatticeStaggeredPropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   */
+  void FermToProp(const LatticeStaggeredFermionF& a, LatticeStaggeredPropagatorF& b, 
+		  int color_index)
+  {
+    LatticeColorMatrixF bb = peekSpin(b, 0, 0);
+    LatticeColorVectorF aa = peekSpin(a, 0);
 
     for(int i = 0; i < Nc; ++i)
       pokeColor(bb, peekColor(aa, i), i, color_index);
 
-    pokeSpin(b, bb, j, spin_index);
+    pokeSpin(b, bb, 0, 0);
   }
-}
 
-//! Insert a LatticeFermion into a LatticeFermion
-/*!
- * \ingroup ferm
- *
- * \param a      Source fermion
- * \param b      Destination propagator
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void FermToProp(const LatticeFermionD& a, LatticePropagatorD& b, 
-		int color_index, int spin_index)
-{
-  for(int j = 0; j < Ns; ++j)
+  //! Insert a LatticeStaggeredFermion into a LatticeStaggeredPropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   */
+  void FermToProp(const LatticeStaggeredFermionD& a, LatticeStaggeredPropagatorD& b, 
+		  int color_index)
   {
-    LatticeColorMatrixD bb = peekSpin(b, j, spin_index);
-    LatticeColorVectorD aa = peekSpin(a, j);
+    LatticeColorMatrixD bb = peekSpin(b, 0, 0);
+    LatticeColorVectorD aa = peekSpin(a, 0);
 
     for(int i = 0; i < Nc; ++i)
       pokeColor(bb, peekColor(aa, i), i, color_index);
 
-    pokeSpin(b, bb, j, spin_index);
+    pokeSpin(b, bb, 0, 0);
   }
-}
 
 
-//! Extract a LatticeFermion from a LatticePropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source propagator
- * \param b      Destination fermion
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void PropToFerm(const LatticePropagatorF& b, LatticeFermionF& a, 
-		int color_index, int spin_index)
-{
-  for(int j = 0; j < Ns; ++j)
+
+
+  //! Extract a LatticeFermion from a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void PropToFerm(const LatticePropagatorF& b, LatticeFermionF& a, 
+		  int color_index, int spin_index)
   {
-    LatticeColorMatrixF bb = peekSpin(b, j, spin_index);
-    LatticeColorVectorF aa = peekSpin(a, j);
+    for(int j = 0; j < Ns; ++j)
+    {
+      LatticeColorMatrixF bb = peekSpin(b, j, spin_index);
+      LatticeColorVectorF aa = peekSpin(a, j);
+
+      for(int i = 0; i < Nc; ++i)
+	pokeColor(aa, peekColor(bb, i, color_index), i);
+
+      pokeSpin(a, aa, j);
+    }
+  }
+
+  //! Extract a LatticeFermion from a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void PropToFerm(const LatticePropagatorD& b, LatticeFermionD& a, 
+		  int color_index, int spin_index)
+  {
+    for(int j = 0; j < Ns; ++j)
+    {
+      LatticeColorMatrixD bb = peekSpin(b, j, spin_index);
+      LatticeColorVectorD aa = peekSpin(a, j);
+
+      for(int i = 0; i < Nc; ++i)
+	pokeColor(aa, peekColor(bb, i, color_index), i);
+
+      pokeSpin(a, aa, j);
+    }
+  }
+
+
+
+  //! Extract a LatticeStaggeredFermion from a LatticeStaggeredPropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   */
+  void PropToFerm(const LatticeStaggeredPropagatorF& b, LatticeStaggeredFermionF& a, 
+		  int color_index)
+  {
+    LatticeColorMatrixF bb = peekSpin(b, 0, 0);
+    LatticeColorVectorF aa = peekSpin(a, 0);
 
     for(int i = 0; i < Nc; ++i)
       pokeColor(aa, peekColor(bb, i, color_index), i);
 
-    pokeSpin(a, aa, j);
+    pokeSpin(a, aa, 0);
   }
-}
 
-//! Extract a LatticeFermion from a LatticePropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source propagator
- * \param b      Destination fermion
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void PropToFerm(const LatticePropagatorD& b, LatticeFermionD& a, 
-		int color_index, int spin_index)
-{
-  for(int j = 0; j < Ns; ++j)
+  //! Extract a LatticeStaggeredFermion from a LatticeStaggeredPropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   */
+  void PropToFerm(const LatticeStaggeredPropagatorD& b, LatticeStaggeredFermionD& a, 
+		  int color_index)
   {
-    LatticeColorMatrixD bb = peekSpin(b, j, spin_index);
-    LatticeColorVectorD aa = peekSpin(a, j);
+    LatticeColorMatrixD bb = peekSpin(b, 0, 0);
+    LatticeColorVectorD aa = peekSpin(a, 0);
 
     for(int i = 0; i < Nc; ++i)
       pokeColor(aa, peekColor(bb, i, color_index), i);
 
-    pokeSpin(a, aa, j);
+    pokeSpin(a, aa, 0);
   }
-}
-
-
-
-
-//! Insert a LatticeStaggeredFermion into a LatticeStaggeredPropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source fermion
- * \param b      Destination propagator
- * \param color_index  Color index
- *
- */
-void FermToProp(const LatticeStaggeredFermion& a, LatticeStaggeredPropagator& b, 
-		int color_index)
-{
-  const int spin_index = 0 ; 
-  const int j = 0; 
-
-  LatticeColorMatrix bb = peekSpin(b, j, spin_index);
-  LatticeColorVector aa = peekSpin(a, j);
-
-  for(int i = 0; i < Nc; ++i)
-    pokeColor(bb, peekColor(aa, i), i, color_index);
-
-  pokeSpin(b, bb, j, spin_index);
-
 
 }
--- lib/util/ferm/transf.h
+++ lib/util/ferm/transf.h
@@ -1,94 +1,157 @@
 // -*- C++ -*-
-// $Id: transf.h,v 1.5 2004-11-12 10:49:38 mcneile Exp $
+// $Id: transf.h,v 1.6 2004-12-29 22:08:48 edwards Exp $
 
 #ifndef __transf_h__
 #define __transf_h__
 
-//! Convert (insert) a LatticeColorVector into a LatticeFermion
-/*!
- * \ingroup ferm
- *
- * \param a      Source color vector
- * \param b      Destination fermion
- * \param spin_index   Spin index
- */
-void CvToFerm(const LatticeColorVectorF& a, LatticeFermionF& b, 
-	      int spin_index);
+namespace Chroma
+{
+ //! Convert (insert) a LatticeColorVector into a LatticeFermion
+ /*!
+  * \ingroup ferm
+  *
+  * \param a      Source color vector
+  * \param b      Destination fermion
+  * \param spin_index   Spin index
+  */
+  void CvToFerm(const LatticeColorVectorF& a, LatticeFermionF& b, 
+		int spin_index);
 
-//! Convert (insert) a LatticeColorVector into a LatticeFermion
-/*!
- * \ingroup ferm
- *
- * \param a      Source color vector
- * \param b      Destination fermion
- * \param spin_index   Spin index
- */
-void CvToFerm(const LatticeColorVectorD& a, LatticeFermionD& b, 
-	      int spin_index);
-
-
-//! Insert a LatticeFermion into a LatticePropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source fermion
- * \param b      Destination propagator
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void FermToProp(const LatticeFermionF& a, LatticePropagatorF& b, 
-		int color_index, int spin_index);
-
-//! Insert a LatticeFermion into a LatticePropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source fermion
- * \param b      Destination propagator
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void FermToProp(const LatticeFermionD& a, LatticePropagatorD& b, 
-		int color_index, int spin_index);
+  //! Convert (insert) a LatticeColorVector into a LatticeFermion
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source color vector
+   * \param b      Destination fermion
+   * \param spin_index   Spin index
+   */
+  void CvToFerm(const LatticeColorVectorD& a, LatticeFermionD& b, 
+		int spin_index);
 
 
-//! Extract a LatticeFermion from a LatticePropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source propagator
- * \param b      Destination fermion
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void PropToFerm(const LatticePropagatorF& b, LatticeFermionF& a, 
-		int color_index, int spin_index);
 
-//! Extract a LatticeFermion from a LatticePropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source propagator
- * \param b      Destination fermion
- * \param color_index  Color index
- * \param spin_index   Spin index
- */
-void PropToFerm(const LatticePropagatorD& b, LatticeFermionD& a, 
-		int color_index, int spin_index);
+ //! Convert (insert) a LatticeColorVector into a LatticeStaggeredFermion
+ /*!
+  * \ingroup ferm
+  *
+  * \param a      Source color vector
+  * \param b      Destination fermion
+  * \param spin_index   Spin index
+  */
+  void CvToFerm(const LatticeColorVectorF& a, LatticeStaggeredFermionF& b);
+
+  //! Convert (insert) a LatticeColorVector into a LatticeStaggeredFermion
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source color vector
+   * \param b      Destination fermion
+   * \param spin_index   Spin index
+   */
+  void CvToFerm(const LatticeColorVectorD& a, LatticeStaggeredFermionD& b);
 
 
-//! Insert a LatticeStaggeredFermion into a LatticeStaggeredPropagator
-/*!
- * \ingroup ferm
- *
- * \param a      Source fermion
- * \param b      Destination propagator
- * \param color_index  Color index
- *
- */
-void FermToProp(const LatticeStaggeredFermion& a, LatticeStaggeredPropagator& b, 
-		int color_index) ; 
 
+  //! Insert a LatticeFermion into a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void FermToProp(const LatticeFermionF& a, LatticePropagatorF& b, 
+		  int color_index, int spin_index);
+
+  //! Insert a LatticeFermion into a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void FermToProp(const LatticeFermionD& a, LatticePropagatorD& b, 
+		  int color_index, int spin_index);
+
+
+
+  //! Insert a LatticeFermion into a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   */
+  void FermToProp(const LatticeStaggeredFermionF& a, LatticeStaggeredPropagatorF& b, 
+		  int color_index);
+
+  //! Insert a LatticeFermion into a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source fermion
+   * \param b      Destination propagator
+   * \param color_index  Color index
+   */
+  void FermToProp(const LatticeStaggeredFermionD& a, LatticeStaggeredPropagatorD& b, 
+		  int color_index);
+
+
+
+  //! Extract a LatticeFermion from a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void PropToFerm(const LatticePropagatorF& b, LatticeFermionF& a, 
+		  int color_index, int spin_index);
+
+  //! Extract a LatticeFermion from a LatticePropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   * \param spin_index   Spin index
+   */
+  void PropToFerm(const LatticePropagatorD& b, LatticeFermionD& a, 
+		  int color_index, int spin_index);
+
+
+
+  //! Extract a LatticeStaggeredFermion from a LatticeStaggeredPropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   */
+  void PropToFerm(const LatticeStaggeredPropagatorF& b, LatticeStaggeredFermionF& a, 
+		  int color_index);
+
+  //! Extract a LatticeStaggeredFermion from a LatticeStaggeredPropagator
+  /*!
+   * \ingroup ferm
+   *
+   * \param a      Source propagator
+   * \param b      Destination fermion
+   * \param color_index  Color index
+   */
+  void PropToFerm(const LatticeStaggeredPropagatorD& b, LatticeStaggeredFermionD& a, 
+		  int color_index);
+}
+
+using namespace Chroma;
 
 #endif
 
