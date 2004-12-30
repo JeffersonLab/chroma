ae0c9a006e48f30206e2c38cd1418ad17c2cbff9
edwards <edwards>
2004-12-30 01:48:01 +0000

    Prototypes.

---
+++ lib/actions/ferm/qprop/dwf_quarkprop4_w.h
@@ -0,0 +1,39 @@
+// $Id: dwf_quarkprop4_w.h,v 1.6 2004-12-30 01:48:01 edwards Exp $
+/*! \file
+ * \brief Full quark propagator solver for domain wall fermions
+ *
+ * Given a complete propagator as a source, this does all the inversions needed
+ */
+
+#ifndef __dwf_quarkprop4_w_h__
+#define __dwf_quarkprop4_w_h__
+
+#include "fermact.h"
+
+namespace Chroma
+{
+  //! Given a complete propagator as a source, this does all the inversions needed
+  /*! \ingroup qprop
+   *
+   * This routine is actually generic to Domain Wall fermions (Array) fermions
+   *
+   * \param q_sol    quark propagator ( Write )
+   * \param q_src    source ( Read )
+   * \param t_src    time slice of source ( Read )
+   * \param j_decay  direction of decay ( Read )
+   * \param invParam inverter parameters ( Read )
+   * \param ncg_had  number of CG iterations ( Write )
+   */
+  void
+  dwf_quarkProp4(LatticePropagator& q_sol, 
+		 XMLWriter& xml_out,
+		 const LatticePropagator& q_src,
+		 int t_src, int j_decay,
+		 const WilsonTypeFermAct5D<LatticeFermion>& S_f,
+		 Handle<const ConnectState> state,
+		 const InvertParam_t& invParam,
+		 int& ncg_had);
+
+}
+
+#endif
