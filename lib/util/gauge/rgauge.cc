0b319a62ffcd952ed44f938cd6c25da759a12dce
edwards <edwards>
2005-01-12 19:30:43 +0000

    Put into chroma namespace.

--- lib/util/gauge/rgauge.cc
+++ lib/util/gauge/rgauge.cc
@@ -1,5 +1,4 @@
-
-// $Id: rgauge.cc,v 1.5 2004-07-28 02:38:06 edwards Exp $
+// $Id: rgauge.cc,v 1.6 2005-01-12 19:30:43 edwards Exp $
 /*! \file
  *  \brief Do a random gauge transformation on the u fields
  */
@@ -11,63 +10,67 @@
 
 using namespace QDP;
 
-//! Do a random gauge transformation on the u fields
-/*!
- * \ingroup gauge
- *
- * Arguments:
- *
- *  \param u          Gauge field                   (Modify)
- *  \param g          Gauge transformation matrices (Write)
- */
-
-void rgauge(multi1d<LatticeColorMatrix>& u, LatticeColorMatrix& g)
+namespace Chroma
 {
-  START_CODE();
-  
-  // g = exp(A), where A = random traceless antihermitian matrix.
-  
-  // NOTE!!!!: we are using a hack. The twelth_order exponentiation followed
-  //   by a reunitarization should be replace by an exact exponentiation.
-  //   So, the gauge field distriubution is NOT from Haar measure, but
-  //   if someone can think of a really good reason why it should be Haar
-  //   measure, then something can be done about it.
-  
-  gaussian(g);
 
-  taproj(g);
-  expm12(g);
-  reunit(g);
-    
-  for(int mu = 0; mu < Nd; ++mu)
+  //! Do a random gauge transformation on the u fields
+  /*!
+   * \ingroup gauge
+   *
+   * Arguments:
+   *
+   *  \param u          Gauge field                   (Modify)
+   *  \param g          Gauge transformation matrices (Write)
+   */
+
+  void rgauge(multi1d<LatticeColorMatrix>& u, LatticeColorMatrix& g)
   {
-    LatticeColorMatrix u_tmp = g * u[mu];
-    u[mu] = u_tmp * shift(adj(g), FORWARD, mu);
-  }
-    
-  END_CODE();
-}
-
-
-//! Do a random gauge transformation on the u fields
-/*!
- * \ingroup gauge
- *
- * Convenience function: does not return gauge transformation matrices
- *
- * Arguments:
- *
- *  \param u          Gauge field                   (Modify)
- */
-
-void rgauge(multi1d<LatticeColorMatrix>& u)
-{
-  START_CODE();
+    START_CODE();
   
-  LatticeColorMatrix g;
-  rgauge(u, g);
+    // g = exp(A), where A = random traceless antihermitian matrix.
+  
+    // NOTE!!!!: we are using a hack. The twelth_order exponentiation followed
+    //   by a reunitarization should be replace by an exact exponentiation.
+    //   So, the gauge field distriubution is NOT from Haar measure, but
+    //   if someone can think of a really good reason why it should be Haar
+    //   measure, then something can be done about it.
+  
+    gaussian(g);
 
-  END_CODE();
+    taproj(g);
+    expm12(g);
+    reunit(g);
+    
+    for(int mu = 0; mu < Nd; ++mu)
+    {
+      LatticeColorMatrix u_tmp = g * u[mu];
+      u[mu] = u_tmp * shift(adj(g), FORWARD, mu);
+    }
+    
+    END_CODE();
+  }
+
+
+  //! Do a random gauge transformation on the u fields
+  /*!
+   * \ingroup gauge
+   *
+   * Convenience function: does not return gauge transformation matrices
+   *
+   * Arguments:
+   *
+   *  \param u          Gauge field                   (Modify)
+   */
+
+  void rgauge(multi1d<LatticeColorMatrix>& u)
+  {
+    START_CODE();
+  
+    LatticeColorMatrix g;
+    rgauge(u, g);
+
+    END_CODE();
+  }
+
+
 }
-
-
--- lib/util/gauge/rgauge.h
+++ lib/util/gauge/rgauge.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: rgauge.h,v 1.3 2003-12-29 19:52:57 edwards Exp $
+// $Id: rgauge.h,v 1.4 2005-01-12 19:30:43 edwards Exp $
 /*! \file
  *  \brief Do a random gauge transformation on the u fields
  */
@@ -7,30 +7,35 @@
 #ifndef __rgauge_h__
 #define __rgauge_h__
 
-//! Do a random gauge transformation on the u fields
-/*!
- * \ingroup gauge
- *
- * Arguments:
- *
- *  \param u          Gauge field                   (Modify)
- *  \param g          Gauge transformation matrices (Write)
- */
+namespace Chroma
+{
 
-void rgauge(multi1d<LatticeColorMatrix>& u, LatticeColorMatrix& g);
+  //! Do a random gauge transformation on the u fields
+  /*!
+   * \ingroup gauge
+   *
+   * Arguments:
+   *
+   *  \param u          Gauge field                   (Modify)
+   *  \param g          Gauge transformation matrices (Write)
+   */
+
+  void rgauge(multi1d<LatticeColorMatrix>& u, LatticeColorMatrix& g);
 
 
-//! Do a random gauge transformation on the u fields
-/*!
- * \ingroup gauge
- *
- * Convenience function: does not return gauge transformation matrices
- *
- * Arguments:
- *
- *  \param u          Gauge field                   (Modify)
- */
+  //! Do a random gauge transformation on the u fields
+  /*!
+   * \ingroup gauge
+   *
+   * Convenience function: does not return gauge transformation matrices
+   *
+   * Arguments:
+   *
+   *  \param u          Gauge field                   (Modify)
+   */
 
-void rgauge(multi1d<LatticeColorMatrix>& u);
+  void rgauge(multi1d<LatticeColorMatrix>& u);
+
+}
 
 #endif
