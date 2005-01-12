4c9c8bedc9427ed856d46a2c4cee0d83d98f1c63
edwards <edwards>
2005-01-12 19:29:18 +0000

    Gauge force tester.

---
+++ mainprogs/tests/t_gauge_force.cc
@@ -0,0 +1,89 @@
+#include "chroma.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  // Initialise QDP
+  QDP_initialize(&argc, &argv);
+
+  // Setup a small lattice
+  const int nrow_arr[] = {4, 4, 4, 4};
+  multi1d<int> nrow(Nd);
+  nrow=nrow_arr;
+  Layout::setLattSize(nrow);
+  Layout::create();
+
+  multi1d<LatticeColorMatrix> u(Nd);
+  {
+    XMLReader file_xml;
+    XMLReader config_xml;
+    Cfg_t foo; foo.cfg_type=CFG_TYPE_DISORDERED;
+    // Cfg_t foo; foo.cfg_type=CFG_TYPE_SZIN; foo.cfg_file="./CFGIN";
+    gaugeStartup(file_xml, config_xml, u, foo);
+  }
+
+  XMLFileWriter xml_out("./XMLDAT");
+  push(xml_out, "t_gauge_force");
+
+  multi1d<LatticeColorMatrix> p(Nd);
+
+
+  // Get Periodic Gauge Boundaries
+  Handle<GaugeBC> gbc(new PeriodicGaugeBC);
+  Real betaMC = Real(5.7);
+  RectGaugeAct S_g_MC(gbc, betaMC);
+
+  multi1d<LatticeColorMatrix> dsdu_1(Nd);
+  multi1d<LatticeColorMatrix> dsdu_2(Nd);
+
+  dsdu_1 = zero;
+  dsdu_2 = zero;
+
+  Handle<const ConnectState> state1(S_g_MC.createState(u));
+  S_g_MC.dsdu(dsdu_1, state1);
+  Double S1 = S_g_MC.S(state1);
+
+  // Test gauge invariance
+  LatticeColorMatrix g;
+  rgauge(u, g);
+
+  Handle<const ConnectState> state2(S_g_MC.createState(u));
+  S_g_MC.dsdu(dsdu_2, state2);
+  Double S2 = S_g_MC.S(state2);
+
+  push(xml_out, "ForceDiff");
+  write(xml_out, "S1", S1);
+  write(xml_out, "S2", S2);
+  write(xml_out, "S_diff", norm2(S1-S2));
+  pop(xml_out);
+
+  for(int mu=0; mu < Nd; mu++) 
+  { 
+//    taproj(dsdu_1[mu]);
+//    taproj(dsdu_2[mu]);
+
+//    push(xml_out, "dsdu");
+//    write(xml_out, "dsdu_1", dsdu_1[mu]);
+//    write(xml_out, "dsdu_2", dsdu_2[mu]);
+//    pop(xml_out);
+  
+    Double sum_diff=norm2(dsdu_1[mu] - adj(g)*dsdu_2[mu]*g);
+    QDPIO::cout << "Mu = " << mu << " Sum Diff=" << sum_diff << endl;
+
+    push(xml_out, "ForceDiff");
+    write(xml_out, "mu", mu);
+    write(xml_out, "dsdu1_norm", norm2(dsdu_1[mu]));
+    write(xml_out, "dsdu2_norm", norm2(dsdu_2[mu]));
+    write(xml_out, "dsdu_diff", sum_diff);
+    pop(xml_out);
+  }
+
+  pop(xml_out);
+  xml_out.close();
+
+    // Finish
+  QDP_finalize();
+
+  exit(0);
+}
+
