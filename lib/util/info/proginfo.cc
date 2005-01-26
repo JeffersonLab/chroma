b15385fbb6041dc1ee2f11024fe36563b22d3116
zbigniew <zbigniew>
2005-01-26 14:26:50 +0000

    Added a few lines to print the code versions and the run date+time. Using strftime for the latter because I couldn't get the time_put<> facet to work in a stable or portable way.

--- lib/util/info/proginfo.cc
+++ lib/util/info/proginfo.cc
@@ -1,4 +1,4 @@
-//  $Id: proginfo.cc,v 1.4 2005-01-14 18:42:38 edwards Exp $
+//  $Id: proginfo.cc,v 1.5 2005-01-26 14:26:50 zbigniew Exp $
 /*! \file
  *  \brief Print out basic info about this program
  */
@@ -6,30 +6,44 @@
 #include "chromabase.h"
 #include "util/info/printgeom.h"
 #include "util/info/proginfo.h"
+#include <ctime>
+
 
 namespace Chroma {
 
-//! Print out basic info about this program
+//! Print out basic information about this program
 /*!
  * \ingroup info
  *
- * Arguments:
  *
- *  \param xml          The xml stream to write the info
+ *  \param xml          The XML stream to which to write the information.
  */
 
 void proginfo(XMLWriter& xml)
 {
-  START_CODE();
+    START_CODE();
 
-  push(xml,"ProgramInfo");
+    push(xml,"ProgramInfo");
 
-//  write(xml,"run_date",date);
-  printgeom(xml);
+    push(xml,"code_version");
+    write(xml, CHROMA_PACKAGE_NAME, CHROMA_PACKAGE_VERSION);
+    write(xml, QDP_PACKAGE_NAME, QDP_PACKAGE_VERSION);
+    pop(xml);
 
-  pop(xml);
+    std::time_t now;
+    if(std::time(&now)==-1)
+	QDPIO::cerr<<"proginfo: Cannot get the time.\n";
+    std::tm *tp = std::localtime(&now);
 
-  END_CODE();
+    char date[64];
+    std::strftime(date, 63, "%d %b %y %X %Z", tp);
+    write(xml,"run_date", date);
+
+    printgeom(xml);
+
+    pop(xml);
+
+    END_CODE();
 }
 
 }  // end namespace Chroma
