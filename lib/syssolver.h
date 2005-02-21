de894b50e728d67e9ae7cd7a5a7aebdac5fd5c43
edwards <edwards>
2005-02-21 19:23:29 +0000

    Added partial specialization of SystemSolver.

--- lib/syssolver.h
+++ lib/syssolver.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: syssolver.h,v 1.2 2005-01-14 20:13:04 edwards Exp $
+// $Id: syssolver.h,v 1.3 2005-02-21 19:23:29 edwards Exp $
 /*! @file
  * @brief Linear system solvers
  */
@@ -31,6 +31,37 @@ namespace Chroma
      * Should the accuracy be specified here ???
      */
     virtual int operator() (T& psi, const T& chi) const = 0;
+
+    //! Return the subset on which the operator acts
+    virtual const OrderedSubset& subset() const = 0;
+  };
+
+
+
+  //-----------------------------------------------------------------------------------
+  //! Partial specialization of Linear system solvers to arrays
+  /*! @ingroup solvers
+   *
+   * Solves linear systems of equations. The solver may only live on a subset.
+   */
+  template<typename T>
+  class SystemSolver< multi1d<T> >
+  {
+  public:
+    //! Virtual destructor to help with cleanup;
+    virtual ~SystemSolver() {}
+
+    //! Expected length of array index
+    virtual int size() const = 0;
+
+    //! Apply the operator onto a source vector
+    /*! 
+     * Solves   A*psi = chi  or  psi = A^(-1)*chi up to some accuracy.
+     * There is the interesting possibility of generalizing to support PLUS/MINUS
+     *
+     * Should the accuracy be specified here ???
+     */
+    virtual int operator() (multi1d<T>& psi, const multi1d<T>& chi) const = 0;
 
     //! Return the subset on which the operator acts
     virtual const OrderedSubset& subset() const = 0;
