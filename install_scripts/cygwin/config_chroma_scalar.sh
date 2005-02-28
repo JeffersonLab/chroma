d1680316608f5c3027081d12a0d00e5e360e2495
edwards <edwards>
2005-02-28 19:55:09 +0000

    Added gmp args.

--- install_scripts/cygwin/config_chroma_scalar.sh
+++ install_scripts/cygwin/config_chroma_scalar.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
+#!/bin/tcsh
 
-../configure --prefix=/usr/local/share/chroma/scalar --with-qdp=/usr/local/share/qdp++/scalar CXXFLAGS="" --enable-sse-wilson-dslash 
-
-# --enable-sse-dwf-cg
+../configure --prefix=/usr/local/share/chroma/scalar --with-qdp=/usr/local/share/qdp++/scalar CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-gmp
