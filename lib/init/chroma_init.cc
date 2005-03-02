8587cc656f57430b1f7843c509b471f5fcacfdb3
edwards <edwards>
2005-03-02 00:41:49 +0000

    New init format.

--- lib/init/chroma_init.cc
+++ lib/init/chroma_init.cc
@@ -1,53 +1,131 @@
-// $Id: chroma_init.cc,v 1.5 2005-01-24 21:31:32 edwards Exp $
+// $Id: chroma_init.cc,v 1.5 2005/01/24 21:31:32 edwards Exp 
+/*! \file
+ *  \brief Initialization of Chroma
+ */
 
-
-//#include "chroma.h"
 #include "init/chroma_init.h"
 #include "io/xmllog_io.h"
-#include <string>
 
-namespace Chroma { 
+namespace Chroma 
+{
 
-  // WARNING THIS CURRENTLY CAUSES A SEGFAULT IN PROPAGATOR SO I AM 
-  // NOT USING IT. LINKAGE HACK STILL NEEDS TO STAY.
-  // bool ChromaLinkage(void);
+//  namespace ChromaUtil
+//  {
+
+  namespace Private
+  {
+    //! The "DATA" filename
+    string input_filename = "DATA";
+
+    //! The "XMLDAT" filename
+    string output_filename = "XMLDAT";
+
+    //! The "XMLLOG" filename
+    string log_filename = "XMLLOG";
+
+    //! The "current working directory" -- prepended to above if set
+    string cwd = ".";
+
+    //! Has the xml output instance been created?
+    bool xmlOutputP = false;
+
+    //! Has the xml log instance been created?
+    bool xmlLogP = false;
+  }
 
 
-  void ChromaInitialize(int* argc, char ***argv) 
+  //! Get input file name
+  string getXMLInputFileName() {return Private::input_filename;}
+
+  //! Get output file name
+  string getXMLOutputFileName() {return Private::output_filename;}
+
+  //! Get log file name
+  string getXMLLogFileName() {return Private::log_filename;}
+
+  //! Get current working directory
+  string getCWD() {return Private::cwd;}
+
+
+  //! Set input file name
+  void setXMLInputFileName(const string& name) {Private::input_filename = name;}
+
+  //! Set output file name
+  void setXMLOutputFileName(const string& name) {Private::output_filename = name;}
+
+  //! Set output logfile name
+  void setXMLLogFileName(const string& name) {Private::log_filename = name;}
+
+  //! Set current working directory
+  void setCWD(const string& name) {Private::cwd = name;}
+
+
+
+  //! Chroma initialisation routine
+  void initialize(int* argc, char ***argv) 
   {
     QDP_initialize(argc, argv);
     
-
-    bool output_found = false;
-    std::string output_filename;
-    
-    
-    for( int i=0; i < *argc; i++) {
-
+    for(int i=0; i < *argc; i++) 
+    {
       // Get argv[i] into a string
-      std::string argv_i = std::string( (*argv)[i] );
+      string argv_i = string( (*argv)[i] );
 
-      /*
-      // Search for -i or --input-xml
-      if( argv_i == std::string("-i") ) {
-	if( i + 1 < *argc ) {
-	  input_filename = std::string( (*argv)[i+1] );
-	  input_found = true;
+      // Search for -i or --chroma-i
+      if( argv_i == string("-h") || argv_i == string("--help") ) 
+      {
+	QDPIO::cerr << "Usage: " << (*argv)[0] << "  <options>" << endl
+		    << "   -h           help\n"
+		    << "   --help       help\n"
+		    << "   -i           [" << getXMLInputFileName() << "]  xml input file name\n"
+		    << "   --chroma-i   [" << getXMLInputFileName() << "]  xml input file name\n"
+		    << "   -o           [" << getXMLOutputFileName() << "]  xml output file name\n"
+		    << "   --chroma-p   [" << getXMLOutputFileName() << "]  xml output file name\n"
+		    << "   -l           [" << getXMLLogFileName() << "]  xml output file name\n"
+		    << "   --chroma-l   [" << getXMLLogFileName() << "]  xml log file name\n"
+		    << "   -cwd         [" << getCWD() << "]  xml log file name\n"
+		    << "   --chroma-cwd [" << getCWD() << "]  xml log file name\n"
+		    << endl;
+	QDP_abort(0);
+      }
+
+      // Search for -i or --chroma-i
+      if( argv_i == string("-i") || argv_i == string("--chroma-i") ) 
+      {
+	if( i + 1 < *argc ) 
+	{
+	  setXMLInputFileName(string( (*argv)[i+1] ));
 	  // Skip over next
 	  i++;
 	}
-	else {
+	else 
+	{
 	  // i + 1 is too big
 	  QDPIO::cerr << "Error: dangling -i specified. " << endl;
 	  QDP_abort(1);
 	}
       }
-      */
  
-      if( argv_i == std::string("-o") ) {
+      // Search for -o or --chroma-o
+      if( argv_i == string("-o") || argv_i == string("--chroma-o") ) 
+      {
 	if( i + 1 < *argc ) {
-	  output_filename = std::string( (*argv)[i+1] );
-	  output_found = true;
+	  setXMLOutputFileName(string( (*argv)[i+1] ));
+	  // Skip over next
+	  i++;
+	}
+	else {
+	  // i + 1 is too big
+	  QDPIO::cerr << "Error: dangling -o specified. " << endl;
+	  QDP_abort(1);
+	}
+      }
+      
+      // Search for -l or --chroma-l
+      if( argv_i == string("-l") || argv_i == string("--chroma-l") ) 
+      {
+	if( i + 1 < *argc ) {
+	  setXMLLogFileName(string( (*argv)[i+1] ));
 	  // Skip over next
 	  i++;
 	}
@@ -59,112 +137,50 @@ namespace Chroma {
       }
       
     }
-    
-    /*
-    if ( ! input_found ) { 
-      input_filename = "./DATA";
-    }
-    */
-
-    if( ! output_found ) { 
-      output_filename = "./XMLDAT";
-    }
-    
-
-    /*
-    QDPIO::cout << "ChromaInitialize: Reading XML Parameters from : " 
-		<< input_filename << endl;
-		TheXMLInputReader::Instance().open(input_filename);
-    */
-    QDPIO::cout << "ChromaInitialize: Writing XML Log file to     : " 
-		<< output_filename << endl;
-    
-    TheXMLOutputWriter::Instance().open(output_filename);
-
-
-    
-    // Linkage
-    // WARNING THIS CURRENTLY CAUSES A SEGFAULT IN PROPAGATOR SO I AM 
-    // NOT USING IT. LINKAGE HACK STILL NEEDS TO STAY.
-    //    bool foo = ChromaLinkage();
 
     // Init done
   }
 
-  void ChromaFinalize(void) {
-    // Finish
-    // Chroma Finalize Stuff
-    /* TheXMLInputReader::Instance().close(); */
-    TheXMLOutputWriter::Instance().close();
+
+  //! Chroma finalization routine
+  void finalize(void)
+  {
     QDP_finalize();
   }
 
-  void ChromaAbort(int i) { 
-    // Cleanup
-    /*    TheXMLInputReader::Instance().close(); */
-    TheXMLOutputWriter::Instance().flush();
-    TheXMLOutputWriter::Instance().close();
+
+  //! Chroma abort routine
+  void abort(int i) 
+  {
     QDP_abort(i);
   }
 
 
-  //! To insure linking of code, place the registered code flags here
-  /*! This is the bit of code that dictates what fermacts are in use */
-
-  // WARNING THIS CURRENTLY CAUSES A SEGFAULT IN PROPAGATOR SO I AM 
-  // NOT USING IT. LINKAGE HACK STILL NEEDS TO STAY.
-  /*
-  bool ChromaLinkage(void)
+  //! Get xml output instance
+  XMLFileWriter& getXMLOutputInstance()
   {
-    bool foo = true;
-    
-    // Propagator Fermact Actions 
-    // 4D actions
-    foo &= EvenOddPrecWilsonFermActEnv::registered;
-    foo &= UnprecWilsonFermActEnv::registered;
-    foo &= OvlapPartFrac4DFermActEnv::registered;
-    foo &= EvenOddPrecParWilsonFermActEnv::registered;
-    foo &= UnprecParWilsonFermActEnv::registered;
-    foo &= UnprecDWFTransfFermActEnv::registered;
+    if (! Private::xmlOutputP)
+    {
+      TheXMLOutputWriter::Instance().open(Chroma::getXMLOutputFileName());
+      Private::xmlOutputP = true;
+    }
 
-    // 5D actions
-    foo &= EvenOddPrecDWFermActArrayEnv::registered;
-    foo &= UnprecDWFermActArrayEnv::registered;
-    foo &= EvenOddPrecNEFFermActArrayEnv::registered;
-    foo &= UnprecNEFFermActArrayEnv::registered;
-    foo &= UnprecOvlapContFrac5DFermActArrayEnv::registered;
-    foo &= UnprecHTContFrac5DFermActArrayEnv::registered;
-    foo &= EvenOddPrecOvlapContFrac5DFermActArrayEnv::registered;
-    foo &= UnprecOvDWFermActArrayEnv::registered;
-    foo &= EvenOddPrecOvDWFermActArrayEnv::registered;
-    foo &= UnprecOvExtFermActArrayEnv::registered;
-    foo &= UnprecZoloNEFFermActArrayEnv::registered;
-    foo &= EvenOddPrecZoloNEFFermActArrayEnv::registered;
-    foo &= EvenOddPrecKNOFermActArrayEnv::registered;
-    foo &= UnprecDWFTransfFermActEnv::registered;
-    
-    // Gauge Monomials
-    foo &= GaugeMonomialEnv::registered;
-    
-    // 4D Ferm Monomials
-    foo &= UnprecTwoFlavorWilsonTypeFermMonomialEnv::registered;
-    foo &= EvenOddPrecTwoFlavorWilsonTypeFermMonomialEnv::registered;
-    
-    // 5D Ferm Monomials
-    foo &= UnprecTwoFlavorWilsonTypeFermMonomial5DEnv::registered;
-    foo &= EvenOddPrecTwoFlavorWilsonTypeFermMonomial5DEnv::registered;
-    
-    // MD Integrators
-    foo &= LatColMatPQPLeapfrogIntegratorEnv::registered;
-    
-    // Chrono predictor
-    foo &= ZeroGuess4DChronoPredictorEnv::registered;
-    foo &= ZeroGuess5DChronoPredictorEnv::registered;
-    foo &= LastSolution4DChronoPredictorEnv::registered;  
-    foo &= LastSolution5DChronoPredictorEnv::registered;
-    
-    return foo;
+    return TheXMLOutputWriter::Instance();
   }
-  */
+  
+  //! Get xml log instance
+  XMLFileWriter& getXMLLogInstance()
+  {
+    if (! Private::xmlLogP)
+    {
+      TheXMLLogWriter::Instance().open(Chroma::getXMLLogFileName());
+      Private::xmlLogP = true;
+    }
 
-};
+    return TheXMLLogWriter::Instance();
+  }
+  
+  
+
+//  }
+}
--- lib/init/chroma_init.h
+++ lib/init/chroma_init.h
@@ -1,20 +1,61 @@
-// $Id: chroma_init.h,v 1.3 2005-01-17 21:08:29 edwards Exp $
+// -*- C++ -*-
+// $Id: chroma_init.h,v 1.4 2005-03-02 00:41:49 edwards Exp $
+/*! \file
+ *  \brief Initialization of Chroma
+ */
 
 #ifndef __CHROMA_INIT_H__
 #define __CHROMA_INIT_H__
 
 #include "chromabase.h"
 
-namespace Chroma {
-
+namespace Chroma 
+{
+  //! Namespace holding basic control functions over Chroma
+//  namespace ChromaUtil
+//  {
   //! Chroma initialisation routine
-  void ChromaInitialize(int* argc, char ***argv);
+  void initialize(int* argc, char ***argv);
 
   //! Chroma finalization routine
-  void ChromaFinalize(void);
+  void finalize(void);
 
   //! Chroma abort routine
-  void ChromaAbort(int i);
+  void abort(int i);
+
+  //! Get input file name
+  string getXMLInputFileName();
+
+  //! Get output file name
+  string getXMLOutputFileName();
+
+  //! Get output logfile name
+  string getXMLLogFileName();
+
+  //! Get current working directory
+  string getCWD();
+
+
+  //! Set input file name
+  void setXMLInputFileName(const string&);
+
+  //! Set output file name
+  void setXMLOutputFileName(const string&);
+
+  //! Set output logfile name
+  void setXMLLogFileName(const string&);
+
+  //! Set current working directory
+  void setCWD(const string&);
+
+
+  //! Get xml output instance
+  XMLFileWriter& getXMLOutputInstance();
+  
+  //! Get xml log instance
+  XMLFileWriter& getXMLLogInstance();
+  
+//  }
 
 }; // End namespace Chroma
 
