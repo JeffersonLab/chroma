eccd5ffee6da96a555ea72b6594d1367e4fa6557
edwards <edwards>
2005-03-09 19:56:53 +0000

    Added dwf cg

--- install_scripts/jlab/config_chroma_parscalar-gigE.sh
+++ install_scripts/jlab/config_chroma_parscalar-gigE.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../configure --prefix=/usr/local/chroma/parscalar-gigE -with-qdp=/usr/local/qdp++/parscalar-gigE CXXFLAGS=-static --enable-sse-wilson-dslash  --enable-gmp  LIBS="-lgmp"
+../configure --prefix=/usr/local/chroma/parscalar-gigE -with-qdp=/usr/local/qdp++/parscalar-gigE CXXFLAGS=-static --enable-sse-wilson-dslash  --enable-sse-dwf-cg  --enable-gmp  LIBS="-lgmp"
