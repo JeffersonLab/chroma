2a317bcae5edc891826eee42cade75e1b3428180
edwards <edwards>
2005-04-07 03:28:38 +0000

    Added include of sftmom.h

--- lib/meas/hadron/formfac_w.h
+++ lib/meas/hadron/formfac_w.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: formfac_w.h,v 1.12 2005-01-14 18:42:36 edwards Exp $
+// $Id: formfac_w.h,v 1.13 2005-04-07 03:28:38 edwards Exp $
 /*! \file
  *  \brief Form-factors 
  *
@@ -9,69 +9,72 @@
 #ifndef __formfac_h__
 #define __formfac_h__
 
-namespace Chroma {
+#include "util/ft/sftmom.h"
 
-/*
- * Structures for hadron parts
- *
- * \ingroup hadron
- *
- * @{
- */
-struct FormFac_momenta_t
+namespace Chroma 
 {
-  int              magic;     // magic number for sanity checks
-  multi1d<int>     inser_mom;
-  multi1d<Complex> local_current;
-  multi1d<Complex> nonlocal_current;
-};
 
-struct FormFac_insertion_t
-{
-  int              gamma_value;
-  multi1d<FormFac_momenta_t> momenta;
-};
+ /*
+  * Structures for hadron parts
+  *
+  * \ingroup hadron
+  *
+  * @{
+  */
+  struct FormFac_momenta_t
+  {
+    int              magic;     // magic number for sanity checks
+    multi1d<int>     inser_mom;
+    multi1d<Complex> local_current;
+    multi1d<Complex> nonlocal_current;
+  };
 
-struct FormFac_insertions_t
-{
-  int  output_version;   // Unique id for each output version of the structures
-  multi1d<FormFac_insertion_t>  formFac;
-};
+  struct FormFac_insertion_t
+  {
+    int              gamma_value;
+    multi1d<FormFac_momenta_t> momenta;
+  };
 
-
-// Readers and writers
-void read(BinaryReader& bin, FormFac_momenta_t& mom);
-void read(BinaryReader& bin, FormFac_insertion_t& mes);
-void read(BinaryReader& bin, FormFac_insertions_t& form);
-void write(BinaryWriter& bin, const FormFac_momenta_t& mom);
-void write(BinaryWriter& bin, const FormFac_insertion_t& mes);
-void write(BinaryWriter& bin, const FormFac_insertions_t& form);
-
-
-/*! @} */  // end of group hadron
+  struct FormFac_insertions_t
+  {
+    int  output_version;   // Unique id for each output version of the structures
+    multi1d<FormFac_insertion_t>  formFac;
+  };
 
 
+  // Readers and writers
+  void read(BinaryReader& bin, FormFac_momenta_t& mom);
+  void read(BinaryReader& bin, FormFac_insertion_t& mes);
+  void read(BinaryReader& bin, FormFac_insertions_t& form);
+  void write(BinaryWriter& bin, const FormFac_momenta_t& mom);
+  void write(BinaryWriter& bin, const FormFac_insertion_t& mes);
+  void write(BinaryWriter& bin, const FormFac_insertions_t& form);
 
-//! Compute contractions for current insertion 3-point functions.
-/*!
- * \ingroup hadron
- *
- * This routine is specific to Wilson fermions!
- *
- * \param form               structures holding formfactors ( Write )
- * \param u                  gauge fields (used for non-local currents) ( Read )
- * \param quark_propagator   quark propagator ( Read )
- * \param seq_quark_prop     sequential quark propagator ( Read )
- * \param phases             fourier transform phase factors ( Read )
- * \param t0                 cartesian coordinates of the source ( Read )
- */
 
-void FormFac(FormFac_insertions_t& form,
-	     const multi1d<LatticeColorMatrix>& u,
-             const LatticePropagator& quark_propagator,
-             const LatticePropagator& seq_quark_prop, 
-             const SftMom& phases,
-             int t0);
+  /*! @} */  // end of group hadron
+
+
+
+  //! Compute contractions for current insertion 3-point functions.
+  /*!
+   * \ingroup hadron
+   *
+   * This routine is specific to Wilson fermions!
+   *
+   * \param form               structures holding formfactors ( Write )
+   * \param u                  gauge fields (used for non-local currents) ( Read )
+   * \param quark_propagator   quark propagator ( Read )
+   * \param seq_quark_prop     sequential quark propagator ( Read )
+   * \param phases             fourier transform phase factors ( Read )
+   * \param t0                 cartesian coordinates of the source ( Read )
+   */
+
+  void FormFac(FormFac_insertions_t& form,
+	       const multi1d<LatticeColorMatrix>& u,
+	       const LatticePropagator& quark_propagator,
+	       const LatticePropagator& seq_quark_prop, 
+	       const SftMom& phases,
+	       int t0);
 
 }  // end namespace Chroma
 
