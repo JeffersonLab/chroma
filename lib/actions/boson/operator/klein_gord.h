76df6d3700d6287694d389e9348764b5a7f3d279
edwards <edwards>
2005-04-11 02:48:11 +0000

    Added doxygen comments.

--- lib/actions/boson/operator/klein_gord.h
+++ lib/actions/boson/operator/klein_gord.h
@@ -1,16 +1,24 @@
 // -*- C++ -*-
-// $Id: klein_gord.h,v 1.4 2005-01-14 20:13:04 edwards Exp $
+// $Id: klein_gord.h,v 1.5 2005-04-11 02:48:11 edwards Exp $
+
+/*! \file
+ * \brief Klein-Gordon operator
+ */
 
 #ifndef KLEIN_GORD_INCLUDE
 #define KLEIN_GORD_INCLUDE
 
 namespace Chroma
 {
+  //! Klein-Gordon operator
+  /*! @ingroup boson */
   void klein_gord(const multi1d<LatticeColorMatrix>& u, 
 		  const LatticeColorVector& psi, 
 		  LatticeColorVector& chi, 
 		  const Real& mass_sq, int j_decay);
 
+  //! Klein-Gordon operator
+  /*! @ingroup boson */
   void klein_gord(const multi1d<LatticeColorMatrix>& u, 
 		  const LatticePropagator& psi, 
 		  LatticePropagator& chi, 
