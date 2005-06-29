e6f2a6aa3ec4b173cb63b4d094be67a02a360657
edwards <edwards>
2005-06-29 19:27:09 +0000

    Turned off public/private.

--- lib/update/heatbath/hb_params.h
+++ lib/update/heatbath/hb_params.h
@@ -1,32 +1,47 @@
+// -*- C++ -*-
+// $Id: hb_params.h,v 1.3 2005-06-29 19:27:09 edwards Exp $
+
+/*! \file
+ * \brief Parameters for heat-bath
+ */
+
 #ifndef HB_PARAMS_H
 #define HB_PARAMS_H
 
-namespace Chroma {
-
-class HBParams 
+namespace Chroma 
 {
-	public:
-		HBParams(int, double, double, bool);
-		int nmax() { return NmaxHB; }
-		Double beta() { return BetaMC; }
-		Double xi() { return XiBare; }
-		Double xi2() { return xi02; }
-		bool aniso() {return AnisoP; }
+
+  //! Heat-bath params
+  /*! \ingroup heatbath */
+  struct HBParams 
+  {
+    int nmax() { return NmaxHB; }
+    Double beta() { return BetaMC; }
+    Double xi() { return xi_0; }
+    Double xi2() { return xi_0*xi_0; }
+    bool aniso() {return anisoP; }
 		
-	private:
-		/**************************************************
-		 * number of maximum HB tries for Creutz or KP a_0, 
-		 * negative or zero value - update every single link
-		 * (try infinitely long)
-		 **************************************************/
-		int NmaxHB;
-		// MC SU(N) Beta
-		Double BetaMC;
-		//the bare anisotropy
-		Double XiBare;
-		Double xi02;
-		bool AnisoP;
-};
+    /**************************************************
+     * number of maximum HB tries for Creutz or KP a_0, 
+     * negative or zero value - update every single link
+     * (try infinitely long)
+     **************************************************/
+    int NmaxHB;
+    // MC SU(N) Beta
+    Double BetaMC;
+    //the bare anisotropy
+    Double xi_0;
+    int  t_dir;
+    int  nOver;
+    bool anisoP;
+  };
+
+  
+  //! Reader
+  void read(XMLReader& xml, const std::string& path, HBParams& p);
+
+  //! Writer
+  void write(XMLWriter& xml, const std::string& path, const HBParams& p);
 
 }  // end namespace Chroma
 
