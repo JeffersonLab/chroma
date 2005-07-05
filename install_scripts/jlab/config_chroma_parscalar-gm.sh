28ce671d22288a751d1f34d8c3216fa6aaa3bcdc
edwards <edwards>
2005-07-05 21:28:03 +0000

    Changed enable-gmp to with-gmp=/usr

--- install_scripts/jlab/config_chroma_parscalar-gm.sh
+++ install_scripts/jlab/config_chroma_parscalar-gm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../configure --prefix=/usr/local/chroma/parscalar-gm --with-qdp=/usr/local/qdp++/parscalar-gm CXXFLAGS="-static" --enable-sse-wilson-dslash  --enable-opt-cfz-linop  --enable-sse-dwf-cg  --enable-gmp  LIBS="-lgmp"
+../configure --prefix=/usr/local/chroma/parscalar-gm --with-qdp=/usr/local/qdp++/parscalar-gm CXXFLAGS="-static" --enable-sse-wilson-dslash  --enable-opt-cfz-linop  --enable-sse-dwf-cg  --with-gmp=/usr
 
--- install_scripts/jlab/config_chroma_scalar.sh
+++ install_scripts/jlab/config_chroma_scalar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../configure --prefix=/usr/local/chroma/scalar --with-qdp=/usr/local/qdp++/scalar CXXFLAGS="" --enable-sse-wilson-dslash --enable-opt-cfz-linop --enable-gmp LIBS="-lgmp"
+../configure --prefix=/usr/local/chroma/scalar --with-qdp=/usr/local/qdp++/scalar CXXFLAGS="" --enable-sse-wilson-dslash --enable-opt-cfz-linop --with-gmp=/usr
 
