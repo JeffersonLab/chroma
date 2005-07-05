c9c67e98fdbaefaead062081d21c78c7f4a6ba1b
edwards <edwards>
2005-07-05 21:33:19 +0000

    Changed enable-gmp to with-gmp=path

--- install_scripts/cygwin/config_chroma_scalar.sh
+++ install_scripts/cygwin/config_chroma_scalar.sh
@@ -1,3 +1,4 @@
 #!/bin/tcsh
 
-../configure --prefix=/usr/local/share/chroma/scalar --with-qdp=/usr/local/share/qdp++/scalar CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-gmp
+../configure --prefix=/usr/local/share/chroma/scalar --with-qdp=/usr/local/share/qdp++/scalar CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr --enable-opt-cfz-linop
+
