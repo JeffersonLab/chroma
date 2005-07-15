9115b29b8e98ba88ffb16be57fe478a5da4a6dc8
mcneile <mcneile>
2005-07-15 16:48:05 +0000

    Added a protoptype for the correlator that is consistent with one of the MILC operators.

--- lib/meas/hadron/baryon_s.h
+++ lib/meas/hadron/baryon_s.h
@@ -20,6 +20,11 @@ void baryon_s(LatticeStaggeredPropagator & quark_propagator_in,
 		int j_decay, int bc_spec) ;
 
 
+  void baryon_local_s(LatticeStaggeredPropagator & quark_propagator_in, 
+		multi1d<Complex> & barprop,
+		multi1d<int> & t_source,
+		      int j_decay, int bc_spec) ;
+
 }  // end namespace Chroma
 
 #endif
