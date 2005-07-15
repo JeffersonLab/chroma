f85b8a7edbd5abd9472cfca01e5456c88894a095
mcneile <mcneile>
2005-07-15 16:42:20 +0000

    I have corrected the code that computes the baryon correlator with fuzzed staggered quarks. This code could be speeded up, but on a 20**3x64 lattice, it only took 30 seconds for oner call, so I can't be bothered.

--- lib/meas/hadron/baryon_s.cc
+++ lib/meas/hadron/baryon_s.cc
@@ -1,5 +1,5 @@
 /* + */
-/* $Id: baryon_s.cc,v 1.5 2005-06-29 12:43:38 mcneile Exp $ ($Date: 2005-06-29 12:43:38 $) */
+/* $Id: baryon_s.cc,v 1.6 2005-07-15 16:42:20 mcneile Exp $ ($Date: 2005-07-15 16:42:20 $) */
 
 /* This routine is specific to staggered fermions! */
 
@@ -183,34 +183,21 @@ namespace Chroma {
 
   }
 
-  //
-  //  Version needed for smearing
-  //
-  //
-  //
 
-
-  void baryon_s(
-		LatticeStaggeredPropagator & quark_propagator_in_a, 
-		LatticeStaggeredPropagator & quark_propagator_in_b, 
-		LatticeStaggeredPropagator & quark_propagator_in_c, 
+  void baryon_local_s(LatticeStaggeredPropagator & quark_propagator_in, 
 		multi1d<Complex> & barprop,
 		multi1d<int> & t_source,
 		int j_decay, int bc_spec)
   { 
-    LatticeColorMatrix  quark_propagator_a;
-    LatticeColorMatrix  quark_propagator_b;
-    LatticeColorMatrix  quark_propagator_c;
+    LatticeColorMatrix  quark_propagator;
 
-    quark_propagator_a =
-      peekSpin(quark_propagator_in_a,0,0);
-    quark_propagator_b =
-      peekSpin(quark_propagator_in_b,0,0);
-    quark_propagator_c =
-      peekSpin(quark_propagator_in_c,0,0);
+    quark_propagator =
+      peekSpin(quark_propagator_in,0,0);
 
 
     LatticeComplex b_prop;
+    LatticeComplex b_prop_local;
+
     LatticeComplex uu_quark;
 
     LatticeComplex quark_cpt_A ; 
@@ -292,14 +279,14 @@ namespace Chroma {
 
 	    /* Build the u-u "diquark" [only direct term (23-->23) is needed!] */
 
-	    quark_cpt_A = peekColor(quark_propagator_a,cf_2,ci_2 );
-	    quark_cpt_B = peekColor(quark_propagator_b,cf_3,ci_3);
+	    quark_cpt_A = peekColor(quark_propagator,cf_2,ci_2 );
+	    quark_cpt_B = peekColor(quark_propagator,cf_3,ci_3);
 
 	    uu_quark =  quark_cpt_A * quark_cpt_B ;
 
 	    /* Finally tie the u-u "diquark" to the d-quark (1) to form the baryon */
 
-	    quark_cpt_A = peekColor(quark_propagator_c,cf_1,ci_1);
+	    quark_cpt_A = peekColor(quark_propagator,cf_1,ci_1);
 	    switch(cc)
 	    {
 	    case +1:
@@ -315,8 +302,18 @@ namespace Chroma {
 	}      /* end sum sink diquark colour (cf_1, cf_2) */
     }        /* end sum second sink u-quark colour (cf_3) */
 
+
+    // now only sum over part of the hypercube
+    LatticeBoolean ltest(true) ;
+
+    for(int m = 0; m < Nd; ++m)
+      if ( m != j_decay )
+        ltest &= (  (Layout::latticeCoordinate(m) % 2) == 0);
+
+    b_prop_local = where(ltest, b_prop, LatticeComplex(zero));
+
     /* Project on zero momentum: Do a slice-wise sum. */
-    hsum = sumMulti(b_prop, timeslice);
+    hsum = sumMulti(b_prop_local, timeslice);
 
     for(t = 0;t  < ( length); ++t )
     {
@@ -335,16 +332,289 @@ namespace Chroma {
   }
 
 
+  void lower_contract(LatticeComplex & b_prop,
+		      const LatticeColorMatrix & quark_propagator_a,
+		      const LatticeColorMatrix & quark_propagator_b,
+		      const LatticeColorMatrix & quark_propagator_c,
+		      int cf_1, int ci_1,
+		      int cf_2, int ci_2,
+		      int cf_3, int ci_3,
+		      int sgn_epsilon,
+		      int wick_sign 
+		      ) ;
+
+
+
+  //
+  //  Version needed for smearing
+  //
+  //
+  //
+
+
+  void baryon_s(
+		LatticeStaggeredPropagator & quark_propagator_in_a, 
+		LatticeStaggeredPropagator & quark_propagator_in_b, 
+		LatticeStaggeredPropagator & quark_propagator_in_c, 
+		multi1d<Complex> & barprop,
+		multi1d<int> & t_source,
+		int j_decay, int bc_spec)
+  { 
+    LatticeColorMatrix  quark_propagator_a;
+    LatticeColorMatrix  quark_propagator_b;
+    LatticeColorMatrix  quark_propagator_c;
+
+    quark_propagator_a =
+      peekSpin(quark_propagator_in_a,0,0);
+    quark_propagator_b =
+      peekSpin(quark_propagator_in_b,0,0);
+    quark_propagator_c =
+      peekSpin(quark_propagator_in_c,0,0);
+
+
+    LatticeComplex b_prop;
+    LatticeComplex b_prop_local;
+
+    Complex cdummy;
+    int t0;
+    int t;
+    int t_eff;
+    int c;
+    int cc;
+    int tmp;
+
+    /* Loop counters */
+    int ci_1;
+    int ci_2;
+    int ci_3;
+    int cf_1;
+    int cf_2;
+    int cf_3;
+
+    StopWatch swatch;
+    swatch.start();
+
+
+    /*# 3 dimensional totally antisymmetric tensor */
+
+    int antisym_tensor[3][3][3] ; 
+    for(int ii=0 ; ii < 3 ; ++ii)
+      for(int jj=0 ; jj < 3 ; ++jj)
+	for(int kk=0 ; kk < 3 ; ++kk)
+	  antisym_tensor[ii][jj][kk] = 0 ; 
+
+    antisym_tensor[2][1][0] =  1;
+    antisym_tensor[1][2][0] = -1;
+    antisym_tensor[2][0][1] = -1;
+    antisym_tensor[0][2][1] =  1;
+    antisym_tensor[1][0][2] =  1;
+    antisym_tensor[0][1][2] = -1;
+
+
+
+
+    // --- pasted from mesons_w ---
+    // Create the time-slice set
+    UnorderedSet timeslice;
+    timeslice.make(TimeSliceFunc(j_decay));
+
+    // Length of lattice in j_decay direction
+    int length = timeslice.numSubsets();
+
+
+    // not sure why this had to be Dcomplex
+    multi1d<DComplex> hsum(length);
+
+    t0 = t_source[j_decay]; 	     /* Note j_decay = 0 is not permitted! */
+
+
+    /* Set baryon propagator to zero */
+    b_prop = 0;
+
+    ci_3 = 2;
+    ci_1 = 0;
+    ci_2 = 1;
+    c = 1;
+
+    int total = 0 ;
+    for(ci_3= 0;ci_3 < Nc; ++ci_3)
+      for(ci_1= 0;ci_1 < Nc; ++ci_1)
+	for(ci_2= 0;ci_2 < Nc; ++ci_2)
+	  {
+
+	    /* Sum over second sink u-quark colour */
+	    for(cf_3 = 0;cf_3  < ( Nc); ++cf_3 )
+	      {
+		for(cf_1 = 0;cf_1  < ( Nc); ++cf_1 )
+		  for(cf_2 = 0;cf_2  < ( Nc); ++cf_2 )
+		    {
+		      tmp = 
+			antisym_tensor[cf_3][cf_2][cf_1] *
+			antisym_tensor[ci_3][ci_2][ci_1];
+
+		      if(tmp != 0)            /* otherwise no contribution! */
+			{
+			  ++total ; 
+			  
+			  int sgn_epsilon = c * tmp;
+			  int wick_sign = 1 ; 
+
+			  lower_contract(b_prop, 
+					 quark_propagator_a,
+					 quark_propagator_b,
+					 quark_propagator_c,
+					 cf_1, ci_1,
+					 cf_2, ci_2,
+					 cf_3, ci_3,
+					 sgn_epsilon,1); 
+
+			  lower_contract(b_prop, 
+					 quark_propagator_a,
+					 quark_propagator_b,
+					 quark_propagator_c,
+					 cf_1, ci_1,
+					 cf_2, ci_3,
+					 cf_3, ci_2,
+					 sgn_epsilon,-1); 
+
+
+			  lower_contract(b_prop, 
+					 quark_propagator_a,
+					 quark_propagator_b,
+					 quark_propagator_c,
+					 cf_1, ci_2,
+					 cf_2, ci_3,
+					 cf_3, ci_1,
+					 sgn_epsilon,1); 
+
+			  lower_contract(b_prop, 
+					 quark_propagator_a,
+					 quark_propagator_b,
+					 quark_propagator_c,
+					 cf_1, ci_2,
+					 cf_2, ci_1,
+					 cf_3, ci_3,
+					 sgn_epsilon,-1); 
+
+			  lower_contract(b_prop, 
+					 quark_propagator_a,
+					 quark_propagator_b,
+					 quark_propagator_c,
+					 cf_1, ci_3,
+					 cf_2, ci_1,
+					 cf_3, ci_2,
+					 sgn_epsilon,1); 
+
+			  lower_contract(b_prop, 
+					 quark_propagator_a,
+					 quark_propagator_b,
+					 quark_propagator_c,
+					 cf_1, ci_3,
+					 cf_2, ci_2,
+					 cf_3, ci_1,
+					 sgn_epsilon,-1); 
+
+
+			}    /* end if cc (sink diquark colour) */
+		    }      /* end sum sink diquark colour (cf_1, cf_2) */
+	      }        /* end sum second sink u-quark colour (cf_3) */
+
+	  } // end loop over ci_1, ci_2, ci_3
+
+    cout << "Number of Wick contractions " << total << endl ; 
+
+    // only sum over part of the origin of each hypercube
+    LatticeBoolean ltest(true) ;
+
+    for(int m = 0; m < Nd; ++m)
+      if ( m != j_decay )
+        ltest &= (  (Layout::latticeCoordinate(m) % 2) == 0);
+
+    b_prop_local = where(ltest, b_prop, LatticeComplex(zero));
+
+    /* Project on zero momentum: Do a slice-wise sum. */
+    hsum = sumMulti(b_prop_local, timeslice);
+
+    for(t = 0;t  < ( length); ++t )
+    {
+      t_eff = (t - t0 + length) % length;
+
+      if ( bc_spec < 0 && (t_eff+t0) >= length)
+      {
+	barprop[t_eff] = -hsum[t] ; 
+      }
+      else
+	barprop[t_eff] = hsum[t];
+    }
+
+
+
+    swatch.stop();
+    double time_in_sec  = swatch.getTimeInSeconds();
+
+    QDPIO::cout << "Baryon_s took " << time_in_sec
+		<< " secs" << endl;
+
+
+
+  }
+
+
 
   //
   //  Some utility routines for the baryon code
   //  These may need to be moved someplace else soon.
   //
 
+  //
+  //  This is meant to be a slow but safe 
+  //  routine for the Wick contractions.
+  //
+  //
+
+  void lower_contract(LatticeComplex & b_prop,
+		      const LatticeColorMatrix & quark_propagator_a,
+		      const LatticeColorMatrix & quark_propagator_b,
+		      const LatticeColorMatrix & quark_propagator_c,
+		      int cf_1, int ci_1,
+		      int cf_2, int ci_2,
+		      int cf_3, int ci_3,
+		      int sgn_epsilon,
+		      int wick_sign 
+		      )
+  {
+    
+    LatticeComplex uu_quark;
+
+    LatticeComplex quark_cpt_A ; 
+    LatticeComplex quark_cpt_B ;
+
+
+    quark_cpt_A = peekColor(quark_propagator_a,cf_2,ci_2 );
+    quark_cpt_B = peekColor(quark_propagator_b,cf_3,ci_3);
+
+    uu_quark =  quark_cpt_A * quark_cpt_B ;
+
+    /* Finally tie the u-u "diquark" to the d-quark (1) to form the baryon */
+
+    quark_cpt_A = peekColor(quark_propagator_c,cf_1,ci_1);
+
+    int cc = sgn_epsilon * wick_sign  ; 
+
+    switch(cc)
+      {
+      case +1:
+	b_prop +=  quark_cpt_A * uu_quark;
+	break;
+      case -1:
+	b_prop -=  quark_cpt_A * uu_quark;
+	break;
+      }
 
 
 
 
+  }
 
 
 }  // end namespace Chroma
