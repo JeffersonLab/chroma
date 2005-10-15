15d5e83e4f7150cb9f64d61533cc61ef527edc12
mcneile <mcneile>
2005-10-15 09:35:24 +0000

    Script and README written by: zbigniew sroczynski

---
+++ scripts/README_buildtest.sh
@@ -0,0 +1,59 @@
+$Id: README_buildtest.sh,v 1.1 2005-10-15 09:35:24 mcneile Exp $
+Original author: Zbigniew Sroczynski
+
+Check the build of the modules QMP, QDP++ and Chroma.
+
+The script takes each specified module and performs the following operations:
+
+1. Update the source from the CVS repository (cvs update)
+2. Build the library (gmake)
+3. Compile and link the test programs (gmake check)
+4. Install the library (gmake install)
+5. Compile an link the test programs against the installed module
+
+Each of  modules, has its own directory.
+In each of these is 
+
+- a subdirectory which is the checked-out CVS module
+e.g. buildtest/qmp/qmp/
+
+- a subdirectory for each type of build
+e.g. buildtest/qmp/mpich/
+
+To work automatically, the script uses anonymous pserver access to the CVS 
+server.  This works if the user has previously performed a cvs login and the
+~/.cvspass file still exists. 
+The CVS module should be checked out, because the script uses cvs update.
+
+In the build-type directories is a directory called build, e.g.
+buildtest/qmp/mpich/build/
+This contains a script, configure.sh, which defines the type of
+build. The library is configured and built in this directory.
+
+For each build, the built library is installed in its own subdirectory. These
+installation directories are defined and created by configure.sh
+At the moment they are in a directory called install/
+next to the build/ directory, e.g.  buildtest/qmp/mpich/install/
+
+The results of each operation are logged to a file in subdirectories of the 
+logs directory corresponding to the module and build, 
+e.g. buildtest/logs/qmp/mpich/
+These directories are created by the script if need be.
+
+When an operation succeeds an empty email is sent to a list of addresses.
+If any operation fails, the log file is emailed.
+
+
+The builds to be done are identified by looking for the configure.sh files 
+in subdirectories. This behaviour is encapsulated in the function builddirs() 
+so you can change it if you want.
+
+The location of the installation directories is set in the configure.sh 
+scripts; it has to be known there for the --prefix argument. 
+This is set to be an install/ directory with build subdirectories, e.g.
+buildtest/qmp/mpich/install/
+These directories are created by configure.sh if need be. The location of the 
+installation directories is arbitrary, but remember that installation location
+of some libraries needs to be known to configure others, e.g. chroma depends 
+on qdp++. This is all encapsulated in the configure.sh files if you want to 
+change the set-up.
---
+++ scripts/build.sh
@@ -0,0 +1,161 @@
+#! /usr/local/bin/bash
+# $Id: build.sh,v 1.1 2005-10-15 09:35:24 mcneile Exp $
+#
+#  Original author: Zbigniew Sroczynski
+#  See README_buildtest.sh for more information.
+#
+
+cd `dirname $0`
+
+checkdir=$PWD
+
+logdir=$checkdir/logs
+
+modules="qmp qdp++ chroma"
+
+
+# Send mail to these addresses
+ 
+mailto="mcneile@amtp.liv.ac.uk"
+
+# Send mail if something goes wrong
+
+failmail(){
+    cat $logfile | mail -s "buildtest: $module $build $1 failed" $mailto &> /dev/null
+}
+
+# Send mail if something goes right
+
+successmail(){
+    mail -s "buildtest: $module $build $1 succeeded" $mailto &> /dev/null <<EOF
+$logfile
+EOF
+}
+
+# Identify the subdirectories for the different types of build
+
+builddirs(){
+    ls $moduledir/*/build/configure.sh | sed -e s,$moduledir/,, -e s,/build/configure.sh,,
+}
+
+
+perform_action(){
+
+    logfile=$logdir/$module/$build/${action_name}_$now
+    uname -a > $logfile
+    echo '------------------------' >> $logfile
+
+    $action >> $logfile 2>&1
+    local status=$?
+
+    if [ $status -ne 0 ]
+    then
+    	failmail $action_name 
+    else
+    	successmail $action_name 
+    fi 
+
+    gzip $logfile
+    return $status 
+}
+
+for module in $modules
+do
+
+    moduledir=$checkdir/$module
+
+    # Check out new code
+
+    srcdir=$moduledir/$module
+
+    cd $srcdir
+
+    now=`date +"%F_%H.%M_%Z"`
+    if ! [ -d $logdir/${module} ] 
+    then	
+	mkdir -p $logdir/${module}
+    fi
+
+    action_name="update"
+    # Assume that the module is already checked out and the .cvspass file 
+    # already exists	
+    action="cvs -d :pserver:anonymous@cvs.jlab.org:/group/lattice/cvsroot update -d -C"
+    build=""
+    perform_action
+    [ $? -eq 0 ] || continue
+    	
+
+    # Build the code
+
+    for build in `builddirs`
+    do
+
+        # Configure
+
+        builddir=$moduledir/$build/build
+        cd $builddir
+
+        if ! [ -d $logdir/$module/$build ] 
+	then	
+	    mkdir -p $logdir/$module/$build
+        fi
+
+	action_name="configure"
+	action="sh configure.sh"
+	perform_action
+	[ $? -eq 0 ] || continue
+
+	# Build 
+
+	action_name="build"
+	action="gmake -k"
+	perform_action
+	[ $? -eq 0 ] || continue
+
+	# Check
+
+	action_name="check"
+	action="gmake -k check"
+	perform_action
+	[ $? -eq 0 ] || continue
+	
+	# Install
+
+	action_name="install"
+	action="gmake install"
+	perform_action
+	[ $? -eq 0 ] || continue
+
+	# Link
+	
+	[ ! -d ../link ] && mkdir ../link
+        cd ../link
+
+	action_name="link"
+	action="perl $checkdir/link $module"
+	perform_action
+	[ $? -eq 0 ] || continue
+
+	cd ..
+	rm -r link
+
+    done	# loop over builds
+		
+done   # loop over modules
+
+
+# Tidy up
+
+for module in $modules
+do
+    moduledir=$checkdir/$module
+    for build in `builddirs`
+    do
+       cd $moduledir/$build/build
+       gmake -k uninstall  distclean &> /dev/null	
+    done
+done
+
+# Remove old logfiles
+
+find $logdir -type f -mtime +14 -exec rm '{}' ';'
