4012312a99eae41ca2193a739e84e55710b09cd9
edwards <edwards>
2005-11-07 18:06:05 +0000

    More smearings.

--- lib/meas/smear/link_smearing_aggregate.cc
+++ lib/meas/smear/link_smearing_aggregate.cc
@@ -1,4 +1,4 @@
-// $Id: link_smearing_aggregate.cc,v 1.2 2005-11-07 06:40:55 edwards Exp $
+// $Id: link_smearing_aggregate.cc,v 1.3 2005-11-07 18:06:05 edwards Exp $
 /*! \file
  *  \brief All link smearing applicators
  */
@@ -6,6 +6,8 @@
 #include "meas/smear/link_smearing_aggregate.h"
 
 #include "meas/smear/ape_link_smearing.h"
+#include "meas/smear/hyp_link_smearing.h"
+#include "meas/smear/stout_link_smearing.h"
 
 namespace Chroma
 {
@@ -19,6 +21,8 @@ namespace Chroma
 
       // link smearing
       success &= APELinkSmearingEnv::registered;
+      success &= HypLinkSmearingEnv::registered;
+      success &= StoutLinkSmearingEnv::registered;
 
       return success;
     }
