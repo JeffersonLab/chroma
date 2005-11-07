dc3ff6c2df39d3ed21c38a1a419e64c675db47a5
edwards <edwards>
2005-11-07 06:26:14 +0000

    Indenting and doxygen comments.

--- lib/meas/sources/z2_src.h
+++ lib/meas/sources/z2_src.h
@@ -1,23 +1,25 @@
 // -*- C++ -*-
-// $Id: z2_src.h,v 1.1 2005-10-28 21:06:41 edwards Exp $
+// $Id: z2_src.h,v 1.2 2005-11-07 06:26:14 edwards Exp $
 /*! \file
  *  \brief Volume source of Z2 noise
  */
 
-
-//
-//
-//
-
 #ifndef  Z2_SRC_INC
 #define  Z2_SRC_INC 
 
-namespace Chroma {
+namespace Chroma 
+{
+  //! Z2-source
+  /*! @ingroup sources */
+  void z2_src(LatticeFermion& a);
 
-void z2_src(LatticeFermion& a) ;
-void z2_src(LatticeStaggeredFermion& a) ; 
+  //! Z2-source
+  /*! @ingroup sources */
+  void z2_src(LatticeStaggeredFermion& a);
 
-void z2_src(LatticeFermion& a, int slice, int mu) ;
+  //! Z2-source
+  /*! @ingroup sources */
+  void z2_src(LatticeFermion& a, int slice, int mu);
 
 }  // end namespace Chroma
 
