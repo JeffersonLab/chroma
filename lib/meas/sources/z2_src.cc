05787a0365a4dabfae9ab16fe5240869989aa513
edwards <edwards>
2005-11-10 04:55:10 +0000

    Did some indenting.

--- lib/meas/sources/z2_src.cc
+++ lib/meas/sources/z2_src.cc
@@ -1,4 +1,4 @@
-// $Id: z2_src.cc,v 1.1 2005-10-28 21:06:41 edwards Exp $
+// $Id: z2_src.cc,v 1.2 2005-11-10 04:55:10 edwards Exp $
 /*! \file
  *  \brief Variety of Z2 noise sources
  */
@@ -24,77 +24,75 @@ namespace Chroma {
  *
  */
 
-template<typename T>
-void z2_src_t(T & a)
-{
+  template<typename T>
+  void z2_src_t(T & a)
+  {
 
-  a = zero ; 
-  LatticeReal rnd ;
-  LatticeReal ar ,ai ;
-  LatticeColorVector colorvec = zero;
+    a = zero ; 
+    LatticeReal rnd ;
+    LatticeReal ar ,ai ;
+    LatticeColorVector colorvec = zero;
 
-  for(int spin_index= 0 ; spin_index < Ns ; ++spin_index)
-  for(int color_index= 0 ; color_index < Nc ; ++color_index)
-    {
-      random(rnd) ; 
-      ar = where( rnd > 0.5 , LatticeReal(1) , LatticeReal(-1) );
-      random(rnd) ; 
-      ai = where( rnd > 0.5 , LatticeReal(1) , LatticeReal(-1) );
-      LatticeComplex c = cmplx(ar,ai) ;
+    for(int spin_index= 0 ; spin_index < Ns ; ++spin_index)
+      for(int color_index= 0 ; color_index < Nc ; ++color_index)
+      {
+	random(rnd) ; 
+	ar = where( rnd > 0.5 , LatticeReal(1) , LatticeReal(-1) );
+	random(rnd) ; 
+	ai = where( rnd > 0.5 , LatticeReal(1) , LatticeReal(-1) );
+	LatticeComplex c = cmplx(ar,ai) ;
 
-      colorvec = peekSpin(a,spin_index);
+	colorvec = peekSpin(a,spin_index);
 
-      pokeSpin(a,pokeColor(colorvec,c,color_index),spin_index);
+	pokeSpin(a,pokeColor(colorvec,c,color_index),spin_index);
+      }
 
-    }
-
-}
-
-
-// template<>
-void z2_src(LatticeFermion& a)
-{
-  z2_src_t<LatticeFermion>(a) ; 
-}
+  }
 
 
-// template<>
-void z2_src(LatticeStaggeredFermion& a)
-{
-  z2_src_t<LatticeStaggeredFermion>(a) ; 
-}
+  // template<>
+  void z2_src(LatticeFermion& a)
+  {
+    z2_src_t<LatticeFermion>(a) ; 
+  }
 
 
-//! Timeslice source of complex Z2 noise
-/*!
- * \ingroup sources
- *
- * This routine is specific to Wilson fermions! 
- *
- * \param a      Source fermion
- * \param slice        time slice
- * \param mu           direction of slice
- *
- *
- *  This type of source is useful for computing hadronic
- *  decay like diagrams. 
- *
- *  QUARK MASS DEPENDENCE OF HADRON MASSES FROM LATTICE QCD.
- * By UKQCD Collaboration (M. Foster et al.)
- * Published in Phys.Rev.D59:074503,1999
- * e-Print Archive: hep-lat/9810021 
- *
- */
+  // template<>
+  void z2_src(LatticeStaggeredFermion& a)
+  {
+    z2_src_t<LatticeStaggeredFermion>(a) ; 
+  }
 
-void z2_src(LatticeFermion& a, int slice, int mu)
-{
 
-  // compute a volume source of z2 noise
-  LatticeFermion tmp; 
-  z2_src(tmp) ;
+  //! Timeslice source of complex Z2 noise
+  /*!
+   * \ingroup sources
+   *
+   * This routine is specific to Wilson fermions! 
+   *
+   * \param a      Source fermion
+   * \param slice        time slice
+   * \param mu           direction of slice
+   *
+   *
+   *  This type of source is useful for computing hadronic
+   *  decay like diagrams. 
+   *
+   *  QUARK MASS DEPENDENCE OF HADRON MASSES FROM LATTICE QCD.
+   * By UKQCD Collaboration (M. Foster et al.)
+   * Published in Phys.Rev.D59:074503,1999
+   * e-Print Archive: hep-lat/9810021 
+   *
+   */
+  void z2_src(LatticeFermion& a, int slice, int mu)
+  {
 
-  a = where(Layout::latticeCoordinate(mu) == slice, tmp, LatticeFermion(zero));
-}
+    // compute a volume source of z2 noise
+    LatticeFermion tmp; 
+    z2_src(tmp) ;
+
+    a = where(Layout::latticeCoordinate(mu) == slice, tmp, LatticeFermion(zero));
+  }
 
 }  // end namespace Chroma
 
