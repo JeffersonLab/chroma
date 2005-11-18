a24b8a48326cc7f73fc95a4cb6dae0c3944de3ac
edwards <edwards>
2005-11-18 21:31:10 +0000

    First version.

---
+++ lib/meas/sources/dilutez3_source_const.cc
@@ -0,0 +1,240 @@
+// $Id: dilutez3_source_const.cc,v 1.1 2005-11-18 21:31:10 edwards Exp $
+/*! \file
+ *  \brief Random Z2 wall source construction
+ */
+
+#include "chromabase.h"
+
+#include "meas/sources/source_const_factory.h"
+#include "meas/sources/dilutez2_source_const.h"
+#include "meas/sources/z2_src.h"
+
+namespace Chroma
+{
+  // Read parameters
+  void read(XMLReader& xml, const string& path, DiluteZ2QuarkSourceConstEnv::Params& param)
+  {
+    DiluteZ2QuarkSourceConstEnv::Params tmp(xml, path);
+    param = tmp;
+  }
+
+  // Writer
+  void write(XMLWriter& xml, const string& path, const DiluteZ2QuarkSourceConstEnv::Params& param)
+  {
+    param.writeXML(xml, path);
+  }
+
+
+
+  // Hooks to register the class
+  namespace DiluteZ2QuarkSourceConstEnv
+  {
+    // Anonymous namespace
+    namespace
+    {
+      //! Callback function
+      QuarkSourceConstruction<LatticeFermion>* createFerm(XMLReader& xml_in,
+							  const std::string& path)
+      {
+	return new SourceConst<LatticeFermion>(Params(xml_in, path));
+      }
+
+      //! Register all the factories
+      bool registerAll()
+      {
+	bool foo = true;
+	foo &= Chroma::TheFermSourceConstructionFactory::Instance().registerObject(name, createFerm);
+	return foo;
+      }
+    }  // end namespace
+
+    //! Name to be used
+    const std::string name("RAND_DILUTE_Z2_SOURCE");
+
+    //! Register the source construction
+    const bool registered = registerAll();
+
+
+    //! Initialize
+    Params::Params()
+    {
+      j_decay = -1;
+      t_source = -1;
+    }
+
+
+    //! Read parameters
+    Params::Params(XMLReader& xml, const string& path)
+    {
+      XMLReader paramtop(xml, path);
+
+      int version;
+      read(paramtop, "version", version);
+
+      switch (version) 
+      {
+      case 1:
+	break;
+
+      default:
+	QDPIO::cerr << __func__ << ": parameter version " << version 
+		    << " unsupported." << endl;
+	QDP_abort(1);
+      }
+
+      read(paramtop, "ran_seed", ran_seed);
+      read(paramtop, "j_decay", j_decay);
+      read(paramtop, "t_source", t_source);
+
+      read(paramtop, "spatial_mask_size", spatial_mask_size);
+      read(paramtop, "spatial_mask", spatial_mask);
+      read(paramtop, "color_mask", color_mask);
+      read(paramtop, "spin_mask", spin_mask);
+    }
+
+
+    // Writer
+    void Params::writeXML(XMLWriter& xml, const string& path) const
+    {
+      push(xml, path);
+
+      int version = 1;
+      write(xml, "version", version);
+      write(xml, "ran_seed", ran_seed);
+      write(xml, "j_decay", j_decay);
+      write(xml, "t_source", t_source);
+
+      write(xml, "spatial_mask_size", spatial_mask_size);
+      write(xml, "spatial_mask", spatial_mask);
+      write(xml, "color_mask", color_mask);
+      write(xml, "spin_mask", spin_mask);
+
+      pop(xml);
+    }
+
+
+
+    //! Construct the source
+    LatticeFermion
+    SourceConst<LatticeFermion>::operator()(const multi1d<LatticeColorMatrix>& u) const
+    {
+      QDPIO::cout << "Diluted random complex Z2 source" << endl;
+
+      //
+      // Sanity checks
+      //
+      if (params.spatial_mask_size.size() != Nd-1)
+      {
+	QDPIO::cerr << name << ": spatial mask size incorrect 1" << endl;
+	QDP_abort(1);
+      }
+
+      if (params.spatial_mask.size() == 0)
+      {
+	QDPIO::cerr << name << ": spatial mask incorrect 2" << endl;
+	QDP_abort(1);
+      }
+
+      multi1d<int> lookup_dir(Nd-1);
+      int mu = 0;
+      for(int j=0; j < params.spatial_mask_size.size(); ++j, ++mu)
+      {
+	if (j == params.j_decay) ++mu;  // bump up to next dir
+
+	lookup_dir[j] = mu;
+      }
+
+      for(int j=0; j < params.spatial_mask.size(); ++j)
+      {
+	if (params.spatial_mask[j].size() != Nd-1)
+	{
+	  QDPIO::cerr << name << ": spatial mask incorrect 3" << endl;
+	  QDP_abort(1);
+	}
+      }
+
+      // More sanity checks
+      for(int c=0; c < params.color_mask.size(); ++c)
+      {
+	if (params.color_mask[c] < 0 || params.color_mask[c] >= Nc)
+	{
+	  QDPIO::cerr << name << ": color mask incorrect 6" << endl;
+	  QDP_abort(1);
+	}
+      }
+
+      for(int s=0; s < params.spin_mask.size(); ++s)
+      {
+	if (params.spin_mask[s] < 0 || params.spin_mask[s] >= Ns)
+	{
+	  QDPIO::cerr << name << ": spin mask incorrect 7" << endl;
+	  QDP_abort(1);
+	}
+      }
+
+      //
+      // Finally, do something useful
+      //
+
+      // Save current seed
+      Seed ran_seed;
+      QDP::RNG::savern(ran_seed);
+
+      // Set the seed to desired value
+      QDP::RNG::setrn(params.ran_seed);
+
+      // Create the noisy quark source on the entire lattice
+      LatticeFermion quark_noise;
+      z2_src(quark_noise);
+
+      // This is the filtered noise source to return
+      LatticeFermion quark_source = zero;
+
+      // Filter over the color and spin indices first
+      for(int s=0; s < params.spin_mask.size(); ++s)
+      {
+	int spin_source = params.spin_mask[s];
+	LatticeColorVector colvec = peekSpin(quark_noise, spin_source);
+	LatticeColorVector dest   = zero;
+
+	for(int c=0; c < params.color_mask.size(); ++c)
+	{ 
+	  int color_source = params.color_mask[c];
+	  LatticeComplex comp = peekColor(colvec, color_source);
+
+	  pokeColor(dest, comp, color_source);
+	}
+
+	pokeSpin(quark_source, dest, spin_source);
+      }
+
+      quark_noise = quark_source;  // reset
+
+      // Filter over the spatial sites
+      LatticeBoolean    mask = false;  // this is the starting mask
+
+      for(int n=0; n < params.spatial_mask.size(); ++n)
+      {
+	LatticeBoolean btmp = true;
+
+	for(int j=0; j < params.spatial_mask[n].size(); ++j)
+	  btmp &= (Layout::latticeCoordinate(lookup_dir[j]) % params.spatial_mask_size[j]) == params.spatial_mask[n][j];
+
+	mask |= btmp;
+      }
+
+      // Filter over the time slices
+      mask &= Layout::latticeCoordinate(params.j_decay) == params.t_source;
+
+      // Zap the unused sites
+      quark_source = where(mask, quark_noise, Fermion(zero));
+
+      // Reset the seed
+      QDP::RNG::setrn(ran_seed);
+
+      return quark_source;
+    }
+
+  } // end namespace
+
+} // end namespace Chroma
---
+++ lib/meas/sources/dilutez3_source_const.h
@@ -0,0 +1,82 @@
+// -*- C++ -*-
+// $Id: dilutez3_source_const.h,v 1.1 2005-11-18 21:31:10 edwards Exp $
+/*! \file
+ *  \brief Random complex Z(2) source construction using dilution
+ *
+ * Uses the Dublin "dilution" stochastic source strategy of hep-lat/0505023
+ */
+
+#ifndef __dilutez2_source_const_h__
+#define __dilutez2_source_const_h__
+
+#include "meas/sources/source_construction.h"
+
+namespace Chroma
+{
+
+  //! Dilute Z2 quark source namespace, parameters, and classes
+  /*! @ingroup sources */
+  namespace DiluteZ2QuarkSourceConstEnv
+  {
+    extern const std::string name;
+    extern const bool registered;
+
+  
+    //! Random complex Z(2) sources using dilution
+    /*! @ingroup sources */
+    struct Params
+    {
+      Params();
+      Params(XMLReader& in, const std::string& path);
+      void writeXML(XMLWriter& in, const std::string& path) const;
+    
+      Seed                     ran_seed;             /*!< Set the seed to this value */
+
+      multi1d<int>             spatial_mask_size;    /*!< Spatial size of periodic mask */
+      multi1d< multi1d<int> >  spatial_mask;         /*!< Sites included in site mask */
+      multi1d<int>             color_mask;           /*!< Color size of periodic mask */
+      multi1d<int>             spin_mask;            /*!< Spin size of periodic mask */
+
+      int                      j_decay;              /*!< decay direction */
+      int                      t_source;             /*!< source time slice location */
+    };
+
+
+    //! Random complex Z(2) sources using dilution
+    /*! @ingroup sources
+     *
+     * Create a random Z(2) using dilution
+     */
+    template<typename T>
+    class SourceConst : public QuarkSourceConstruction<T>
+    {
+    public:
+      //! Full constructor
+      SourceConst(const Params& p) : params(p) {}
+
+      //! Construct the source
+      T operator()(const multi1d<LatticeColorMatrix>& u) const;
+
+    private:
+      //! Hide partial constructor
+      SourceConst() {}
+
+    private:
+      Params  params;   /*!< source params */
+    };
+
+  }  // end namespace DiluteZ2QuarkSourceConstEnv
+
+
+  //! Reader
+  /*! @ingroup sources */
+  void read(XMLReader& xml, const string& path, DiluteZ2QuarkSourceConstEnv::Params& param);
+
+  //! Writer
+  /*! @ingroup sources */
+  void write(XMLWriter& xml, const string& path, const DiluteZ2QuarkSourceConstEnv::Params& param);
+
+}  // end namespace Chroma
+
+
+#endif
