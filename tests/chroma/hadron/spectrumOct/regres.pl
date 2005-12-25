6cdb3c3330fdf50d89a1e96e59157b29c92655f6
edwards <edwards>
2005-12-25 06:51:32 +0000

    Fixed directory of tests.

--- tests/chroma/hadron/spectrumOct/regres.pl
+++ tests/chroma/hadron/spectrumOct/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 1.1 2005-12-25 04:33:10 edwards Exp $
+#  $Id: regres.pl,v 1.2 2005-12-25 06:51:32 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -13,9 +13,9 @@
      {
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "chroma" , 
-	 input       => "$test_dir/chroma/hadron/spectrum/spectrumOct-v11.ini.xml" , 
+	 input       => "$test_dir/chroma/hadron/spectrumOct/spectrumOct-v11.ini.xml" , 
 	 output      => "spectrumOct-v11.candidate.xml",
-	 metric      => "$test_dir/chroma/hadron/spectrum/spectrumOct-v11.metric.xml" ,
-	 controlfile => "$test_dir/chroma/hadron/spectrum/spectrumOct-v11.out.xml" ,
+	 metric      => "$test_dir/chroma/hadron/spectrumOct/spectrumOct-v11.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/spectrumOct/spectrumOct-v11.out.xml" ,
      }
      );
