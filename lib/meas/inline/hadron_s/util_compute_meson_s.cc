432ab9f85d4278cb8dbd04e8a83a970a0b6f99cb
egregory <egregory>
2006-02-03 14:26:16 +0000

    added arguments to explicitly specify type of shift

--- lib/meas/inline/hadron_s/util_compute_meson_s.cc
+++ lib/meas/inline/hadron_s/util_compute_meson_s.cc
@@ -1,4 +1,4 @@
-// $Id: util_compute_meson_s.cc,v 2.2 2006-02-02 16:25:00 egregory Exp $
+// $Id: util_compute_meson_s.cc,v 2.3 2006-02-03 14:26:16 egregory Exp $
 /*! \file
  * \brief Wrapper code to compute staggered meson correlators.
  *
@@ -22,19 +22,36 @@ namespace Chroma
 
 
   void compute_vary_ps(
-		       LatticeStaggeredPropagator & quark_propagator_Lsink_Lsrc,
-		       LatticeStaggeredPropagator & quark_propagator_Fsink_Lsrc,
-		       LatticeStaggeredPropagator & quark_propagator_Lsink_Fsrc,
-		       LatticeStaggeredPropagator & quark_propagator_Fsink_Fsrc,
-		       const multi1d<LatticeColorMatrix> & u , 
-		       XMLWriter & xml_out,
-		       int j_decay,int t_length, int t_source)
-{
+		      LatticeStaggeredPropagator & quark_propagator_Lsink_Lsrc,
+		      LatticeStaggeredPropagator & quark_propagator_Fsink_Lsrc,
+		      LatticeStaggeredPropagator & quark_propagator_Lsink_Fsrc,
+		      LatticeStaggeredPropagator & quark_propagator_Fsink_Fsrc,
+		      const multi1d<LatticeColorMatrix> & u , 
+		      bool gauge_shift, bool sym_shift ,
+		      XMLWriter & xml_out,
+		      int j_decay,int t_length, int t_source){
+
+    Stag_shift_option type_of_shift;
+
+    if((gauge_shift) && (sym_shift)){
+      type_of_shift=SYM_GAUGE_INVAR;
+    }else{
+      if((gauge_shift) && (!sym_shift)){
+	type_of_shift=GAUGE_INVAR;
+      }else{
+	if((!gauge_shift) && (sym_shift)){
+	  type_of_shift=SYM_NON_GAUGE_INVAR;
+	}else{
+	  if((!gauge_shift) && (!sym_shift)){
+	    type_of_shift=NON_GAUGE_INVAR ;
+	  }
+	}
+      }
+    }
 
 
-  printf("begin pion\n");fflush(stdout);
-  staggered_local_pion pion(t_length,u) ;
-  printf("done pion\n");fflush(stdout);
+  staggered_local_pion pion(t_length,u, type_of_shift) ;
+
 
 push(xml_out,"Meson_correlators");
 
