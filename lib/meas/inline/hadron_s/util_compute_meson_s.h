50fea6e2fc9602a3b753a7fbe09972039e1cc969
egregory <egregory>
2006-02-03 14:26:51 +0000

    changes to accompany changes to util_compute_meson_s.cc

--- lib/meas/inline/hadron_s/util_compute_meson_s.h
+++ lib/meas/inline/hadron_s/util_compute_meson_s.h
@@ -1,17 +1,23 @@
 #ifndef UTIL_COMPUTE_MESON_S__H 
 #define UTIL_COMPUTE_MESON_S__H 
 
-namespace Chroma 
-{ 
+namespace Chroma { 
 
   void compute_vary_ps(
-		       LatticeStaggeredPropagator & quark_propagator_Lsink_Lsrc,
-		       LatticeStaggeredPropagator & quark_propagator_Fsink_Lsrc,
-		       LatticeStaggeredPropagator & quark_propagator_Lsink_Fsrc,
-		       LatticeStaggeredPropagator & quark_propagator_Fsink_Fsrc,
-		       const multi1d<LatticeColorMatrix> & u , 
-		       XMLWriter & xml_out,
-		       int j_decay,int t_length, int t_source) ; 
+		      LatticeStaggeredPropagator & quark_propagator_Lsink_Lsrc,
+		      LatticeStaggeredPropagator & quark_propagator_Fsink_Lsrc,
+		      LatticeStaggeredPropagator & quark_propagator_Lsink_Fsrc,
+		      LatticeStaggeredPropagator & quark_propagator_Fsink_Fsrc,
+		      const multi1d<LatticeColorMatrix> & u , 
+		      bool gauge_shift, bool sym_shift ,
+		      XMLWriter & xml_out,
+		      int j_decay,int t_length, int t_source);
+
+  void compute_8_pions(	multi1d<LatticeStaggeredPropagator> & stag_prop,
+			const multi1d<LatticeColorMatrix> & u ,
+			bool gauge_shift, bool sym_shift ,
+			XMLWriter & xml_out, 
+			int j_decay, int t_length, int t_source);
 
 }
 
