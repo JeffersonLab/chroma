f8e32636e77eb0e10aa5563ef2afb5867e98dbca
edwards <edwards>
2006-02-09 02:02:49 +0000

    New regression tests.

--- tests/chroma/hadron/seqsource/regres.pl
+++ tests/chroma/hadron/seqsource/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 1.1 2005-12-18 23:51:30 edwards Exp $
+#  $Id: regres.pl,v 1.2 2006-02-09 02:02:49 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -17,5 +17,21 @@
 	 output      => "seqsource-v2.candidate.xml",
 	 metric      => "$test_dir/chroma/hadron/seqsource/seqsource-v2.metric.xml" ,
 	 controlfile => "$test_dir/chroma/hadron/seqsource/seqsource-v2.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/seqsource/seqsource-meson-v1.ini.xml" , 
+	 output      => "seqsource-meson-v1.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/seqsource/seqsource-meson-v1.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/seqsource/seqsource-meson-v1.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/seqsource/seqsource-baryon-v1.ini.xml" , 
+	 output      => "seqsource-baryon-v1.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/seqsource/seqsource-baryon-v1.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/seqsource/seqsource-baryon-v1.out.xml" ,
      }
      );
