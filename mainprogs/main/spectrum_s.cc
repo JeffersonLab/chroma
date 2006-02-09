4a0b68e19ee476d6cffb5ebaecf13876a43219c3
egregory <egregory>
2006-02-09 17:54:38 +0000

    added coloumb gauge-fixing

--- mainprogs/main/spectrum_s.cc
+++ mainprogs/main/spectrum_s.cc
@@ -1,9 +1,10 @@
-// $Id: spectrum_s.cc,v 2.0 2005-09-25 21:04:46 edwards Exp $
+// $Id: spectrum_s.cc,v 2.1 2006-02-09 17:54:38 egregory Exp $
 /*! \file
  * \brief Main code for spectrum measurements
  */
 
 #include "chroma.h"
+#include "meas/gfix/coulgauge.h"
 
 using namespace Chroma;
 
@@ -83,6 +84,37 @@ int main(int argc, char **argv)
 
   // Output
   XMLFileWriter& xml_out = Chroma::getXMLOutputInstance();
+
+
+  bool gauge_invar_oper = false;
+  read(xml_in, "/spectrum_s/Param/gauge_invar_oper",gauge_invar_oper );
+
+    // GAUGE FIX 
+    if( !gauge_invar_oper){
+      //not using covariant shifts, should gauge-fix
+
+      //read some input the sloppy way
+      Real GFAccu;  // Gauge fixing tolerance 
+      Real OrPara;  // Gauge fixing over-relaxation param
+      int  GFMax;   // Maximum gauge fixing iterations
+      int  j_decay; // the t-direction
+
+      read(xml_in, "/spectrum_s/Param/GFAccu", GFAccu );
+      read(xml_in, "/spectrum_s/Param/OrPara", OrPara );
+      read(xml_in, "/spectrum_s/Param/GFMax", GFMax );
+      read(xml_in, "/spectrum_s/Param/j_decay",j_decay  );
+      int  n_gf;
+
+
+      QDPIO::cout << "Not using covariant shift operators.";
+      QDPIO::cout << "Must gauge-fix" <<  endl;
+
+      QDPIO::cout << "Starting to fix to Coulomb gauge" <<  endl;
+      coulGauge(u, n_gf, j_decay, GFAccu, GFMax, true,	OrPara);
+      QDPIO::cout << "No. of gauge fixing iterations =" << n_gf << endl;
+    }
+
+
 
   unsigned long cur_update = 0;
   meas(u, config_xml, cur_update, xml_out);
