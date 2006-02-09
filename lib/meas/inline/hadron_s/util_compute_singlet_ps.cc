a85c89473f39df3956d5cff30481d59a376b477a
egregory <egregory>
2006-02-09 18:03:32 +0000

    formatting

--- lib/meas/inline/hadron_s/util_compute_singlet_ps.cc
+++ lib/meas/inline/hadron_s/util_compute_singlet_ps.cc
@@ -91,8 +91,6 @@ namespace Chroma {
     }
 
 
-
-
     /*** generate the 4-link quark propagator ****/
     push(xml_out,"Computation_4link_pseudoscalar");
 
@@ -114,6 +112,8 @@ namespace Chroma {
 	  // start with local source 
 	  coord[0]=0; coord[1] = 0; coord[2] = 0; coord[3] = 0;
 	  srcfil(q_source, coord,color_source ) ;
+
+
 	  
 	  // now do the shift
 	  coord[0]=1; coord[1] = 1; coord[2] = 1; coord[3] = 1;
