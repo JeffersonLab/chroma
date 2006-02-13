bcdca4ad45dbe64eae9ec30bc4e11a5bc1f50424
edwards <edwards>
2006-02-13 20:47:51 +0000

    First version.

---
+++ lib/meas/hadron/photon_seqsrc_w.cc
@@ -0,0 +1,226 @@
+// $Id: photon_seqsrc_w.cc,v 1.1 2006-02-13 20:47:51 edwards Exp $
+/*! \file
+ *  \brief Construct a photon sequential sources via LSZ reduction
+ */
+
+#include "meas/hadron/photon_seqsrc_w.h"
+#include "meas/hadron/seqsource_factory_w.h"
+
+namespace Chroma 
+{
+
+  // Read parameters
+  void read(XMLReader& xml, const string& path, PhotonRhoSeqSourceEnv::Params& param)
+  {
+    PhotonRhoSeqSourceEnv::Params tmp(xml, path);
+    param = tmp;
+  }
+
+  // Writer
+  void write(XMLWriter& xml, const string& path, const PhotonRhoSeqSourceEnv::Params& param)
+  {
+    param.writeXML(xml, path);
+  }
+
+
+
+
+
+  //! Meson sequential sources
+  /*! \ingroup hadron */
+  namespace PhotonRhoSeqSourceEnv
+  { 
+    //! Anonymous namespace
+    namespace
+    {
+      //! Construct a0-photon sequential source
+      /*!
+       * \ingroup hadron
+       */
+      HadronSeqSource<LatticePropagator>* mesA0PhotonSeqSrc(XMLReader& xml_in,
+							    const std::string& path)
+      {
+	return new PhotonRhoSeqSource(Params(xml_in, path));
+      }
+
+    } // end anonymous namespace
+
+
+    //! Initialize
+    Params::Params()
+    {
+      Q_sq     = zero;
+      c_sq     = 1.0;
+      xi       = 1.0;
+      pol_dir  = -1;
+      j_decay  = -1;
+      t_sink   = -1;
+      t_sink_start = -1;
+      t_sink_end   = -1;
+      sink_mom.resize(Nd-1);
+      sink_mom = 0;
+    }
+
+
+    //! Read parameters
+    Params::Params(XMLReader& xml, const string& path)
+    {
+      XMLReader paramtop(xml, path);
+
+      int version;
+      read(paramtop, "version", version);
+
+      switch (version) 
+      {
+      case 1:
+	break;
+
+      default:
+	QDPIO::cerr << __func__ << ": parameter version " << version 
+		    << " unsupported." << endl;
+	QDP_abort(1);
+      }
+
+      read(paramtop, "Q_sq", Q_sq);
+      read(paramtop, "c_sq", c_sq);
+      read(paramtop, "xi", xi);
+      read(paramtop, "pol_dir", pol_dir);
+      read(paramtop, "j_decay", j_decay);
+      read(paramtop, "sink_mom", sink_mom);
+      read(paramtop, "t_sink_start", t_sink_start);
+      read(paramtop, "t_sink_end", t_sink_end);
+    }
+
+
+    // Writer
+    void Params::writeXML(XMLWriter& xml, const string& path) const
+    {
+      push(xml, path);
+
+      int version = 1;
+      write(xml, "version", version);
+
+      write(xml, "Q_sq", Q_sq);
+      write(xml, "c_sq", c_sq);
+      write(xml, "xi", xi);
+      write(xml, "pol_dir", pol_dir);
+      write(xml, "j_decay", j_decay);
+      write(xml, "t_sink_start", t_sink_start);
+      write(xml, "t_sink_end", t_sink_end);
+      write(xml, "t_sink", t_sink);
+      write(xml, "sink_mom", sink_mom);
+      pop(xml);
+    }
+
+
+    //! Construct the source
+    LatticePropagator
+    PhotonRhoSeqSource::operator()(const multi1d<LatticeColorMatrix>& u,
+				   const multi1d<LatticePropagator>& quark_propagators) const
+    {
+      QDPIO::cout << "Photon sequential source " << endl;
+
+      START_CODE();
+
+      if (quark_propagators.size() != 1)
+      {
+	QDPIO::cerr << __func__ << ": expect only 1 prop" << endl;
+	QDP_abort(1);
+      }
+
+      if (params.j_decay < 0 || params.j_decay >= Nd)
+      {
+	QDPIO::cerr << __func__ << ": j_decay out of bounds: j_decay = " << params.j_decay << endl;
+	QDP_abort(1);
+      }
+
+      if (params.pol_dir < 0 || params.pol_dir >= Nd)
+      {
+	QDPIO::cerr << __func__ << ": pol_dir out of bounds: pol_dir = " << params.pol_dir << endl;
+	QDP_abort(1);
+      }
+
+      // Initial sequential source
+      LatticePropagator seq_src_tmp;
+      {
+	int G5 = Ns*Ns-1;
+	LatticePropagator tmp = adj(quark_propagators[0]) * Gamma(G5) * Gamma(1 << params.pol_dir);
+      
+	// Now take hermitian conjugate and multiply on both sides with gamma_5 = Gamma(15)
+	seq_src_tmp = Gamma(15) * adj(tmp) * Gamma(15);
+      }
+        
+      //  We now inject momentum at sink if required
+      multi1d<Real> pp_f(Nd-1);
+      LatticeReal p_dot_x = zero;
+      bool nonzeroP = false;
+      for(int mu=0, j=0; mu < Nd; mu++)
+      {
+	if (mu != params.j_decay)
+	{
+	  pp_f[j] = params.sink_mom[j] * twopi / Real(Layout::lattSize()[mu]);
+
+	  if (params.sink_mom[j] != 0)
+	  {
+	    nonzeroP = true;
+	    p_dot_x += Layout::latticeCoordinate(mu) * pp_f[j];
+	  }
+
+	  j++;
+	}
+      }
+
+      if (nonzeroP)
+	seq_src_tmp *= cmplx(cos(p_dot_x),sin(p_dot_x));
+
+      // Photon energy determined from virtuality, \f$Q_f^2 = c^2|\vec{p_f}|^2 - \omega_f^2\f$
+      Real omega;
+      {
+	Real norm2_ppf = zero;
+	for(int i=0; i < pp_f.size(); ++i)
+	  norm2_ppf += pp_f[i] * pp_f[i];
+
+	Real c_sq      =  params.c_sq;
+	Real xi        =  params.xi;
+	Real xi_sq     =  xi * xi;
+
+	omega = sqrt( Real(c_sq/xi_sq)*norm2_ppf - params.Q_sq );
+      }
+      
+      // Now integrate over sink
+      LatticePropagator fin = zero;
+
+      for(int t_f=params.t_sink_start; t_f <= params.t_sink_end; ++t_f)
+      {
+	// Note positive sign of omega
+	fin += where(Layout::latticeCoordinate(params.j_decay) == t_f,
+		     exp(omega*t_f) * seq_src_tmp,
+		     LatticePropagator(zero));
+      }
+
+      END_CODE();
+
+      return fin;
+    }
+
+
+    // Register all the possible simple mesons
+    bool registerAll(void) 
+    {
+      bool success = true;
+
+      //! Register all the factories
+      success &= Chroma::TheWilsonHadronSeqSourceFactory::Instance().registerObject(string("A0-PHOTON"), 
+										    mesA0PhotonSeqSrc);
+
+      return success;
+    }
+
+    const bool registered = registerAll();
+
+  }  // end namespace PhotonRhoSeqSourceEnv
+
+}  // end namespace Chroma
+
+
+  
---
+++ lib/meas/hadron/photon_seqsrc_w.h
@@ -0,0 +1,77 @@
+// -*- C++ -*-
+// $Id: photon_seqsrc_w.h,v 1.1 2006-02-13 20:47:51 edwards Exp $
+/*! \file
+ *  \brief Construct a photon sequential sources via LSZ reduction
+ */
+
+#ifndef __photon_seqsrc_w_h__
+#define __photon_seqsrc_w_h__
+
+#include "meas/hadron/hadron_seqsource.h"
+
+namespace Chroma 
+{
+
+  //! Name and registration
+  /*! @ingroup hadron */
+  namespace PhotonRhoSeqSourceEnv
+  {
+    extern const bool registered;
+
+  
+    //! Construct a photon sequential sources via LSZ reduction
+    /*! @ingroup hadron */
+    struct Params
+    {
+      Params();
+      Params(XMLReader& in, const std::string& path);
+      void writeXML(XMLWriter& in, const std::string& path) const;
+
+      int              pol_dir;         /*!< Polarization direction */
+      int              t_sink_start;    /*!< time slice of sink (inclusive) to start integration */
+      int              t_sink_end;      /*!< time slice of sink (inclusive) to end integration */
+      Real             Q_sq;            /*!< Photon virtuality, \f$Q_f^2 = c^2|\vec{p_f}|^2 - \omega_f^2 */
+      Real             c_sq;            /*!< Photon speed of light */
+      Real             xi;              /*!< Renormalized anisotropy */
+
+      multi1d<int>     sink_mom;        /*!< sink momentum */
+      int              j_decay;         /*!< Decay direction */
+
+      int              t_sink;          /*!< will always be -1 here */
+    };
+
+
+    //! Construct a photon sequential sources via LSZ reduction
+    /*! @ingroup hadron
+     *
+     *  Photon source via a rho
+     */
+    class PhotonRhoSeqSource : public HadronSeqSource<LatticePropagator>
+    {
+    public:
+      //! Full constructor
+      PhotonRhoSeqSource(const Params& p) : params(p) {}
+
+      //! Construct the source
+      LatticePropagator operator()(const multi1d<LatticeColorMatrix>& u,
+				   const multi1d<LatticePropagator>& forward_props) const;
+
+    private:
+      Params  params;   /*!< Seqsource params */
+    };
+
+  }  // end namespace
+
+
+  //! Reader
+  /*! @ingroup hadron */
+  void read(XMLReader& xml, const string& path, PhotonRhoSeqSourceEnv::Params& param);
+
+  //! Writer
+  /*! @ingroup hadron */
+  void write(XMLWriter& xml, const string& path, const PhotonRhoSeqSourceEnv::Params& param);
+
+
+}  // end namespace Chroma
+
+#endif
