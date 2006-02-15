461e8eb23419d74546b50feb016cfe7890e90bdd
edwards <edwards>
2006-02-15 05:24:08 +0000

    New aggegrates.

---
+++ lib/actions/gauge/gaugeacts/gaugeacts_aggregate.cc
@@ -0,0 +1,46 @@
+// $Id: gaugeacts_aggregate.cc,v 1.1 2006-02-15 05:24:08 edwards Exp $
+/*! \file
+ *  \brief Generic gauge action wrapper
+ */
+
+#include "chromabase.h"
+
+#include "actions/gauge/gaugeacts/gaugeacts_aggregate.h"
+
+#include "actions/gauge/gaugeacts/gaugeact_factory.h"
+#include "actions/gauge/gaugeacts/plaq_gaugeact.h"
+#include "actions/gauge/gaugeacts/rect_gaugeact.h"
+#include "actions/gauge/gaugeacts/pg_gaugeact.h"
+#include "actions/gauge/gaugeacts/wilson_gaugeact.h"
+#include "actions/gauge/gaugeacts/lw_tree_gaugeact.h"
+#include "actions/gauge/gaugeacts/lw_1loop_gaugeact.h"
+#include "actions/gauge/gaugeacts/rg_gaugeact.h"
+#include "actions/gauge/gaugeacts/rbc_gaugeact.h"
+
+
+namespace Chroma
+{
+
+  //! Registration aggregator
+  namespace GaugeActsEnv
+  {
+    bool registerAll() 
+    {
+      bool success = true;
+
+      success &= PlaqGaugeActEnv::registered;
+      success &= RectGaugeActEnv::registered;
+      success &= PgGaugeActEnv::registered;
+      success &= WilsonGaugeActEnv::registered;
+      success &= LWTreeGaugeActEnv::registered;
+      success &= LW1LoopGaugeActEnv::registered;
+      success &= RGGaugeActEnv::registered;
+      success &= RBCGaugeActEnv::registered;
+
+      return success;
+    }
+
+    const bool registered = registerAll();
+  }
+
+}
---
+++ lib/actions/gauge/gaugeacts/gaugeacts_aggregate.h
@@ -0,0 +1,23 @@
+// -*- C++ -*-
+// $Id: gaugeacts_aggregate.h,v 1.1 2006-02-15 05:24:08 edwards Exp $
+/*! \file
+ *  \brief All gauge actions
+ */
+
+#ifndef __gaugeactss_aggregate_w_h__
+#define __gaugeactss_aggregate_w_h__
+
+#include "chromabase.h"
+
+namespace Chroma
+{
+  //! Registration aggregator
+  /*! @ingroup gaugeacts */
+  namespace GaugeActsEnv
+  {
+    extern const bool registered;
+  }
+
+}
+
+#endif
