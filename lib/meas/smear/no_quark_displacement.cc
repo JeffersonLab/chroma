493fad499d61200f9191004151556d14d16bf8ae
edwards <edwards>
2006-02-22 03:27:01 +0000

    No quark displacement.

---
+++ lib/meas/smear/no_quark_displacement.cc
@@ -0,0 +1,87 @@
+// $Id: no_quark_displacement.cc,v 1.1 2006-02-22 03:27:01 edwards Exp $
+/*! \file
+ *  \brief No quark displacement
+ */
+
+#include "chromabase.h"
+
+#include "meas/smear/quark_displacement_factory.h"
+#include "meas/smear/no_quark_displacement.h"
+
+namespace Chroma 
+{
+
+  // Read parameters
+  void read(XMLReader& xml, const string& path, NoQuarkDisplacementEnv::Params& param)
+  {
+    NoQuarkDisplacementEnv::Params tmp(xml, path);
+    param = tmp;
+  }
+
+  //! Parameters for running code
+  void write(XMLWriter& xml, const string& path, const NoQuarkDisplacementEnv::Params& param)
+  {
+    param.writeXML(xml, path);
+  }
+
+
+  //! Hooks to register the class
+  namespace NoQuarkDisplacementEnv
+  {
+    //! Callback function
+    QuarkDisplacement<LatticePropagator>* createProp(XMLReader& xml_in,
+						     const std::string& path)
+    {
+      return new QuarkDisplace<LatticePropagator>(Params(xml_in, path));
+    }
+
+    //! Callback function
+    QuarkDisplacement<LatticeFermion>* createFerm(XMLReader& xml_in,
+						  const std::string& path)
+    {
+      return new QuarkDisplace<LatticeFermion>(Params(xml_in, path));
+    }
+    
+    //! Callback function
+    QuarkDisplacement<LatticeColorVector>* createColorVec(XMLReader& xml_in,
+							  const std::string& path)
+    {
+      return new QuarkDisplace<LatticeColorVector>(Params(xml_in, path));
+    }
+    
+    //! Name to be used
+    const std::string name = "NONE";
+
+    //! Register all the factories
+    bool registerAll()
+    {
+      bool foo = true;
+      foo &= Chroma::ThePropDisplacementFactory::Instance().registerObject(name, createProp);
+      foo &= Chroma::TheFermDisplacementFactory::Instance().registerObject(name, createFerm);
+      foo &= Chroma::TheColorVecDisplacementFactory::Instance().registerObject(name, createColorVec);
+      return foo;
+    }
+
+    //! Register the source construction
+    const bool registered = registerAll();
+
+
+    //! Parameters for running code
+    Params::Params(XMLReader& xml, const string& path)
+    {
+    }
+
+
+    //! Parameters for running code
+    void Params::writeXML(XMLWriter& xml, const string& path) const
+    {
+      push(xml, path);
+    
+      write(xml, "DisplacementType", name);
+
+      pop(xml);
+    }
+
+  }  // end namespace
+}  // end namespace Chroma
+
---
+++ lib/meas/smear/no_quark_displacement.h
@@ -0,0 +1,71 @@
+// -*- C++ -*-
+// $Id: no_quark_displacement.h,v 1.1 2006-02-22 03:27:01 edwards Exp $
+/*! \file
+ *  \brief No quark displacement
+ */
+
+#ifndef __no_quark_displacement_h__
+#define __no_quark_displacement_h__
+
+#include "meas/smear/quark_displacement.h"
+#include "meas/smear/displacement.h"
+
+namespace Chroma 
+{
+  //! Name and registration
+  /*! @ingroup smear */
+  namespace NoQuarkDisplacementEnv
+  {
+    extern const std::string name;
+    extern const bool registered;
+  
+
+    //! Params for no quark displacement
+    /*! @ingroup smear */
+    struct Params
+    {
+      Params() {}
+      Params(XMLReader& in, const std::string& path);
+      void writeXML(XMLWriter& in, const std::string& path) const;
+    };
+
+
+    //! No quark displacement
+    /*! @ingroup smear
+     *
+     * No quark displacement object
+     */
+    template<typename T>
+    class QuarkDisplace : public QuarkDisplacement<T>
+    {
+    public:
+      //! Full constructor
+      QuarkDisplace(const Params& p) : params(p) {}
+      
+      //! Displace the quark
+      void operator()(T& quark, 
+		      const multi1d<LatticeColorMatrix>& u,
+		      enum PlusMinus isign) const
+	{ /* nop */ }
+
+    private:
+      //! Hide partial constructor
+      QuarkDisplace() {}
+
+    private:
+      Params  params;   /*!< displacement params */
+    };
+
+  }  // end namespace
+
+  //! Reader
+  /*! @ingroup smear */
+  void read(XMLReader& xml, const string& path, NoQuarkDisplacementEnv::Params& param);
+
+  //! Writer
+  /*! @ingroup smear */
+  void write(XMLWriter& xml, const string& path, const NoQuarkDisplacementEnv::Params& param);
+
+}  // end namespace Chroma
+
+#endif
