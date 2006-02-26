fe3c6dba075405837195e6647b797b7526974a12
edwards <edwards>
2006-02-26 21:18:34 +0000

    Put TimeSliceFunc in an anonymous namespace. This avoids colliding with chroma/lib/util/ft/sftmom.cc . However, really this stag. code should use the phases stuff to simply their life.

--- lib/meas/hadron/scalar_loops_s.cc
+++ lib/meas/hadron/scalar_loops_s.cc
@@ -1,64 +1,69 @@
-// $Id: scalar_loops_s.cc,v 2.0 2005-09-25 21:04:35 edwards Exp $
+// $Id: scalar_loops_s.cc,v 2.1 2006-02-26 21:18:34 edwards Exp $
 
 #include "chromabase.h"
 #include"scalar_loops_s.h"
 #include "util/gauge/stag_phases_s.h"
 
-namespace Chroma {
-
-// Standard Time Slicery
-class TimeSliceFunc : public SetFunc
+namespace Chroma 
 {
-public:
-  TimeSliceFunc(int dir): dir_decay(dir) {}
+
+  // Anonymous namespace
+  namespace
+  {
+    // Standard Time Slicery
+    class TimeSliceFunc : public SetFunc
+    {
+    public:
+      TimeSliceFunc(int dir): dir_decay(dir) {}
  
-  int operator() (const multi1d<int>& coordinate) const {return coordinate[dir_decay];}
-  int numSubsets() const {return Layout::lattSize()[dir_decay];}
+      int operator() (const multi1d<int>& coordinate) const {return coordinate[dir_decay];}
+      int numSubsets() const {return Layout::lattSize()[dir_decay];}
  
-  int dir_decay;
+      int dir_decay;
  
-private:
-  TimeSliceFunc() {}  // hide default constructor
-};
+    private:
+      TimeSliceFunc() {}  // hide default constructor
+    };
+  }
 
 
-void local_scalar_loop::compute(LatticeStaggeredFermion & q_source, 
-				LatticeStaggeredFermion & psi, int isample)
-{
-  UnorderedSet timeslice;
-  timeslice.make(TimeSliceFunc(Nd-1));
+  void local_scalar_loop::compute(LatticeStaggeredFermion & q_source, 
+				  LatticeStaggeredFermion & psi, int isample)
+  {
+    UnorderedSet timeslice;
+    timeslice.make(TimeSliceFunc(Nd-1));
 
-  LatticeComplex TrG_s0 ; 
-  TrG_s0 = localInnerProduct(q_source, psi);
+    LatticeComplex TrG_s0 ; 
+    TrG_s0 = localInnerProduct(q_source, psi);
 
-  corr_fn[isample] = sumMulti(TrG_s0, timeslice);
-  corr += corr_fn[isample] ;
+    corr_fn[isample] = sumMulti(TrG_s0, timeslice);
+    corr += corr_fn[isample] ;
 
-}
+  }
 
 
 
-void non_local_scalar_loop::compute(LatticeStaggeredFermion & q_source, 
-				LatticeStaggeredFermion & psi, int isample)
-{
-  UnorderedSet timeslice;
-  timeslice.make(TimeSliceFunc(Nd-1));
+  void non_local_scalar_loop::compute(LatticeStaggeredFermion & q_source, 
+				      LatticeStaggeredFermion & psi, int isample)
+  {
+    UnorderedSet timeslice;
+    timeslice.make(TimeSliceFunc(Nd-1));
 
-  LatticeStaggeredFermion psi_sca1 ;
+    LatticeStaggeredFermion psi_sca1 ;
 
-  // Array to describe shifts in cube
-  multi1d<int> delta(Nd);
-  delta = 0;
-  delta[3] = 1;
-  psi_sca1 = shift_deltaProp(delta, psi); 
-  LatticeComplex TrG_s1 ; 
+    // Array to describe shifts in cube
+    multi1d<int> delta(Nd);
+    delta = 0;
+    delta[3] = 1;
+    psi_sca1 = shift_deltaProp(delta, psi); 
+    LatticeComplex TrG_s1 ; 
 
-  using namespace StagPhases;
-  TrG_s1 = alpha(3)*localInnerProduct(q_source, psi_sca1);
+    using namespace StagPhases;
+    TrG_s1 = alpha(3)*localInnerProduct(q_source, psi_sca1);
 
-  corr_fn[isample] = sumMulti(TrG_s1, timeslice);
-  corr += corr_fn[isample] ;
+    corr_fn[isample] = sumMulti(TrG_s1, timeslice);
+    corr += corr_fn[isample] ;
 
-}
+  }
 
 }  // end namespace Chroma
