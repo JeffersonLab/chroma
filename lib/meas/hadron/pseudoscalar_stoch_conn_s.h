8342d5dfd15ffbea6d0f898dc3862a03ba671f8a
egregory <egregory>
2006-03-13 14:39:04 +0000

    class for 4-link pseudoscalar tatse sinbglet stochastic connected correlator measurement.

---
+++ lib/meas/hadron/pseudoscalar_stoch_conn_s.h
@@ -0,0 +1,53 @@
+// $Id: pseudoscalar_stoch_conn_s.h,v 1.1 2006-03-13 14:39:04 egregory Exp $
+#ifndef PSEUDOSCALAR_STOCH_CONN_H
+#define PSEUDOSCALAR_STOCH_CONN_H
+
+#include "meas/hadron/stoch_conn_s.h"
+
+namespace Chroma {
+
+  class stoch_conn_corr ; 
+
+  class fourlink_pseudoscalar_stoch_conn  : public stoch_conn_corr
+  {
+  public :
+
+
+    void compute(LatticeStaggeredFermion & q_source1,
+		 LatticeStaggeredFermion & q_source2,
+		 LatticeStaggeredFermion & psi1,
+		 LatticeStaggeredFermion & psi2,
+		 int isample);
+
+    fourlink_pseudoscalar_stoch_conn(int t_len, int nsample,
+			       const multi1d<LatticeColorMatrix> & uin,
+			       Stag_shift_option type_of_shift_in)  
+      : stoch_conn_corr(t_len,nsample,uin,type_of_shift_in)
+      {
+
+	outer_tag = "stoch_conn_g5_cross_1"  ; 
+	inner_tag1 = "stoch_conn_corr1" ; 
+	inner_tag2 = "stoch_conn_corr2" ; 
+      }
+
+    virtual ~fourlink_pseudoscalar_stoch_conn()
+      {
+      }
+
+
+  protected:
+
+
+  } ; 
+
+
+
+
+
+
+
+
+
+}  // end namespace Chroma
+
+#endif
