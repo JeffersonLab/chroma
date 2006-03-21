5bc792e07ff01a38133ea3289d5059541960dd83
edwards <edwards>
2006-03-21 19:11:28 +0000

    Added doxygen comments.

--- lib/io/hamiltonian_io.h
+++ lib/io/hamiltonian_io.h
@@ -1,3 +1,8 @@
+// -*- C++ -*-
+// $Id: hamiltonian_io.h,v 2.1 2006-03-21 19:11:28 edwards Exp $
+/*! \file
+ *  \brief Hamiltonion IO
+ */
 
 #ifndef HAMILTONIAN_IO_H
 #define HAMILTONIAN_IO_H
@@ -7,8 +12,10 @@
 #include "update/molecdyn/hamiltonian/exact_hamiltonian.h"
 
 
-namespace Chroma { 
+namespace Chroma 
+{ 
 
+  /*! \ingroup io */
   void read(XMLReader& xml, const std::string& path, 
 	    Handle< ExactLatColMatHamiltonian >& H_handle);
 
--- lib/io/monomial_io.h
+++ lib/io/monomial_io.h
@@ -1,3 +1,9 @@
+// -*- C++ -*-
+// $Id: monomial_io.h,v 2.1 2006-03-21 19:11:28 edwards Exp $
+/*! \file
+ *  \brief Monomial IO
+ */
+
 #ifndef MONOMIAL_IO_H
 #define MONOMIAL_IO_H
 
@@ -5,15 +11,17 @@
 #include "update/molecdyn/monomial/abs_monomial.h"
 
 
-namespace Chroma { 
+namespace Chroma 
+{ 
 
+  /*! \ingroup io */
   void read(XMLReader& xml, 
 	    const std::string& path, 
 	    Handle< Monomial< multi1d<LatticeColorMatrix>, 
 	                      multi1d<LatticeColorMatrix> > >& mon_handle );
 
-  // Read a monomial from an XML reader, use a factory 
-  // to create and assign the pointer to the handle...
+  //! Read a monomial from an XML reader, use a factory to create and assign the pointer to the handle...
+  /*! \ingroup io */
   void read(XMLReader& xml, 
 	    const std::string& path, 
 	    Handle< ExactMonomial< multi1d<LatticeColorMatrix>, 
