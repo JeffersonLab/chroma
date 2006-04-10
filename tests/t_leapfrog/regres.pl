7e701c9f64334ab5df4531a9a9a7deeeeac939d7
edwards <edwards>
2006-04-10 23:48:16 +0000

    Updated and fixed name of SF regression test

--- tests/t_leapfrog/regres.pl
+++ tests/t_leapfrog/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 3.1 2006-04-10 21:06:34 edwards Exp $
+#  $Id: regres.pl,v 3.2 2006-04-10 23:48:16 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -29,10 +29,10 @@
      {
 	 exec_path   => "$top_builddir/mainprogs/tests" , 
 	 execute     => "t_leapfrog" , 
-	 input       => "$test_dir/t_leapfrog/t_leapfrog.prec_2flav_clover.sfnonpert.ini.xml" , 
-	 output      => "t_leapfrog.prec_2flav_clover.sfnonpert.candidate.xml",
-	 metric      => "$test_dir/t_leapfrog/t_leapfrog.prec_2flav_clover.sfnonpert.metric.xml" ,
-	 controlfile => "$test_dir/t_leapfrog/t_leapfrog.prec_2flav_clover.sfnonpert.out.xml" ,
+	 input       => "$test_dir/t_leapfrog/t_leapfrog.prec_2flav_clover.sfnonpt.ini.xml" , 
+	 output      => "t_leapfrog.prec_2flav_clover.sfnonpt.candidate.xml",
+	 metric      => "$test_dir/t_leapfrog/t_leapfrog.prec_2flav_clover.sfnonpt.metric.xml" ,
+	 controlfile => "$test_dir/t_leapfrog/t_leapfrog.prec_2flav_clover.sfnonpt.out.xml" ,
      },
 
 
