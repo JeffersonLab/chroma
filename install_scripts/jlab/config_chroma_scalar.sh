4c057e9bbd947ed197b92e4ac7584f83497d9bb5
edwards <edwards>
2006-05-22 14:45:03 +0000

    Modified for new jlab use

--- install_scripts/jlab/config_chroma_scalar.sh
+++ install_scripts/jlab/config_chroma_scalar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../configure --prefix=/usr/local/chroma/scalar --with-qdp=/usr/local/qdp++/scalar/qdp1-20-2 CXXFLAGS="" --enable-sse-wilson-dslash --enable-opt-cfz-linop --with-gmp=/usr
+../configure --prefix=$HOME/arch/chroma/scalar --with-qdp=/usr/local/qdp++/qdp1-20-5/scalar CXXFLAGS="" --enable-sse-wilson-dslash --enable-opt-cfz-linop --with-gmp=/usr  --enable-testcase-runner=trivial
 
