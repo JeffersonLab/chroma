7caa2b3badb2e6b660107bd0c251bdbb0f68389c
edwards <edwards>
2006-08-16 15:44:25 +0000

    Turned regression tests back on.

--- tests/regres.pl
+++ tests/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 3.11 2006-08-15 13:17:25 bjoo Exp $
+#  $Id: regres.pl,v 3.12 2006-08-16 15:44:25 edwards Exp $
 #
 #  This is the top-level script used by chroma/scripts/run_chroma_xmldiff.pl
 #
@@ -15,42 +15,42 @@ sub regresDirs
     # So, spell out all the many regression dirs and source them individually.
     #
 #    return (
-	    "$test_dir/chroma/hadron/propagator/regres.pl"
+#	    "$test_dir/chroma/hadron/propagator/regres.pl"
 #    );
 
-#     return ( 
-#	    "$test_dir/chroma/io/szin_write_obj/regres.pl",
-#	    "$test_dir/chroma/io/szin_read_obj/regres.pl",
-#	    "$test_dir/chroma/io/qio_write_obj/regres.pl",
-#	    "$test_dir/chroma/io/qio_read_obj/regres.pl",
-#	    "$test_dir/chroma/io/nersc_write_obj/regres.pl",
-#	    "$test_dir/chroma/io/nersc_read_obj/regres.pl",
-#	    "$test_dir/chroma/gfix/coulgauge/regres.pl",
-#	    "$test_dir/chroma/glue/fuzwilp/regres.pl",
-#	    "$test_dir/chroma/glue/wilslp/regres.pl",
-#	    "$test_dir/chroma/eig/regres.pl",
-#	    "$test_dir/chroma/hadron/make_source/regres.pl",
-#	    "$test_dir/chroma/hadron/propagator/regres.pl",
-#	    "$test_dir/chroma/hadron/sink_smear/regres.pl",
-#	    "$test_dir/chroma/hadron/seqsource/regres.pl",
-#	    "$test_dir/chroma/hadron/building_blocks/regres.pl",
-#	    "$test_dir/chroma/hadron/noisy_building_blocks/regres.pl",
-#	    "$test_dir/chroma/hadron/bar3ptfn/regres.pl",
-#	    "$test_dir/chroma/hadron/hadspec/regres.pl",
-#	    "$test_dir/chroma/hadron/spectrum/regres.pl",
-#	    "$test_dir/chroma/hadron/spectrumOct/regres.pl",
-#	    "$test_dir/chroma/hadron/mesonspec/regres.pl",
-#	    "$test_dir/chroma/hadron/mres/regres.pl",
-#	    "$test_dir/chroma/hadron/stoch_meson/regres.pl",
-#	    "$test_dir/chroma/hadron/stoch_baryon/regres.pl",
-#	    "$test_dir/chroma/hadron_s/spectrum_s/regres.pl",
-#	    "$test_dir/chroma/schrfun/regres.pl",
-#	    "$test_dir/chroma/smear/link_smear/regres.pl",
-#	    "$test_dir/t_leapfrog/regres.pl",
-#	    "$test_dir/spectrum_s/regres.pl",
-#	    "$test_dir/hmc/regres.pl",
-#	    "$test_dir/purgaug/regres.pl"
-#     );
+     return ( 
+	    "$test_dir/chroma/io/szin_write_obj/regres.pl",
+	    "$test_dir/chroma/io/szin_read_obj/regres.pl",
+	    "$test_dir/chroma/io/qio_write_obj/regres.pl",
+	    "$test_dir/chroma/io/qio_read_obj/regres.pl",
+	    "$test_dir/chroma/io/nersc_write_obj/regres.pl",
+	    "$test_dir/chroma/io/nersc_read_obj/regres.pl",
+	    "$test_dir/chroma/gfix/coulgauge/regres.pl",
+	    "$test_dir/chroma/glue/fuzwilp/regres.pl",
+	    "$test_dir/chroma/glue/wilslp/regres.pl",
+	    "$test_dir/chroma/eig/regres.pl",
+	    "$test_dir/chroma/hadron/make_source/regres.pl",
+	    "$test_dir/chroma/hadron/propagator/regres.pl",
+	    "$test_dir/chroma/hadron/sink_smear/regres.pl",
+	    "$test_dir/chroma/hadron/seqsource/regres.pl",
+	    "$test_dir/chroma/hadron/building_blocks/regres.pl",
+	    "$test_dir/chroma/hadron/noisy_building_blocks/regres.pl",
+	    "$test_dir/chroma/hadron/bar3ptfn/regres.pl",
+	    "$test_dir/chroma/hadron/hadspec/regres.pl",
+	    "$test_dir/chroma/hadron/spectrum/regres.pl",
+	    "$test_dir/chroma/hadron/spectrumOct/regres.pl",
+	    "$test_dir/chroma/hadron/mesonspec/regres.pl",
+	    "$test_dir/chroma/hadron/mres/regres.pl",
+	    "$test_dir/chroma/hadron/stoch_meson/regres.pl",
+	    "$test_dir/chroma/hadron/stoch_baryon/regres.pl",
+	    "$test_dir/chroma/hadron_s/spectrum_s/regres.pl",
+	    "$test_dir/chroma/schrfun/regres.pl",
+	    "$test_dir/chroma/smear/link_smear/regres.pl",
+	    "$test_dir/t_leapfrog/regres.pl",
+	    "$test_dir/spectrum_s/regres.pl",
+	    "$test_dir/hmc/regres.pl",
+	    "$test_dir/purgaug/regres.pl"
+     );
 }
 
 # Return a true value for a require statement
