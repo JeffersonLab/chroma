0fc2185eba6e5967482aa7013b25928e1eea72cb
edwards <edwards>
2006-08-26 05:51:10 +0000

    Doxygen fix.

--- lib/meas/gfix/polar_dec.h
+++ lib/meas/gfix/polar_dec.h
@@ -1,19 +1,19 @@
 // -*- C++ -*-
-// $Id: polar_dec.h,v 3.0 2006-04-03 04:58:57 edwards Exp $
+// $Id: polar_dec.h,v 3.1 2006-08-26 05:51:10 edwards Exp $
 /*! \file
- *  \brief Decompose a complex matrix as C = exp(i\alpha) V P
+ *  \brief Decompose a complex matrix as \f$C = exp(i\alpha) V P\f$
  */
 
 #ifndef __polar_dec_h__
 #define __polar_dec_h__
 
 namespace Chroma {
-//! Decompose a complex matrix as C = exp(i\alpha) V P
+//! Decompose a complex matrix as \f$C = exp(i\alpha) V P\f#
 /*!
  * \ingroup gfix
  *
- * Decompose a complex matrix as C = exp(i\alpha) V P
- * with V SU(Nc) and P = (C^\dagger C)^{1/2} positive hermitian
+ * Decompose a complex matrix as \f$C = exp(i\alpha) V P\f#
+ * with V SU(Nc) and \f#P = (C^\dagger C)^{1/2}\f# positive hermitian
  *
  * \param c        complex Nc x Nc matrix ( Modify )
  *                 on exit it contains the hermitian matrix P
@@ -26,5 +26,5 @@ namespace Chroma {
 void polar_dec(LatticeColorMatrix& c, LatticeColorMatrix& v,
 	       LatticeReal& alpha, const Real& JacAccu, int JacMax);
 
-};
+}
 #endif
