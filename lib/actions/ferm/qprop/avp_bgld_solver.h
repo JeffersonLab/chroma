c62dba57e067ad3870263aac40f831e8ea63ecac
bjoo <bjoo>
2006-09-18 20:28:31 +0000

    Initial conversions for BlueGene

---
+++ lib/actions/ferm/qprop/avp_bgld_solver.h
@@ -0,0 +1,57 @@
+#ifndef AVP_BGLD_SOLVER_H
+#define AVP_BGLD_SOLVER_H
+
+#include "actions/ferm/qprop/avp_inverter_interface.h"
+
+extern "C" {
+  struct MIT_bluelightd_DWF_Gauge;
+  struct MIT_bluelightd_DWF_Fermion;
+};
+
+
+using namespace QDP;
+namespace Chroma { 
+  namespace AVPSolver { 
+    
+    class BGLDWFSolverD : public AVPSolverInterface< MIT_bluelightd_DWF_Gauge, MIT_bluelightd_DWF_Fermion > {
+    public:
+    protected:
+      MIT_bluelightd_DWF_Fermion* loadFermionRHS(const void* OuterFermion) const; 
+      MIT_bluelightd_DWF_Fermion* loadFermionGuess(const void *OuterFermion) const;
+      MIT_bluelightd_DWF_Fermion* allocateFermion(void) const;
+      void saveFermionSolver(void *OuterFermion, 
+		       MIT_bluelightd_DWF_Fermion* CGFermion) const;
+
+      void saveFermionOperator(void *OuterFermion, 
+			       MIT_bluelightd_DWF_Fermion* CGFermion) const;
+      void deleteFermion(MIT_bluelightd_DWF_Fermion* ptr) const;
+      int cgInternal(MIT_bluelightd_DWF_Fermion       *psi,
+		     double        *out_eps,
+		     int           *out_iter,
+		     double        M,
+		     double        m_f,
+		     const MIT_bluelightd_DWF_Fermion *x0,
+		     const MIT_bluelightd_DWF_Fermion *eta,
+		     double        eps,
+		     int           min_iter,
+		     int           max_iter)  const;
+
+    public:
+     void loadGauge(const void *u,
+		    const void *v);
+     
+     void deleteGauge(void);
+
+     int init(const int lattice[5],
+	      void *(*allocator)(size_t size),
+	      void (*deallocator)(void *));
+     
+     // Finalize - destructor call
+     void fini(void);
+    private:
+     MIT_bluelightd_DWF_Gauge *g;
+    };
+  };
+};
+
+#endif
---
+++ lib/actions/ferm/qprop/avp_bglf_solver.cc
@@ -0,0 +1,74 @@
+#include "avp_bglf_solver.h"
+
+using namespace QDP;
+namespace Chroma { 
+  namespace AVPSolver { 
+    
+#include <dwf-bluelightf.h>
+
+    MIT_bluelightf_DWF_Fermion* BGLDWFSolverF::loadFermionRHS(const void* OuterFermion) const {
+      return MIT_bluelightf_DWF_load_fermion(OuterFermion, NULL, &AVPSolverFunctions::fermionReaderRHS);
+    }
+
+    MIT_bluelightf_DWF_Fermion* BGLDWFSolverF::loadFermionGuess(const void *OuterFermion) const {
+	return MIT_bluelightf_DWF_load_fermion(OuterFermion, NULL, &AVPSolverFunctions::fermionReaderGuess);
+    }
+
+    MIT_bluelightf_DWF_Fermion* BGLDWFSolverF::allocateFermion(void) const {
+      return MIT_bluelightf_DWF_allocate_fermion();
+    }
+
+    void BGLDWFSolverF::saveFermionSolver(void *OuterFermion, 
+					  MIT_bluelightf_DWF_Fermion* CGFermion) const {
+      MIT_bluelightf_DWF_save_fermion(OuterFermion, NULL, &AVPSolverFunctions::fermionWriterSolver, CGFermion);
+    }
+
+    void BGLDWFSolverF::saveFermionOperator(void *OuterFermion, 
+					    MIT_bluelightf_DWF_Fermion* CGFermion) const {
+      MIT_bluelightf_DWF_save_fermion(OuterFermion, NULL, &AVPSolverFunctions::fermionWriterOperator, CGFermion);
+    }
+    
+    void BGLDWFSolverF::deleteFermion(MIT_bluelightf_DWF_Fermion* ptr) const {
+      MIT_bluelightf_DWF_delete_fermion(ptr);
+    }
+
+    
+    int BGLDWFSolverF::cgInternal(MIT_bluelightf_DWF_Fermion       *psi,
+				  double        *out_eps,
+				  int           *out_iter,
+				  double        M,
+				  double        m_f,
+				  const MIT_bluelightf_DWF_Fermion *x0,
+				  const MIT_bluelightf_DWF_Fermion *eta,
+				  double        eps,
+				  int           min_iter,
+				  int           max_iter)  const {
+      return MIT_bluelightf_DWF_cg_solver(psi, out_eps, out_iter, g, M, m_f,
+				    x0, eta, eps, min_iter, max_iter);
+    }
+    
+    void BGLDWFSolverF::loadGauge(const void *u,
+				  const void *v) { 
+      g=MIT_bluelightf_DWF_load_gauge(u, v, NULL, &AVPSolverFunctions::gaugeReader);
+    }
+     
+    void BGLDWFSolverF::deleteGauge(void) {
+      MIT_bluelightf_DWF_delete_gauge(g);
+    }
+
+     
+    // Init the system -- Constructor call?
+    int BGLDWFSolverF::init(const int lattice[5],
+			    void *(*allocator)(size_t size),
+			    void (*deallocator)(void *)) {
+      return MIT_bluelightf_DWF_init(lattice, allocator, deallocator);
+    }
+     
+    // Finalize - destructor call
+    void BGLDWFSolverF::fini(void) {
+      MIT_bluelightf_DWF_fini();
+    }
+  };
+};
+
+
