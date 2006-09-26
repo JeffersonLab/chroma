38ef08207ca0cd9fa81c5637dbcd3f66b3cc77b6
edwards <edwards>
2006-09-26 13:47:27 +0000

    Fixed to point to gauge state tests and not fermstate!

--- tests/chroma/glue/gaugestate/regres.pl
+++ tests/chroma/glue/gaugestate/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 1.1 2006-09-21 20:18:15 edwards Exp $
+#  $Id: regres.pl,v 1.2 2006-09-26 13:47:27 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -13,9 +13,9 @@
      {
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "chroma" , 
-	 input       => "$test_dir/chroma/hadron/fermstate/fermstate.ini.xml" , 
-	 output      => "fermstate.candidate.xml",
-	 metric      => "$test_dir/chroma/hadron/fermstate/fermstate.metric.xml" ,
-	 controlfile => "$test_dir/chroma/hadron/fermstate/fermstate.out.xml" ,
+	 input       => "$test_dir/chroma/glue/gaugestate/gaugestate.ini.xml" , 
+	 output      => "gaugestate.candidate.xml",
+	 metric      => "$test_dir/chroma/glue/gaugestate/gaugestate.metric.xml" ,
+	 controlfile => "$test_dir/chroma/glue/gaugestate/gaugestate.out.xml" ,
      }
      );
