2486d28fe161ed33aaacd68420d7468a573d1206
edwards <edwards>
2006-10-15 16:18:00 +0000

    Updated scripts

--- install_scripts/jlab/config_chroma_scalar.sh
+++ install_scripts/jlab/config_chroma_scalar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-../configure --prefix=$HOME/arch/chroma/scalar --with-qdp=/usr/local/qdp++/qdp1-20-5/scalar CXXFLAGS="" --enable-sse-wilson-dslash --enable-opt-cfz-linop --with-gmp=/usr  --enable-testcase-runner=trivial
+../configure --prefix=$HOME/arch/chroma/scalar --with-qdp=/dist/scidac/qdp++/qdp1-21-3/scalar CXXFLAGS="" --enable-sse-wilson-dslash --enable-opt-cfz-linop --with-gmp=/usr  --enable-testcase-runner=trivial
 
