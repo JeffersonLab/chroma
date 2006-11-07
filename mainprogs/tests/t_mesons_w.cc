41b4c9796d39b57ec8314b2a98d03b581ffff440
edwards <edwards>
2006-11-07 21:50:10 +0000

    Removed Log stuff.

--- mainprogs/tests/t_mesons_w.cc
+++ mainprogs/tests/t_mesons_w.cc
@@ -1,49 +1,7 @@
-// $Id: t_mesons_w.cc,v 3.0 2006-04-03 04:59:15 edwards Exp $
-//
-//! \file
-//  \brief Test the Wilson mesons() routine
-//
-// $Log: t_mesons_w.cc,v $
-// Revision 3.0  2006-04-03 04:59:15  edwards
-// Major overhaul of fermion and gauge action interface. Basically,
-// all fermacts and gaugeacts now carry out  <T,P,Q>  template parameters. These are
-// the fermion type, the "P" - conjugate momenta, and "Q" - generalized coordinates
-// in the sense of Hamilton's equations. The fermbc's have been rationalized to never
-// be over multi1d<T>. The "createState" within the FermionAction is now fixed meaning
-// the "u" fields are now from the coordinate type. There are now "ConnectState" that
-// derive into FermState<T,P,Q> and GaugeState<P,Q>.
-//
-// Revision 2.0  2005/09/25 21:04:47  edwards
-// Moved to version 2.0
-//
-// Revision 1.8  2005/03/02 00:44:19  edwards
-// Changed to new Chroma initialize/finalize format. Changed
-// all XMLReader("DATA") to use a command-line param arg.
-// Changed all XMLFileWriter(XMLDAT) to use the singleton instance.
-//
-// Revision 1.7  2005/01/14 20:13:09  edwards
-// Removed all using namespace QDP/Chroma from lib files. The library
-// should now be 100% in the Chroma namespace. All mainprogs need a
-// using namespace Chroma.
-//
-// Revision 1.6  2004/02/11 12:51:35  bjoo
-// Stripped out Read() and Write()
-//
-// Revision 1.5  2003/10/09 20:36:49  edwards
-// Changed all cout/cerr to QDPIO::cout/cerr. Changed QDP_info calls
-// to QDPIO::cout.
-//
-// Revision 1.4  2003/09/11 00:46:04  edwards
-// Changed all programs to exit(0) instead of return 0
-//
-// Revision 1.3  2003/06/24 03:24:44  edwards
-// Changed from nml to xml.
-//
-// Revision 1.2  2003/03/14 05:14:32  flemingg
-// rewrite of mesons_w.cc to use the new SftMom class.  mesons_w.cc still
-// needs to be cleaned up once the best strategy is resolved.  But for now,
-// the library and test program compiles and runs.
-//
+// $Id: t_mesons_w.cc,v 3.1 2006-11-07 21:50:10 edwards Exp $
+/*! \file
+ *  \brief Test the Wilson mesons() routine
+ */
 
 #include <iostream>
 #include <cstdio>
