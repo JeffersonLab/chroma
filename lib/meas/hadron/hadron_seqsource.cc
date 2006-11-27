cb5c35946cd91f5cae4572b8cce159ebd63916ad
edwards <edwards>
2006-11-27 20:08:31 +0000

    Split off phase stuff to a call to  singlePhase().

--- lib/meas/hadron/hadron_seqsource.cc
+++ lib/meas/hadron/hadron_seqsource.cc
@@ -1,9 +1,9 @@
-// $Id: hadron_seqsource.cc,v 3.6 2006-11-27 04:33:35 edwards Exp $
+// $Id: hadron_seqsource.cc,v 3.7 2006-11-27 20:08:31 edwards Exp $
 /*! \file
  *  \brief Construct hadron sequential sources
  */
 
-#include "chromabase.h"
+#include "util/ft/single_phase.h"
 #include "meas/hadron/hadron_seqsource.h"
 
 namespace Chroma 
@@ -13,52 +13,6 @@ namespace Chroma
   /*! @ingroup hadron */
   namespace
   {
-    //! Construct hadron sequential sources
-    /*! @ingroup hadron */
-    LatticeComplex hadSeqSourcePhase(const multi1d<int>& t_srce, 
-				     const multi1d<int>& sink_mom, 
-				     int j_decay)
-    {
-      START_CODE();
-
-      if (j_decay < 0 || j_decay >= Nd)
-      {
-	QDPIO::cerr << __func__ << ": j_decay out of bounds" << endl;
-	QDP_abort(1);
-      }
-
-      /*
-       *  We now inject momentum at sink if required
-       */
-      LatticeReal p_dot_x = zero;
-      bool nonzeroP = false;
-      for(int mu=0, j=0; mu < Nd; mu++)
-      {
-	if (mu != j_decay)
-	{
-	  if (sink_mom[j] != 0)
-	  {
-	    nonzeroP = true;
-	    p_dot_x += (Layout::latticeCoordinate(mu) - t_srce[mu]) * sink_mom[j] 
-	      * twopi / Real(Layout::lattSize()[mu]);
-	  }
-	  j++;
-	}
-      }
-
-      LatticeComplex phase;
-
-      if (nonzeroP)
-	phase = cmplx(cos(p_dot_x),sin(p_dot_x));
-      else
-	phase = 1.0;
-
-      END_CODE();
-
-      return phase;
-    }
-
-
     //! Construct hadron sequential sources
     /*! @ingroup hadron */
     template<typename T> 
@@ -134,7 +88,7 @@ namespace Chroma
   LatticeComplex
   HadronSeqSource<LatticePropagator>::phases() const
   {
-    return hadSeqSourcePhase(getTSrce(), getSinkMom(), getDecayDir());
+    return singlePhase(getTSrce(), getSinkMom(), getDecayDir());
   }
 
 
