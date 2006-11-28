463c89638b4795839594b1f738be13a16483830e
bjoo <bjoo>
2006-11-28 16:51:57 +0000

    Oops

--- tests/regres.pl
+++ tests/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 3.18 2006-11-28 16:49:43 bjoo Exp $
+#  $Id: regres.pl,v 3.19 2006-11-28 16:51:57 bjoo Exp $
 #
 #  This is the top-level script used by chroma/scripts/run_chroma_xmldiff.pl
 #
@@ -14,11 +14,11 @@ sub regresDirs
     # includes other files (in subdirs) is not how the perl "do" works.
     # So, spell out all the many regression dirs and source them individually.
     #
-    return (
-	    "$test_dir/hmc/regres.pl",
+#    return (
+#	    "$test_dir/hmc/regres.pl",
 #	    "$test_dir/chroma/hadron_s/propagator/regres.pl",
 #	    "$test_dir/chroma/hadron_s/sink_smear/regres.pl",
-    );
+#    );
 
      return ( 
 	    "$test_dir/chroma/io/szin_write_obj/regres.pl",
