af4ecc1cf2f5df602c96da221ea4eb3b56815884
edwards <edwards>
2006-12-02 21:40:29 +0000

    Updated.

--- tests/hmc/regres.pl
+++ tests/hmc/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 3.1 2006-11-28 16:45:32 bjoo Exp $
+#  $Id: regres.pl,v 3.2 2006-12-02 21:40:30 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -14,9 +14,9 @@
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "hmc" , 
 	 input       => "$test_dir/hmc/hmc.prec_wilson.ini.xml" , 
-	 output      => "hmc.prec_wilson.candidate.xml",
+	 log         => "hmc.prec_wilson.candidate.xml",
 	 metric      => "$test_dir/hmc/hmc.prec_wilson.metric.xml" ,
-	 controlfile => "$test_dir/hmc/hmc.prec_wilson.out.xml" ,
+	 controlfile => "$test_dir/hmc/hmc.prec_wilson.log.xml" ,
      },
      {
 	 exec_path   => "$top_builddir/mainprogs/main" ,
