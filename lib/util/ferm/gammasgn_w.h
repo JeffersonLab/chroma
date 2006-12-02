7d0e764494638a2df714dd52a3a82f458ad40250
edwards <edwards>
2006-12-02 18:13:37 +0000

    Indented.

--- lib/util/ferm/gammasgn_w.h
+++ lib/util/ferm/gammasgn_w.h
@@ -1,22 +1,23 @@
 // -*- C++ -*-
-// $Id: gammasgn_w.h,v 3.0 2006-04-03 04:59:11 edwards Exp $
+// $Id: gammasgn_w.h,v 3.1 2006-12-02 18:13:37 edwards Exp $
 /*! \file
  *  \brief Compute gamma matrix multiplication table factors
  */
 
-#ifndef __gammasgn_h__
-#define __gammasgn_h__
+#ifndef __gammasgn_w_h__
+#define __gammasgn_w_h__
 
-namespace Chroma {
+namespace Chroma 
+{
 
-//! Return gamma matrix multiplication table factors
-/*!
- * \ingroup ferm
- *
- * Initialize signs needed for  Gamma(n)*Gamma(m)=sgn(n,m)*Gamma(n ^ m)
- */
+  //! Return gamma matrix multiplication table factors
+  /*!
+   * \ingroup ferm
+   *
+   * Initialize signs needed for  Gamma(n)*Gamma(m)=sgn(n,m)*Gamma(n ^ m)
+   */
 
-int gammaSgn(int n, int m);
+  int gammaSgn(int n, int m);
 
 }  // end namespace Chroma
 
