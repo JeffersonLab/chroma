6b2b144695a4d4d7fb8fb81449984b75b8c60e04
edwards <edwards>
2006-12-07 18:24:44 +0000

    Added a staggered version.

--- lib/actions/ferm/invert/syssolver_linop_cg.cc
+++ lib/actions/ferm/invert/syssolver_linop_cg.cc
@@ -1,4 +1,4 @@
-// $Id: syssolver_linop_cg.cc,v 3.2 2006-09-20 20:28:00 edwards Exp $
+// $Id: syssolver_linop_cg.cc,v 3.3 2006-12-07 18:24:44 edwards Exp $
 /*! \file
  *  \brief Solve a M*psi=chi linear system by CG2
  */
@@ -22,6 +22,15 @@ namespace Chroma
       return new LinOpSysSolverCG<LatticeFermion>(A, SysSolverCGParams(xml_in, path));
     }
 
+    //! Callback function
+    LinOpSystemSolver<LatticeStaggeredFermion>* createStagFerm(
+      XMLReader& xml_in,
+      const std::string& path,
+      Handle< LinearOperator<LatticeStaggeredFermion> > A)
+    {
+      return new LinOpSysSolverCG<LatticeStaggeredFermion>(A, SysSolverCGParams(xml_in, path));
+    }
+
     //! Name to be used
     const std::string name("CG_INVERTER");
 
@@ -35,6 +44,7 @@ namespace Chroma
       if (! registered)
       {
 	success &= Chroma::TheLinOpFermSystemSolverFactory::Instance().registerObject(name, createFerm);
+	success &= Chroma::TheLinOpStagFermSystemSolverFactory::Instance().registerObject(name, createStagFerm);
 	registered = true;
       }
       return success;
