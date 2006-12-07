df619edf58289d22b525b7ecc70ba5a758193235
edwards <edwards>
2006-12-07 18:36:53 +0000

    Added a Klein-Gordon operator.

--- tests/chroma/hadron_s/propagator/regres.pl
+++ tests/chroma/hadron_s/propagator/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 1.1 2006-11-17 02:10:16 edwards Exp $
+#  $Id: regres.pl,v 1.2 2006-12-07 18:36:53 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -17,5 +17,13 @@
 	 output      => "asqtad.candidate.xml",
 	 metric      => "$test_dir/chroma/hadron_s/propagator/asqtad.metric.xml" ,
 	 controlfile => "$test_dir/chroma/hadron_s/propagator/asqtad.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron_s/propagator/klein_gordon.ini.xml" , 
+	 output      => "klein_gordon.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron_s/propagator/klein_gordon.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron_s/propagator/klein_gordon.out.xml" ,
      }
      );
