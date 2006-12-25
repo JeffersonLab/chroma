6241f524ad146ee9161ed4db75585c0f85380297
bjoo <bjoo>
2006-12-25 21:08:35 +0000

    Removed old HMC Files

--- lib/update/molecdyn/hmc/hmc.h
+++ lib/update/molecdyn/hmc/hmc.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: hmc.h,v 3.1 2006-11-28 16:49:42 bjoo Exp $
+// $Id: hmc.h,v 3.2 2006-12-25 21:08:35 bjoo Exp $
 
 /*! \file
  * \brief HMC trajectories
@@ -16,9 +16,7 @@
 #ifndef HMC_H
 #define HMC_H
 
-#include "update/molecdyn/hmc/abs_hmc.h"
 #include "update/molecdyn/hmc/abs_hmc_new.h"
-#include "update/molecdyn/hmc/lcm_hmc.h"
 #include "update/molecdyn/hmc/lcm_hmc_new.h"
 
 #endif
