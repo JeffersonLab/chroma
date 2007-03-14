7783c521a482408830de456deb196651953f99dd
bjoo <bjoo>
2007-03-14 18:21:47 +0000

    Renamed spectrum_s to inline spectrum_s

--- tests/chroma/hadron_s/spectrum_s/regres.pl
+++ tests/chroma/hadron_s/spectrum_s/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 3.0 2006-04-03 04:59:28 edwards Exp $
+#  $Id: regres.pl,v 3.1 2007-03-14 18:21:47 bjoo Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -14,7 +14,7 @@
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "chroma" , 
 	 input       => "$test_dir/chroma/hadron_s/spectrum_s/spectrum_s.ini.xml" , 
-	 output      => "spectrum_s.candidate.xml",
+	 output      => "inline_spectrum_s.candidate.xml",
 	 metric      => "$test_dir/chroma/hadron_s/spectrum_s/spectrum_s.metric.xml" ,
 	 controlfile => "$test_dir/chroma/hadron_s/spectrum_s/spectrum_s.out.xml" ,
      }
