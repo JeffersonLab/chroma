41ae3e91274158d727e5fe2382b4abe5577cba7d
egregory <egregory>
2007-04-11 21:44:46 +0000

    put in fuzzed scalar loops

--- lib/meas/hadron/scalar_loops_s.cc
+++ lib/meas/hadron/scalar_loops_s.cc
@@ -1,4 +1,4 @@
-// $Id: scalar_loops_s.cc,v 3.1 2007-02-22 21:11:49 bjoo Exp $
+// $Id: scalar_loops_s.cc,v 3.2 2007-04-11 21:44:46 egregory Exp $
 
 #include "chromabase.h"
 #include"scalar_loops_s.h"
@@ -56,6 +56,50 @@ namespace Chroma
     delta = 0;
     delta[3] = 1;
     psi_sca1 = shift_deltaProp(delta, psi); 
+    LatticeComplex TrG_s1 ; 
+
+    using namespace StagPhases;
+    TrG_s1 = alpha(3)*localInnerProduct(q_source, psi_sca1);
+
+    corr_fn[isample] = sumMulti(TrG_s1, timeslice);
+    corr += corr_fn[isample] ;
+
+  }
+
+
+  //now the fuzzed computes, which are the same, basically
+
+  void local_scalar_loop_fuzz::compute(LatticeStaggeredFermion & q_source, 
+				  LatticeStaggeredFermion & psi_fuzz, 
+				       int isample)
+  {
+    UnorderedSet timeslice;
+    timeslice.make(TimeSliceFunc(Nd-1));
+
+    LatticeComplex TrG_s0 ; 
+    TrG_s0 = localInnerProduct(q_source, psi_fuzz);
+
+    corr_fn[isample] = sumMulti(TrG_s0, timeslice);
+    corr += corr_fn[isample] ;
+
+  }
+
+
+
+  void non_local_scalar_loop_fuzz::compute(LatticeStaggeredFermion & q_source, 
+				      LatticeStaggeredFermion & psi_fuzz, 
+					   int isample)
+  {
+    UnorderedSet timeslice;
+    timeslice.make(TimeSliceFunc(Nd-1));
+
+    LatticeStaggeredFermion psi_sca1 ;
+
+    // Array to describe shifts in cube
+    multi1d<int> delta(Nd);
+    delta = 0;
+    delta[3] = 1;
+    psi_sca1 = shift_deltaProp(delta, psi_fuzz); 
     LatticeComplex TrG_s1 ; 
 
     using namespace StagPhases;
