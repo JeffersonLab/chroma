58379a31710d391763043d8938bd2b4bba14dd0e
edwards <edwards>
2007-04-11 03:41:01 +0000

    Updated to new API.

--- lib/actions/ferm/invert/invmr.cc
+++ lib/actions/ferm/invert/invmr.cc
@@ -1,5 +1,4 @@
-// $Id: invmr.cc,v 3.1 2007-02-22 21:11:46 bjoo Exp $
-
+// $Id: invmr.cc,v 3.2 2007-04-11 03:41:01 edwards Exp $
 /*! \file
  *  \brief Minimal-Residual (MR) for a generic fermion Linear Operator
  */
@@ -7,143 +6,206 @@
 #include "chromabase.h"
 #include "actions/ferm/invert/invmr.h"
 
-namespace Chroma {
+using namespace QDP::Hints;
 
-/*! \ingroup invert
- * This subroutine uses the Minimal Residual (MR) algorithm to determine
- * the solution of the set of linear equations. Here we allow A to be nonhermitian.
-
- *   	    Chi  =  A . Psi 
-
- * Algorithm:
-
- *  Psi[0]                                      Argument
- *  r[0]    :=  Chi  -  A . Psi[0] ;            Initial residual
- *  IF |r[0]| <= RsdMR |Chi| THEN RETURN;       Converged?
- *  FOR k FROM 1 TO MaxCG DO                    MR iterations
- *      a[k-1]  := <A.r[k-1],r[k-1]> / <A.r[k-1],A.r[k-1]> ;
- *      ap[k-1] := MRovpar * a[k] ;             Overrelaxtion step
- *      Psi[k]  += ap[k-1] r[k-1] ;   	        New solution vector
- *      r[k]    -= ap[k-1] A . r[k-1] ;         New residual
- *      IF |r[k]| <= RsdMR |Chi| THEN RETURN;   Converged?
-
- * Arguments:
-
- *  \param M       Linear Operator    	       (Read)
- *  \param chi     Source	               (Read)
- *  \param psi     Solution    	    	       (Modify)
- *  \param RsdMR   MR residual accuracy        (Read)
- *  \param MRovpar Overrelaxation parameter    (Read)
- *  \param MaxCG   Maximum CG iterations       (Read)
- *  \param n_count Number of CG iteration      (Write)
-
- * Local Variables:
-
- *  r   	Residual vector
- *  cp  	| r[k] |**2
- *  c   	| r[k-1] |**2
- *  k   	MR iteration counter
- *  a   	a[k]
- *  d   	< M.r[k], M.r[k] >
- *  R_Aux       Temporary for  A.Psi
- *  Mr 	        Temporary for  A.r
-
- * Global Variables:
-
- *  MaxCG       Maximum number of MR iterations allowed
- *  RsdMR       Maximum acceptable MR residual (relative to source)
- *
- * Subroutines:
- *
- *  A           Apply matrix to vector
- *
- * Operations:
- *
- *  ???? 
- */
-
-void InvCG2(const LinearOperator& A,
-	    const LatticeFermion& chi,
-	    LatticeFermion& psi,
-	    const Real& RsdMR, 
-	    const Real& MRovpar;
-	    int MaxCG, 
-	    int& n_count)
+namespace Chroma 
 {
-  START_CODE();
 
-  const Subset& s = M.subset();
+  //! Minimal-residual (MR) algorithm for a generic Linear Operator 
+  /*! \ingroup invert
+   * This subroutine uses the Minimal Residual (MR) algorithm to determine
+   * the solution of the set of linear equations. Here we allow M to be nonhermitian.
+   *
+   *   	    Chi  =  M . Psi 
+   *
+   * Algorithm:
+   *
+   *  Psi[0]                                      Argument
+   *  r[0]    :=  Chi  -  M . Psi[0] ;            Initial residual
+   *  IF |r[0]| <= RsdCG |Chi| THEN RETURN;       Converged?
+   *  FOR k FROM 1 TO MaxCG DO                    MR iterations
+   *      a[k-1]  := <M.r[k-1],r[k-1]> / <M.r[k-1],M.r[k-1]> ;
+   *      ap[k-1] := MRovpar * a[k] ;             Overrelaxtion step
+   *      Psi[k]  += ap[k-1] r[k-1] ;   	        New solution vector
+   *      r[k]    -= ap[k-1] A . r[k-1] ;         New residual
+   *      IF |r[k]| <= RsdCG |Chi| THEN RETURN;   Converged?
 
-  LatticeFermion Ar;
-  Complex a;
-  DComplex c;
-  Double d;
-  int k;
+   * Arguments:
 
-  Real rsd_sq = (RsdMR * RsdMR) * Real(norm2(chi,s));
+   *  \param M       Linear Operator             (Read)
+   *  \param chi     Source                      (Read)
+   *  \param psi     Solution                    (Modify)
+   *  \param RsdCG   MR residual accuracy        (Read)
+   *  \param MRovpar Overrelaxation parameter    (Read)
+   *  \param MaxCG   Maximum MR iterations       (Read)
+
+   * Local Variables:
+
+   *  r   	Residual vector
+   *  cp  	| r[k] |**2
+   *  c   	| r[k-1] |**2
+   *  k   	MR iteration counter
+   *  a   	a[k]
+   *  d   	< M.r[k], M.r[k] >
+   *  R_Aux     Temporary for  M.Psi
+   *  Mr        Temporary for  M.r
+
+   * Global Variables:
+
+   *  MaxCG       Maximum number of MR iterations allowed
+   *  RsdCG       Maximum acceptable MR residual (relative to source)
+   *
+   * Subroutines:
+   *
+   *  M           Apply matrix to vector
+   *
+   * @{
+   */
+
+  template<typename T, typename C>
+  SystemSolverResults_t 
+  InvMR_a(const C& M,
+	  const T& chi,
+	  T& psi,
+	  const Real& MRovpar,
+	  const Real& RsdCG, 
+	  int MaxCG)
+  {
+    START_CODE();
+
+    const Subset& s = M.subset();
+
+    SystemSolverResults_t  res;
+    moveToFastMemoryHint(psi,true);
+    T Mr;                moveToFastMemoryHint(Mr);
+    T chi_internal;      moveToFastMemoryHint(chi_internal);
+
+    chi_internal[s] = chi;
+
+    Complex a;
+    DComplex c;
+    Double d;
+    int k;
+
+    QDPIO::cout << "InvMR: starting" << endl;
+    FlopCounter flopcount;
+    flopcount.reset();
+    StopWatch swatch;
+    swatch.reset();
+    swatch.start();
+
+    Real rsd_sq = (RsdCG * RsdCG) * Real(norm2(chi_internal,s));
+    flopcount.addSiteFlops(4*Nc*Ns,s);
         
-  /*  r[0]  :=  Chi - A . Psi[0] */
-  /*  r  :=  A . Psi  */
-  LatticeFermion r;
-  r[s] = chi - A(psi, PLUS);
-  
-  /*  Cp = |r[0]|^2 */
-  Double cp = norm2(r, s);                 /* 2 Nc Ns  flops */
+    /*  r[0]  :=  Chi - M . Psi[0] */
+    /*  r  :=  M . Psi  */
+    M(Mr, psi, PLUS);
+    flopcount.addFlops(M.nFlops());
 
-  /*  IF |r[0]| <= RsdMR |Chi| THEN RETURN; */
-  /*#erite(nml_out,*) "&Minimal_Residual_iterations"; */
-  /*#printf("InvMRm: k = 0  cp = %g\n", cp); */
-  /*#write(nml_out,*) "ClovInv: k = 0  cp = ", cp; */
+    T r;                moveToFastMemoryHint(r);
+    r[s] = chi_internal - Mr;
+    flopcount.addSiteFlops(2*Nc*Ns,s);
   
-  if ( toBool(cp  <=  rsd_sq) )
-  {
-    n_count = 0;
+    /*  Cp = |r[0]|^2 */
+    Double cp = norm2(r, s);                 /* 2 Nc Ns  flops */
+    flopcount.addSiteFlops(4*Nc*Ns, s);
+
+//  QDPIO::cout << "InvMR: k = 0  cp = " << cp << "  rsd_sq = " << rsd_sq << endl;
+
+    /*  IF |r[0]| <= RsdCG |Chi| THEN RETURN; */
+    if ( toBool(cp  <=  rsd_sq) )
+    {
+      res.n_count = 0;
+      res.resid   = sqrt(cp);
+      swatch.stop();
+      flopcount.report("invcg2", swatch.getTimeInSeconds());
+      revertFromFastMemoryHint(psi,true);
+      END_CODE();
+      return res;
+    }
+
+    /*  FOR k FROM 1 TO MaxCG DO */
+    k = 0;
+    while( (k < MaxCG) && (toBool(cp > rsd_sq)) )
+    {
+      ++k;
+
+      /*  a[k-1] := < M.r[k-1], r[k-1] >/ < M.r[k-1], M.r[k-1] > ; */
+      /*  Mr = M * r  */
+      M(Mr, r, PLUS);  flopcount.addFlops(M.nFlops());
+
+      /*  c = < M.r, r > */
+      c = innerProduct(Mr, r, s);  flopcount.addSiteFlops(4*Nc*Ns,s);
+    
+      /*  d = | M.r | ** 2  */
+      d = norm2(Mr, s);  flopcount.addSiteFlops(4*Nc*Ns,s);
+
+      /*  a = c / d */
+      a = c / d;
+    
+      /*  a[k-1] *= MRovpar ; */
+      a = a * MRovpar;
+
+      /*  Psi[k] += a[k-1] r[k-1] ; */
+      psi[s] += r * a;    flopcount.addSiteFlops(4*Nc*Ns,s);
+
+      /*  r[k] -= a[k-1] M . r[k-1] ; */
+      r[s] -= Mr * a;    flopcount.addSiteFlops(4*Nc*Ns,s);
+
+      /*  cp  =  | r[k] |**2 */
+      cp = norm2(r, s);    flopcount.addSiteFlops(4*Nc*Ns,s);
+
+//    QDPIO::cout << "InvMR: k = " << k << "  cp = " << cp << endl;
+    }
+    res.n_count = k;
+    res.resid   = sqrt(cp);
+    swatch.stop();
+    QDPIO::cout << "InvMR: k = " << k << "  cp = " << cp << endl;
+    flopcount.report("invmr", swatch.getTimeInSeconds());
+    revertFromFastMemoryHint(psi,true);
+
+    // Compute the actual residual
+    {
+      M(Mr, psi, PLUS);
+      Double actual_res = norm2(chi_internal - Mr,s);
+      res.resid = sqrt(actual_res);
+    }
+
+    if ( res.n_count == MaxCG )
+      QDPIO::cerr << "Nonconvergence Warning" << endl;
+    
     END_CODE();
-    return;
+    return res;
   }
 
-  /*  FOR k FROM 1 TO MaxCG DO */
-  k = 0;
-  /*print *, "MaxCG = ", MaxCG; */
-  /*print *, "(RsdMR * chi_norm)**2 = ", rsq_sq; */
-  while( (k < MaxCG) && (toBool(cp > rsd_sq)) )
+
+  // Fix here for now
+  template<>
+  SystemSolverResults_t 
+  InvMR(const LinearOperator<LatticeFermion>& M,
+	const LatticeFermion& chi,
+	LatticeFermion& psi,
+	const Real& MRovpar,
+	const Real& RsdCG, 
+	int MaxCG)
   {
-    ++k;
-
-    /*  a[k-1] := < A.r[k-1], r[k-1] >/ < A.r[k-1], A.r[k-1] > ; */
-    /*  Ar = A * r  */
-    Ar[s] = A(r, PLUS);
-
-    /*  c = < A.r, r > */
-    c = innerProduct(Ar, r, s);
-    
-    /*  d = | A.r | ** 2  */
-    d = norm2(Ar, s);	                /* 2 Nc Ns  flops */
-
-    /*  a = c / d */
-    a = c / d;
-    
-    /*  a[k-1] *= MRovpar ; */
-    a = a * MRovpar;
-
-    /*  Psi[k] += a[k-1] r[k-1] ; */
-    psi[s] += r * a;	                /* 2 Nc Ns  flops */
-
-    /*  r[k] -= a[k-1] M . r[k-1] ; */
-    r[s] -= Ar * a;                     /* 2 Nc Ns  flops */
-
-    /*  cp  =  | r[k] |**2 */
-    cp = norm2(r, s);                   /* 2 Nc Ns  flops */
-
-    /*#  printf("InvMRm: k = %d  cp = %g\n", k, cp); */
-    /*#  write(nml_out,*) "InvMRm: k = ",k,"  cp = ", cp; */
+    return InvMR_a(M, chi, psi, MRovpar, RsdCG, MaxCG);
   }
-  n_count = k;
-  if ( n_count == MaxCG )
-    QDP_error_exit("too many MR iterations: iters=%d", k);
 
-  END_CODE();
-  return;
-}
+
+  // Fix here for now
+  template<>
+  SystemSolverResults_t 
+  InvMR(const LinearOperator<LatticeStaggeredFermion>& M,
+	const LatticeStaggeredFermion& chi,
+	LatticeStaggeredFermion& psi,
+	const Real& MRovpar,
+	const Real& RsdCG, 
+	int MaxCG)
+  {
+    return InvMR_a(M, chi, psi, MRovpar, RsdCG, MaxCG);
+  }
+
+  /*! @} */  // end of group invert
 
 }  // end namespace Chroma
--- lib/actions/ferm/invert/invmr.h
+++ lib/actions/ferm/invert/invmr.h
@@ -1,20 +1,79 @@
 // -*- C++ -*-
-// $Id: invmr.h,v 3.0 2006-04-03 04:58:49 edwards Exp $
+// $Id: invmr.h,v 3.1 2007-04-11 03:41:01 edwards Exp $
+/*! \file
+ *  \brief Minimal-Residual (MR) for a generic fermion Linear Operator
+ */
 
 #ifndef __invmr_h__
 #define __invmr_h__
 
 #include "linearop.h"
+#include "syssolver.h"
 
-namespace Chroma {
+namespace Chroma 
+{
 
-void InvMR(const LinearOperator& A,
-	   const LatticeFermion& chi,
-	   LatticeFermion& psi,
-	   const Real& RsdMR, 
-	   const Real& MRovpar;
-	   int MaxCG, 
-	   int& n_count);
+  //! Minimal-residual (MR) algorithm for a generic Linear Operator 
+  /*! \ingroup invert
+   * This subroutine uses the Minimal Residual (MR) algorithm to determine
+   * the solution of the set of linear equations. Here we allow M to be nonhermitian.
+   *
+   *   	    Chi  =  M . Psi 
+   *
+   * Algorithm:
+   *
+   *  Psi[0]                                      Argument
+   *  r[0]    :=  Chi  -  M . Psi[0] ;            Initial residual
+   *  IF |r[0]| <= RsdCG |Chi| THEN RETURN;       Converged?
+   *  FOR k FROM 1 TO MaxCG DO                    MR iterations
+   *      a[k-1]  := <M.r[k-1],r[k-1]> / <M.r[k-1],M.r[k-1]> ;
+   *      ap[k-1] := MRovpar * a[k] ;             Overrelaxtion step
+   *      Psi[k]  += ap[k-1] r[k-1] ;             New solution vector
+   *      r[k]    -= ap[k-1] A . r[k-1] ;         New residual
+   *      IF |r[k]| <= RsdCG |Chi| THEN RETURN;   Converged?
+
+   * Arguments:
+
+   *  \param M       Linear Operator             (Read)
+   *  \param chi     Source                      (Read)
+   *  \param psi     Solution                    (Modify)
+   *  \param RsdCG   MR residual accuracy        (Read)
+   *  \param MRovpar Overrelaxation parameter    (Read)
+   *  \param MaxCG   Maximum MR iterations       (Read)
+
+   * Local Variables:
+
+   *  r   	Residual vector
+   *  cp  	| r[k] |**2
+   *  c   	| r[k-1] |**2
+   *  k   	MR iteration counter
+   *  a   	a[k]
+   *  d   	< M.r[k], M.r[k] >
+   *  R_Aux     Temporary for  M.Psi
+   *  Mr        Temporary for  M.r
+
+   * Global Variables:
+
+   *  MaxCG       Maximum number of MR iterations allowed
+   *  RsdCG       Maximum acceptable MR residual (relative to source)
+   *
+   * Subroutines:
+   *
+   *  M           Apply matrix to vector
+   *
+   * @{
+   */
+
+  template<typename T>
+  SystemSolverResults_t 
+  InvMR(const LinearOperator<T>& M,
+	const T& chi,
+	T& psi,
+	const Real& MRovpar,
+	const Real& RsdCG, 
+	int MaxCG);
+
+  /*! @} */  // end of group invert
 
 }  // end namespace Chroma
 
