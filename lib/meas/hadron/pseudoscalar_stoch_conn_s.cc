05c86482e266f76b06855acff0118bd847ab90f8
egregory <egregory>
2007-05-14 13:40:04 +0000

    added fuzzed stochastic connected pseudoscalar correlator for testing purposes

--- lib/meas/hadron/pseudoscalar_stoch_conn_s.cc
+++ lib/meas/hadron/pseudoscalar_stoch_conn_s.cc
@@ -1,7 +1,8 @@
-// $Id: pseudoscalar_stoch_conn_s.cc,v 3.1 2007-02-22 21:11:49 bjoo Exp $
+// $Id: pseudoscalar_stoch_conn_s.cc,v 3.2 2007-05-14 13:40:04 egregory Exp $
 #include "chromabase.h"
 #include "pseudoscalar_stoch_conn_s.h"
 #include "util/gauge/stag_phases_s.h"
+#include "meas/smear/fuzz_smear.h"
 
 namespace Chroma {
 
@@ -65,6 +66,75 @@ void fourlink_pseudoscalar_stoch_conn::compute(
 
   corr_fn1[isample] = sumMulti(TrG_eta4, timeslice);
   corr1 += corr_fn1[isample] ;
+
+  TrG_eta4 = 
+    -alpha(0)*alpha(1)*alpha(2)*alpha(3)*localInnerProduct(q_source2, 
+							   psi_eta4_1);
+  corr_fn2[isample] = sumMulti(TrG_eta4, timeslice);
+  corr2 += corr_fn2[isample] ;
+}
+
+void fourlink_pseudoscalar_stoch_conn::compute(
+      				    LatticeStaggeredFermion & q_source1,
+	       			    LatticeStaggeredFermion & q_source2,
+				    LatticeStaggeredFermion & psi1, 
+				    LatticeStaggeredFermion & psi2,
+				    //		    bool fuzz_src, bool fuzz_sink,
+				    const multi1d<LatticeColorMatrix> & u_smr,
+				    int fuzz_width,
+				    int j_decay,	
+				    int isample){
+
+  // Array to describe shifts in cube
+  multi1d<int> delta(Nd);
+
+
+  Set timeslice;
+  timeslice.make(TimeSliceFunc(Nd-1));
+
+  LatticeStaggeredFermion psi_eta4_1 ;
+  LatticeStaggeredFermion psi_eta4_2 ;
+
+  LatticeStaggeredFermion  fuzz_q_source1;
+  LatticeStaggeredFermion  fuzz_q_source2;
+  LatticeStaggeredFermion  fuzz_psi_1;
+
+  delta = 0;
+  delta[0] = delta[1] = delta[2] = delta[3] = 1;
+
+
+  if(Fsink){
+	fuzz_smear(u_smr, psi1, fuzz_psi_1, fuzz_width, j_decay) ;
+	psi_eta4_1 = shift_deltaProp(delta, fuzz_psi_1);
+
+  }else{
+    psi_eta4_1 = shift_deltaProp(delta, psi1);
+  }
+
+
+  psi_eta4_2 = shift_deltaProp(delta, psi2);
+
+  LatticeComplex TrG_eta4 ; 
+
+  using namespace StagPhases;
+
+  if(Fsrc){
+    fuzz_smear(u_smr, q_source1, fuzz_q_source1, fuzz_width, j_decay) ;
+
+    TrG_eta4 = 
+      -alpha(0)*alpha(1)*alpha(2)*alpha(3)*localInnerProduct(fuzz_q_source1,
+							     psi_eta4_2);
+  }else{
+    TrG_eta4 = 
+      -alpha(0)*alpha(1)*alpha(2)*alpha(3)*localInnerProduct(q_source1, 
+							     psi_eta4_2);
+  }
+
+
+
+  corr_fn1[isample] = sumMulti(TrG_eta4, timeslice);
+  corr1 += corr_fn1[isample] ;
+
 
   TrG_eta4 = 
     -alpha(0)*alpha(1)*alpha(2)*alpha(3)*localInnerProduct(q_source2, 
