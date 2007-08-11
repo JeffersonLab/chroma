85b0eaee676e32a72987413659fd4b549c7890e8
edwards <edwards>
2007-08-11 22:48:12 +0000

    Moved around regressions into subdirs.

--- tests/chroma/hadron/hadron_contract/regres.pl
+++ tests/chroma/hadron/hadron_contract/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 1.1 2007-06-13 14:14:08 edwards Exp $
+#  $Id: regres.pl,v 1.2 2007-08-11 22:48:12 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -13,9 +13,17 @@
      {
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "chroma" , 
-	 input       => "$test_dir/chroma/hadron/hadron_contract/hadron-meson.ini.xml" , 
-	 output      => "hadron-meson.candidate.xml",
-	 metric      => "$test_dir/chroma/hadron/hadron_contract/hadron-meson.metric.xml" ,
-	 controlfile => "$test_dir/chroma/hadron/hadron_contract/hadron-meson.out.xml" ,
+	 input       => "$test_dir/chroma/hadron/hadron_contract/meson_2pt/meson_2pt.ini.xml" , 
+	 output      => "meson_2pt.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/hadron_contract/meson_2pt/meson_2pt.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/hadron_contract/meson_2pt/meson_2pt.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/hadron_contract/stoch_condensates/stoch_condensates.ini.xml" , 
+	 output      => "stoch_condensates.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/hadron_contract/stoch_condensates/stoch_condensates.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/hadron_contract/stoch_condensates/stoch_condensates.out.xml" ,
      }
      );
