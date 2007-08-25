11c38b1d6b871e36eb990d8240d56f069cfe89c0
edwards <edwards>
2007-08-25 04:56:13 +0000

    Made path explicit.

--- lib/meas/smear/quark_displacement_aggregate.cc
+++ lib/meas/smear/quark_displacement_aggregate.cc
@@ -1,4 +1,4 @@
-// $Id: quark_displacement_aggregate.cc,v 3.8 2007-08-25 04:51:01 edwards Exp $
+// $Id: quark_displacement_aggregate.cc,v 3.9 2007-08-25 04:56:13 edwards Exp $
 /*! \file
  *  \brief All quark displacements
  */
@@ -54,7 +54,7 @@ namespace Chroma
       write(xml_tmp, "Displacement", non);
       nope.xml  = xml_tmp.str();
       nope.id   = NoQuarkDisplacementEnv::name;
-      nope.path = "Displacement";
+      nope.path = "/Displacement";
 
       return nope;
     }
