a42cd4f476a35b957ee8b3d2a108970fb9c37fec
edwards <edwards>
2007-11-10 21:33:20 +0000

    Fixed paths.

--- tests/chroma/hadron/gauge_transf/regres.pl
+++ tests/chroma/hadron/gauge_transf/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 1.1 2007-11-09 21:24:23 edwards Exp $
+#  $Id: regres.pl,v 1.2 2007-11-10 21:33:20 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -13,9 +13,9 @@
      {
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "chroma" , 
-	 input       => "$test_dir/chroma/gauge_transf/gauge_transf/gauge_transf.ini.xml" , 
+	 input       => "$test_dir/chroma/hadron/gauge_transf/gauge_transf.ini.xml" , 
 	 output      => "gauge_transf.candidate.xml",
-	 metric      => "$test_dir/chroma/gauge_transf/gauge_transf/gauge_transf.metric.xml" ,
-	 controlfile => "$test_dir/chroma/gauge_transf/gauge_transf/gauge_transf.out.xml" ,
+	 metric      => "$test_dir/chroma/hadron/gauge_transf/gauge_transf.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/gauge_transf/gauge_transf.out.xml" ,
      }
      );
