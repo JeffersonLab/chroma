2be1a5b82d4be2e3578e2097cce68f1b60154f35
bjoo <bjoo>
2008-01-08 17:52:08 +0000

    Regressions for various boundary conditions on temp prec actions

--- tests/chroma/hadron/propagator/regres.tprec.pl
+++ tests/chroma/hadron/propagator/regres.tprec.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.tprec.pl,v 3.1 2007-12-12 21:42:59 bjoo Exp $
+#  $Id: regres.tprec.pl,v 3.2 2008-01-08 17:52:08 bjoo Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -21,6 +21,30 @@
 	 output      => "unprec_s_cprec_t_wilson.candidate.xml",
 	 metric      => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson.metric.xml" ,
 	 controlfile => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson.out.xml" ,
+     },   
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr1link.ini.xml" , 
+	 output      => "unprec_s_cprec_t_wilson_schr1link.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr1link.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr1link.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr2link.ini.xml" , 
+	 output      => "unprec_s_cprec_t_wilson_schr2link.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr2link.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr2link.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr2link_space.ini.xml" , 
+	 output      => "unprec_s_cprec_t_wilson_schr2link_space.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr2link_space.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/propagator/unprec_s_cprec_t_wilson_schr2link_space.out.xml" ,
      },
      {
         exec_path   => "$top_builddir/mainprogs/main" ,
@@ -30,6 +54,30 @@
         metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson.metric.xml" ,
         controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson.out.xml" ,
      },
+     {
+        exec_path   => "$top_builddir/mainprogs/main" ,
+        execute     => "chroma" ,
+        input       => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr1link.ini.xml" ,
+        output      => "iluprec_s_cprec_t_wilson_schr1link.candidate.xml",
+        metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr1link.metric.xml" ,
+        controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr1link.out.xml" ,
+     },
+     {
+        exec_path   => "$top_builddir/mainprogs/main" ,
+        execute     => "chroma" ,
+        input       => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr2link.ini.xml" ,
+        output      => "iluprec_s_cprec_t_wilson_schr2link.candidate.xml",
+        metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr2link.metric.xml" ,
+        controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr2link.out.xml" ,
+     },
+     {
+        exec_path   => "$top_builddir/mainprogs/main" ,
+        execute     => "chroma" ,
+        input       => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr2link_space.ini.xml" ,
+        output      => "iluprec_s_cprec_t_wilson_schr2link_space.candidate.xml",
+        metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr2link_space.metric.xml" ,
+        controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_wilson_schr2link_space.out.xml" ,
+     },
     {
 	 exec_path   => "$top_builddir/mainprogs/main" , 
 	 execute     => "chroma" , 
@@ -37,6 +85,30 @@
 	 output      => "iluprec_s_cprec_t_clover.candidate.xml",
 	 metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover.metric.xml" ,
 	 controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover.out.xml" ,
+     },
+    {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr1link.ini.xml" , 
+	 output      => "iluprec_s_cprec_t_clover_schr1link.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr1link.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr1link.out.xml" ,
+     },
+    {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr2link.ini.xml" , 
+	 output      => "iluprec_s_cprec_t_clover_schr2link.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr2link.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr2link.out.xml" ,
+     },
+    {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "chroma" , 
+	 input       => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr2link_space.ini.xml" , 
+	 output      => "iluprec_s_cprec_t_clover_schr2link_space.candidate.xml",
+	 metric      => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr2link_space.metric.xml" ,
+	 controlfile => "$test_dir/chroma/hadron/propagator/iluprec_s_cprec_t_clover_schr2link_space.out.xml" ,
      },
      {
         exec_path   => "$top_builddir/mainprogs/main" ,
