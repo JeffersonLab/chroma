020982339c2b2b4a032557da7d50a5113293c354
edwards <edwards>
2008-01-23 15:44:06 +0000

    Added wilson_coarse_fine_1loop test.

--- tests/purgaug/regres.pl
+++ tests/purgaug/regres.pl
@@ -1,5 +1,5 @@
 #
-#  $Id: regres.pl,v 3.2 2008-01-20 19:46:05 edwards Exp $
+#  $Id: regres.pl,v 3.3 2008-01-23 15:44:06 edwards Exp $
 #
 #  This is the portion of a script this is included recursively
 #
@@ -33,5 +33,13 @@
 	 output      => "purgaug.2+2.candidate.xml",
 	 metric      => "$test_dir/purgaug/purgaug.2+2.metric.xml" ,
 	 controlfile => "$test_dir/purgaug/purgaug.2+2.out.xml" ,
+     },
+     {
+	 exec_path   => "$top_builddir/mainprogs/main" , 
+	 execute     => "purgaug" , 
+	 input       => "$test_dir/purgaug/purgaug.2+2.1loop.ini.xml" , 
+	 output      => "purgaug.2+2.1loop.candidate.xml",
+	 metric      => "$test_dir/purgaug/purgaug.2+2.1loop.metric.xml" ,
+	 controlfile => "$test_dir/purgaug/purgaug.2+2.1loop.out.xml" ,
      }
      );
