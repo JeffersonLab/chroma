23dfd7727d89d58e2baaba49071b72c5832c86d7
bjoo <bjoo>
2008-04-25 18:30:43 +0000

    Shell for Prop Writers (they don't actually work yet)

---
+++ lib/meas/inline/io/inline_usqcd_write_ddpairs_prop.cc
@@ -0,0 +1,149 @@
+// $Id: inline_usqcd_write_ddpairs_prop.cc,v 1.1 2008-04-25 18:30:43 bjoo Exp $
+/*! \file
+ * \brief Inline task to write an object from a named buffer
+ *
+ * Named object writing
+ */
+
+#include "meas/inline/abs_inline_measurement_factory.h"
+#include "meas/inline/io/inline_usqcd_write_ddpairs_prop.h"
+#include "meas/inline/io/named_objmap.h"
+#include "io/enum_io/enum_io.h"
+
+namespace Chroma 
+{ 
+  namespace InlineUSQCDWriteDDPairsPropEnv 
+  { 
+    namespace
+    {
+      AbsInlineMeasurement* createMeasurement(XMLReader& xml_in, 
+					      const std::string& path) 
+      {
+	return new InlineUSQCDWriteDDPairsProp(InlineUSQCDWriteDDPairsPropParams(xml_in, path));
+      }
+
+      //! Local registration flag
+      bool registered = false;
+    }
+
+    const std::string name = "USQCD_WRITE_DD_PAIRS_PROP";
+
+    //! Register all the factories
+    bool registerAll() 
+    {
+      bool success = true; 
+      if (! registered)
+      {
+	// Inline measurement
+	success &= TheInlineMeasurementFactory::Instance().registerObject(name, createMeasurement);
+
+	registered = true;
+      }
+      return success;
+    }
+  }
+
+
+  //! Object buffer
+  void write(XMLWriter& xml, const string& path, const InlineUSQCDWriteDDPairsPropParams& p)
+  {
+    push(xml, path);
+    write(xml, "Frequency", p.frequency);
+    push(xml, "Param");
+    write(xml, "OutputFile", p.output_file_name);
+    write(xml, "OutputVolfmt", p.qio_volfmt);
+    pop(xml); // Param
+    
+    push(xml, "NamedObject");
+    write(xml, "prop_id", p.prop_id); // ID Of prop to create
+    pop(xml);
+
+    if ( p.xml_file != "") { 
+      write(xml, "xml_file", p.xml_file);
+    }
+    pop(xml);
+  }
+
+  // Param stuff
+  InlineUSQCDWriteDDPairsPropParams::InlineUSQCDWriteDDPairsPropParams() { frequency = 0; xml_file=""; }
+
+  InlineUSQCDWriteDDPairsPropParams::InlineUSQCDWriteDDPairsPropParams(XMLReader& xml_in, const std::string& path) 
+  {
+    try 
+    {
+      XMLReader paramtop(xml_in, path);
+
+      if (paramtop.count("Frequency") == 1)
+	read(paramtop, "Frequency", frequency);
+      else
+	frequency = 1;
+
+
+      read(paramtop, "Param/OutputFile", output_file_name);
+      read(paramtop, "Param/OutputVolfmt", qio_volfmt);
+
+
+      read(paramtop, "./NamedObject/prop_id", prop_id);
+
+      if( paramtop.count("xml_file") == 1 ) { 
+	read(paramtop, "./xml_file", xml_file);
+      }
+      else { 
+	xml_file="";
+      }
+    }
+    catch(const std::string& e) 
+    {
+      QDPIO::cerr << __func__ << ": caught Exception reading XML: " << e << endl;
+      QDP_abort(1);
+    }
+  }
+
+
+
+
+  void 
+  InlineUSQCDWriteDDPairsProp::operator()(unsigned long update_no,
+				    XMLWriter& xml_out) 
+  {
+    START_CODE();
+    if( params.xml_file == "" ) { 
+      func(update_no, xml_out);
+    }
+    else { 
+      XMLFileWriter separate_output(params.xml_file);
+      func(update_no, separate_output);
+    }
+
+    END_CODE();
+  } 
+
+
+  void InlineUSQCDWriteDDPairsProp::func(unsigned long update_no, XMLWriter& xml_out)
+  {
+    START_CODE();
+
+    push(xml_out, "USQCDWriteDDPairsProp");
+    write(xml_out, "update_no", update_no);
+
+    QDPIO::cout << InlineUSQCDWriteDDPairsPropEnv::name << ":" << endl;
+    StopWatch swatch;
+    QDPIO::cout << "Attempt to write object name = " << params.prop_id << endl;
+    QDPIO::cout << "Output file = " << params.output_file_name << endl;
+
+
+    XMLBufferWriter testout;
+    write(testout, "InputParams", params);
+   
+    QDPIO::cout << "Echo Of Input Params" << endl;
+    QDPIO::cout << testout.str();
+
+   
+    
+    QDPIO::cout << InlineUSQCDWriteDDPairsPropEnv::name << ": ran successfully" << endl;
+
+    pop(xml_out);  // qio_write_named_obj
+
+    END_CODE();
+}
+};
---
+++ lib/meas/inline/io/inline_usqcd_write_ddpairs_prop.h
@@ -0,0 +1,66 @@
+// -*- C++ -*-
+// $Id: inline_usqcd_write_ddpairs_prop.h,v 1.1 2008-04-25 18:30:43 bjoo Exp $
+/*! \file
+ * \brief Inline task to read a USQCD DD Pairs Prop 
+ *
+ *
+ */
+
+#ifndef __inline_usqcd_write_ddpairs_prop_h__
+#define __inline_usqcd_write_ddpairs_prop_h__
+
+#include "chromabase.h"
+#include "meas/inline/abs_inline_measurement.h"
+#include "io/xml_group_reader.h"
+
+namespace Chroma 
+{ 
+  /*! \ingroup inlineio */
+  namespace InlineUSQCDWriteDDPairsPropEnv
+  {
+    extern const std::string name;
+    bool registerAll();
+  }
+
+  //! Parameter structure
+  /*! \ingroup inlineio */
+  struct InlineUSQCDWriteDDPairsPropParams
+  {
+    InlineUSQCDWriteDDPairsPropParams();
+    InlineUSQCDWriteDDPairsPropParams(XMLReader& xml_in, const std::string& path);
+    void write(XMLWriter& xml_out, const std::string& path);
+
+    unsigned long frequency;
+
+    // We will recreate the source from the header
+    std::string   prop_id;
+    
+    std::string   output_file_name;
+    QDP_volfmt_t  qio_volfmt;
+
+    std::string   xml_file;
+  };
+
+  //! Inline writing of memory objects
+  /*! \ingroup inlineio */
+  class InlineUSQCDWriteDDPairsProp : public AbsInlineMeasurement 
+  {
+  public:
+    ~InlineUSQCDWriteDDPairsProp() {}
+    InlineUSQCDWriteDDPairsProp(const InlineUSQCDWriteDDPairsPropParams& p) : params(p) {}
+    InlineUSQCDWriteDDPairsProp(const InlineUSQCDWriteDDPairsProp& p) : params(p.params) {}
+
+    unsigned long getFrequency(void) const {return params.frequency;}
+
+    //! Do the writing
+    void operator()(const unsigned long update_no,
+		    XMLWriter& xml_out); 
+
+  private:
+    void func(unsigned long update_no, XMLWriter& xml_out);
+    InlineUSQCDWriteDDPairsPropParams params;
+  };
+
+};
+
+#endif
