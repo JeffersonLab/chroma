79df895b5c143cb67086cb515882a3a21919d34a
kostas <kostas>
2009-01-31 05:13:38 +0000

    added block overlap code for three quarks (baryons) and started adding new Lapack routines needed for the disconnected

--- lib/util/ferm/block_couplings.cc
+++ lib/util/ferm/block_couplings.cc
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: block_couplings.cc,v 1.2 2009-01-30 21:07:21 kostas Exp $
+// $Id: block_couplings.cc,v 1.3 2009-01-31 05:13:38 kostas Exp $
 /*! \file
  *  \brief Caclulates the couplings between neighboring blocks given a displacement path
  */
@@ -26,6 +26,22 @@ namespace Chroma
     return out;    
   }
 
+  void displace(LatticeInteger& blk2,
+		const int len, const multi1d<int>& disp){
+    for(int i=0; i < disp.size(); ++i)
+      if (disp[i] > 0){
+	int disp_dir = disp[i] - 1;
+	int disp_len = len;
+	blk2 = displace(blk2, disp_len, disp_dir);
+      }
+      else if (disp[i] < 0){
+	int disp_dir = -disp[i] - 1;
+	int disp_len = -len;
+	blk2 = displace(blk2, disp_len, disp_dir);
+      }
+  }
+
+
   vector<int> block_couplings(const int b,
 			      const Set& S,const multi1d<int>& disp,
 			      const int len){
@@ -33,22 +49,37 @@ namespace Chroma
     LatticeInteger blk = zero ;
     LatticeInteger One = 1 ;
     blk[S[b]] = One ;
+
     for(int b2(0) ; b2<S.numSubsets();b2++){
       LatticeInteger blk2 = zero ;
-      blk[S[b2]] = One ;
-      for(int i=0; i < disp.size(); ++i)
-	if (disp[i] > 0){
-	  int disp_dir = disp[i] - 1;
-	  int disp_len = len;
-	  blk2 = displace(blk2, disp_len, disp_dir);
-	}
-	else if (disp[i] < 0){
-	  int disp_dir = -disp[i] - 1;
-	  int disp_len = -len;
-	  blk2 = displace(blk2, disp_len, disp_dir);
-	}
+      blk2[S[b2]] = One ;
+      displace(blk2,len,disp);
 
       int flag = toInt(sum(blk2*blk));
+      if(flag>0)
+	b_list.push_back(b2);
+    }
+
+    return b_list ;
+      
+  }
+
+  vector<int> block_couplings(const int b, const int b1,
+			      const Set& S,const multi1d<int>& disp,
+			      const int len){
+    vector<int> b_list ;
+    LatticeInteger blk = zero ;
+    LatticeInteger blk1 = zero ;
+    LatticeInteger One = 1 ;
+    blk[S[b]] = One ;
+    blk1[S[b1]] = One ;
+
+    for(int b2(0) ; b2<S.numSubsets();b2++){
+      LatticeInteger blk2 = zero ;
+      blk2[S[b2]] = One ;
+      displace(blk2,len,disp);
+
+      int flag = toInt(sum(blk2*blk1*blk));
       if(flag>0)
 	b_list.push_back(b2);
     }
--- lib/util/ferm/block_couplings.h
+++ lib/util/ferm/block_couplings.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: block_couplings.h,v 1.2 2009-01-30 21:07:21 kostas Exp $
+// $Id: block_couplings.h,v 1.3 2009-01-31 05:13:38 kostas Exp $
 /*! \file
  *  \brief Caclulates the couplings between neighboring blocks given a displacement path
  */
@@ -12,7 +12,12 @@
 
 namespace Chroma
 {
+
   vector<int> block_couplings(const int b,
+			      const Set& S,const multi1d<int>& disp,
+			      const int len);
+
+  vector<int> block_couplings(const int b, const int b1,
 			      const Set& S,const multi1d<int>& disp,
 			      const int len);
 
