ecd340fd93e5d37220174ced00dbe94226c772ea
mcneile <mcneile>
2009-03-18 13:37:12 +0000

    Protoptypes for j=3/2 baryon correlators.

--- lib/meas/hadron/baryon_s.h
+++ lib/meas/hadron/baryon_s.h
@@ -24,6 +24,37 @@ void baryon_s(LatticeStaggeredPropagator & quark_propagator_in,
 		multi1d<Complex> & barprop,
 		multi1d<int> & t_source,
 		      int j_decay, int bc_spec) ;
+  //
+  // spin j = 3/2
+  //
+
+  void baryon_class7_s(
+		LatticeStaggeredPropagator & quark_propagator_in_a,
+		LatticeStaggeredPropagator & quark_propagator_in_b,
+		LatticeStaggeredPropagator & quark_propagator_in_c,
+		multi1d<Complex> & barprop,
+		multi1d<int> & t_source,
+		int j_decay, int bc_spec)  ; 
+
+
+  void baryon_class7_NLT_s(
+		LatticeStaggeredPropagator & quark_propagator_in_a,
+		LatticeStaggeredPropagator & quark_propagator_in_b,
+		LatticeStaggeredPropagator & quark_propagator_in_c,
+		multi1d<LatticeColorMatrix> & u , 
+		multi1d<Complex> & barprop,
+		multi1d<int> & t_source,
+		int j_decay, int bc_spec)  ; 
+
+  void baryon_class4_s(
+		LatticeStaggeredPropagator & quark_propagator_in_a,
+		LatticeStaggeredPropagator & quark_propagator_in_b,
+		LatticeStaggeredPropagator & quark_propagator_in_c,
+		multi1d<Complex> & barprop,
+		multi1d<int> & t_source,
+		int j_decay, int bc_spec)  ;
+
+
 
 }  // end namespace Chroma
 
