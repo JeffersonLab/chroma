c2da090002dea26d33e7cf6402a9bd09c3fe0623
mcneile <mcneile>
2009-05-08 09:56:16 +0000

    Binary writer from the qcdoc version of the code.

--- lib/meas/hadron/hadron_corr_s.h
+++ lib/meas/hadron/hadron_corr_s.h
@@ -1,4 +1,4 @@
-// $Id: hadron_corr_s.h,v 3.0 2006-04-03 04:58:59 edwards Exp $
+// $Id: hadron_corr_s.h,v 3.1 2009-05-08 09:56:16 mcneile Exp $
 
 #ifndef HADRON_CORR_S_H
 #define HADRON_CORR_S_H
@@ -61,6 +61,60 @@ namespace Chroma
 
 
     }
+
+    void binary_dump(int t_source, std::string start_name)
+    {
+      const int magic_number = 66618 ;
+      string filename ;
+      string resized_tag;
+
+
+      filename = start_name + outer_tag + "."+inner_tag ;
+      QDPIO::cout << "out tag, in tag: " <<outer_tag << inner_tag << endl;
+      BinaryFileWriter speedy ;
+      speedy.open(filename);
+      write(speedy,magic_number) ;
+      write(speedy,t_length) ;
+      write(speedy,no_channel) ;
+
+    QDPIO::cout << "FILENAME: "<< filename << endl;
+
+     multi2d<Real> pi_corr(no_channel, t_length);
+      multi2d<Real> re_corr_fn(no_channel, t_length);
+      pi_corr = zero;
+
+      // Take the real part of the correlator and average over the sources
+      for(int i=0; i < no_channel ; i++){
+	for(int t=0; t < t_length; t++){
+	  int t_eff = (t - t_source + t_length)% t_length;
+	  re_corr_fn[i][t_eff] = real(corr_fn[i][t]);
+	}
+      }
+                  
+      multi1d<Real> Pi(t_length) ; 
+
+      for(int i=0; i < no_channel; i++) {
+	Pi = re_corr_fn[i];
+	resized_tag.assign(tag_names[i]);
+	resized_tag.resize(45,' ');
+
+
+	write(speedy, tag_names[i]);
+	write(speedy, Pi);
+
+      }
+
+
+      speedy.close();
+
+
+
+    }
+
+
+
+
+
 
     staggered_hadron_corr(int t_len, int t_chan, 
 			  const multi1d<LatticeColorMatrix> & uin, 
