0baa5b3ac79bef5600428c7592981a66080b25d3
bjoo <bjoo>
2009-06-11 15:20:54 +0000

    Solvers seem OK. Do not use ssed-clover for now. It seems to produce nonreproducibility in solvers

--- lib/actions/ferm/invert/syssolver_linop_cg.h
+++ lib/actions/ferm/invert/syssolver_linop_cg.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: syssolver_linop_cg.h,v 3.6 2008-04-05 19:04:38 edwards Exp $
+// $Id: syssolver_linop_cg.h,v 3.7 2009-06-11 15:20:54 bjoo Exp $
 /*! \file
  *  \brief Solve a M*psi=chi linear system by CG2
  */
@@ -57,25 +57,39 @@ namespace Chroma
      */
     SystemSolverResults_t operator() (T& psi, const T& chi) const
       {
-	START_CODE();
+	START_CODE();	
+	SystemSolverResults_t res;  // initialized by a constructor
+	StopWatch swatch;
+	swatch.reset();
+	swatch.start();
 
 	T chi_tmp;
 	(*A)(chi_tmp, chi, MINUS);
-
-	SystemSolverResults_t res;  // initialized by a constructor
-	{
-	  res = InvCG2(*A, chi_tmp, psi, invParam.RsdCG, invParam.MaxCG);
+	res = InvCG2(*A, chi_tmp, psi, invParam.RsdCG, invParam.MaxCG);
 
 #ifdef CHROMA_DO_ONE_CG_RESTART
 	  // Save existing n_count
-	  int n_count = res.n_count;
+	int n_count = res.n_count;
 
-	  // One automatic restart (if enabled)
-	  res = InvCG2(*A, chi_tmp, psi, invParam.RsdCGRestart, invParam.MaxCGRestart);
-	  res.n_count += n_count;
+	// One automatic restart (if enabled)
+	res = InvCG2(*A, chi_tmp, psi, invParam.RsdCGRestart, invParam.MaxCGRestart);
+	res.n_count += n_count;
 #endif
-	
+	swatch.stop();
+	double time = swatch.getTimeInSeconds();
+
+	{ 
+	  T r;
+	  r[A->subset()]=chi;
+	  T tmp;
+	  (*A)(tmp, psi, PLUS);
+	  r[A->subset()] -= tmp;
+	  res.resid = sqrt(norm2(r, A->subset()));
 	}
+	QDPIO::cout << "CG_SOLVER: " << res.n_count << " iterations. Rsd = " << res.resid << " Relative Rsd = " << res.resid/sqrt(norm2(chi,A->subset())) << endl;
+      QDPIO::cout << "CG_SOLVER_TIME: "<<time<< " sec" << endl;
+
+	
 
 	END_CODE();
 
