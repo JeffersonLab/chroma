54f39a85ba05cfc3cf29d3a3eeaef55488fd0d69
jbulava <jbulava>
2009-06-30 15:48:10 +0000

    Support for keys of LapH-diluted props.

---
+++ lib/util/ferm/key_laph_dil_prop.cc
@@ -0,0 +1,75 @@
+// -*- C++ -*-
+// $Id: key_laph_dil_prop.cc,v 1.1 2009-06-30 15:48:10 jbulava Exp $
+/*! \file
+ * \brief Key for propagator colorvector matrix elements
+ */
+
+#include "util/ferm/key_laph_dil_prop.h"
+
+namespace Chroma 
+{ 
+ 
+		bool operator<(const KeyLaphDilutedProp_t& a, 
+				const KeyLaphDilutedProp_t& b)
+		{
+			multi1d<int> lgaa(4);
+			lgaa[0] = a.spin_dil;
+			lgaa[1] = a.evec_dil;
+			lgaa[2] = a.noise_src;
+			lgaa[3] = a.src_or_snk;
+
+			multi1d<int> lgbb(4);
+			lgbb[0] = b.spin_dil;
+			lgbb[1] = b.evec_dil;
+			lgbb[2] = b.noise_src;
+			lgbb[3] = b.src_or_snk;
+   
+			return (lgaa < lgbb);
+		}
+
+		void write(BinaryWriter& bin, const KeyLaphDilutedProp_t& param)
+		{
+			write(bin, param.src_or_snk);
+			write(bin, param.spin_dil);
+			write(bin, param.evec_dil);
+			write(bin, param.noise_src);
+		}
+
+		void read(BinaryReader& bin, KeyLaphDilutedProp_t& param)
+		{
+			read(bin, param.src_or_snk);
+			read(bin, param.spin_dil);
+			read(bin, param.evec_dil);
+			read(bin, param.noise_src);
+		}
+
+		//! KeyPropColorVec reader
+  void read(XMLReader& xml, const std::string& path, 
+			KeyLaphDilutedProp_t& param)
+  {
+    XMLReader paramtop(xml, path);
+    
+    read(paramtop, "src_or_snk", param.src_or_snk);
+    read(paramtop, "spin_dil", param.spin_dil);
+    read(paramtop, "evec_dil", param.evec_dil);
+    read(paramtop, "noise_src", param.noise_src);
+  
+	}
+
+  // KeyPropColorVec writer
+  void write(XMLWriter& xml, const std::string& path, 
+			const KeyLaphDilutedProp_t& param)
+  {
+    push(xml, path);
+
+    write(xml, "src_or_snk", param.src_or_snk);
+    write(xml, "spin_dil", param.spin_dil);
+    write(xml, "evec_dil", param.evec_dil);
+    write(xml, "noise_src", param.noise_src);
+
+    pop(xml);
+  }
+
+
+
+} // namespace Chroma
---
+++ lib/util/ferm/key_laph_dil_prop.h
@@ -0,0 +1,54 @@
+// -*- C++ -*-
+// $Id: key_laph_dil_prop.h,v 1.1 2009-06-30 15:48:10 jbulava Exp $
+/*! \file
+ * \brief Key for propagator colorvector matrix elements
+ */
+
+#ifndef __key_laph_dil_prop_h__
+#define __key_laph_dil_prop_h__
+
+#include "chromabase.h"
+#include "util/ferm/key_val_db.h"
+
+namespace Chroma
+{
+  //----------------------------------------------------------------------------
+  /*!
+   * \ingroup ferm
+   * @{
+   */
+  //! Prop operator
+
+	struct KeyLaphDilutedProp_t
+		{
+			int src_or_snk; //Is this a source or solution? Will be either
+															//0 = "SRC" or  1 = "SNK"
+
+			int t0; //If the above string is 'SRC', this will always be 0. If it is 
+							//"SNK", this will be the t0 on which it was created
+
+			int spin_dil; //The spin dilution component
+
+			int evec_dil; //The eigenvector dilution component
+
+			int noise_src; //The noise index
+
+		};
+
+		bool operator<(const KeyLaphDilutedProp_t& a, 
+				const KeyLaphDilutedProp_t& b);
+
+		void write(BinaryWriter& bin, const KeyLaphDilutedProp_t& param);
+		
+		void read(BinaryReader& bin, KeyLaphDilutedProp_t& param);
+
+		void read(XMLReader& xml, const std::string& path, 
+			KeyLaphDilutedProp_t& param);
+
+		void write(XMLWriter& xml, const std::string& path, 
+			const KeyLaphDilutedProp_t& param);
+
+ 
+} // namespace Chroma
+
+#endif
