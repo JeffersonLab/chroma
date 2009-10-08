7464bde1f3cedd3557b88335d2fa64c8dcbe3626
edwards <edwards>
2009-10-08 17:45:36 +0000

    Added a version that returns the gauge rotation matrix. Commented out the bit that would broadcast the last time slice.

--- lib/meas/gfix/axgauge.cc
+++ lib/meas/gfix/axgauge.cc
@@ -1,4 +1,4 @@
-// $Id: axgauge.cc,v 3.1 2009-10-06 20:35:48 bjoo Exp $
+// $Id: axgauge.cc,v 3.2 2009-10-08 17:45:36 edwards Exp $
 /*! \file
  *  \brief Axial gauge fixing 
  */
@@ -6,87 +6,101 @@
 #include "chromabase.h"
 #include "meas/gfix/axgauge.h"
 
-namespace Chroma {
-
-//! Axial gauge fixing
-/*!
- * \ingroup gfix
- *
- * Transfroms a gauge configuration, in place, into axial
- *            gauge with special direction j_decay.
- *
- * Note: The non-unity time-like gauge fields from the last time slice
- *       will be copied to all other time slices.
- *
- * \param ug         gauge field and its axial gauge transform (Modify)
- * \param j_decay    time direction (Read) 
- */
-
-void axGauge(multi1d<LatticeColorMatrix>& ug, int j_decay)
+namespace Chroma 
 {
-  LatticeColorMatrix g;
-  axGauge(ug, g, j_decay);
-}
 
-//! Axial gauge fixing
-/*!
- * \ingroup gfix
- *
- * Transfroms a gauge configuration, in place, into axial
- *            gauge with special direction j_decay.
- *
- * Note: The non-unity time-like gauge fields from the last time slice
- *       will be copied to all other time slices.
- *
- * \param ug         gauge field and its axial gauge transform (Modify)
- * \param g          The Gauge Fixing Matrices
- * \param j_decay    time direction (Read) 
- */
+  //! Axial gauge fixing
+  /*!
+   * \ingroup gfix
+   *
+   * Transfroms a gauge configuration, in place, into axial
+   *            gauge with special direction decay_dir.
+   *
+   * Note: The non-unity time-like gauge fields from the last time slice
+   *       will be copied to all other time slices.
+   *
+   * \param ug         gauge field and its axial gauge transform (Modify)
+   * \param v          gauge rotation matrix (Write)
+   * \param decay_dir  time direction (Read) 
+   */
 
-  void axGauge(multi1d<LatticeColorMatrix>& ug, LatticeColorMatrix& g,int j_decay)
-{
-  START_CODE();
-
-  int lsizet = Layout::lattSize()[j_decay];
-
-  g = 1;
-  LatticeInteger t_coord = Layout::latticeCoordinate(j_decay);
-
-  /* Transform the "time-like" links to unity, slice by slice except for the */
-  /* last slice and thereby construct the gauge transformation V. */
-  for(int t = 1; t < lsizet; ++t)
+  void axGauge(multi1d<LatticeColorMatrix>& ug, LatticeColorMatrix& v, int decay_dir)
   {
-    LatticeBoolean btmp = t_coord == t;
+    START_CODE();
 
-    LatticeColorMatrix tmp_1 = shift(ug[j_decay], BACKWARD, j_decay);
-    copymask(g, btmp, tmp_1);
-    LatticeColorMatrix tmp_2 = tmp_1 * ug[j_decay];
-    copymask(ug[j_decay], btmp, tmp_2);
-  }
-
-  /* Now do the gauge transformation on the space-like links */
-
-  // This bit does: g(x) U(x,mu) g^{-1}(x+mu) 
-  for(int mu = 0; mu < Nd; ++mu)
-  {
-    if ( mu != j_decay )
+    // Sanity check
+    if (decay_dir < 0 || decay_dir >= ug.size())
     {
-      LatticeColorMatrix tmp_2 = ug[mu] * shift(adj(g), FORWARD, mu);
-      ug[mu] = g * tmp_2;
+      QDPIO::cerr << __func__ << ": invalid decay_dir= " << decay_dir << std::endl;
+      QDP_abort(1);
     }
+
+    // Initialize
+    int lsizet = Layout::lattSize()[decay_dir];
+    LatticeInteger t_coord = Layout::latticeCoordinate(decay_dir);
+    v = 1;
+
+    /* Transform the "time-like" links to unity, slice by slice except for the */
+    /* last slice and thereby construct the gauge transformation V. */
+    for(int t = 1; t < lsizet; ++t)
+    {
+      LatticeBoolean btmp = t_coord == t;
+
+      LatticeColorMatrix tmp_1 = shift(ug[decay_dir], BACKWARD, decay_dir);
+      copymask(v, btmp, tmp_1);
+      LatticeColorMatrix tmp_2 = tmp_1 * ug[decay_dir];
+      copymask(ug[decay_dir], btmp, tmp_2);
+    }
+
+    /* Now do the gauge transformation on the space-like links */
+    for(int mu = 0; mu < Nd; ++mu)
+    {
+      if ( mu != decay_dir )
+      {
+	LatticeColorMatrix tmp_2 = ug[mu] * shift(adj(v), FORWARD, mu);
+	ug[mu] = v * tmp_2;
+      }
+    }
+
+//
+// RGE: Dunno why we really want this. This is probably some SZIN historical anachronism.
+//      I suspect having the same value on all time made it slightly easier to grab and manipulate.
+//      Otherwise, it does mess with the field
+//
+//    /* Finally "broadcast" the t-link from the last time slice to all others */
+//    for(int t = lsizet-2; t >= 0; --t)
+//    {
+//      LatticeBoolean btmp = t_coord == t;
+//    
+//      copymask(ug[decay_dir], btmp, LatticeColorMatrix(shift(ug[decay_dir], FORWARD, decay_dir)));
+//    }
+
+    END_CODE();
   }
 
-  /* Finally "broadcast" the t-link from the last time slice to all others */
-  /* What does this part do and why ? SHouldn't the temporal links be one? */
-  for(int t = lsizet-2; t >= 0; --t)
+
+  //! Axial gauge fixing
+  /*!
+   * \ingroup gfix
+   *
+   * Transfroms a gauge configuration, in place, into axial
+   *            gauge with special direction decay_dir.
+   *
+   * Note: The non-unity time-like gauge fields from the last time slice
+   *       will be copied to all other time slices.
+   *
+   * \param ug           gauge field and its axial gauge transform (Modify)
+   * \param decay_dir    time direction (Read) 
+   */
+
+  void axGauge(multi1d<LatticeColorMatrix>& ug, int decay_dir)
   {
-    LatticeBoolean btmp = t_coord == t;
-    
-    copymask(ug[j_decay], btmp, LatticeColorMatrix(shift(ug[j_decay], FORWARD, j_decay)));
+    START_CODE();
+
+    LatticeColorMatrix v;
+    axGauge(ug, v, decay_dir);
+
+    END_CODE();
   }
-
-
-  END_CODE();
-}
 
 }  // end namespace Chroma
--- lib/meas/gfix/axgauge.h
+++ lib/meas/gfix/axgauge.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: axgauge.h,v 3.1 2009-10-06 20:35:48 bjoo Exp $
+// $Id: axgauge.h,v 3.2 2009-10-08 17:45:37 edwards Exp $
 /*! \file
  *  \brief Axial gauge fixing 
  */
@@ -7,40 +7,40 @@
 #ifndef __axgauge_h__
 #define __axgauge_h__
 
-namespace Chroma {
+namespace Chroma 
+{
 
-//! Axial gauge fixing
-/*!
- * \ingroup gfix
- *
- * Transfroms a gauge configuration, in place, into axial
- *            gauge with special direction j_decay.
- *
- * Note: The non-unity time-like gauge fields from the last time slice
- *       will be copied to all other time slices.
- *
- * \param ug         gauge field and its axial gauge transform (Modify)
- * \param j_decay    time direction (Read) 
- */
+  //! Axial gauge fixing
+  /*!
+   * \ingroup gfix
+   *
+   * Transfroms a gauge configuration, in place, into axial
+   *            gauge with special direction decay_dir.
+   *
+   * Note: The non-unity time-like gauge fields from the last time slice
+   *       will be copied to all other time slices.
+   *
+   * \param ug         gauge field and its axial gauge transform (Modify)
+   * \param v          gauge rotation matrix (Write)
+   * \param decay_dir  time direction (Read) 
+   */
+  void axGauge(multi1d<LatticeColorMatrix>& ug, LatticeColorMatrix& v, int decay_dir);
 
-  void axGauge(multi1d<LatticeColorMatrix>& ug, int j_decay);
 
-//! Axial gauge fixing
-/*!
- * \ingroup gfix
- *
- * Transfroms a gauge configuration, in place, into axial
- *            gauge with special direction j_decay.
- *
- * Note: The non-unity time-like gauge fields from the last time slice
- *       will be copied to all other time slices.
- *
- * \param ug         gauge field and its axial gauge transform (Modify)
- * \param  g         the gauge fixing matrices (Write)
- * \param j_decay    time direction (Read) 
- */
-
-  void axGauge(multi1d<LatticeColorMatrix>& ug, LatticeColorMatrix& g, int j_decay);
+  //! Axial gauge fixing
+  /*!
+   * \ingroup gfix
+   *
+   * Transfroms a gauge configuration, in place, into axial
+   *            gauge with special direction decay_dir.
+   *
+   * Note: The non-unity time-like gauge fields from the last time slice
+   *       will be copied to all other time slices.
+   *
+   * \param ug           gauge field and its axial gauge transform (Modify)
+   * \param decay_dir    time direction (Read) 
+   */
+  void axGauge(multi1d<LatticeColorMatrix>& ug, int decay_dir);
 
 }  // end namespace Chroma
 
