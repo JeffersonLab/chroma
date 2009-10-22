f9d94c229f13297d2dc9e95423ea23caff87d2ab
kostas <kostas>
2009-10-22 03:22:26 +0000

    Initial checking of eigbicg solver. (a copy of eigcg for now...)

---
+++ lib/actions/ferm/invert/syssolver_OPTeigbicg.cc
@@ -0,0 +1,306 @@
+// $Id: syssolver_OPTeigbicg.cc,v 1.1 2009-10-22 03:22:26 kostas Exp $
+/*! \file
+ *  \brief Solve a M^dag*M*psi=chi linear system by EigCG
+ */
+
+
+#include "qdp-lapack_Complex.h"
+#include "qdp-lapack_eigpcg.h"  
+#include "qdp-lapack_IncrEigpcg.h"
+
+#include "actions/ferm/invert/syssolver_mdagm_factory.h"
+#include "actions/ferm/invert/syssolver_mdagm_aggregate.h"
+
+#include "actions/ferm/invert/syssolver_mdagm_OPTeigcg.h"
+#include "containers.h"
+
+
+namespace Chroma
+{
+
+  //! CG1 system solver namespace
+  namespace MdagMSysSolverOptEigCGEnv
+  {
+    //! Callback function
+    MdagMSystemSolver<LatticeFermion>* createFerm(XMLReader& xml_in,
+						  const std::string& path,
+						  Handle< FermState< LatticeFermion, multi1d<LatticeColorMatrix>, multi1d<LatticeColorMatrix> > > state, 
+						  Handle< LinearOperator<LatticeFermion> > A)
+    {
+      return new MdagMSysSolverOptEigCG<LatticeFermion>(A, SysSolverOptEigCGParams(xml_in, path));
+    }
+
+#if 0
+    //! Callback function
+    MdagMSystemSolver<LatticeStaggeredFermion>* createStagFerm(
+      XMLReader& xml_in,
+      const std::string& path,
+      Handle< LinearOperator<LatticeStaggeredFermion> > A)
+    {
+      return new MdagMSysSolverOptEigCG<LatticeStaggeredFermion>(A, SysSolverOptEigCGParams(xml_in, path));
+    }
+#endif
+
+    //! Name to be used
+    const std::string name("EIG_CG_INVERTER");
+
+    //! Local registration flag
+    static bool registered = false;
+
+    //! Register all the factories
+    bool registerAll() 
+    {
+      bool success = true; 
+      if (! registered)
+      {
+	success &= Chroma::TheMdagMFermSystemSolverFactory::Instance().registerObject(name, createFerm);
+//	success &= Chroma::TheMdagMStagFermSystemSolverFactory::Instance().registerObject(name, createStagFerm);
+	registered = true;
+      }
+      return success;
+    }
+  }
+
+
+  //! Anonymous namespace holding method
+  namespace
+  {
+     template<typename T>
+     struct MatVecArg{
+       T XX ;
+       T YY ;
+       Handle< LinearOperator<T> > MdagM;
+    };
+
+    template<typename T>
+    void MatrixMatvec(void *x, void *y, void *params) {
+      
+      MatVecArg<T> &arg = *((MatVecArg<T> *) params) ;
+      
+      //Works only in single precision CHROMA
+      RComplex<float> *px = (RComplex<float> *) x;
+      RComplex<float> *py = (RComplex<float> *) y;
+
+      //XX.getF() = (Complex *) x ; //AN DOULEPSEI AUTO NA ME FTUSEIS
+      //YY.getF() = (Complex *) y ; //AN DOULEPSEI AUTO NA ME FTUSEIS
+
+      //Alliws kanoume copy
+      //copy x into XX
+      Subset s = arg.MdagM->subset() ; 
+      if(s.hasOrderedRep()){
+	/**
+	int one(1);
+        int VecSize = s.numSiteTable()*Nc*Ns ;
+	BLAS_DCOPY(&VecSize, 
+	       (double *)&arg.XX.elem(s.start()).elem(0).elem(0).real(),
+	       &one,
+	       (double *)x, &one);
+	**/
+	/**/
+	int count=0 ;
+	//can be done with ccopy for speed...
+	for(int i=s.start(); i <= s.end(); i++)
+	  for(int ss(0);ss<Ns;ss++)
+	    for(int c(0);c<Nc;c++){
+	      arg.XX.elem(i).elem(ss).elem(c)  = *(px+count);
+	      count++;
+	    }
+	/**/
+      }
+      else{
+	int i ;
+	const int *tab = s.siteTable().slice();
+	int count=0;
+	for(int x=0; x < s.numSiteTable(); ++x){
+	  i = tab[x] ;
+	  for(int ss(0);ss<Ns;ss++)
+	    for(int c(0);c<Nc;c++){
+	      arg.XX.elem(i).elem(ss).elem(c) = *(px+count);
+	      count++;
+	    }
+	}
+      }
+      
+
+      (*arg.MdagM)(arg.YY,arg.XX,PLUS) ;
+      //T foo,boo;
+      //*(arg.MdagM)(boo,foo,PLUS) ;
+
+      //copy back..
+      if(s.hasOrderedRep()){
+	int count=0 ;
+	//can be done with ccopy for speed...
+	for(int i=s.start(); i <= s.end(); i++)
+	  for(int ss(0);ss<Ns;ss++)
+	    for(int c(0);c<Nc;c++){
+	      *(py+count) = arg.YY.elem(i).elem(ss).elem(c) ;
+	      count++;
+	    }
+      }
+      else{
+	int i ;
+	int count=0;
+	const int *tab = s.siteTable().slice();
+	for(int x=0; x < s.numSiteTable(); ++x){
+	  i = tab[x] ;
+	  for(int ss(0);ss<Ns;ss++)
+	    for(int c(0);c<Nc;c++){
+	      *(py+count) = arg.YY.elem(i).elem(ss).elem(c) ;
+	      count++;
+	    }
+	}
+      }
+    }
+    
+
+    //! Solver the linear system
+    /*!
+     * \param psi      solution ( Modify )
+     * \param chi      source ( Read )
+     * \return syssolver results
+     */
+    template<typename T>
+    SystemSolverResults_t sysSolver(T& psi, const T& chi, 
+				    const LinearOperator<T>& A, 
+				    Handle< LinearOperator<T> > MdagM, 
+				    const SysSolverOptEigCGParams& invParam)
+    {
+      START_CODE();
+
+      SystemSolverResults_t res;  // initialized by a constructor
+
+      LinAlg::OptEigInfo& EigInfo = TheNamedObjMap::Instance().getData< LinAlg::OptEigInfo >(invParam.eigen_id);
+
+      QDPIO::cout<<"EigInfo.N= "<<EigInfo.N<<endl ;
+      QDPIO::cout<<"EigInfo.lde= "<<EigInfo.lde<<endl ;
+      QDPIO::cout<<"EigInfo.ldh= "<<EigInfo.evals.size()<<endl ;
+      QDPIO::cout<<"EigInfo.ncurEvals= "<<EigInfo.ncurEvals<<endl ;
+      QDPIO::cout<<"EigInfo.restartTol= "<<EigInfo.restartTol<<endl ;
+
+      Subset s = A.subset() ;
+
+      Complex_C *X ; 
+      Complex_C *B ; 
+      Complex_C *work=NULL  ;
+      Complex_C *V=NULL     ;
+      Complex_C *ework=NULL ;
+
+      // OK, for now Weirdly enough psi and chi have to be
+      // explicit single precision, so downcast those...
+      // Most generically the single prec type of say a 
+      // type T should be given by the QDP++ Type trait:
+      // SinglePrecType<T>::Type_t
+      typename SinglePrecType<T>::Type_t psif = psi;
+      typename SinglePrecType<T>::Type_t chif = chi;
+
+      if(s.hasOrderedRep()){
+	X = (Complex_C *) &psif.elem(s.start()).elem(0).elem(0).real();
+	B = (Complex_C *) &chif.elem(s.start()).elem(0).elem(0).real();
+      }
+      else{//need to copy
+	//X = allocate space for them
+	//B =  allocate space for them...
+	QDPIO::cout<<"OPPS! I have not implemented OPT_EigCG for Linops with non contigius subset\n";
+	exit(1);
+      }
+
+      Complex_C *evecs = (Complex_C *) &EigInfo.evecs[0] ;
+      float *evals = (float *) &EigInfo.evals[0].elem() ;
+      Complex_C *H  = (Complex_C *) &EigInfo.H[0] ;
+      Complex_C *HU = (Complex_C *) &EigInfo.HU[0] ;
+      MatVecArg<T> arg ;
+      arg.MdagM = MdagM ;
+      int esize = invParam.esize*Layout::sitesOnNode()*Nc*Ns ;
+
+      QDPIO::cout<<"OPT_EIGCG_SYSSOLVER= "<<esize<<endl ;
+      //multi1d<Complex_C> ework(esize);
+      float resid = (float) invParam.RsdCG.elem().elem().elem().elem();
+      float AnormEst = invParam.NormAest.elem().elem().elem().elem();
+
+      float restartTol;
+      if (EigInfo.ncurEvals < EigInfo.evals.size()) 
+         restartTol = 0.0; 		  // Do not restart the first phase 
+      else {
+        // set it to the user parameter:
+	restartTol = invParam.restartTol.elem().elem().elem().elem();
+
+        // restartTol = EigInfo.restartTol; //or restart with tol as computed 
+      }
+
+      IncrEigpcg(EigInfo.N, EigInfo.lde, 1, X, B, 
+		 &EigInfo.ncurEvals, EigInfo.evals.size(), 
+		 evecs, evals, H, HU, 
+		 MatrixMatvec<T>, NULL, (void *)&arg, work, V, 
+		 ework, esize, 
+		 resid, &restartTol,
+		 AnormEst, invParam.updateRestartTol, 
+		 invParam.MaxCG, invParam.PrintLevel, 
+		 invParam.Neig, invParam.Nmax, stdout);
+
+      /* Update the restartTol in the EigInfo function */
+      EigInfo.restartTol = restartTol;
+
+      // Copy result back
+      psi = psif;
+
+      T tt;
+      (*MdagM)(tt,psi,PLUS);
+      QDPIO::cout<<"OPT_EICG_SYSSOLVER: True residual after solution : "<<sqrt(norm2(tt-chi,s))<<endl ;
+      QDPIO::cout<<"OPT_EICG_SYSSOLVER: norm of  solution            : "<<sqrt(norm2(psi,s))<<endl ;
+      QDPIO::cout<<"OPT_EICG_SYSSOLVER: norm of rhs                  : "<<sqrt(norm2(chi,s))<<endl ;
+      
+
+
+      if(!s.hasOrderedRep()){
+	QDPIO::cout<<"OPPS! I have no implemented OPT_EigCG for Linops with non contigius subset\n";
+      }
+      END_CODE();
+
+      return res;
+    }
+
+  } // anonymous namespace
+
+
+  //
+  // Wrappers
+  //
+
+  // LatticeFermionF
+  template<>
+  SystemSolverResults_t
+  MdagMSysSolverOptEigCG<LatticeFermionF>::operator()(LatticeFermionF& psi, const LatticeFermionF& chi) const
+  {
+    return sysSolver(psi, chi, *A, MdagM, invParam);
+  }
+
+  // LatticeFermionD
+  template<>
+  SystemSolverResults_t
+  MdagMSysSolverOptEigCG<LatticeFermionD>::operator()(LatticeFermionD& psi, const LatticeFermionD& chi) const
+  {
+    return sysSolver(psi, chi, *A, MdagM, invParam);
+  }
+
+#if 1
+ 
+
+  // Not quite ready yet for these - almost there
+  // LatticeStaggeredFermionF
+  template<>
+  SystemSolverResults_t
+  MdagMSysSolverOptEigCG<LatticeStaggeredFermionF>::operator()(LatticeStaggeredFermionF& psi, const LatticeStaggeredFermionF& chi) const
+  {
+    return sysSolver(psi, chi, *A, MdagM, invParam);
+  }
+
+  // LatticeStaggeredFermionD
+  template<>
+  SystemSolverResults_t
+  MdagMSysSolverOptEigCG<LatticeStaggeredFermionD>::operator()(LatticeStaggeredFermionD& psi, const LatticeStaggeredFermionD& chi) const
+  {
+    return sysSolver(psi, chi, *A, MdagM, invParam);
+  }
+#endif
+
+}
---
+++ lib/actions/ferm/invert/syssolver_OPTeigbicg.h
@@ -0,0 +1,284 @@
+// -*- C++ -*-
+// $Id: syssolver_OPTeigbicg.h,v 1.1 2009-10-22 03:22:26 kostas Exp $
+/*! \file
+ *  \brief Solve a M^dag*M*psi=chi linear system by EigCG
+ */
+
+#ifndef __syssolver_mdagm_OPTeigcg_h__
+#define __syssolver_mdagm_OPTeigcg_h__
+
+#include "handle.h"
+#include "syssolver.h"
+#include "linearop.h"
+#include "lmdagm.h"
+#include "named_obj.h"
+#include "meas/inline/io/named_objmap.h"
+
+#include "actions/ferm/invert/syssolver_mdagm.h"
+#include "actions/ferm/invert/syssolver_OPTeigcg_params.h"
+#include "actions/ferm/invert/containers.h"
+
+#include "util/info/unique_id.h"
+
+namespace Chroma
+{
+
+  //! Eigenvector accelerated CG system solver namespace
+  namespace MdagMSysSolverOptEigCGEnv
+  {
+    //! Register the syssolver
+    bool registerAll();
+  }
+
+
+  //! Solve a M*psi=chi linear system by CG2 with eigenvectors
+  /*! \ingroup invert
+   */
+  template<typename T>
+  class MdagMSysSolverOptEigCG : public MdagMSystemSolver<T>
+  {
+  public:
+
+    //! Write out an OptEigInfo Type                         
+    void QIOWriteOptEvecs(){
+      StopWatch swatch;
+      swatch.reset();
+      swatch.start();
+
+      // A shorthand for the object                                      
+      const LinAlg::OptEigInfo& obj =
+	TheNamedObjMap::Instance().getData<LinAlg::OptEigInfo>(invParam.eigen_id);
+
+      // File XML                                            
+      XMLBufferWriter file_xml;
+      push(file_xml, "OptEigInfo");
+      write(file_xml, "id", uniqueId());
+      write(file_xml, "N", obj.N);
+      write(file_xml, "ncurEvals", obj.ncurEvals);
+      write(file_xml, "restartTol", obj.restartTol);
+      write(file_xml, "lde", obj.lde);
+      write(file_xml, "ldh", obj.evals.size());
+      pop(file_xml);
+
+      // Open file                                  
+      QDPFileWriter to(file_xml,
+		       invParam.file.file_name,
+		       invParam.file.file_volfmt,
+		       QDPIO_SERIAL,QDPIO_OPEN);
+
+      for(int v(0);v<obj.ncurEvals;v++){
+	LatticeFermion lf ;
+	obj.CvToLatFerm(lf,subset(),v);
+	XMLBufferWriter record_xml;
+	push(record_xml, "EigenVector");
+	write(record_xml,"no",v);
+	pop(record_xml);
+	write(to, record_xml, lf);
+      }
+      
+      {
+	XMLBufferWriter record_xml;
+	push(record_xml, "EigenValues");
+	pop(record_xml);
+	write(to, record_xml, obj.evals);
+      }
+      {
+	XMLBufferWriter record_xml;
+	push(record_xml, "H");
+	pop(record_xml);
+	write(to, record_xml, obj.H);
+      }
+      {
+	XMLBufferWriter record_xml;
+	push(record_xml, "HU");
+	pop(record_xml);
+	write(to, record_xml, obj.HU);
+      }
+
+      // Close                                                 
+      close(to);
+      swatch.stop();
+      QDPIO::cout<<" QIOWriteOptEvecs: Time to write evecs= "
+		 << swatch.getTimeInSeconds() <<" secs "<<endl ;
+    }
+
+    //-----------------------------------------------------------------------
+    //! Read a OptEigInfo Type                                           
+    void QIOReadOptEvecs()
+    {
+
+      StopWatch swatch;
+      swatch.reset();
+      swatch.start();
+
+      // File XML                                      
+      XMLReader file_xml;
+
+      // Open file     
+      QDPFileReader to(file_xml,invParam.file.file_name,QDPIO_SERIAL);
+
+      // A shorthand for the object         
+      LinAlg::OptEigInfo& obj =
+	TheNamedObjMap::Instance().getData<LinAlg::OptEigInfo>(invParam.eigen_id);
+
+      XMLReader record_xml;
+      //TheNamedObjMap::Instance().get(buffer_id).setRecordXML(record_xml);
+
+      int ldh,lde,N ;
+      float restartTol ;
+      read(file_xml, "/OptEigInfo/ldh", ldh);
+      read(file_xml, "/OptEigInfo/lde", lde);
+      read(file_xml, "/OptEigInfo/N",    N);
+      read(file_xml, "/OptEigInfo/ncurEvals", obj.ncurEvals);
+      read(file_xml, "/OptEigInfo/restartTol", restartTol);
+
+      QDPIO::cout <<__func__ << " : Reading object with following properties"<<endl ; 
+      QDPIO::cout <<__func__ << " :   lde= " << lde << endl;
+      QDPIO::cout <<__func__ << " :   ldh= " << ldh << endl;
+      QDPIO::cout <<__func__ << " :   N  = " << N   << endl;
+
+      QDPIO::cout << __func__ << " :   ncurEvals  = " << obj.ncurEvals<< endl;
+      QDPIO::cout << __func__ << " :   restartTol = " << restartTol<< endl;
+      
+      if(obj.evals.size()< obj.ncurEvals){
+	QDPIO::cerr<<__func__<< " : ldh of the current object is not large enough to hold the vectors" ;
+	QDP_abort(1);
+      }
+      
+      for(int v(0);v<obj.ncurEvals;v++){
+        LatticeFermion lf ;
+	XMLReader record_xml;
+	read(to, record_xml, lf);
+	obj.CvToEigCGvec(lf, subset(), v) ;
+      }
+      //this is just fine
+      {
+	XMLReader record_xml;
+	multi1d<Real>    evals(ldh) ;
+	multi1d<Complex> H(ldh*ldh)     ;
+	multi1d<Complex> HU(ldh*ldh)    ;
+	read(to, record_xml, evals);
+	read(to, record_xml, H);
+	read(to, record_xml, HU);
+	if(ldh<=obj.evals.size()){
+	  for(int i(0);i<ldh;i++)
+	    obj.evals[i] = evals[i] ;
+	  for(int i(0);i<H.size();i++){
+	    obj.H[i] = H[i] ;
+	    obj.HU[i] = HU[i] ;
+	  }
+	}
+	else{
+	  QDPIO::cerr<<__func__<< " : ldh of the current object is not large enough to hold the Cholesky factors" ;
+	  QDP_abort(1);
+	}
+      }
+
+    // Close                             
+    close(to);
+
+    swatch.stop();
+    QDPIO::cout<<" QIOReadOptEvecs: Time to read evecs= "
+	       << swatch.getTimeInSeconds() <<" secs "<<endl ;
+  }
+
+    //! Constructor
+    /*!
+     * \param M_         Linear operator ( Read )
+     * \param invParam_  inverter parameters ( Read )
+     */
+    MdagMSysSolverOptEigCG(Handle< LinearOperator<T> > A_,
+			   const SysSolverOptEigCGParams& invParam_) : 
+      MdagM(new MdagMLinOp<T>(A_)), A(A_), invParam(invParam_) 
+      {
+	numMatvecs = 0 ;
+	// NEED to grab the eignvectors from the named buffer here
+	if (! TheNamedObjMap::Instance().check(invParam.eigen_id))
+	{
+	  TheNamedObjMap::Instance().create< LinAlg::OptEigInfo >(invParam.eigen_id);
+	  LinAlg::OptEigInfo& EigInfo = 
+	    TheNamedObjMap::Instance().getData< LinAlg::OptEigInfo >(invParam.eigen_id);
+	  int N = Layout::sitesOnNode()*Nc*Ns ;
+	  int VectorSpaceSize =  Nc*Ns*(A->subset()).numSiteTable();
+	  EigInfo.init(invParam.Neig_max, N, VectorSpaceSize) ;
+	  EigInfo.restartTol =  invParam.restartTol.elem().elem().elem().elem();
+	  if(invParam.file.read){
+	    QDPIO::cout<<"MdagMSysSolverOptEigCG : reading evecs from disk"<<endl ;
+	    QIOReadOptEvecs() ;
+	  }
+	}
+      }
+
+    //! Destructor is automatic
+    ~MdagMSysSolverOptEigCG()
+      {
+	if(invParam.file.write){
+	  QDPIO::cout<<"MdagMSysSolverOptEigCG : writing evecs to disk"<<endl ;
+	  QIOWriteOptEvecs() ;
+	}
+	if (invParam.cleanUpEvecs)
+	{
+	  TheNamedObjMap::Instance().erase(invParam.eigen_id);
+	}
+      }
+
+    //! Return the subset on which the operator acts
+    const Subset& subset() const {return A->subset();}
+
+    //! Solver the linear system
+    /*!
+     * \param psi      solution ( Modify )
+     * \param chi      source ( Read )
+     * \return syssolver results
+     *
+     * Definitions supplied in the correspond .cc file
+     */
+    SystemSolverResults_t operator() (T& psi, const T& chi) const;
+
+
+    //! Solve the linear system starting with a chrono guess 
+    /*! 
+     * \param psi solution (Write)
+     * \param chi source   (Read)
+     * \param predictor   a chronological predictor (Read)
+     * \return syssolver results
+     */
+
+    SystemSolverResults_t operator()(T& psi, const T& chi, 
+				     AbsChronologicalPredictor4D<T>& predictor) const 
+    {
+      
+      START_CODE();
+
+      // This solver uses InvCG2, so A is just the matrix.
+      // I need to predict with A^\dagger A
+      {
+	Handle< LinearOperator<T> > MdagM( new MdagMLinOp<T>(A) );
+	predictor(psi, (*MdagM), chi);
+      }
+      // Do solve
+      SystemSolverResults_t res=(*this)(psi,chi);
+
+      // Store result
+      predictor.newVector(psi);
+      END_CODE();
+      return res;
+    }
+
+
+  private:
+
+    // Hide default constructor
+    MdagMSysSolverOptEigCG() {}
+    int numMatvecs ;
+    Handle< LinearOperator<T> > MdagM;
+    Handle< LinearOperator<T> > A;
+    SysSolverOptEigCGParams invParam;
+  };
+
+} // End namespace
+
+
+
+#endif 
+
