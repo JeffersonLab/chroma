373ceda8e14ba0c3d285693d34955a9728d16934
kostas <kostas>
2009-10-22 03:15:34 +0000

    Initial version of eigbicg parameters

---
+++ lib/actions/ferm/invert/syssolver_OPTeigbicg_params.cc
@@ -0,0 +1,118 @@
+// $Id: syssolver_OPTeigbicg_params.cc,v 1.1 2009-10-22 03:15:34 kostas Exp $
+/*! \file
+ *  \brief Params of EigCG inverter
+ */
+
+#include "actions/ferm/invert/syssolver_OPTeigbicg_params.h"
+#include "io/qprop_io.h"
+
+namespace Chroma
+{
+
+  //! File output                                                                                        
+  void write(XMLWriter& xml, const string& path, const SysSolverOptEigBiCGParams::File_t& input){
+    push(xml, path);
+
+    write(xml, "read", input.read);
+    write(xml, "write", input.write);
+    write(xml, "file_name", input.file_name);
+    write(xml, "file_volfmt", input.file_volfmt);
+
+    pop(xml);
+  }
+
+
+  //! File output                                                                                             
+  void read(XMLReader& xml, const string& path, SysSolverOptEigBiCGParams::File_t& input){
+    XMLReader inputtop(xml, path);
+
+    read(inputtop, "read", input.read);
+    read(inputtop, "write", input.write);
+    read(inputtop, "file_name", input.file_name);
+    read(inputtop, "file_volfmt", input.file_volfmt);
+  }
+
+
+  // Read parameters
+  void read(XMLReader& xml, const string& path, SysSolverOptEigBiCGParams& param)
+  {
+    XMLReader paramtop(xml, path);
+
+    param.defaults();
+
+    read(paramtop, "RsdCG", param.RsdCG);
+    read(paramtop, "MaxCG", param.MaxCG);
+
+    if( paramtop.count("PrintLevel") > 0 ) { 
+      read(paramtop, "PrintLevel", param.PrintLevel);
+    }
+
+    read(paramtop, "Nmax", param.Nmax);
+    read(paramtop, "Neig", param.Neig);
+    if(paramtop.count("Neig_max")!=0){
+      read(paramtop, "Neig_max", param.Neig_max);
+    }
+
+    if( paramtop.count("esize") > 0 ) { 
+      read(paramtop, "esize", param.esize);
+    }
+
+    read(paramtop, "eigen_id", param.eigen_id);
+    
+    if( paramtop.count("restartTol") > 0 ) { 
+      read(paramtop, "restartTol", param.restartTol);
+    }
+
+    if( paramtop.count("NormAest") > 0 ) { 
+      read(paramtop, "NormAest", param.NormAest);
+    }
+
+    if( paramtop.count("updateRestartTol") > 0 ) { 
+      read(paramtop, "updateRestartTol", param.updateRestartTol);
+    }
+
+    read(paramtop, "cleanUpEvecs", param.cleanUpEvecs);
+
+    if(paramtop.count("FileIO")!=0){
+      read(paramtop, "FileIO", param.file);
+    }
+    
+  }
+
+  // Writer parameters
+  void write(XMLWriter& xml, const string& path, const SysSolverOptEigBiCGParams& param)
+  {
+    push(xml, path);
+
+//  int version = 1;
+//  write(xml, "version", version);
+    write(xml, "invType", "OPT_EIG_CG_INVERTER");
+    write(xml, "RsdCG", param.RsdCG);
+    write(xml, "MaxCG", param.MaxCG);
+    write(xml, "Nmax", param.Nmax);
+    write(xml, "Neig", param.Neig);
+    write(xml, "Neig_max", param.Neig_max);
+    write(xml, "restartTol", param.restartTol);
+    write(xml, "updateRestartTol", param.updateRestartTol);
+    write(xml, "NormAest", param.NormAest);
+    write(xml, "cleanUpEvecs", param.cleanUpEvecs);
+    write(xml, "eigen_id", param.eigen_id);
+
+    write(xml, "FileIO",param.file);
+
+    pop(xml);
+  }
+
+  //! Default constructor
+  SysSolverOptEigBiCGParams::SysSolverOptEigBiCGParams()
+  {
+    defaults();
+  }
+
+  //! Read parameters
+  SysSolverOptEigBiCGParams::SysSolverOptEigBiCGParams(XMLReader& xml, const string& path)
+  {
+    read(xml, path, *this);
+  }
+
+}
---
+++ lib/actions/ferm/invert/syssolver_OPTeigbicg_params.h
@@ -0,0 +1,94 @@
+// -*- C++ -*-
+// $Id: syssolver_OPTeigbicg_params.h,v 1.1 2009-10-22 03:15:34 kostas Exp $
+/*! \file
+ *  \brief Solve a biCG system
+ */
+
+#ifndef __syssolver_OPTeigbicg_params_h__
+#define __syssolver_OPTeigbicg_params_h__
+
+#include "chromabase.h"
+
+
+namespace Chroma
+{
+
+  //! Params for EigBiCG inverter
+  /*! \ingroup invert */
+  struct SysSolverOptEigBiCGParams
+  {
+    SysSolverOptEigBiCGParams();
+    SysSolverOptEigBiCGParams(XMLReader& in, const std::string& path);
+    
+    Real RsdCG ;           /*!< CG residual */
+    int  MaxCG ;           /*!< Maximum CG iterations */
+    int  PrintLevel ;      /*!< Debugg level */
+    int  Neig       ;      /*!< number of eigenvectors to compute  */
+    int  Nmax       ;      /*!< number of basis vectors */
+    int  esize      ;      /*!< 2 <= esize <= 2*Neig + 1 */ 
+    int  Neig_max   ;      /*!< maximum number of eigenvectors to be refined*/
+
+    Real restartTol ;      /*!< CG  restart tolerence: restart when 
+			      |res|<restartTol*|b-A x(0)| */
+
+    int updateRestartTol ;  /*!< Whether to update restartTol from eresids
+			     * Expensive. Requires computation of residuals
+			     * =0 Never update restartTol
+			     * =1 Compute all eigenresiduals and update
+			     *    when ncurEvals=ldh for the first time
+			     * =2 Update based on up to 10 eres picked from
+			     *    ncurEvals, on every rhs that adds evecs
+			     * =3 Compute all eres and update on every rhs
+			     *    that adds evecs--unnecessarily expensive
+			     * If updateRestartTol>0 Cholesky is not used */
+    Real NormAest ; /* an estimate of the norm2 of the Matrix */
+    
+    bool  cleanUpEvecs ; /*!< clean up evecs upon destruction of SystemSolver*/
+    string eigen_id ; /*!< named buffer holding the eigenvectors */
+
+    struct File_t
+    {
+      bool read ;
+      bool write ;
+      std::string   file_name;
+      QDP_volfmt_t  file_volfmt;
+    } file;
+
+    
+    void defaults(){
+      RsdCG = 1.0e-8;
+      MaxCG = 1000;
+      PrintLevel=2;
+      restartTol = zero;
+      updateRestartTol = 1;
+      Neig =0 ;
+      Neig_max =0 ;
+      esize = 4 ;
+      NormAest = 25.0 ;
+      
+      cleanUpEvecs=false;
+      eigen_id="NULL";
+
+      //IO control                                                                                               
+      file.file_name = eigen_id ;
+      file.file_volfmt = QDPIO_SINGLEFILE ;
+
+      file.read   = false;
+      file.write  = false;
+
+    }
+
+  };
+
+
+  // Reader/writers
+  /*! \ingroup invert */
+  void read(XMLReader& xml, const string& path, SysSolverOptEigBiCGParams& param);
+
+  /*! \ingroup invert */
+  void write(XMLWriter& xml, const string& path, const SysSolverOptEigBiCGParams& param);
+
+} // End namespace
+
+#endif 
+
