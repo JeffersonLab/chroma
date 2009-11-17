2bfa5ab38155da830fa80cf752f86965a95755d7
Balint Joo <bjoo@qcd7ni01.(none)>
2009-11-16 20:17:54 -0500

    Committing to pull up to laptop

---
+++ lib/meas/inline/io/inline_read_map_obj_disk.cc
@@ -0,0 +1,188 @@
+// $Id: inline_nersc_read_obj.cc,v 3.2 2006-09-20 20:28:03 edwards Exp $
+/*! \file
+ * \brief Inline task to read an object from a named buffer
+ *
+ * Named object writing
+ */
+
+#include "chromabase.h"
+#include "qdp_iogauge.h"
+#include "meas/inline/abs_inline_measurement_factory.h"
+#include "meas/inline/io/inline_read_map_obj_disk.h"
+#include "meas/inline/io/named_objmap.h"
+
+namespace Chroma 
+{ 
+  namespace InlineReadMapObjDiskEnv 
+  { 
+    namespace
+    {
+      AbsInlineMeasurement* createMeasurement(XMLReader& xml_in, 
+					      const std::string& path) 
+      {
+	return new InlineReadMapObjDisk(InlineReadMapObjDiskParams(xml_in, path));
+      }
+
+      //! Local registration flag
+      bool registered = false;
+    }
+
+    const std::string name = "READ_MAP_OBJECT_DISK";
+
+    //! Register all the factories
+    bool registerAll() 
+    {
+      bool success = true; 
+      if (! registered)
+      {
+	success &= TheInlineMeasurementFactory::Instance().registerObject(name, createMeasurement);
+	registered = true;
+      }
+      return success;
+    }
+  }
+
+
+  //! Object buffer
+  void write(XMLWriter& xml, const string& path, const InlineReadMapObjDiskParams::NamedObject_t& input)
+  {
+    push(xml, path);
+
+    write(xml, "object_id", input.object_id);
+
+    pop(xml);
+  }
+
+  //! File output
+  void write(XMLWriter& xml, const string& path, const InlineReadMapObjDiskParams::File_t& input)
+  {
+    push(xml, path);
+
+    write(xml, "MapObject", input.file_name);
+
+    pop(xml);
+  }
+
+
+  //! Object buffer
+  void read(XMLReader& xml, const string& path, InlineReadMapObjDiskParams::NamedObject_t& input)
+  {
+    XMLReader inputtop(xml, path);
+
+    read(inputtop, "object_id", input.object_id);
+  }
+
+  //! File output
+  void read(XMLReader& xml, const string& path, InlineReadMapObjDiskParams::File_t& input)
+  {
+    XMLReader inputtop(xml, path);
+
+    read(inputtop, "file_name", input.file_name);
+  }
+
+
+  // Param stuff
+  InlineReadMapObjDiskParams::InlineReadMapObjDiskParams() { frequency = 0; }
+
+  InlineReadMapObjDiskParams::InlineReadMapObjDiskParams(XMLReader& xml_in, const std::string& path) 
+  {
+    try 
+    {
+      XMLReader paramtop(xml_in, path);
+
+      if (paramtop.count("Frequency") == 1)
+	read(paramtop, "Frequency", frequency);
+      else
+	frequency = 1;
+
+      // Parameters for source construction
+      read(paramtop, "NamedObject", named_obj);
+
+      read(paramtop, "MapObject", map_obj_disk_p );
+    }
+    catch(const std::string& e) 
+    {
+      QDPIO::cerr << __func__ << ": caught Exception reading XML: " << e << endl;
+      QDP_abort(1);
+    }
+  }
+
+
+  void
+  InlineReadMapObjDiskParams::write(XMLWriter& xml_out, const std::string& path) const
+  {
+    push(xml_out, path);
+    
+    // Parameters for source construction
+    Chroma::write(xml_out, "NamedObject", named_obj);
+    
+    // Write out the destination
+    Chroma::write(xml_out, "File", file);
+
+    pop(xml_out);
+  }
+
+
+  void 
+  InlineReadMapObjDisk::operator()(unsigned long update_no,
+				       XMLWriter& xml_out) 
+  {
+    START_CODE();
+
+    push(xml_out, "nersc_read_named_obj");
+    write(xml_out, "update_no", update_no);
+
+    QDPIO::cout << InlineReadMapObjDiskEnv::name << ": object reader" << endl;
+    StopWatch swatch;
+
+    // Read the object
+    // ONLY SciDAC output format is supported in this task
+    // Other tasks could support other disk formats
+    QDPIO::cout << "Attempt to read object name = " << params.named_obj.object_id << endl;
+    write(xml_out, "object_id", params.named_obj.object_id);
+    try
+    {
+      swatch.reset();
+
+      XMLReader record_xml;
+      multi1d<LatticeColorMatrix> u(Nd);
+
+      // Read the object
+      swatch.start();
+      QDP::readArchiv(record_xml, u, params.file.file_name);
+      swatch.stop();
+
+      XMLBufferWriter file_xml;
+      push(file_xml, "NERSC");
+      pop(file_xml);
+
+      TheNamedObjMap::Instance().create< multi1d<LatticeColorMatrix> >(params.named_obj.object_id);
+      TheNamedObjMap::Instance().getData< multi1d<LatticeColorMatrix> >(params.named_obj.object_id) = u;
+      TheNamedObjMap::Instance().get(params.named_obj.object_id).setFileXML(file_xml);
+      TheNamedObjMap::Instance().get(params.named_obj.object_id).setRecordXML(record_xml);
+
+      QDPIO::cout << "Object successfully written: time= " 
+		  << swatch.getTimeInSeconds() 
+		  << " secs" << endl;
+    }
+    catch( std::bad_cast ) 
+    {
+      QDPIO::cerr << InlineReadMapObjDiskEnv::name << ": cast error" 
+		  << endl;
+      QDP_abort(1);
+    }
+    catch (const string& e) 
+    {
+      QDPIO::cerr << InlineReadMapObjDiskEnv::name << ": error message: " << e 
+		  << endl;
+      QDP_abort(1);
+    }
+    
+    QDPIO::cout << InlineReadMapObjDiskEnv::name << ": ran successfully" << endl;
+
+    pop(xml_out);  // read_named_obj
+
+    END_CODE();
+  } 
+
+};
---
+++ lib/meas/inline/io/inline_read_map_obj_disk.h
@@ -0,0 +1,63 @@
+// -*- C++ -*-
+/*! \file
+ * \brief Inline task to read an object from a named buffer
+ *
+ * Named object writing
+ */
+
+#ifndef __inline_read_map_obj_disk_h__
+#define __inline_read_map_obj_disk_h__
+
+#include "chromabase.h"
+#include "meas/inline/abs_inline_measurement.h"
+#include "util/ferm/map_obj/map_obj_disk.h"
+namespace Chroma 
+{ 
+  /*! \ingroup inlineio */
+  namespace InlineReadMapObjDiskEnv 
+  {
+    extern const std::string name;
+    bool registerAll();
+  }
+
+  //! Parameter structure
+  /*! \ingroup inlineio */
+  class InlineReadMapObjDiskParams 
+  {
+    InlineReadMapObjDiskParams(XMLReader& xml_in, const std::string& path);
+    void write(XMLWriter& xml_out, const std::string& path);
+
+    unsigned long frequency;
+
+    MapObjectDiskParams map_obj_disk_p;
+
+    struct NamedObject_t
+    {
+      std::string   object_id;
+    } named_obj;
+
+    
+  };
+
+  //! Inline writing of memory objects
+  /*! \ingroup inlineio */
+  class InlineReadMapObjDisk : public AbsInlineMeasurement 
+  {
+  public:
+    ~InlineReadMapObjDisk() {}
+    InlineReadMapObjDisk(const InlineReadMapObjDiskParams& p) : params(p) {}
+    InlineReadMapObjDisk(const InlineReadMapObjDisk& p) : params(p.params) {}
+
+    unsigned long getFrequency(void) const {return params.frequency;}
+
+    //! Do the writing
+    void operator()(const unsigned long update_no,
+		    XMLWriter& xml_out); 
+
+  private:
+    InlineReadMapObjDiskParams params;
+  };
+
+};
+
+#endif
