920770bf01dc8c1945152d9626c85ea43571514b
Balint Joo <bjoo@jlab.org>
2010-03-15 16:23:22 -0400

    *) Added test for Dslash3D 	-- while this was a shell for a Dslash 4D under the hood, it was OK (mechanics work)         -- now that I have 3d dslash, probably I'll have to pack based on xyzt coordinates rather than 	   on site order, or I need to work out a composition of linear->4D->3D type indexing function 	   when I pack the gauge field and the fermions for QUDA

--- mainprogs/tests/t_quda_tprec.cc
+++ mainprogs/tests/t_quda_tprec.cc
@@ -24,6 +24,175 @@ typedef multi1d<LatticeColorMatrixF> PF;
 #include "quda.h"
 #include "util_quda.h"
 
+bool testQudaDslash3D(const QF& u, enum PlusMinus isign, int cb)
+{
+  // I want to test the QUDA dslash
+  // First make a reference dslash: 4D for now
+  multi1d<int> boundary(4); boundary[0]=boundary[1]=boundary[2]=1;
+  boundary[3]=-1;
+
+  Handle< FermBC<TF,PF,QF> > bc_handle(new SimpleFermBC<TF,PF,QF>(boundary));
+
+  Handle< FermState<TF,PF,QF> > fs_handle(new SimpleFermState<TF,PF,QF>(bc_handle, u));
+
+  WilsonDslash3D D_me(fs_handle);
+
+  // Now set up a QUDA Dslash
+  // ----------**************************--------------
+  QudaGaugeParam q_gauge_param=newQudaGaugeParam();
+  QudaInvertParam quda_inv_param=newQudaInvertParam();
+
+  QudaPrecision_s cpu_prec=QUDA_SINGLE_PRECISION;
+  QudaPrecision_s gpu_prec=QUDA_SINGLE_PRECISION;
+  QudaPrecision_s gpu_half_prec=QUDA_SINGLE_PRECISION;
+
+  // Setup Boundaries
+  if(boundary[3]==-1){ 
+    q_gauge_param.t_boundary = QUDA_ANTI_PERIODIC_T;
+  }
+  else { 
+    q_gauge_param.t_boundary = QUDA_PERIODIC_T;
+  }
+
+  // Setup Gauge Order 
+  q_gauge_param.gauge_order = QUDA_QDP_GAUGE_ORDER;
+  q_gauge_param.reconstruct = QUDA_RECONSTRUCT_12;
+  q_gauge_param.reconstruct_sloppy = QUDA_RECONSTRUCT_12;
+
+  // Setup precisions
+  q_gauge_param.cpu_prec=cpu_prec;
+  q_gauge_param.cuda_prec=gpu_prec;
+  q_gauge_param.cuda_prec_sloppy=gpu_half_prec;
+
+  // Setup lattice size
+  const multi1d<int>& latdims = Layout::lattSize();      
+  q_gauge_param.X[0] = latdims[0];
+  q_gauge_param.X[1] = latdims[1];
+  q_gauge_param.X[2] = latdims[2];
+  q_gauge_param.X[3] = latdims[3];
+  
+  // Setup gauge fixing
+  q_gauge_param.gauge_fix = QUDA_GAUGE_FIXED_NO;
+
+  // Setup padding
+  unsigned int vol = latdims[0]*latdims[1]*latdims[2]*latdims[3];
+  unsigned int vol2 = vol/2;
+  if( vol % (1<<14) == 0) { 
+    // Commended out -- padding this way seems to cause some divergence
+    // Will revert when Mike tells me to. For now
+    // I'll take the hit
+    //quda_inv_param.sp_pad = q_gauge_param.ga_pad = quda_inv_param.cl_pad =( latdims[0]*latdims[1]*latdims[2])/2;
+    quda_inv_param.sp_pad = q_gauge_param.ga_pad = quda_inv_param.cl_pad =0;
+    
+  }
+  else {
+    quda_inv_param.sp_pad = q_gauge_param.ga_pad = quda_inv_param.cl_pad =0;
+  }
+
+  // Setup gauge anisotropy
+  q_gauge_param.anisotropy = 1.0;
+
+  
+  // Make the links
+  QF links_single(Nd);
+  
+  // Repack gauge, so that for links_single[mu] has gauge field with 3d cb being contiguous
+  int lin;
+  for(int mu=0; mu < Nd; mu++) {
+    lin=0;
+    for(int cb3=0; cb3 < 2; cb3++) { 
+      const int* tab=rb3[cb3].siteTable().slice();
+
+      for(int site=0; site < vol2; site++) {
+	for(int r=0; r < 3; r++) { 
+	  for(int c=0; c < 3; c++) { 
+	    links_single[mu].elem(lin).elem().elem(r,c).real() = ((fs_handle->getLinks())[mu]).elem(tab[site]).elem().elem(r,c).real();
+	    links_single[mu].elem(lin).elem().elem(r,c).imag() = ((fs_handle->getLinks())[mu]).elem(tab[site]).elem().elem(r,c).imag();
+
+	  }
+	}
+	lin++;
+      }
+    }
+  }
+  // Set up the links
+  void* gauge[4];
+  for(int mu=0; mu < Nd; mu++) { 
+    gauge[mu] = (void *)&(links_single[mu].elem(all.start()).elem().elem(0,0).real());
+  }
+  loadGaugeQuda((void *)gauge, &q_gauge_param);
+  
+  // Now set up inverter params.
+  quda_inv_param.dslash_type = QUDA_WILSON_DSLASH; // Sets Wilson Matrix
+  quda_inv_param.cpu_prec = cpu_prec;
+  quda_inv_param.cuda_prec = gpu_prec;
+  quda_inv_param.cuda_prec_sloppy = gpu_half_prec;
+  quda_inv_param.dirac_order = QUDA_DIRAC_ORDER;
+
+  TF src, dst1,dst2;
+  gaussian(src);
+  gaussian(dst1); // Junk these
+  gaussian(dst2);
+  D_me.apply(dst1,src,isign,cb); // fills out dst[rb[0]]
+
+  // Compute buffers for transfer to GPU
+
+  unsigned int h_size=4*3*2*vol2;
+  multi1d<float> buffer_in(h_size);
+  multi1d<float> buffer_out(h_size);
+
+
+
+  int otherCB=1-cb;
+  int daggerBit = (isign == MINUS ? 1 : 0);
+
+  // Pack buffer_in: output cb = rb3[0], input cb=rb3[1]
+  lin=0;
+  for(int site=0; site < rb3[1].siteTable().size(); site++) { 
+    for(int spin=0; spin < 4; spin++) {		      
+      for(int col=0; col < 3; col++) {
+	buffer_in[lin++] = src.elem(rb3[otherCB].siteTable()[site]).elem(spin).elem(col).real();
+	buffer_in[lin++] = src.elem(rb3[otherCB].siteTable()[site]).elem(spin).elem(col).imag();
+      }
+    }
+  }
+
+
+  dslash3DQuda((void *)buffer_out.slice(),
+	       (void *)buffer_in.slice(),
+	       &quda_inv_param,
+	       cb,      // source parity =1, dst parity=0
+	       daggerBit);     // no dagger
+
+  // Unpack
+  lin=0;
+  for(int site=0; site < rb3[1].siteTable().size(); site++) { 
+    for(int spin=0; spin < 4; spin++) {		      
+      for(int col=0; col < 3; col++) {
+	dst2.elem(rb3[cb].siteTable()[site]).elem(spin).elem(col).real() = buffer_out[lin++];
+	dst2.elem(rb3[cb].siteTable()[site]).elem(spin).elem(col).imag() = buffer_out[lin++];
+      }
+    }
+  }
+
+  TF diff=zero;
+  diff[rb3[cb]] = dst1-dst2;
+
+  // Free QUDA data structures
+  Double diff_norm = sqrt(norm2(diff,rb3[cb]))/Double(vol2);
+  
+  bool ret_val;
+  if ( toBool( diff_norm < Double(2.0e-8)  ) ) {
+    ret_val = true;
+  }
+  else {
+    ret_val = false; 
+  }
+
+  return ret_val;
+}
+
+
 bool testQudaDslash(const QF& u, enum PlusMinus isign, int cb)
 {
   // I want to test the QUDA dslash
@@ -116,7 +285,7 @@ bool testQudaDslash(const QF& u, enum PlusMinus isign, int cb)
   gaussian(src);
   gaussian(dst1); // Junk these
   gaussian(dst2);
-  D_me.apply(dst1,src,isign,cb); // fills out dst[rb[0]]
+  D_me.apply(dst1,src,isign,cb);
 
   // Compute buffers for transfer to GPU
   unsigned int vol2 = vol/2;
@@ -266,6 +435,47 @@ int main(int argc, char **argv)
   else { 
     QDPIO::cout << "\t OK" << endl;
   }
+
+  // 3D Dslash testsd
+
+  QDPIO::cout << "Test: Dslash3D PLUS, cb=0";
+  if ( ! testQudaDslash3D(u, PLUS, 0)  ) { 
+    QDPIO::cout << "\t FAILED" << endl;
+    QDP_abort(1);
+  }
+  else { 
+    QDPIO::cout << "\t OK" << endl;
+  }
+
+  QDPIO::cout << "Test: Dslash3D MINUS, cb=0";
+  if ( ! testQudaDslash3D(u, MINUS, 0)  ) { 
+    QDPIO::cout << "\t FAILED" << endl;
+    QDP_abort(1);
+  }
+  else { 
+    QDPIO::cout << "\t OK" << endl;
+  }
+
+  QDPIO::cout << "Test: Dslash3D PLUS, cb=1" ;
+  if ( ! testQudaDslash3D(u, PLUS, 1)  ) { 
+    QDPIO::cout << "\t FAILED" << endl;
+    QDP_abort(1);
+  }
+  else { 
+    QDPIO::cout << "\t OK" << endl;
+  }
+
+  QDPIO::cout << "Test: Dslash3D MINUS, cb=1" ;
+  if ( ! testQudaDslash3D(u, MINUS, 1)  ) { 
+    QDPIO::cout << "\t FAILED" << endl;
+    QDP_abort(1);
+  }
+  else { 
+    QDPIO::cout << "\t OK" << endl;
+  }
+
+
+
 
   QDPIO::cout << "All tests passed" << endl;
   pop(xml_out);
