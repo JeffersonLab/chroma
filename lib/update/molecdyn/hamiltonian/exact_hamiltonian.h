e3e09a96fd6f1464fd4691c8db732d14de46b8fc
Paulo Silva <psilva@qcd10i1.(none)>
2010-09-21 12:03:13 -0400

    Formulae for PB's in exact_hamiltonian

--- lib/update/molecdyn/hamiltonian/exact_hamiltonian.h
+++ lib/update/molecdyn/hamiltonian/exact_hamiltonian.h
@@ -198,23 +198,92 @@ namespace Chroma
         }
 
         for (int j=0; j< nmon; j++){
-	  monomials[i]->dsdq(*G[i][j],(*s_new)); 
+	  monomials[j]->dsdq(*G[i][j],(*s_new)); 
         }
       }
 
+      //Getting momenta
+      P pp;
+      pp.resize(Nd);
+      pp=s.getP();
+
+       //***** Computation of the Poisson Brackets
+       //Declaring...
+       multi1d<Double> tst, tttst;
+       multi2d<Double> sst, ttsst, sttst;
+       multi3d<Double> stsst;
+
+       //Resizing...
+       tst.resize(nmon); tttst.resize(nmon); 
+       sst.resize(nmon, nmon); 
+       ttsst.resize(nmon, nmon);
+       sttst.resize(nmon, nmon);
+       stsst.resize(nmon, nmon, nmon);
+
+       //Initializing...
+       for (int i=0; i<nmon; i++) {
+         tst[i]=zero; tttst[i]=zero;
+         for (int j=0; j<nmon; j++) {           
+           sst[i][j]=zero; ttsst[i][j]=zero; sttst[i][j]=zero;
+           for (int k=0; k<nmon; k++) {
+             stsst[i][j][k]=zero;
+	}}}
+
+
+       //NEED FOR AN ADDITIONAL FACTOR OF 2... NOT YET IMPLEMENTED
+
+      for(int mu=0; mu < Nd; mu++) {
+
+	 for (int i=0; i<nmon; i++) {
+
+	   tst[i] += Double(-1.0)*sum(real(trace((*F[i])[1][mu]*pp[mu]))); 
+           tttst[i] += Double(-1.0)*sum(real(trace((*F[i])[3][mu]*pp[mu])));
+
+	   for (int j=0; j<nmon; j++) {
+
+             LatticeColorMatrix com_i, com_j, com1p, com2p;
+           
+	     sst[i][j] += Double(1.0)*sum(real(trace((*F[i])[0][mu]*(*F[j])[0][mu])));
+
+             ttsst[i][j] += Double(2.0)*sum(real(trace((*F[i])[1][mu]*(*F[j])[1][mu])));
+             ttsst[i][j] += Double(1.0)*sum(real(trace((*F[i])[0][mu]*(*F[j])[2][mu])));
+             ttsst[i][j] += Double(1.0)*sum(real(trace((*F[j])[0][mu]*(*F[i])[2][mu]))); 
+
+ 
+	     com1p = (*F[i])[0][mu]*(*F[j])[1][mu]-(*F[j])[1][mu]*(*F[i])[0][mu];
+	     com2p = (*F[i])[1][mu]*(*F[j])[0][mu]-(*F[j])[0][mu]*(*F[i])[1][mu];  
+
+             sttst[i][j] += Double(-2.0)*sum(real(trace(com1p*pp[mu])));
+             sttst[i][j] += Double(1.0)*sum(real(trace(com2p*pp[mu])));
+
+	     com_i=(*F[i])[0][mu]*pp[mu]-pp[mu]*(*F[i])[0][mu];
+             com_j=(*F[j])[0][mu]*pp[mu]-pp[mu]*(*F[j])[0][mu];
+
+ 	     sttst[i][j] += Double(-1.0)*sum(real(trace(com_i*com_j))); 
+             sttst[i][j] += Double(1.0)*sum(real(trace((*F[i])[0][mu]*(*F[j])[2][mu])));
+ 	     sttst[i][j] += Double(-2.0)*sum(real(trace((*F[i])[1][mu]*(*F[j])[1][mu])));
+
+ 	     for (int k=0; k<nmon; k++) {
+
+                LatticeColorMatrix aux1, aux2, aux3, aux4;
+                aux1=(*F[j])[0][mu];
+                aux2=(*G[i][k])[1][mu];
+
+                aux3=(*F[k])[0][mu];
+                aux4=(*G[i][j])[1][mu];
+
+                stsst[i][j][k] += Double(-1.0)*sum(real(trace(aux1*aux2+aux3*aux4)));   
+
+ 	     }
+	   }
+	 }
+
+
+       }//mu
 
 
 
-
-//       Handle< AbsFieldState<P,Q> > s_new(s.clone());
-
-//       for(int mu=0; mu < Nd; mu++) { 
-// 	s_new->getP()[mu] = F[mu][0];
-//       }
-
-//       dsdq(G, (*s_new));
-
-     
+      //NOW, OUTPUT ALL THESE NUMBERS... :-S
 
       END_CODE();
 
