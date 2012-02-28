a3194bf99945741f517930583a1be6e6e38a7da9
Craig McNeile <mcneile@uni-wuppertal.de>
2012-02-28 10:52:22 +0100

    I have modified this code.

--- mainprogs/tests/t_meas_wilson_flow_loop.cc
+++ mainprogs/tests/t_meas_wilson_flow_loop.cc
@@ -19,19 +19,6 @@
 using namespace Chroma;
 
 
- bool linkageHack(void)
-{
-  bool foo = true;
-
-  // Inline Measurements
-  foo &= InlineAggregateEnv::registerAll();
-  foo &= GaugeInitEnv::registerAll();
-
-  return foo;
-}
-
-
-
 
 
 /*
@@ -142,107 +129,110 @@ int main(int argc, char **argv)
 
   QDPIO::cout << "Measure the Wilson flow " << endl;
   QDPIO::cout << "Calculation for SU(" << Nc << ")" << endl;
-  linkageHack();
 
-  // Input parameter structure
-  Propagator_input_t  input;
+  string queuefile_name = Chroma::getXMLInputFileName() ;
+  TextFileReader text_in ;
 
-  // Instantiate xml reader for DATA
-  XMLReader xml_in ; 
-  string in_name = Chroma::getXMLInputFileName() ; 
   try
   {
-    xml_in.open(in_name);
+    text_in.open(queuefile_name);
   }
-    catch (...) 
+  catch (...)
   {
-    QDPIO::cerr << "Error reading input file " << in_name << endl;
+    QDPIO::cerr << "Error reading input file " << queuefile_name << endl;
     QDPIO::cerr << "The input file name can be passed via the -i flag " << endl;
     QDPIO::cerr << "The default name is ./DATA" << endl;
     throw;
   }
 
+  std::string name1;
+  std::string name2;
 
-  // Read data
-  read(xml_in, "/WilsonFlow", input);
+  bool        successful_read=true;
 
-
-  // Specify lattice size, shape, etc.
-  Layout::setLattSize(input.param.nrow);
-  Layout::create();
-
-  // Read in the configuration along with relevant information.
-  multi1d<LatticeColorMatrix> u(Nd);
-
-
-
-  XMLReader gauge_file_xml, gauge_xml;
-  // Start up the gauge field
-  gaugeStartup(gauge_file_xml, gauge_xml, u, input.cfg);
+  while(successful_read){
+    text_in.read(name1);
+    text_in.read(name2);
+    text_in.fail(     );
+    successful_read=!text_in.fail(     );
+    if (successful_read){
+      const string in_name(name1);
+      const string out_name(name2);
 
 
-  // Check if the gauge field configuration is unitarized
-  unitarityCheck(u);
+      QDPIO::cout << "Starting run with in_name:\t"<< in_name << endl;
+      QDPIO::cout << "Output name = out_name:\t"<< out_name << endl ;
 
-  // Instantiate XML writer for XMLDAT
-  XMLFileWriter& xml_out = Chroma::getXMLOutputInstance();
-  push(xml_out, "wilsonFlow");
+      // Input parameter structure
+      Propagator_input_t  input;
 
-  // Write out the input
-  write(xml_out, "Input", xml_in);
+      Chroma::setXMLInputFileName(in_name);
+      // Instantiate xml reader for DATA
+      XMLReader xml_in ; 
+      try
+	{
+	  xml_in.open(in_name);
+	}
+      catch (...) 
+	{
+	  QDPIO::cerr << "Error reading input file " << in_name << endl;
+	  QDPIO::cerr << "The input file name can be passed via the -i flag " << endl;
+	  throw;
+	}
 
-
-  push(xml_out, "Output_version");
-  write(xml_out, "out_version", 1);
-  pop(xml_out);
-
-  xml_out.flush();
-
-
-  // Calculate some gauge invariant observables just for info.
-  MesPlq(xml_out, "Observables", u);
-  xml_out.flush();
-
-  // 
-  //  gauge invariance test
-  //  
-
-  if( input.param.do_gauge_transform )
-    {
-      // gauge transform the gauge fields
-      multi1d<LatticeColorMatrix> u_trans(Nd);
-
-      // create a random gauge transform
-       LatticeColorMatrix v ;
-  
-       gaussian(v);
-       reunit(v) ; 
-
-       for(int dir = 0 ; dir < Nd ; ++dir)
-	 {
-	   u_trans[dir] = v*u[dir]*adj(shift(v,FORWARD,dir)) ;
-	   u[dir] = u_trans[dir] ;
-	 }
-
-       QDPIO::cout << "Random gauge transform done" << endl;
-
-    } // end of gauge transform
-  else
-    {
-       QDPIO::cout << "NO RANDOM GAUGE TRANSFORM" << endl;
-    }
+      
+      // Read data
+      read(xml_in, "/WilsonFlow", input);
 
 
+      // Specify lattice size, shape, etc.
+      Layout::setLattSize(input.param.nrow);
+      Layout::create();
 
-  int j_decay = Nd-1;
-  int jomit=3 ; 
-  wilson_flow(xml_out, u, input.param.nstep,input.param.wflow_eps ,jomit) ;
+      // Read in the configuration along with relevant information.
+      multi1d<LatticeColorMatrix> u(Nd);
+
+      XMLReader gauge_file_xml, gauge_xml;
+      // Start up the gauge field
+      gaugeStartup(gauge_file_xml, gauge_xml, u, input.cfg);
 
 
-  pop(xml_out);
+      // Check if the gauge field configuration is unitarized
+      unitarityCheck(u);
+      
+      // Instantiate XML writer for XMLDAT
+      XMLFileWriter xml_out(out_name);
+      push(xml_out, "wilsonFlow_output");
 
-  xml_out.close();
-  xml_in.close();
+      // Write out the input
+      write(xml_out, "Input", xml_in);
+
+
+      push(xml_out, "Output_version");
+      write(xml_out, "out_version", 1);
+      pop(xml_out);
+
+      xml_out.flush();
+
+
+      // Calculate some gauge invariant observables just for info.
+      MesPlq(xml_out, "Observables", u);
+      xml_out.flush();
+
+
+      int j_decay = Nd-1;
+      int jomit=3 ; 
+      QDPIO::cout << "FLOW for " << input.cfg.cfg_file  << "\n" ;
+      wilson_flow(xml_out, u, input.param.nstep,input.param.wflow_eps ,jomit) ;
+
+      pop(xml_out);
+      
+      xml_out.close();
+      xml_in.close();
+      
+    } // succesful read
+
+  } // read files in input parameter
 
 
   // Time to bolt
