dcd09f8d60d13d4a414489cc6b2b1afa072cbe98
Robert Edwards <edwards@jlab.org>
2013-09-02 17:55:06 -0400

    Commented out unused code.

--- tests/chroma/glue/wilson_flow/calc_w0.pl
+++ tests/chroma/glue/wilson_flow/calc_w0.pl
@@ -16,7 +16,7 @@ $eps = $time[1]-$time[0] ;
 foreach $i (1..$#time-1){
     #$w = $time[$i]*0.5*($time[$i+1]**2*$act[$i+1] - $time[$i-1]**2*$act[$i-1] )/$eps;
     $w = $time[$i]*(2.0*$time[$i]*$act[$i] + $time[$i]**2*0.5*($act[$i+1]-$act[$i-1])/$eps);
-    push @w0,$w;
+    #push @w0,$w;
     print "$time[$i] $w\n";
 }
 
