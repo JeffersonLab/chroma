184d6d95301533db9701300058c5842f60616bfc
Kostas Orginos <kostas@wm.edu>
2016-04-06 16:33:44 -0400

    added single phase for 4d

--- lib/util/ft/single_phase.cc
+++ lib/util/ft/single_phase.cc
@@ -66,4 +66,48 @@ namespace Chroma
     return phase;
   }
 
+  LatticeComplex singlePhase(const multi1d<int>& t_srce, 
+			     const multi1d<int>& sink_mom)
+  {
+    START_CODE();
+
+    if (t_srce.size() != Nd)
+    {
+      QDPIO::cerr << __func__ << ": t_srce not of size = " << Nd << std::endl;
+      QDP_abort(1);
+    }
+
+    if (sink_mom.size() != Nd)
+    {
+      QDPIO::cerr << __func__ << ": mom not of size = " << Nd << std::endl;
+      QDP_abort(1);
+    }
+
+    /*
+     *  We now inject momentum at sink if required
+     */
+    LatticeReal p_dot_x = zero;
+    bool nonzeroP = false;
+    for(int mu=0, j=0; mu < Nd; mu++)
+    {
+      if (sink_mom[mu] != 0)
+	{
+	  nonzeroP = true;
+	  p_dot_x += (Layout::latticeCoordinate(mu) - t_srce[mu]) * sink_mom[mu]
+	    * twopi / Real(Layout::lattSize()[mu]);
+	}
+    }
+
+    LatticeComplex phase;
+
+    if (nonzeroP)
+      phase = cmplx(cos(p_dot_x),sin(p_dot_x));
+    else
+      phase = 1.0;
+
+    END_CODE();
+
+    return phase;
+  }
+
 }  // end namespace Chroma
