4a68789da1908bf72a9c3a3e8968805f88883a2c
Balint Joo <bjoo@jlab.org>
2017-07-18 14:13:36 -0400

    Another missing std

--- lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_w.h
+++ lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_w.h
@@ -298,7 +298,7 @@ namespace Chroma
       }
 
       swatch.stop();
-      QDPIO::cout << "QPHIX_MDAGM_SOLVER: total time: " << swatch.getTimeInSeconds() << " (sec)" << endl;
+      QDPIO::cout << "QPHIX_MDAGM_SOLVER: total time: " << swatch.getTimeInSeconds() << " (sec)" << std::endl;
       END_CODE();
       return res;
     }
