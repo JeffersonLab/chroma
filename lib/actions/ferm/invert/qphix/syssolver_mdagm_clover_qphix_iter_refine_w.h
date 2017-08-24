97bfbaf4086d0381f94cf2bb104a0ced24452874
Martin Ueding <dev@martin-ueding.de>
2017-08-24 12:06:34 +0200

    swatch call is done outside

--- lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_iter_refine_w.h
+++ lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_iter_refine_w.h
@@ -643,10 +643,6 @@ namespace Chroma
     		QDP_abort(1);
     	}
 #endif
-    	 swatch.stop();
-    	 QDPIO::cout << "QPHIX_MDAGM_SOLVER: total time: " << swatch.getTimeInSeconds() << " (sec)" << std::endl;
-    	 END_CODE();
-
     	return res;
     }
 
