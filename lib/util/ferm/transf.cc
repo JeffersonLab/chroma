a223bdb3b04231e35be5b71e94b424a16d22d36c
Robert Edwards <edwards@jlab.org>
2018-11-16 14:55:20 -0500

    Bug fix in double-prec version of LatticeColorVectorSpinMatrix

--- lib/util/ferm/transf.cc
+++ lib/util/ferm/transf.cc
@@ -167,11 +167,7 @@ namespace Chroma
 		  int spin_index)
   {
     for(int j = 0; j < Ns; ++j)
-    {
-      LatticeColorVectorD bb = peekSpin(b, j, spin_index);
-
-      pokeSpin(b, bb, j, spin_index);
-    }
+      pokeSpin(b, peekSpin(a, j), j, spin_index);
   }
 #endif
   
