10ecb2cf1c39525fb9db93bcfe29af97dd26d5f1
Balint Joo <bjoo@jlab.org>
2019-02-19 21:17:16 -0500

    Test for Unprec Shifted Op in QUDA passes

--- mainprogs/tests/quda_cmat_tests.cc
+++ mainprogs/tests/quda_cmat_tests.cc
@@ -24,6 +24,10 @@
 #endif
 
 #include "symm_prec_xml.h"
+#include "actions/ferm/linop/shifted_linop_w.h"
+#include "actions/ferm/linop/unprec_clover_plus_igmuA2_linop_w.h"
+
+
 using namespace Chroma;
 using namespace QDP;
 using namespace SymmPrecTesting;
@@ -170,3 +174,103 @@ TEST_F(QudaFixture, TestUnprecCloverMat)
 	}
 }
 
+TEST_F(QudaFixture, TestUnprecTwistedMatDispatchZeroTwist)
+{
+	auto the_quda_solver = getSolver();
+	auto quda_inv_param = the_quda_solver.getQudaInvertParam();
+	quda_inv_param.solution_type = QUDA_MAT_SOLUTION;
+	quda_inv_param.matpc_type = QUDA_MATPC_INVALID;
+	quda_inv_param.dslash_type = QUDA_CLOVER_HASENBUSCH_TWIST_DSLASH;
+	quda_inv_param.m5 = 0.0;
+	QDPIO::cout << "Twist is " << quda_inv_param.m5 << std::endl;
+	for(int dagger = 0; dagger < 2; ++dagger) {
+
+		enum PlusMinus isign = ( dagger == 0 ) ? PLUS : MINUS;
+		quda_inv_param.dagger = (dagger == 0 ) ? QUDA_DAG_NO : QUDA_DAG_YES;
+		std::string op_str = (dagger == 0 ) ? "Op :" : "Dag :" ;
+		T src=zero;
+		T res=zero;
+		T res_quda = zero;
+
+		// Apply symmetric clover operator
+		gaussian(src);
+		(*M_symm).unprecLinOp(res,src,isign);
+		auto src_ptr = (void *)&(src.elem(rb[0].start()).elem(0).elem(0).real());
+		auto res_quda_ptr = (void *)&(res_quda.elem(rb[0].start()).elem(0).elem(0).real());
+
+
+		//quda_inv_param.matpc_type = QUDA_MATPC_EVEN_EVEN;
+		// Now I want to apply the QUDA operator.
+		MatQuda(res_quda_ptr, src_ptr, &quda_inv_param);
+
+		T diff = zero;
+		diff = res_quda - res;
+		Double norm_diff = norm2(diff);
+		QDPIO::cout << op_str << " || QUDA - Chroma || = " << sqrt(norm_diff) << std::endl;
+		Double sites = Double(Layout::vol());
+		Double norm_diff_per_site = sqrt(norm_diff/sites);
+		QDPIO::cout << op_str << " || QUDA - Chroma ||/site = " << norm_diff_per_site << std::endl;
+
+		ASSERT_LT( toDouble(norm_diff_per_site), 1.0e-14);
+	}
+}
+
+TEST_F(QudaFixture, TestUnprecTwistedMatDispatchNonZeroTwist)
+{
+	auto the_quda_solver = getSolver();
+	auto quda_inv_param = the_quda_solver.getQudaInvertParam();
+	quda_inv_param.solution_type = QUDA_MAT_SOLUTION;
+	quda_inv_param.matpc_type = QUDA_MATPC_INVALID;
+	quda_inv_param.dslash_type = QUDA_CLOVER_HASENBUSCH_TWIST_DSLASH;
+
+
+	CloverFermActParams p;
+	p.Mass = Real(0.1);
+	p.clovCoeffR=Real(1.0);
+	p.clovCoeffT=Real(1.0);
+	p.u0 = Real(1);
+	p.anisoParam.anisoP=false;
+	p.anisoParam.t_dir=3;
+	p.anisoParam.xi_0 =Real(1);
+	p.anisoParam.nu=Real(1);
+	p.twisted_m_usedP = true;
+	p.twisted_m = Real(0.2345);
+
+	// There is a normalization difference with a sign of -4
+	quda_inv_param.m5 =-4*toDouble(p.twisted_m);
+
+
+	UnprecCloverPlusIG5MuA2Linop M_u_tw(state,p);
+
+	QDPIO::cout << "Twist is " << quda_inv_param.m5 << std::endl;
+	for(int dagger = 0; dagger < 2; ++dagger) {
+
+		enum PlusMinus isign = ( dagger == 0 ) ? PLUS : MINUS;
+		quda_inv_param.dagger = (dagger == 0 ) ? QUDA_DAG_NO : QUDA_DAG_YES;
+		std::string op_str = (dagger == 0 ) ? "Op :" : "Dag :" ;
+		T src=zero;
+		T res=zero;
+		T res_quda = zero;
+
+		// Apply symmetric clover operator
+		gaussian(src);
+		M_u_tw(res,src,isign);
+		auto src_ptr = (void *)&(src.elem(rb[0].start()).elem(0).elem(0).real());
+		auto res_quda_ptr = (void *)&(res_quda.elem(rb[0].start()).elem(0).elem(0).real());
+
+
+		//quda_inv_param.matpc_type = QUDA_MATPC_EVEN_EVEN;
+		// Now I want to apply the QUDA operator.
+		MatQuda(res_quda_ptr, src_ptr, &quda_inv_param);
+
+		T diff = zero;
+		diff = res_quda - res;
+		Double norm_diff = norm2(diff);
+		QDPIO::cout << op_str << " || QUDA - Chroma || = " << sqrt(norm_diff) << std::endl;
+		Double sites = Double(Layout::vol());
+		Double norm_diff_per_site = sqrt(norm_diff/sites);
+		QDPIO::cout << op_str << " || QUDA - Chroma ||/site = " << norm_diff_per_site << std::endl;
+
+		ASSERT_LT( toDouble(norm_diff_per_site), 1.0e-14);
+	}
+}
