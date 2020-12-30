dac881db90689b323db136d296d764c1523cb20c
Robert Edwards <edwards@jlab.org>
2020-12-30 14:13:36 -0500

    Added a "FAKE" inverter. Does not actually invert, just copies the right-hand-side to the solution vector. Useful for testing/debugging.

---
+++ lib/actions/ferm/invert/syssolver_linop_fake.cc
@@ -0,0 +1,79 @@
+/*! \file
+ *  \brief Solve a M*psi=chi linear system by CG2
+ */
+#include "state.h"
+#include "actions/ferm/invert/syssolver_linop_factory.h"
+#include "actions/ferm/invert/syssolver_linop_aggregate.h"
+
+#include "actions/ferm/invert/syssolver_linop_cg.h"
+
+namespace Chroma
+{
+
+  //! CG1 system solver namespace
+  namespace LinOpSysSolverCGEnv
+  {
+    //! Anonymous namespace
+    namespace
+    {
+      //! Name to be used
+      const std::string name("FAKE_INVERTER");
+
+      //! Local registration flag
+      bool registered = false;
+    }
+
+
+    //! Callback function
+    LinOpSystemSolver<LatticeFermion>* createFerm(XMLReader& xml_in,
+						  const std::string& path,
+						  Handle< FermState<
+						                     LatticeFermion, 
+						                     multi1d<LatticeColorMatrix>,
+						                     multi1d<LatticeColorMatrix> 
+					 	  > 
+							  > state, 
+
+						  Handle< LinearOperator<LatticeFermion> > A)
+    {
+      return new LinOpSysSolverCG<LatticeFermion>(A, SysSolverCGParams(xml_in, path));
+    }
+
+    //! Callback function
+    LinOpSystemSolver<LatticeFermionF>* createFermF(XMLReader& xml_in,
+						  const std::string& path,
+						  Handle< FermState<
+						                     LatticeFermionF, 
+						                     multi1d<LatticeColorMatrixF>,
+						                     multi1d<LatticeColorMatrixF> 
+						  > 
+							  > state, 
+
+						  Handle< LinearOperator<LatticeFermionF> > A)
+    {
+      return new LinOpSysSolverCG<LatticeFermionF>(A, SysSolverCGParams(xml_in, path));
+    }
+
+    //! Callback function
+    LinOpSystemSolver<LatticeStaggeredFermion>* createStagFerm(XMLReader& xml_in,
+							       const std::string& path,
+							       Handle< LinearOperator<LatticeStaggeredFermion> > A)
+    {
+      return new LinOpSysSolverCG<LatticeStaggeredFermion>(A, SysSolverCGParams(xml_in, path));
+    }
+
+    //! Register all the factories
+    bool registerAll() 
+    {
+      bool success = true; 
+      if (! registered)
+      {
+	success &= Chroma::TheLinOpFermSystemSolverFactory::Instance().registerObject(name, createFerm);
+	success &= Chroma::TheLinOpFFermSystemSolverFactory::Instance().registerObject(name, createFermF);
+	success &= Chroma::TheLinOpStagFermSystemSolverFactory::Instance().registerObject(name, createStagFerm);
+	registered = true;
+      }
+      return success;
+    }
+  }
+}
---
+++ lib/actions/ferm/invert/syssolver_linop_fake.h
@@ -0,0 +1,81 @@
+// -*- C++ -*-
+/*! \file
+ *  \brief A fake inverter
+ */
+
+#ifndef __syssolver_linop_fake_h__
+#define __syssolver_linop_fake_h__
+
+#include "chroma_config.h"
+#include "handle.h"
+#include "syssolver.h"
+#include "linearop.h"
+#include "actions/ferm/invert/syssolver_linop.h"
+
+
+namespace Chroma
+{
+
+  //! CG system solver namespace
+  namespace LinOpSysSolverFakeEnv
+  {
+    //! Register the syssolver
+    bool registerAll();
+  }
+
+
+  //! Solve a M*psi=chi linear system by CG2
+  /*! \ingroup invert
+   */
+  template<typename T>
+  class LinOpSysSolverFake : public LinOpSystemSolver<T>
+  {
+  public:
+    //! Constructor
+    /*!
+     * \param M_        Linear operator ( Read )
+     */
+    LinOpSysSolverFake(Handle< LinearOperator<T> > A_) : A(A_)
+      {}
+
+    //! Destructor is automatic
+    ~LinOpSysSolverFake() {}
+
+    //! Return the subset on which the operator acts
+    const Subset& subset() const {return A->subset();}
+
+    //! Solver the linear system
+    /*!
+     * \param psi      solution ( Modify )
+     * \param chi      source ( Read )
+     * \return syssolver results
+     */
+    SystemSolverResults_t operator() (T& psi, const T& chi) const
+      {
+	START_CODE();	
+	SystemSolverResults_t res;  // initialized by a constructor
+
+        const Subset& s = M.subset();
+
+        psi[s] = chi;
+
+        res.n_count = 0;
+        res.resid   = 0;
+
+	END_CODE();
+
+	return res;
+      }
+
+
+  private:
+    // Hide default constructor
+    LinOpSysSolverFake() {}
+
+    Handle< LinearOperator<T> > A;
+  };
+
+} // End namespace
+
+#endif 
+
