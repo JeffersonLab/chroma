JeffersonLab/chroma

commit f2290fde7c448ba755989be2a1c40f0ac57b73d2

Reworked use of vol() where appropriate changed vol()-> Double(vol()) to prevent overflows. Integers dealing with vol() changed to size_t
Overhauled sanitizer options: undefined only, made sanitizer compile time option PRIVATE

lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_iter_refine_w.h
@@ -415,7 +415,7 @@ namespace Chroma
 #endif
     	QDPIO::cout << "Done" << std::endl << std::flush;
     	double rsd_final;
-    	int num_cb_sites = Layout::vol()/2;
+    	size_t num_cb_sites = Layout::vol()/2;
 
     	unsigned long site_flops1=0;
     	unsigned long mv_apps1=0;
@@ -431,8 +431,9 @@ namespace Chroma
     	double end = omp_get_wtime();
 
 
-    	unsigned long total_flops = (site_flops1 + (1320+504+1320+504+48)*mv_apps1)*num_cb_sites;
-    	double gflops = (double)(total_flops)/(1.0e9);
+    	double total_flops = ( stati_cast<double>(site_flops1) + static_cast<double>(1320+504+1320+504+48)
+														*static_cast<double>(mv_apps1))*static_cast<double>(num_cb_sites);
+    	double gflops = total_flops/1.0e9;
     	double total_time = end - start;
 
     	Double r_final = sqrt(toDouble(rsd_final));
@@ -443,8 +444,9 @@ namespace Chroma
     	start = omp_get_wtime();
     	(*mixed_solver)(psi_qphix,tmp_qphix, toDouble(invParam.RsdTarget), n_count2, rsd_final, site_flops2, mv_apps2, +1, invParam.VerboseP);
     	end = omp_get_wtime();
-    	total_flops = (site_flops2 + (1320+504+1320+504+48)*mv_apps2)*num_cb_sites;
-    	gflops = (double)(total_flops)/(1.0e9);
+    	total_flops = (static_cast<double>(site_flops2) + static_cast<double>(1320+504+1320+504+48)
+								*static_cast<double>(mv_apps2))*static_cast<double>(num_cb_sites);
+    	gflops = total_flops/1.0e9;
     	total_time = end - start;
 
 #ifndef QDP_IS_QDPJIT

lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_w.h
@@ -411,9 +411,10 @@ namespace Chroma
 #endif
       }  
 
-      int num_cb_sites = Layout::vol()/2;
-      unsigned long total_flops = (site_flops + (1320+504+1320+504+48)*mv_apps)*num_cb_sites;
-      double gflops = (double)(total_flops)/(1.0e9);
+      size_t num_cb_sites = Layout::vol()/2;
+      double total_flops = (static_cast<double>(site_flops) + static_cast<double>(1320+504+1320+504+48)
+								*static_cast<double>(mv_apps))*static_cast<double>(num_cb_sites);
+      double gflops = total_flops/1.0e9;
 
       double total_time = end - start;
       QDPIO::cout << "QPHIX_CLOVER_CG_SOLVER: Solver Time="<< total_time <<" (sec)  Performance=" << gflops / total_time << " GFLOPS" << std::endl;
@@ -468,7 +469,7 @@ namespace Chroma
 #endif
       QDPIO::cout << "Done" << std::endl << std::flush;
       double rsd_final;
-      int num_cb_sites = Layout::vol()/2;
+      size_t num_cb_sites = Layout::vol()/2;
 
       unsigned long site_flops1=0;
       unsigned long mv_apps1=0;
@@ -484,8 +485,9 @@ namespace Chroma
       double end = omp_get_wtime();
 
 
-      unsigned long total_flops = (site_flops1 + (1320+504+1320+504+48)*mv_apps1)*num_cb_sites;
-      double gflops = (double)(total_flops)/(1.0e9);
+      double total_flops = (static_cast<double>(site_flops1) + static_cast<double>(1320+504+1320+504+48)*
+							static_cast<double>(mv_apps1))*static_cast<double(num_cb_sites);
+      double gflops = total_flops/1.0e9;
       double total_time = end - start;
 
       Double r_final = sqrt(toDouble(rsd_final)/norm2(chi,A->subset()));
@@ -496,8 +498,9 @@ namespace Chroma
       start = omp_get_wtime();
       (*bicgstab_solver)(psi_qphix,tmp_qphix, toDouble(invParam.RsdTarget), n_count2, rsd_final, site_flops2, mv_apps2, +1, invParam.VerboseP);
       end = omp_get_wtime();
-      total_flops = (site_flops2 + (1320+504+1320+504+48)*mv_apps2)*num_cb_sites;
-      gflops = (double)(total_flops)/(1.0e9);
+      total_flops = (static_cast<double>(site_flops2) + static_cast<double>(1320+504+1320+504+48)
+										*static_cast<double>(mv_apps2))*static_cast<double>(num_cb_sites);
+      gflops = total_flops/1.0e9;
       total_time = end - start;
 
 #ifndef QDP_IS_QDPJIT

CMakeLists.txt
@@ -104,7 +104,7 @@ if( Chroma_ENABLE_SANITIZERS )
        OR ( CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 4.8) )
 
        message(STATUS "Chroma: Setting Sanitizer options")
-       set(Chroma_SANITIZER_OPTS "-fsanitize=address,undefined")
+       set(Chroma_SANITIZER_OPTS "-fsanitize=undefined")
        set(CPPWilsonDslash_ENABLE_SANITIZERS ON)
        set(QDPLapack_ENABLE_SANITIZERS ON)
    else()

lib/CMakeLists.txt
@@ -1980,12 +1980,15 @@ target_sources(chromalib PRIVATE
 	update/molecdyn/predictor/quda_predictor.cc
 )
 
-# QUDA injects an unnecessary dependency to hip::device through roc::rocprim_hip.
-# The inclusion of hip::device adds flags for hipcc, forcing chroma to use hipcc.
-# Being not sure how to avoid QUDA injecting the hip::device depenency, the easiest
-# solution (hack) is to empty of content hip::device.
-set_target_properties(hip::device PROPERTIES INTERFACE_COMPILE_OPTIONS ""
+if( QUDA_TARGET_HIP )
+  # QUDA injects an unnecessary dependency to hip::device through roc::rocprim_hip.
+  # The inclusion of hip::device adds flags for hipcc, forcing chroma to use hipcc.
+  # Being not sure how to avoid QUDA injecting the hip::device depenency, the easiest
+  # solution (hack) is to empty of content hip::device.
+  set_target_properties(hip::device PROPERTIES INTERFACE_COMPILE_OPTIONS ""
                                              INTERFACE_LINK_LIBRARIES "")
+
+endif()
 
 target_link_libraries(chromalib PUBLIC QUDA::quda)
 endif()
@@ -2017,7 +2020,7 @@ endif()
 
 
 if( Chroma_SANITIZER_OPTS )
-   target_compile_options(chromalib PUBLIC ${Chroma_SANITIZER_OPTS})
+   target_compile_options(chromalib PRIVATE ${Chroma_SANITIZER_OPTS})
    target_link_options(chromalib PUBLIC ${Chroma_SANITIZER_OPTS})
 endif()
 ################################################

lib/actions/ferm/invert/qphix/multi_syssolver_mdagm_cg_clover_qphix_w.h
@@ -429,9 +429,10 @@ public:
 			}
 		}
 
-		int num_cb_sites = Layout::vol()/2;
-		unsigned long total_flops = (site_flops + (1320+504+1320+504+48)*mv_apps)*num_cb_sites;
-		double gflops = (double)(total_flops)/(1.0e9);
+		size_t num_cb_sites = Layout::vol()/2;
+		double total_flops = (static_cast<double>(site_flops) + static_cast<double>(1320+504+1320+504+48)
+															*static_cast<double>(mv_apps))*static_cast<double>(num_cb_sites);
+		double gflops = total_flops/1.0e9;
 
 		double total_time = end - start;
 

lib/actions/ferm/invert/qphix/syssolver_linop_clover_qphix_iter_refine_w.h
@@ -435,9 +435,10 @@ namespace Chroma
       }
 #endif
 
-      int num_cb_sites = Layout::vol()/2;
-      unsigned long total_flops = (site_flops + (1320+504+1320+504+48)*mv_apps)*num_cb_sites;
-      double gflops = (double)(total_flops)/(1.0e9);
+      size_t num_cb_sites = Layout::vol()/2;
+      double total_flops = (static_cast<double>(site_flops) + static_cast<double>(1320+504+1320+504+48)
+															*static_cast<double>(mv_apps))*static_cast<double>(num_cb_sites);
+      double gflops = total_flops/1.0e9;
 
       double total_time = end - start;
       QDPIO::cout << "QPHIX_CLOVER_BICGSTAB_ITER_REFINE_SOLVER: Solver Time="<< total_time <<" (sec)  Performance=" << gflops / total_time << " GFLOPS" << std::endl;

lib/actions/ferm/invert/qphix/syssolver_linop_clover_qphix_w.h
@@ -377,9 +377,11 @@ namespace Chroma
       }
 
 
-      int num_cb_sites = Layout::vol()/2;
-      unsigned long total_flops = (site_flops + (1320+504+1320+504+48)*mv_apps)*num_cb_sites;
-      double gflops = (double)(total_flops)/(1.0e9);
+      size_t num_cb_sites = Layout::vol()/2;
+      double total_flops = (static_cast<double>(site_flops) 
+														+ static_cast<double>(1320+504+1320+504+48)*static_cast<double>(mv_apps))
+														* static_cast<double>(num_cb_sites);
+      double gflops = total_flops/1.0e9;
 
       double total_time = end - start;
       QDPIO::cout << "QPHIX_CLOVER_CG_SOLVER: Solver Time="<< total_time <<" (sec)  Performance=" << gflops / total_time << " GFLOPS" << std::endl;
@@ -440,9 +442,10 @@ namespace Chroma
       }
 #endif
 
-      int num_cb_sites = Layout::vol()/2;
-      unsigned long total_flops = (site_flops + (1320+504+1320+504+48)*mv_apps)*num_cb_sites;
-      double gflops = (double)(total_flops)/(1.0e9);
+      size_t num_cb_sites = Layout::vol()/2;
+      double total_flops = (static_cast<double>(site_flops) + static_cast<double>(1320+504+1320+504+48)
+													*static_cast<double>(mv_apps))*static_cast<double>(num_cb_sites);
+      double gflops = total_flops/1.0e9;
 
       double total_time = end - start;
       QDPIO::cout << "QPHIX_CLOVER_BICGSTAB_SOLVER: Solver Time="<< total_time <<" (sec)  Performance=" << gflops / total_time << " GFLOPS" << std::endl;

lib/io/readcppacs.cc
@@ -95,7 +95,7 @@ void readCPPACS(CPPACSGauge_t& header, multi1d<LatticeColorMatrix>& u, const std
   ColorMatrixD  uuuD ; 
   ColorMatrix  uuu ; 
 
-  for(int site=0; site < Layout::vol(); ++site)
+  for(size_t site=0; site < Layout::vol(); ++site)
   {
     multi1d<int> coord = crtesn(site, Layout::lattSize()); // The coordinate
     // read in a single site

commit d11dcf55f5448ffd385d44406a79c775baf8198d

Fix minor compilation issue introduced in commit f2290fde7c

lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_iter_refine_w.h
@@ -431,7 +431,7 @@ namespace Chroma
     	double end = omp_get_wtime();
 
 
-    	double total_flops = ( stati_cast<double>(site_flops1) + static_cast<double>(1320+504+1320+504+48)
+    	double total_flops = ( static_cast<double>(site_flops1) + static_cast<double>(1320+504+1320+504+48)
 														*static_cast<double>(mv_apps1))*static_cast<double>(num_cb_sites);
     	double gflops = total_flops/1.0e9;
     	double total_time = end - start;

lib/actions/ferm/invert/qphix/syssolver_mdagm_clover_qphix_w.h
@@ -486,7 +486,7 @@ namespace Chroma
 
 
       double total_flops = (static_cast<double>(site_flops1) + static_cast<double>(1320+504+1320+504+48)*
-							static_cast<double>(mv_apps1))*static_cast<double(num_cb_sites);
+							static_cast<double>(mv_apps1))*static_cast<double>(num_cb_sites);
       double gflops = total_flops/1.0e9;
       double total_time = end - start;
 